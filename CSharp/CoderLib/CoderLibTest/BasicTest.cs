723b0fe61cf28c5583ae9fb7b5ffef29563cfe8d
Keiho Sakapon <git@saka-pon.net>
2019-08-14 14:44:07 +0900

    Add methods

--- CSharp/CoderLib/CoderLibTest/BasicTest.cs
+++ CSharp/CoderLib/CoderLibTest/BasicTest.cs
@@ -6,8 +6,49 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 [TestClass]
 public class BasicTest
 {
-	[TestMethod]
-	public void TestMethod1()
+	// Tab for indentation.
+
+	static int Gcd(int x, int y)
 	{
+		int r;
+		while ((r = x % y) != 0) { x = y; y = r; }
+		return y;
+	}
+
+	static int Lcm(int x, int y) => x / Gcd(x, y) * y;
+
+	// For negative integers.
+	static int Mod(int v, int mod) => (v %= mod) < 0 ? v + mod : v;
+
+	[TestMethod]
+	public void Gcd()
+	{
+		Assert.AreEqual(1, Gcd(1, 1));
+		Assert.AreEqual(1, Gcd(1, 2));
+		Assert.AreEqual(2, Gcd(4, 6));
+		Assert.AreEqual(6, Gcd(6, 6));
+		Assert.AreEqual(3, Gcd(15, 21));
+		Assert.AreEqual(15, Gcd(45, 105));
+	}
+
+	[TestMethod]
+	public void Lcm()
+	{
+		Assert.AreEqual(1, Lcm(1, 1));
+		Assert.AreEqual(2, Lcm(1, 2));
+		Assert.AreEqual(12, Lcm(4, 6));
+		Assert.AreEqual(6, Lcm(6, 6));
+		Assert.AreEqual(105, Lcm(15, 21));
+		Assert.AreEqual(315, Lcm(45, 105));
+	}
+
+	[TestMethod]
+	public void Mod()
+	{
+		var mod = 6;
+		for (var i = 0; i <= 3 * mod; i++)
+			Assert.AreEqual(i % mod, Mod(i, mod));
+		for (var i = -1; i >= -3 * mod; i--)
+			Assert.AreEqual((i + 4 * mod) % mod, Mod(i, mod));
 	}
 }
