bf91c7fa0a70ae237aff0b142a2c868f5f2cdc36
Keiho Sakapon <git@saka-pon.net>
2019-08-14 19:34:13 +0900

    Add features

--- CSharp/Versions/CSharp6/A.cs
+++ CSharp/Versions/CSharp6/A.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using System.Linq;
+// using static
+using static System.Console;
+using static System.Math;
 
 class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var a = ReadLine().Split().Select(int.Parse).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		Func<int, string> ToText = i => $"{i}";
+		// ?. Operator
+		var length = a.Select(ToText).FirstOrDefault()?.Length ?? 0;
+		WriteLine(Environment.CurrentDirectory);
+
+		var v = new V(2, 3);
+		// String Interpolation
+		WriteLine($"{v.X} {v.Y} {v.Norm:F3}");
+
+		// C# 7
+		//WriteLine((0, 1));
+		//string ToText(int i) => $"{i}";
 	}
+}
+
+struct V
+{
+	public int X, Y;
+	// Expression-bodied Property
+	public double Norm => Sqrt(X * X + Y * Y);
+	public V(int x, int y) { X = x; Y = y; }
+}
+
+class C
+{
+	// Property Initializer
+	public int Id { get; } = 1;
+	// Expression-bodied Method
+	// String Interpolation
+	// nameof Operator
+	public string GetName() => $"{nameof(Id)}: {Id}";
 }
