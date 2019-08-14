fe93a67a5a03b679bb22d2dfff6766b0cd490b3d
Keiho Sakapon <git@saka-pon.net>
2019-08-14 16:28:37 +0900

    Add ModInv function

--- CSharp/CoderLib/CoderLibTest/BasicTest.cs
+++ CSharp/CoderLib/CoderLibTest/BasicTest.cs
@@ -29,6 +29,19 @@ public class BasicTest
 		return t * t * Pow(b, i % 2);
 	}
 
+	// b < mod
+	static int ModPow(int b, int i, int mod)
+	{
+		if (i == 0) return 1;
+		if (i == 1) return b;
+
+		var t = ModPow(b, i / 2, mod);
+		return (int)((long)t * t % mod * ModPow(b, i % 2, mod) % mod);
+	}
+
+	// x < p
+	static int ModInv(int x, int p) => ModPow(x, p - 2, p);
+
 	#region Test
 
 	[TestMethod]
@@ -72,6 +85,36 @@ public class BasicTest
 		b = 3;
 		for (var i = 0; i <= 30; i++)
 			Assert.AreEqual((long)Math.Pow(b, i), Pow(b, i));
+	}
+
+	[TestMethod]
+	public void ModPow()
+	{
+		var p = 7;
+		for (var i = 1; i < p; i++)
+			Assert.AreEqual(1, ModPow(i, p - 1, p));
+		p = 101;
+		for (var i = 1; i < p; i++)
+			Assert.AreEqual(1, ModPow(i, p - 1, p));
+	}
+
+	[TestMethod]
+	public void ModInv()
+	{
+		var p = 7;
+		for (var i = 1; i < p; i++)
+		{
+			var actual = ModInv(i, p);
+			Assert.AreEqual(1, i * actual % p);
+			Console.WriteLine($"{i}^-1 = {actual}");
+		}
+		p = 101;
+		for (var i = 1; i < p; i++)
+		{
+			var actual = ModInv(i, p);
+			Assert.AreEqual(1, i * actual % p);
+			Console.WriteLine($"{i}^-1 = {actual}");
+		}
 	}
 	#endregion
 }
