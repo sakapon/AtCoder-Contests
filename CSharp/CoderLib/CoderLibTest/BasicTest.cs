34d2fbf363a070112ddf44246524bd29aba3f3e4
Keiho Sakapon <git@saka-pon.net>
2019-08-14 13:24:28 +0900

    Update project settings

---
+++ CSharp/CoderLib/CoderLibTest/BasicTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class BasicTest
+{
+	[TestMethod]
+	public void TestMethod1()
+	{
+	}
+}
