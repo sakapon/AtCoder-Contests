9fb2177617c87f929e45a7ac5c127f68893ef2af
Keiho Sakapon <git@saka-pon.net>
2019-08-15 15:43:31 +0900

    Move classes

--- CSharp/CoderLib/CoderLibTest/BasicTest.cs
+++ CSharp/CoderLib/CoderLibTest/BasicTest.cs
@@ -12,9 +12,6 @@ public class BasicTest
 
 	static int Lcm(int x, int y) => x / Gcd(x, y) * y;
 
-	// For negative integers.
-	static int Mod(int v, int mod) => (v %= mod) < 0 ? v + mod : v;
-
 	static long Pow(long b, int i)
 	{
 		if (i == 0) return 1;
@@ -32,19 +29,7 @@ public class BasicTest
 		}
 	}
 
-	// b < mod
-	static int ModPow(int b, int i, int mod)
-	{
-		if (i == 0) return 1;
-		if (i == 1) return b;
-		var t = ModPow(b, i / 2, mod);
-		return (int)((long)t * t % mod * ModPow(b, i % 2, mod) % mod);
-	}
-
-	// x < p
-	static int ModInv(int x, int p) => ModPow(x, p - 2, p);
-
-	#region Test
+	#region Test Methods
 
 	[TestMethod]
 	public void Gcd()
@@ -69,16 +54,6 @@ public class BasicTest
 	}
 
 	[TestMethod]
-	public void Mod()
-	{
-		var mod = 6;
-		for (var i = 0; i <= 3 * mod; i++)
-			Assert.AreEqual(i % mod, Mod(i, mod));
-		for (var i = -1; i >= -3 * mod; i--)
-			Assert.AreEqual((i + 4 * mod) % mod, Mod(i, mod));
-	}
-
-	[TestMethod]
 	public void Pow()
 	{
 		var b = 2;
@@ -87,36 +62,6 @@ public class BasicTest
 		b = 3;
 		for (var i = 0; i <= 30; i++)
 			Assert.AreEqual((long)Math.Pow(b, i), Pow(b, i));
-	}
-
-	[TestMethod]
-	public void ModPow()
-	{
-		var p = 7;
-		for (var i = 1; i < p; i++)
-			Assert.AreEqual(1, ModPow(i, p - 1, p));
-		p = 101;
-		for (var i = 1; i < p; i++)
-			Assert.AreEqual(1, ModPow(i, p - 1, p));
-	}
-
-	[TestMethod]
-	public void ModInv()
-	{
-		var p = 7;
-		for (var i = 1; i < p; i++)
-		{
-			var actual = ModInv(i, p);
-			Assert.AreEqual(1, i * actual % p);
-			Console.WriteLine($"{i}^-1 = {actual}");
-		}
-		p = 101;
-		for (var i = 1; i < p; i++)
-		{
-			var actual = ModInv(i, p);
-			Assert.AreEqual(1, i * actual % p);
-			Console.WriteLine($"{i}^-1 = {actual}");
-		}
 	}
 	#endregion
 }
---
+++ CSharp/CoderLib/CoderLibTest/ModuloTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ModuloTest
+{
+	// For negative integers.
+	static int Mod(int v, int mod) => (v %= mod) < 0 ? v + mod : v;
+
+	// b < mod
+	static int ModPow(int b, int i, int mod)
+	{
+		if (i == 0) return 1;
+		if (i == 1) return b;
+		var t = ModPow(b, i / 2, mod);
+		return (int)((long)t * t % mod * ModPow(b, i % 2, mod) % mod);
+	}
+
+	// x < p
+	static int ModInv(int x, int p) => ModPow(x, p - 2, p);
+
+	#region Test Methods
+
+	[TestMethod]
+	public void Mod()
+	{
+		var mod = 6;
+		for (var i = 0; i <= 3 * mod; i++)
+			Assert.AreEqual(i % mod, Mod(i, mod));
+		for (var i = -1; i >= -3 * mod; i--)
+			Assert.AreEqual((i + 4 * mod) % mod, Mod(i, mod));
+	}
+
+	[TestMethod]
+	public void ModPow()
+	{
+		var p = 7;
+		for (var i = 1; i < p; i++)
+			Assert.AreEqual(1, ModPow(i, p - 1, p));
+		p = 101;
+		for (var i = 1; i < p; i++)
+			Assert.AreEqual(1, ModPow(i, p - 1, p));
+	}
+
+	[TestMethod]
+	public void ModInv()
+	{
+		var p = 7;
+		for (var i = 1; i < p; i++)
+		{
+			var actual = ModInv(i, p);
+			Assert.AreEqual(1, i * actual % p);
+			Console.WriteLine($"{i}^-1 = {actual}");
+		}
+		p = 101;
+		for (var i = 1; i < p; i++)
+		{
+			var actual = ModInv(i, p);
+			Assert.AreEqual(1, i * actual % p);
+			Console.WriteLine($"{i}^-1 = {actual}");
+		}
+	}
+	#endregion
+}
