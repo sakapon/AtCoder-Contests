1d4713b98fc3d4ba62575c250813b5e41b710f55
Keiho Sakapon <git@saka-pon.net>
2019-08-15 20:33:42 +0900

    Add functions for modulo

--- CSharp/CoderLib/CoderLibTest/ModuloTest.cs
+++ CSharp/CoderLib/CoderLibTest/ModuloTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
@@ -28,6 +29,14 @@ public class ModuloTest
 
 	// x < p
 	static int ModInv(int x, int p) => ModPow(x, p - 2, p);
+
+	static int ModFactorial(int n, int mod) => Enumerable.Range(1, n).Aggregate((x, y) => x * y % mod);
+
+	// nPr
+	static int ModNpr(int n, int r, int mod) => Enumerable.Range(n - r + 1, r).Aggregate((x, y) => x * y % mod);
+
+	// nCr
+	static int ModNcr(int n, int r, int p) => ModNpr(n, r, p) * ModInv(ModFactorial(r, p), p) % p;
 
 	#region Test Methods
 
@@ -69,6 +78,34 @@ public class ModuloTest
 			Assert.AreEqual(1, i * actual % p);
 			Console.WriteLine($"{i}^-1 = {actual}");
 		}
+	}
+
+	[TestMethod]
+	public void ModFactorial()
+	{
+		Assert.AreEqual(3628800 % 13, ModFactorial(10, 13));
+		Assert.AreEqual(3628800 % 23, ModFactorial(10, 23));
+		Assert.AreEqual(3628800 % 31, ModFactorial(10, 31));
+		Assert.AreEqual(3628800 % 32, ModFactorial(10, 32));
+	}
+
+	[TestMethod]
+	public void ModNpr()
+	{
+		Assert.AreEqual(151200 % 13, ModNpr(10, 6, 13));
+		Assert.AreEqual(151200 % 23, ModNpr(10, 6, 23));
+		Assert.AreEqual(151200 % 31, ModNpr(10, 6, 31));
+		Assert.AreEqual(151200 % 32, ModNpr(10, 6, 32));
+	}
+
+	[TestMethod]
+	public void ModNcr()
+	{
+		Assert.AreEqual(210 % 13, ModNcr(10, 6, 13));
+		Assert.AreEqual(210 % 23, ModNcr(10, 6, 23));
+		Assert.AreEqual(210 % 31, ModNcr(10, 6, 31));
+		// 素数でないと利用できません。
+		//Assert.AreEqual(210 % 32, ModNcr(10, 6, 32));
 	}
 	#endregion
 }
