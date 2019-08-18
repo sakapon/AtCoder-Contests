52b1eafc7ba414e4bc27478f88af60766ccb03d1
Keiho Sakapon <git@saka-pon.net>
2019-08-18 12:08:02 +0900

    Improve code

--- CSharp/Contests2019/ABC132/A.cs
+++ CSharp/Contests2019/ABC132/A.cs
@@ -5,7 +5,7 @@ class A
 {
 	static void Main()
 	{
-		var d = Console.ReadLine().GroupBy(c => c).ToDictionary(g => g.Key, g => g.ToArray());
-		Console.WriteLine(d.Count == 2 && d.First().Value.Length == 2 ? "Yes" : "No");
+		var d = Console.ReadLine().GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+		Console.WriteLine(d.Count == 2 && d.First().Value == 2 ? "Yes" : "No");
 	}
 }
---
+++ CSharp/Contests2019/ABC132/A2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+class A2
+{
+	static void Main()
+	{
+		var a = Console.ReadLine().OrderBy(c => c).ToArray();
+		Console.WriteLine(a[0] == a[1] && a[2] == a[3] && a[0] != a[2] ? "Yes" : "No");
+	}
+}
