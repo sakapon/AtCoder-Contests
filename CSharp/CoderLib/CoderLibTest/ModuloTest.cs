7849a0a39c3cc32710316dd263f3938b218cdf96
Keiho Sakapon <git@saka-pon.net>
2019-08-18 14:40:09 +0900

    Improve code

--- CSharp/CoderLib/CoderLibTest/ModuloTest.cs
+++ CSharp/CoderLib/CoderLibTest/ModuloTest.cs
@@ -9,25 +9,26 @@ public class ModuloTest
 	static int Mod(int v, int mod) => (v %= mod) < 0 ? v + mod : v;
 
 	// b < mod
-	static int ModPow(int b, int i, int mod)
+	static long ModPow(long b, int i, int mod)
 	{
-		for (var r = 1; ; b = (int)((long)b * b % mod))
+		for (var r = 1L; ; b = b * b % mod)
 		{
-			if (i % 2 > 0) r = (int)((long)r * b % mod);
+			if (i % 2 > 0) r = r * b % mod;
 			if ((i /= 2) < 1) return r;
 		}
 	}
 
 	// x < p
-	static int ModInv(int x, int p) => ModPow(x, p - 2, p);
+	static long ModInv(int x, int p) => ModPow(x, p - 2, p);
 
-	static int ModFactorial(int n, int mod) => Enumerable.Range(1, n).Aggregate((x, y) => x * y % mod);
-
-	// nPr
-	static int ModNpr(int n, int r, int mod) => Enumerable.Range(n - r + 1, r).Aggregate((x, y) => x * y % mod);
-
-	// nCr
-	static int ModNcr(int n, int r, int p) => n - r < r ? ModNcr(n, n - r, p) : ModNpr(n, r, p) * ModInv(ModFactorial(r, p), p) % p;
+	// n >= 0
+	static long ModFactorial(int n, int mod) { for (long x = 1, i = 1; ; x = x * ++i % mod) if (i >= n) return x; }
+	static long ModNpr(int n, int r, int mod)
+	{
+		if (n < r) return 0;
+		for (long x = 1, i = n - r; ; x = x * ++i % mod) if (i >= n) return x;
+	}
+	static long ModNcr(int n, int r, int p) => n < r ? 0 : n - r < r ? ModNcr(n, n - r, p) : ModNpr(n, r, p) * ModInv((int)ModFactorial(r, p), p) % p;
 
 	#region Test Methods
 
