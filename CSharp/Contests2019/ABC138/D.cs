aafb204742180988a03c07ce4e44819c58f4f54b
Keiho Sakapon <git@saka-pon.net>
2019-08-18 15:57:06 +0900

    Create projects

---
+++ CSharp/Contests2019/ABC138/D.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class D
+{
+	static void Main()
+	{
+		Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+		Console.WriteLine(string.Join(" ", a));
+	}
+}
