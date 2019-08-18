afa5bbdfb8a85b16d573f4e0dd8e969bf2cce189
Keiho Sakapon <git@saka-pon.net>
2019-08-18 15:34:55 +0900

    Refactor

--- CSharp/Contests2019/ABC132/D2.cs
+++ CSharp/Contests2019/ABC132/D2.cs
@@ -6,10 +6,9 @@ class D2
 {
 	static void Main()
 	{
-		var M = 1000000007;
 		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
 		BigInteger c = a[0] - a[1] + 1;
-		for (var i = 1; i <= a[1]; Console.WriteLine(c % M), c = c * (a[0] - a[1] + 2 - ++i) / i * (a[1] - i + 1) / (i - 1)) ;
+		for (var i = 1; i <= a[1]; Console.WriteLine(c % 1000000007), c = c * (a[0] - a[1] + 2 - ++i) / i * (a[1] - i + 1) / (i - 1)) ;
 	}
 }
