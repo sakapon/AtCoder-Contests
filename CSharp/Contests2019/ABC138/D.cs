99c4bdece3d80637ff94415a5bfbf2cad00cf377
Keiho Sakapon <git@saka-pon.net>
2019-08-18 23:51:47 +0900

    Solve task

--- CSharp/Contests2019/ABC138/D.cs
+++ CSharp/Contests2019/ABC138/D.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var h = read();
+		map = Enumerable.Range(0, h[0] - 1).Select(i => read()).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Select(r => r[1]).ToArray());
+		ops = Enumerable.Range(0, h[1]).Select(i => read()).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Sum(r => r[1]));
 
-		Console.WriteLine(string.Join(" ", a));
+		c = new int[h[0]];
+		FindPoints(1);
+		Console.WriteLine(string.Join(" ", c));
+	}
+
+	static Dictionary<int, int[]> map;
+	static Dictionary<int, int> ops;
+	static int[] c;
+	static int sum;
+
+	static void FindPoints(int p)
+	{
+		if (ops.ContainsKey(p)) sum += ops[p];
+		c[p - 1] = sum;
+		if (map.ContainsKey(p)) foreach (var r in map[p]) FindPoints(r);
+		if (ops.ContainsKey(p)) sum -= ops[p];
 	}
 }
