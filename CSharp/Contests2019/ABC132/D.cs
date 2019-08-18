0d8dad0fd49d3c8a2e97ff48f9d4a0b06b0268f2
Keiho Sakapon <git@saka-pon.net>
2019-08-18 15:04:45 +0900

    Improve lib

--- CSharp/Contests2019/ABC132/D.cs
+++ CSharp/Contests2019/ABC132/D.cs
@@ -9,19 +9,23 @@ class D
 
 		var M = 1000000007;
 		for (int i = 1; i <= a[1]; i++)
-			Console.WriteLine(i > a[0] - a[1] + 1 ? 0 : (long)ModNcr(a[0] - a[1] + 1, i, M) * ModNcr(a[1] - 1, i - 1, M) % M);
+			Console.WriteLine(ModNcr(a[0] - a[1] + 1, i, M) * ModNcr(a[1] - 1, i - 1, M) % M);
 	}
 
-	static int ModPow(int b, int i, int mod)
+	static long ModPow(long b, int i, int mod)
 	{
-		for (var r = 1; ; b = (int)((long)b * b % mod))
+		for (var r = 1L; ; b = b * b % mod)
 		{
-			if (i % 2 > 0) r = (int)((long)r * b % mod);
+			if (i % 2 > 0) r = r * b % mod;
 			if ((i /= 2) < 1) return r;
 		}
 	}
-	static int ModInv(int x, int p) => ModPow(x, p - 2, p);
-	static int ModFactorial(int n, int mod) => n == 0 ? 1 : (int)Enumerable.Range(1, n).Select(x => (long)x).Aggregate((x, y) => x * y % mod);
-	static int ModNpr(int n, int r, int mod) => r == 0 ? 1 : (int)Enumerable.Range(n - r + 1, r).Select(x => (long)x).Aggregate((x, y) => x * y % mod);
-	static int ModNcr(int n, int r, int p) => n - r < r ? ModNcr(n, n - r, p) : (int)((long)ModNpr(n, r, p) * ModInv(ModFactorial(r, p), p) % p);
+	static long ModInv(int x, int p) => ModPow(x, p - 2, p);
+	static long ModFactorial(int n, int mod) { for (long x = 1, i = 1; ; x = x * ++i % mod) if (i >= n) return x; }
+	static long ModNpr(int n, int r, int mod)
+	{
+		if (n < r) return 0;
+		for (long x = 1, i = n - r; ; x = x * ++i % mod) if (i >= n) return x;
+	}
+	static long ModNcr(int n, int r, int p) => n < r ? 0 : n - r < r ? ModNcr(n, n - r, p) : ModNpr(n, r, p) * ModInv((int)ModFactorial(r, p), p) % p;
 }
