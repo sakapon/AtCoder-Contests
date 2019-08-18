e82ce4af1339c2a935a3d0c35ccc53705ad2d36d
Keiho Sakapon <git@saka-pon.net>
2019-08-18 20:53:05 +0900

    Add OrderedList class with key

--- CSharp/CoderLib/CoderLibTest/OrderedListTest.cs
+++ CSharp/CoderLib/CoderLibTest/OrderedListTest.cs
@@ -21,6 +21,38 @@ class OrderedList : List<int>
 	}
 }
 
+class OrderedList<T, TKey> : List<KeyValuePair<TKey, T>>
+{
+	static readonly Comparer<TKey> _comparer = Comparer<TKey>.Default;
+	Func<T, TKey> _keySelector;
+
+	public OrderedList(Func<T, TKey> keySelector)
+	{
+		_keySelector = keySelector;
+	}
+
+	public OrderedList(Func<T, TKey> keySelector, IEnumerable<T> collection) : base(collection.Select(o => new KeyValuePair<TKey, T>(keySelector(o), o)).OrderBy(p => p.Key))
+	{
+		_keySelector = keySelector;
+	}
+
+	public void AddForOrder(T o)
+	{
+		var p = new KeyValuePair<TKey, T>(_keySelector(o), o);
+		Insert(Search(p.Key), p);
+	}
+
+	// 挿入先の番号を二分探索で求めます。値が重複する場合は後ろの番号です。
+	int Search(TKey k) => Count > 0 ? Search(k, 0, Count) : 0;
+	int Search(TKey k, int start, int count)
+	{
+		if (count == 1) return _comparer.Compare(k, this[start].Key) < 0 ? start : start + 1;
+		var c = count / 2;
+		var s = start + c;
+		return _comparer.Compare(k, this[s].Key) < 0 ? Search(k, start, c) : Search(k, s, count - c);
+	}
+}
+
 [TestClass]
 public class OrderedListTest
 {
@@ -42,5 +74,15 @@ public class OrderedListTest
 		foreach (var v in values)
 			actual.AddForOrder(v);
 		CollectionAssert.AreEqual(values.OrderBy(x => x).ToArray(), actual);
+	}
+
+	[TestMethod]
+	public void AddForOrder_Key()
+	{
+		var values = new[] { "ddd", "a", "cc", "bb", "", "c" };
+		var actual = new OrderedList<string, int>(s => s.Length);
+		foreach (var v in values)
+			actual.AddForOrder(v);
+		CollectionAssert.AreEqual(values.OrderBy(s => s.Length).ToArray(), actual.Select(p => p.Value).ToArray());
 	}
 }
