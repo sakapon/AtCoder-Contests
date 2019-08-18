23358c79a7e781b3deaba2df0ad507d857c52c91
Keiho Sakapon <git@saka-pon.net>
2019-08-18 21:37:59 +0900

    First challenge

---
+++ CSharp/Contests2019/ABC138/E.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class E
+{
+	static void Main()
+	{
+		var s = Console.ReadLine();
+		var t = Console.ReadLine();
+
+		var i = 0L;
+		var ti = 0;
+		for (; i < s.Length; i++)
+		{
+			if (s[(int)(i % s.Length)] == t[ti])
+			{
+				ti++;
+				if (ti == t.Length) { Console.WriteLine(i + 1); return; }
+			}
+		}
+		if (ti == 0) { Console.WriteLine(-1); return; }
+
+		for (; i < int.MaxValue; i++)
+		{
+			if (s[(int)(i % s.Length)] == t[ti])
+			{
+				ti++;
+				if (ti == t.Length) { Console.WriteLine(i + 1); return; }
+			}
+		}
+		Console.WriteLine(-1);
+	}
+}
