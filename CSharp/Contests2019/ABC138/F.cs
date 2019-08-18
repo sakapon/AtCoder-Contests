a11c92bbe79d661a4c22e83fdba5df1c1bf6d059
Keiho Sakapon <git@saka-pon.net>
2019-08-18 22:34:06 +0900

    Timeout error

---
+++ CSharp/Contests2019/ABC138/F.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class F
+{
+	static void Main()
+	{
+		var M = 1000000007;
+		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+		var c = 0L;
+		for (var x = a[0]; x <= a[1]; x++)
+			for (var y = x; y <= a[1]; y++)
+				if ((y % x) == (y ^ x)) c++;
+		Console.WriteLine(c);
+	}
+}
