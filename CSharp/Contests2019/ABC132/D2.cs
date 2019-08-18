ee7a3a4c27a261f310ad0ae8f03b8637e32ef6d8
Keiho Sakapon <git@saka-pon.net>
2019-08-18 15:31:58 +0900

    Use BigInteger

---
+++ CSharp/Contests2019/ABC132/D2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+class D2
+{
+	static void Main()
+	{
+		var M = 1000000007;
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+		BigInteger c = a[0] - a[1] + 1;
+		for (var i = 1; i <= a[1]; Console.WriteLine(c % M), c = c * (a[0] - a[1] + 2 - ++i) / i * (a[1] - i + 1) / (i - 1)) ;
+	}
+}
