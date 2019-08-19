d3d8553ec5a73f1f10edbbdb5573c09e5b091ad1
Keiho Sakapon <git@saka-pon.net>
2019-08-20 03:05:31 +0900

    Time limit error

---
+++ CSharp/Contests2019/ABC132/F.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class F
+{
+	static void Main()
+	{
+		var M = 1000000007;
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		int n = a[0], k = a[1];
+
+		var c = new int[n + 1];
+		int j = 1, m = n / 2;
+		for (; j <= m; j++) c[j] = n / j;
+		for (; j <= n; j++) c[j] = 1;
+
+		var v = c.ToArray();
+		var d = new int[n + 1];
+		for (var i = 3; i <= k; i++)
+		{
+			for (j = 1; j <= n; j++) d[j] = (d[j - 1] + v[j]) % M;
+			for (j = 1; j <= n; j++) v[j] = d[c[j]];
+		}
+		Console.WriteLine(v.Aggregate((x, y) => (x + y) % M));
+	}
+}
