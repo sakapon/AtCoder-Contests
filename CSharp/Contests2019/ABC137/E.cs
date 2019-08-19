1a00854ece9b0c87f473d777c5e5d5a30e54823f
Keiho Sakapon <git@saka-pon.net>
2019-08-19 22:01:45 +0900

    Solve task

--- CSharp/Contests2019/ABC137/E.cs
+++ CSharp/Contests2019/ABC137/E.cs
@@ -8,46 +8,35 @@ class E
 	{
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		map = Enumerable.Range(0, h[1]).Select(i => read()).Select(r => new R { A = r[0], B = r[1], C = r[2] - h[2] }).GroupBy(r => r.A).ToDictionary(g => g.Key, g => g.ToArray());
+		var rs = Enumerable.Range(0, h[1]).Select(i => read()).Select(r => new R { A = r[0], B = r[1], C = r[2] - h[2] }).ToArray();
+		map = rs.GroupBy(r => r.A).ToDictionary(g => g.Key, g => g.ToArray());
 
-		// 無限増加する閉区間の起点を検出します。
-		FindCircuit(1);
-		// TODO: その頂点から終点に到達できるかを判定します。
-		if (holes.Any()) { Console.WriteLine(-1); return; }
-
-		var d = new Dictionary<int, long> { [1] = 0 };
+		var cs = Enumerable.Repeat(long.MinValue, h[0] + 1).ToArray();
+		cs[1] = 0;
+		long c;
 		for (var i = 0; i < h[0]; i++)
-			foreach (var p in d.Keys.Where(p => map.ContainsKey(p)).ToArray())
-				foreach (var r in map[p])
-				{
-					var c = d[p] + r.C;
-					if (!d.ContainsKey(r.B) || d[r.B] < c) d[r.B] = c;
-				}
-		Console.WriteLine(Math.Max(d[h[0]], 0));
+			foreach (var r in rs)
+				if (cs[r.A] != long.MinValue && cs[r.B] < (c = cs[r.A] + r.C)) cs[r.B] = c;
+
+		foreach (var r in rs)
+		{
+			if (outs.Contains(r.B)) continue;
+			if (cs[r.A] == long.MinValue || cs[r.B] >= cs[r.A] + r.C) continue;
+
+			FindPath(r.B);
+			if (outs.Contains(h[0])) { Console.WriteLine(-1); return; }
+		}
+		Console.WriteLine(Math.Max(cs[h[0]], 0));
 	}
 
 	static Dictionary<int, R[]> map;
-	static HashSet<int> holes = new HashSet<int>();
-	static HashSet<int> ps = new HashSet<int>();
-	static Stack<R> rs = new Stack<R>();
+	static HashSet<int> outs = new HashSet<int>();
 
-	static void FindCircuit(int p)
+	static void FindPath(int p)
 	{
+		if (!outs.Add(p)) return;
 		if (!map.ContainsKey(p)) return;
-
-		if (!ps.Add(p))
-		{
-			if (rs.Reverse().SkipWhile(r => r.A != p).Sum(r => r.C) > 0) holes.Add(p);
-			return;
-		}
-
-		foreach (var r in map[p])
-		{
-			rs.Push(r);
-			FindCircuit(r.B);
-			rs.Pop();
-		}
-		ps.Remove(p);
+		foreach (var r in map[p]) FindPath(r.B);
 	}
 }
 
