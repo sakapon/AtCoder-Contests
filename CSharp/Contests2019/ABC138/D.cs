f3938c09d061e515ef7e7536a6f095655f419901
Keiho Sakapon <git@saka-pon.net>
2019-08-19 12:54:41 +0900

    Time Limit Error

--- CSharp/Contests2019/ABC138/D.cs
+++ CSharp/Contests2019/ABC138/D.cs
@@ -8,11 +8,12 @@ class D
 	{
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		map = Enumerable.Range(0, h[0] - 1).Select(i => read()).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Select(r => r[1]).ToArray());
-		ops = Enumerable.Range(0, h[1]).Select(i => read()).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Sum(r => r[1]));
+		var roots = Enumerable.Range(0, h[0] - 1).Select(i => read()).ToArray();
+		map = roots.Concat(roots.Select(r => new[] { r[1], r[0] })).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Select(r => r[1]).ToArray());
+		ops = Enumerable.Range(0, h[1]).Select(i => read()).GroupBy(a => a[0]).ToDictionary(g => g.Key, g => g.Sum(a => a[1]));
 
 		c = new int[h[0]];
-		FindPoints(1);
+		FindPoints(1, 0);
 		Console.WriteLine(string.Join(" ", c));
 	}
 
@@ -21,11 +22,11 @@ class D
 	static int[] c;
 	static int sum;
 
-	static void FindPoints(int p)
+	static void FindPoints(int p, int p0)
 	{
 		if (ops.ContainsKey(p)) sum += ops[p];
 		c[p - 1] = sum;
-		if (map.ContainsKey(p)) foreach (var r in map[p]) FindPoints(r);
+		if (map.ContainsKey(p)) foreach (var q in map[p].Where(q => q != p0)) FindPoints(q, p);
 		if (ops.ContainsKey(p)) sum -= ops[p];
 	}
 }
