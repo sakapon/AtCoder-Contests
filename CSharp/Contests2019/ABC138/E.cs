0b5228c84a8af534ee01d86d46a6a851930f23ad
Keiho Sakapon <git@saka-pon.net>
2019-08-19 16:47:35 +0900

    Solve task

--- CSharp/Contests2019/ABC138/E.cs
+++ CSharp/Contests2019/ABC138/E.cs
@@ -9,7 +9,7 @@ class E
 		var s = Console.ReadLine();
 		var t = Console.ReadLine();
 
-		var d = Enumerable.Range('a', 26).ToDictionary(x => (char)x, x => new HashSet<int>());
+		var d = Enumerable.Range('a', 26).ToDictionary(x => (char)x, x => new List<int>());
 		for (var i = 0; i < s.Length; i++) d[s[i]].Add(i);
 
 		int turn = 0, si = -1;
@@ -17,22 +17,26 @@ class E
 		{
 			if (d[c].Count == 0) { Console.WriteLine(-1); return; }
 
-			var found = false;
-			foreach (var ci in d[c])
+			var ci = Search(d[c], si);
+			if (ci != d[c].Count)
 			{
-				if (si < ci)
-				{
-					si = ci;
-					found = true;
-					break;
-				}
+				si = d[c][ci];
 			}
-			if (!found)
+			else
 			{
 				turn++;
-				si = d[c].First();
+				si = d[c][0];
 			}
 		}
 		Console.WriteLine((long)s.Length * turn + si + 1);
+	}
+
+	static int Search(IList<int> l, int v) => l.Count > 0 ? Search(l, v, 0, l.Count) : 0;
+	static int Search(IList<int> l, int v, int start, int count)
+	{
+		if (count == 1) return start + (v < l[start] ? 0 : 1);
+		var c = count / 2;
+		var s = start + c;
+		return v < l[s] ? Search(l, v, start, c) : Search(l, v, s, count - c);
 	}
 }
