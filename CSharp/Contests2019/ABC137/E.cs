a884de5fdd6c1b81e1336bf730ebbffb2483d8bd
Keiho Sakapon <git@saka-pon.net>
2019-08-19 20:32:42 +0900

    Revert "Wrong Answer and Timeout"
    This reverts commit b3ed2a8061dca6b584091b8228b09d8a19c39190.

--- CSharp/Contests2019/ABC137/E.cs
+++ CSharp/Contests2019/ABC137/E.cs
@@ -10,16 +10,26 @@ class E
 		var h = read();
 		map = Enumerable.Range(0, h[1]).Select(i => read()).Select(r => new R { A = r[0], B = r[1], C = r[2] - h[2] }).GroupBy(r => r.A).ToDictionary(g => g.Key, g => g.ToArray());
 
-		cs = Enumerable.Repeat(long.MinValue, h[0] + 1).ToArray();
-		cs[1] = 0;
+		// 無限増加する閉区間の起点を検出します。
 		FindCircuit(1);
-		Console.WriteLine(cs[h[0]] == long.MaxValue ? -1 : Math.Max(cs[h[0]], 0));
+		// TODO: その頂点から終点に到達できるかを判定します。
+		if (holes.Any()) { Console.WriteLine(-1); return; }
+
+		var d = new Dictionary<int, long> { [1] = 0 };
+		for (var i = 0; i < h[0]; i++)
+			foreach (var p in d.Keys.Where(p => map.ContainsKey(p)).ToArray())
+				foreach (var r in map[p])
+				{
+					var c = d[p] + r.C;
+					if (!d.ContainsKey(r.B) || d[r.B] < c) d[r.B] = c;
+				}
+		Console.WriteLine(Math.Max(d[h[0]], 0));
 	}
 
 	static Dictionary<int, R[]> map;
+	static HashSet<int> holes = new HashSet<int>();
 	static HashSet<int> ps = new HashSet<int>();
 	static Stack<R> rs = new Stack<R>();
-	static long[] cs;
 
 	static void FindCircuit(int p)
 	{
@@ -27,20 +37,12 @@ class E
 
 		if (!ps.Add(p))
 		{
-			var loop = rs.Reverse().SkipWhile(r => r.A != p).ToArray();
-			if (loop.Sum(r => r.C) > 0) foreach (var r in loop) cs[r.A] = long.MaxValue;
+			if (rs.Reverse().SkipWhile(r => r.A != p).Sum(r => r.C) > 0) holes.Add(p);
 			return;
 		}
 
 		foreach (var r in map[p])
 		{
-			if (cs[p] == long.MaxValue) cs[r.B] = cs[p];
-			else
-			{
-				var c = cs[p] + r.C;
-				if (cs[r.B] < c) cs[r.B] = c;
-			}
-
 			rs.Push(r);
 			FindCircuit(r.B);
 			rs.Pop();
