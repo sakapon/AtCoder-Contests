01f8df8f4edbe4fd4ec93520bc6015a6942a47fc
Keiho Sakapon <git@saka-pon.net>
2019-08-21 01:00:24 +0900

    Wrong Answer

--- CSharp/Contests2019/ABC138/F.cs
+++ CSharp/Contests2019/ABC138/F.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 
 class F
 {
@@ -8,13 +9,25 @@ class F
 		var M = 1000000007;
 		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
 		long L = a[0], R = a[1];
-
 		var c = 0L;
-		for (var x = L; x <= R; x++)
+
+		var pl = (int)Math.Ceiling(Math.Log(L, 2));
+		var pr = (int)Math.Floor(Math.Log(R + 1, 2));
+		if (pl < pr)
+			c += (long)((BigInteger.Pow(3, pr) - BigInteger.Pow(3, pl)) / 2 % M);
+
+		var max1 = (long)Math.Pow(2, pl) - 1;
+		for (var x = L; x <= max1; x++)
 		{
-			var p = Math.Floor(Math.Log(x, 2));
-			var y_max = Math.Min((long)Math.Pow(2, p + 1) - 1, R);
-			for (var y = x; y <= y_max; y++)
+			for (var y = x; y <= max1; y++)
+				if (y - x == (y ^ x)) c++;
+			c %= M;
+		}
+
+		var min2 = (long)Math.Pow(2, pr);
+		for (var x = min2; x <= R; x++)
+		{
+			for (var y = x; y <= R; y++)
 				if (y - x == (y ^ x)) c++;
 			c %= M;
 		}
