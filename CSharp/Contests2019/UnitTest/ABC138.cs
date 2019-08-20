63b45d997c5c0b11a0e3fa324ed41160d440de23
Keiho Sakapon <git@saka-pon.net>
2019-08-20 22:52:25 +0900

    Add test cases

---
+++ CSharp/Contests2019/UnitTest/ABC138.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class ABC138
+	{
+		[TestMethod] public void All() => TestAll();
+		[TestMethod] public void F_1() => TestOne();
+		[TestMethod] public void F_2() => TestOne();
+		[TestMethod] public void F_3() => TestOne();
+
+		#region Common Part
+
+		// 静的フィールドは共通で、テストケースごとに初期化されません。複数のテストを実行すると不正確になる場合があります。
+		[ClassInitialize] public static void ClassInitialize(TestContext context) => TestJudge.LoadTestClass(context);
+
+		public TestContext TestContext { get; set; }
+
+		void TestAll() => TestJudge.TestAll(TestContext);
+		void TestOne() => TestJudge.TestOne(TestContext);
+
+		#endregion
+	}
+}
