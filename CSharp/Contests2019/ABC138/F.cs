5c38331202596588fdcefc10d651af9d50e9b2bf
Keiho Sakapon <git@saka-pon.net>
2019-08-20 23:24:20 +0900

    Filter range

--- CSharp/Contests2019/ABC138/F.cs
+++ CSharp/Contests2019/ABC138/F.cs
@@ -7,11 +7,17 @@ class F
 	{
 		var M = 1000000007;
 		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+		long L = a[0], R = a[1];
 
 		var c = 0L;
-		for (var x = a[0]; x <= a[1]; x++)
-			for (var y = x; y <= a[1]; y++)
-				if ((y % x) == (y ^ x)) c++;
+		for (var x = L; x <= R; x++)
+		{
+			var p = Math.Floor(Math.Log(x, 2));
+			var y_max = Math.Min((long)Math.Pow(2, p + 1) - 1, R);
+			for (var y = x; y <= y_max; y++)
+				if (y - x == (y ^ x)) c++;
+			c %= M;
+		}
 		Console.WriteLine(c);
 	}
 }
