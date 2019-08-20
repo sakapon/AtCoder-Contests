868e84de399a6470ae4d909cfd85cead4e0781c7
Keiho Sakapon <git@saka-pon.net>
2019-08-20 18:37:43 +0900

    Add TestJudge class

--- CSharp/Contests2019/UnitTest/ABC137.cs
+++ CSharp/Contests2019/UnitTest/ABC137.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
@@ -12,47 +7,19 @@ namespace UnitTest
 	public class ABC137
 	{
 		[TestMethod] public void All() => TestAll();
-		[TestMethod] public void E_1() => Test();
-		[TestMethod] public void E_2() => Test();
-		[TestMethod] public void E_3() => Test();
+		[TestMethod] public void E_1() => TestOne();
+		[TestMethod] public void E_2() => TestOne();
+		[TestMethod] public void E_3() => TestOne();
 
-		struct TestCase
-		{
-			public string Name, Input, Output;
-		}
+		#region Common Part
 
-		static readonly Assembly TargetAssembly = LoadAssembly();
-		static Assembly LoadAssembly([CallerFilePath]string path = "")
-		{
-			var thisDll = Assembly.GetExecutingAssembly();
-			var dllName = Path.GetFileNameWithoutExtension(path);
-			return Assembly.LoadFile(thisDll.Location.Replace("UnitTest", dllName));
-		}
+		[ClassInitialize] public static void ClassInitialize(TestContext context) => TestJudge.LoadTestClass(context);
 
-		static readonly Dictionary<string, TestCase> TestCases = GetTestCases();
-		static Dictionary<string, TestCase> GetTestCases([CallerFilePath]string path = "") =>
-			File.ReadAllText($@"..\..\..\..\{Path.GetFileNameWithoutExtension(path)}\TestCases.txt")
-				.Split("---").Select(s => s.Split('/'))
-				.Select(a => new TestCase { Name = a[0].Trim(), Input = a[1].Trim(), Output = a[2].Trim() })
-				.ToDictionary(t => t.Name);
+		public TestContext TestContext { get; set; }
 
-		static void TestAll()
-		{
-		}
+		void TestAll() => TestJudge.TestAll(TestContext);
+		void TestOne() => TestJudge.TestOne(TestContext);
 
-		static void Test([CallerFilePath]string path = "", [CallerMemberName]string name = "")
-		{
-			using (var reader = new StringReader(TestCases[name].Input))
-			using (var writer = new StringWriter())
-			{
-				Console.SetIn(reader);
-				Console.SetOut(writer);
-
-				var type = TargetAssembly.DefinedTypes.First(t => t.Name == name.Split('_')[0]);
-				type.GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null);
-
-				Assert.AreEqual(TestCases[name].Output, writer.ToString().Trim());
-			}
-		}
+		#endregion
 	}
 }
---
+++ CSharp/Contests2019/UnitTest/TestJudge.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MainMethodMap = System.Collections.Generic.Dictionary<string, System.Reflection.MethodInfo>;
+using TestCaseMap = System.Collections.Generic.Dictionary<string, UnitTest.TestCase>;
+
+namespace UnitTest
+{
+	public static class TestJudge
+	{
+		static readonly Dictionary<string, MainMethodMap> TargetDlls = new Dictionary<string, MainMethodMap>();
+		static readonly Dictionary<string, TestCaseMap> TestCases = new Dictionary<string, TestCaseMap>();
+
+		public static void LoadTestClass(TestContext context)
+		{
+			var testClassName = Type.GetType(context.FullyQualifiedTestClassName).Name;
+
+			var thisDll = Assembly.GetExecutingAssembly();
+			var targetDll = Assembly.LoadFile(thisDll.Location.Replace(thisDll.GetName().Name, testClassName));
+			TargetDlls[testClassName] = targetDll.DefinedTypes
+				.Where(t => t.Name.Length == 1 && 'A' <= t.Name[0] && t.Name[0] <= 'F')
+				.ToDictionary(t => t.Name, t => t.GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static));
+
+			TestCases[testClassName] = File.ReadAllText($@"..\..\..\..\{testClassName}\TestCases.txt")
+				.Split("---").Select(s => s.Split('/'))
+				.Select(a => new TestCase { Name = a[0].Trim(), Input = a[1].Trim(), Output = a[2].Trim() })
+				.ToDictionary(t => t.Name);
+		}
+
+		public static void TestAll(TestContext context)
+		{
+		}
+
+		public static void TestOne(TestContext context)
+		{
+			var testClassName = Type.GetType(context.FullyQualifiedTestClassName).Name;
+			var mainMethod = TargetDlls[testClassName][context.TestName.Split('_')[0]];
+			var testCase = TestCases[testClassName][context.TestName];
+
+			using (var reader = new StringReader(testCase.Input))
+			using (var writer = new StringWriter())
+			{
+				Console.SetIn(reader);
+				Console.SetOut(writer);
+
+				mainMethod.Invoke(null, null);
+
+				Assert.AreEqual(testCase.Output, writer.ToString().Trim());
+			}
+		}
+	}
+
+	struct TestCase
+	{
+		public string Name, Input, Output;
+	}
+}
