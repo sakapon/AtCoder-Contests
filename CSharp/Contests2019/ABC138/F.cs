8fdc098a3683d6a9a84e1c607982d665c9c30583
Keiho Sakapon <git@saka-pon.net>
2019-08-21 12:48:49 +0900

    Cache powers

--- CSharp/Contests2019/ABC138/F.cs
+++ CSharp/Contests2019/ABC138/F.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using System.Numerics;
 
@@ -6,31 +7,44 @@ class F
 {
 	static void Main()
 	{
-		var M = 1000000007;
 		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
 		long L = a[0], R = a[1];
-		var c = 0L;
 
-		var pl = (int)Math.Ceiling(Math.Log(L, 2));
-		var pr = (int)Math.Floor(Math.Log(R + 1, 2));
-		if (pl < pr)
-			c += (long)((BigInteger.Pow(3, pr) - BigInteger.Pow(3, pl)) / 2 % M);
+		BigInteger c = 0;
+		var pow2 = new long[61];
+		var pow3 = new BigInteger[61];
+		pow2[0] = 1;
+		pow3[0] = 1;
+		for (var i = 1; i <= 60; pow2[i] = 2 * pow2[i - 1], pow3[i] = 3 * pow3[i - 1], i++) ;
 
-		var max1 = (long)Math.Pow(2, pl) - 1;
-		for (var x = L; x <= max1; x++)
+		for (int i = 0; i < 60; i++)
 		{
-			for (var y = x; y <= max1; y++)
-				if (y - x == (y ^ x)) c++;
-			c %= M;
-		}
+			var p2l = pow2[i];
+			var p2r = pow2[i + 1] - 1;
 
-		var min2 = (long)Math.Pow(2, pr);
-		for (var x = min2; x <= R; x++)
-		{
-			for (var y = x; y <= R; y++)
-				if (y - x == (y ^ x)) c++;
-			c %= M;
+			if (R < p2l) break;
+			if (p2r < L) continue;
+			if (L <= p2l && p2r <= R) { c += pow3[i]; continue; }
+
+			if (p2l < L)
+			{
+				for (var x = L; x <= p2r; c += pow2[CountBit0(x, i - 1)], x++) ;
+			}
+			else
+			{
+				for (var x = p2l; x <= R; x++)
+					for (var y = x; y <= R; y++)
+						if (y - x == (y ^ x)) c++;
+			}
 		}
-		Console.WriteLine(c);
+		Console.WriteLine(c % 1000000007);
+	}
+
+	static int CountBit0(long v, int maxBit)
+	{
+		var c = 0;
+		var b = new BitArray(new[] { (int)v, (int)(v >> 32) });
+		for (var i = 0; i <= maxBit; i++) if (!b[i]) c++;
+		return c;
 	}
 }
