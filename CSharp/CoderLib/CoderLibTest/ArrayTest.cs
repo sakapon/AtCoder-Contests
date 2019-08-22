a2b00bd1222f0e2920b7437d028c7fb737e808b3
Keiho Sakapon <git@saka-pon.net>
2019-08-22 11:39:55 +0900

    Add functions for array vector

---
+++ CSharp/CoderLib/CoderLibTest/ArrayTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ArrayTest
+{
+	static int DotProduct(int[] a, int[] b)
+	{
+		var r = 0;
+		for (var i = 0; i < a.Length; i++) r += a[i] * b[i];
+		return r;
+	}
+
+	#region Test Methods
+
+	[TestMethod]
+	public void DotProduct()
+	{
+		Assert.AreEqual(32, DotProduct(new[] { 1, 2, 3 }, new[] { 4, 5, 6 }));
+		Assert.AreEqual(89, DotProduct(new[] { 2, 3, 5, 7 }, new[] { 11, 7, 5, 3 }));
+	}
+	#endregion
+}
