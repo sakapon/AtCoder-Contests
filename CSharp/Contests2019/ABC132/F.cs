ba431de6ff5614f35dd71e76a96044dd253847ea
Keiho Sakapon <git@saka-pon.net>
2019-08-22 22:54:44 +0900

    Simplify weight vector (Time limit error)

--- CSharp/Contests2019/ABC132/F.cs
+++ CSharp/Contests2019/ABC132/F.cs
@@ -7,16 +7,18 @@ class F
 	static void Main()
 	{
 		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		int n = a[0], k = a[1];
+		int n = a[0], k = a[1], n1 = (int)Math.Sqrt(n), n2 = n / n1 - 1;
 
-		var w = Enumerable.Range(1, n).GroupBy(i => n / i).Select(g => g.Count()).ToArray();
+		var w = new int[n1 + n2];
 		var vl = w.Length;
+		for (int i = 0; i < n1; i++) w[i] = 1;
+		for (int i = 1, s = n; i <= n2; i++) s -= (w[vl - i] = s - n / (i + 1));
 
 		Func<BigInteger[], BigInteger[]> next = x =>
 		{
 			var t = new BigInteger[vl];
 			t[vl - 1] = x[0];
-			for (int i = 1; i < vl; i++) t[vl - 1 - i] = t[vl - i] + w[i] * x[i];
+			for (var i = 1; i < vl; i++) t[vl - 1 - i] = t[vl - i] + w[i] * x[i];
 			return t;
 		};
 
