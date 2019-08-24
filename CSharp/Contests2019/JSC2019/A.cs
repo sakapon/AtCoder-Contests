5766b18e0e0f44e11d1122e12e22f5ef61887b53
Keiho Sakapon <git@saka-pon.net>
2019-08-25 00:00:58 +0900

    Refactor

--- CSharp/Contests2019/JSC2019/A.cs
+++ CSharp/Contests2019/JSC2019/A.cs
@@ -6,15 +6,6 @@ class A
 	static void Main()
 	{
 		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		int m = a[0], d = a[1];
-
-		var c = 0;
-		for (int i = 4; i <= m; i++)
-			for (int j = 1; j <= d; j++)
-			{
-				int d1 = j % 10, d2 = j / 10;
-				if (d1 >= 2 && d2 >= 2 && i == d1 * d2) c++;
-			}
-		Console.WriteLine(c);
+		Console.WriteLine(Enumerable.Range(1, a[0]).SelectMany(i => Enumerable.Range(1, a[1]).Select(j => new { i, d1 = j % 10, d2 = j / 10 })).Count(_ => _.d1 >= 2 && _.d2 >= 2 && _.i == _.d1 * _.d2));
 	}
 }
--- CSharp/Contests2019/JSC2019/B.cs
+++ CSharp/Contests2019/JSC2019/B.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Numerics;
+using static System.Console;
 
 class B
 {
 	static void Main()
 	{
-		var M = 1000000007;
-		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		int n = h[0];
-		BigInteger k = h[1];
+		var h = ReadLine().Split().Select(int.Parse).ToArray();
+		var a = ReadLine().Split().Select(int.Parse).ToArray();
 
-		var m1 = new int[n];
-		var m2 = new int[n];
-		for (int i = 0; i < h[0]; i++)
-		{
-			for (int j = 0; j < i; j++) if (a[j] < a[i]) m1[i]++;
-			for (int j = i + 1; j < n; j++) if (a[j] < a[i]) m2[i]++;
-		}
-		Console.WriteLine((k * (k - 1) / 2 * m1.Aggregate((x, y) => (x + y) % M) + k * (k + 1) / 2 * m2.Aggregate((x, y) => (x + y) % M)) % M);
+		BigInteger x1 = Enumerable.Range(0, h[0]).SelectMany(i => Enumerable.Range(0, i).Select(j => new { i, j })).Count(_ => a[_.j] < a[_.i]);
+		BigInteger x2 = Enumerable.Range(0, h[0]).SelectMany(i => Enumerable.Range(i + 1, h[0] - i - 1).Select(j => new { i, j })).Count(_ => a[_.j] < a[_.i]);
+		WriteLine((x1 * (h[1] - 1) + x2 * (h[1] + 1)) * h[1] / 2 % 1000000007);
 	}
 }
