350486df4adce92066439363f58880e16536c02d
Keiho Sakapon <git@saka-pon.net>
2019-08-25 13:27:50 +0900

    Solve task

---
+++ CSharp/Contests2019/JSC2019/D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+class D
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = i + 1; j < n; j++) Console.Write($"{FindDiff(i, j) + 1} ");
+			Console.WriteLine();
+		}
+	}
+
+	static int FindDiff(int x, int y)
+	{
+		var z = new BitArray(new[] { x ^ y });
+		for (var i = 0; ; i++) if (z[i]) return i;
+	}
+}
