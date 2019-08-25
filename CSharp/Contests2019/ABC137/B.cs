78ebd49aef7a14bfe724e75cb3d4a5edad9e267e
Keiho Sakapon <git@saka-pon.net>
2019-08-25 13:58:07 +0900

    Improve code

--- CSharp/Contests2019/ABC137/B.cs
+++ CSharp/Contests2019/ABC137/B.cs
@@ -3,11 +3,9 @@ using System.Linq;
 
 class B
 {
-    static void Main()
-    {
-        var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        var m = Math.Max(a[1] - a[0] + 1, -1000000);
-        var M = Math.Min(a[1] + a[0] - 1, 1000000);
-        Console.WriteLine(string.Join(" ", Enumerable.Range(m, M - m + 1)));
-    }
+	static void Main()
+	{
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		Console.WriteLine(string.Join(" ", Enumerable.Range(a[1] - a[0] + 1, 2 * a[0] - 1)));
+	}
 }
