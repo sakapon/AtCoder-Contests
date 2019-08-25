60b71715278cde92587796b91245bebc6040ea36
Keiho Sakapon <git@saka-pon.net>
2019-08-25 10:56:40 +0900

    Solve task

--- CSharp/Contests2019/JSC2019/C.cs
+++ CSharp/Contests2019/JSC2019/C.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using System.Linq;
+﻿using System.Collections.Generic;
+using static System.Console;
 
 class C
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var n = int.Parse(ReadLine());
+		var s = ReadLine();
 
-		Console.WriteLine(string.Join(" ", a));
+		var rs = new List<int>();
+		int t = 0, o = 0;
+		for (var i = 0; i < s.Length; i++)
+		{
+			if (s[i] == 'W' ^ t % 2 == 0)
+			{
+				if (o == n) { WriteLine(0); return; }
+				t++;
+				o++;
+			}
+			else
+			{
+				if (t == 0) { WriteLine(0); return; }
+				t--;
+				rs.Add(i);
+			}
+		}
+
+		var r = 1L;
+		for (var i = 0; i < n; i++) r = r * (rs[i] - 2 * i) * (i + 1) % 1000000007;
+		WriteLine(r);
 	}
 }
