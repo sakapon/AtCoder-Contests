cb673ddcb5172ae64f38b2608745e26d0c6e29d6
Keiho Sakapon <git@saka-pon.net>
2019-08-27 21:53:15 +0900

    Simplify code

--- CSharp/Contests2019/ABC138/D.cs
+++ CSharp/Contests2019/ABC138/D.cs
@@ -8,25 +8,22 @@ class D
 	{
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		var roots = Enumerable.Range(0, h[0] - 1).Select(i => read()).ToArray();
-		map = roots.Concat(roots.Select(r => new[] { r[1], r[0] })).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Select(r => r[1]).ToArray());
-		ops = Enumerable.Range(0, h[1]).Select(i => read()).GroupBy(a => a[0]).ToDictionary(g => g.Key, g => g.Sum(a => a[1]));
+		var rs = new int[h[0] - 1].Select(_ => read()).ToArray();
+		map = rs.Concat(rs.Select(r => new[] { r[1], r[0] })).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Select(r => r[1]).ToArray());
+		ops = new int[h[1]].Select(_ => read()).GroupBy(a => a[0]).ToDictionary(g => g.Key, g => g.Sum(a => a[1]));
 
-		c = new int[h[0]];
+		c = new int[h[0] + 1];
 		FindPoints(1, 0);
-		Console.WriteLine(string.Join(" ", c));
+		Console.WriteLine(string.Join(" ", c.Skip(1)));
 	}
 
 	static Dictionary<int, int[]> map;
 	static Dictionary<int, int> ops;
 	static int[] c;
-	static int sum;
 
 	static void FindPoints(int p, int p0)
 	{
-		if (ops.ContainsKey(p)) sum += ops[p];
-		c[p - 1] = sum;
+		c[p] = c[p0] + (ops.ContainsKey(p) ? ops[p] : 0);
 		if (map.ContainsKey(p)) foreach (var q in map[p]) if (q != p0) FindPoints(q, p);
-		if (ops.ContainsKey(p)) sum -= ops[p];
 	}
 }
