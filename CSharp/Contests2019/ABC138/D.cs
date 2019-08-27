7f69b8a052ee55bc0592b6eb941c108f26e6a4e6
Keiho Sakapon <git@saka-pon.net>
2019-08-27 22:05:16 +0900

    Use List

--- CSharp/Contests2019/ABC138/D.cs
+++ CSharp/Contests2019/ABC138/D.cs
@@ -9,7 +9,12 @@ class D
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
 		var rs = new int[h[0] - 1].Select(_ => read()).ToArray();
-		map = rs.Concat(rs.Select(r => new[] { r[1], r[0] })).GroupBy(r => r[0]).ToDictionary(g => g.Key, g => g.Select(r => r[1]).ToArray());
+		map = new int[h[0] + 1].Select(_ => new List<int>()).ToArray();
+		foreach (var r in rs)
+		{
+			map[r[0]].Add(r[1]);
+			map[r[1]].Add(r[0]);
+		}
 		ops = new int[h[1]].Select(_ => read()).GroupBy(a => a[0]).ToDictionary(g => g.Key, g => g.Sum(a => a[1]));
 
 		c = new int[h[0] + 1];
@@ -17,13 +22,13 @@ class D
 		Console.WriteLine(string.Join(" ", c.Skip(1)));
 	}
 
-	static Dictionary<int, int[]> map;
+	static List<int>[] map;
 	static Dictionary<int, int> ops;
 	static int[] c;
 
 	static void FindPoints(int p, int p0)
 	{
 		c[p] = c[p0] + (ops.ContainsKey(p) ? ops[p] : 0);
-		if (map.ContainsKey(p)) foreach (var q in map[p]) if (q != p0) FindPoints(q, p);
+		foreach (var q in map[p]) if (q != p0) FindPoints(q, p);
 	}
 }
