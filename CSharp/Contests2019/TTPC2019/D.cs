3ef849c4c4be3dfc52c085a13e50173e93ecbdc0
Keiho Sakapon <git@saka-pon.net>
2019-08-31 17:55:41 +0900

    Solve task

--- CSharp/Contests2019/TTPC2019/D.cs
+++ CSharp/Contests2019/TTPC2019/D.cs
@@ -9,9 +9,9 @@ class D
 		Console.ReadLine();
 		var X = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var ps = Primes(X.Max());
-		var d = new int[4]; d[0] = X.Length;
+		var d = new int[4];
 		foreach (var i in X.Select(x => x == 7 ? 3 : ps.Contains(x - 2) ? 2 : 1)) d[i]++;
-		Console.WriteLine(Wins(d) ? "An" : "Ai");
+		Console.WriteLine(d.Skip(1).Select(x => x % 2).GroupBy(x => x).Count() > 1 ? "An" : "Ai");
 	}
 
 	static HashSet<int> Primes(int M)
@@ -23,49 +23,5 @@ class D
 			if (ps.TakeWhile(p => p <= ri).All(p => i % p != 0)) ps.Add(i);
 		}
 		return new HashSet<int>(ps);
-	}
-
-	static bool Wins(int[] d)
-	{
-		if (d[0] == 1) return true;
-		if (d[0] == 2) return d[1] == 1 || d[2] == 1 || d[3] == 1;
-
-		if (d[1] > 0)
-		{
-			var d2 = (int[])d.Clone();
-			d2[0]--; d2[1]--;
-			if (!Wins(d2)) return true;
-		}
-		if (d[2] > 0)
-		{
-			var d2 = (int[])d.Clone();
-			d2[0]--; d2[2]--;
-			if (!Wins(d2)) return true;
-		}
-		if (d[2] > 0)
-		{
-			var d2 = (int[])d.Clone();
-			d2[1]++; d2[2]--;
-			if (!Wins(d2)) return true;
-		}
-		if (d[3] > 0)
-		{
-			var d2 = (int[])d.Clone();
-			d2[0]--; d2[3]--;
-			if (!Wins(d2)) return true;
-		}
-		if (d[2] > 0)
-		{
-			var d2 = (int[])d.Clone();
-			d2[1]++; d2[3]--;
-			if (!Wins(d2)) return true;
-		}
-		if (d[2] > 0)
-		{
-			var d2 = (int[])d.Clone();
-			d2[2]++; d2[3]--;
-			if (!Wins(d2)) return true;
-		}
-		return false;
 	}
 }
