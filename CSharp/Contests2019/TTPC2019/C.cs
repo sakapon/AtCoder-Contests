e6af833cd969bcff26d07719a68fcfd8cba737e7
Keiho Sakapon <git@saka-pon.net>
2019-08-31 19:10:19 +0900

    Refactor

--- CSharp/Contests2019/TTPC2019/C.cs
+++ CSharp/Contests2019/TTPC2019/C.cs
@@ -15,21 +15,23 @@ class C
 		for (var i = 0; i < n; i++) if (a[i] == -1) bis.Add(i);
 		var q = bis.Count == n ? x : a.Where(v => v != -1).Aggregate((y, z) => y ^ z) ^ x;
 
-		if (!bis.Any()) { Console.WriteLine(q == 0 ? string.Join(" ", a) : "-1"); return; }
-		if (bis.Count == 1)
+		if (bis.Count == 0)
+		{
+			if (q > 0) { Console.WriteLine(-1); return; }
+		}
+		else if (bis.Count == 1)
 		{
 			if (q > x) { Console.WriteLine(-1); return; }
 			a[bis[0]] = q;
-			Console.WriteLine(string.Join(" ", a));
 		}
 		else
 		{
-			var r = (int)Math.Pow(2, (int)Math.Log(q, 2));
-			if (r > x) { Console.WriteLine(-1); return; }
-			a[bis[0]] = r;
-			a[bis[1]] = q ^ r;
+			var p = (int)Math.Pow(2, (int)Math.Log(q, 2));
+			if (p > x) { Console.WriteLine(-1); return; }
+			a[bis[0]] = p;
+			a[bis[1]] = p ^ q;
 			for (var i = 2; i < bis.Count; i++) a[bis[i]] = 0;
-			Console.WriteLine(string.Join(" ", a));
 		}
+		Console.WriteLine(string.Join(" ", a));
 	}
 }
