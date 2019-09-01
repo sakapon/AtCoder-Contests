703fa2ec68408f1d88bf625fec6289f7d12141da
Keiho Sakapon <git@saka-pon.net>
2019-09-02 01:19:05 +0900

    Update code

--- CSharp/Contests2019/ABC139/F.cs
+++ CSharp/Contests2019/ABC139/F.cs
@@ -6,24 +6,23 @@ class F
 {
 	static void Main()
 	{
+		var div = 360;
+		var d = 2 * PI / div;
 		var n = int.Parse(Console.ReadLine());
 		var vs = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
 
 		var mi = -1;
 		var ms = 0.0;
-		var div = 360;
-		var d = 2 * PI / div;
-		for (int i = 0; i < div; i++)
+		var mvs = new int[0][];
+		for (var i = 0; i < div; i++)
 		{
 			var a = i * d;
-			var s = vs.Select(v => v[0] * Cos(a) + v[1] * Sin(a)).Where(x => x > 0).Sum();
-			if (ms < s) { mi = i; ms = s; }
+			var vs2 = vs.Select(v => new { v, x = v[0] * Cos(a) + v[1] * Sin(a) }).Where(_ => _.x > 0).ToArray();
+			var s = vs2.Sum(_ => _.x);
+			if (ms < s) { mi = i; ms = s; mvs = vs2.Select(_ => _.v).ToArray(); }
 		}
-
-		var ma = mi * d;
-		var mvs = vs.Select(v => new { v, x = v[0] * Cos(ma) + v[1] * Sin(ma) }).Where(_ => _.x > 0).ToArray();
-		double xs = mvs.Sum(_ => _.v[0]);
-		double ys = mvs.Sum(_ => _.v[1]);
-		Console.WriteLine(Sqrt(xs * xs + ys * ys));
+		double mx = mvs.Sum(v => v[0]);
+		double my = mvs.Sum(v => v[1]);
+		Console.WriteLine(Sqrt(mx * mx + my * my));
 	}
 }
