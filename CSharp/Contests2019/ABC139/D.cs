95a26e9043c081794935ff9980b5bf04459969d9
Keiho Sakapon <git@saka-pon.net>
2019-09-01 22:27:56 +0900

    Code for research

--- CSharp/Contests2019/ABC139/D.cs
+++ CSharp/Contests2019/ABC139/D.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static void Main()
 	{
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		Console.WriteLine(Perm(n, n).Max(p => p.Select((pi, i) => i % pi).Sum()));
+	}
 
-		Console.WriteLine(string.Join(" ", a));
+	static IEnumerable<int[]> Perm(int n, int r) => Perm(new SortedSet<int>(Enumerable.Range(1, n)), r, r);
+	static IEnumerable<int[]> Perm(SortedSet<int> set, int r, int k)
+	{
+		if (r == 0) return new[] { new int[k] };
+		if (r == 1) return set.Select(i => { var a = new int[k]; a[k - 1] = i; return a; });
+
+		return set.ToArray().SelectMany(i =>
+		{
+			set.Remove(i);
+			var p = Perm(set, r - 1, k).Select(a => { a[k - r] = i; return a; }).ToArray();
+			set.Add(i);
+			return p;
+		});
 	}
 }
