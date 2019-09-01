0af16499787fa31d50ff860ce316058ca16d9d69
Keiho Sakapon <git@saka-pon.net>
2019-09-01 20:55:29 +0900

    Update template

--- CSharp/Contests2019/ABC000/D.cs
+++ CSharp/Contests2019/ABC000/D.cs
@@ -8,6 +8,7 @@ class D
 		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
 		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
 
 		Console.WriteLine(string.Join(" ", a));
 	}
