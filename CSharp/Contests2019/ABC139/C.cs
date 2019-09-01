51c337a0665b54e9b962473ed25bc3cf6ef7fb40
Keiho Sakapon <git@saka-pon.net>
2019-09-01 20:57:34 +0900

    Create project

---
+++ CSharp/Contests2019/ABC139/C.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class C
+{
+	static void Main()
+	{
+		var s = Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+
+		Console.WriteLine(string.Join(" ", a));
+	}
+}
---
+++ CSharp/Contests2019/ABC139/F.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class F
+{
+	static void Main()
+	{
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var h = read();
+		var ps = new int[h[0]].Select(_ => read()).ToArray();
+
+		Console.WriteLine(string.Join(" ", h));
+	}
+}
