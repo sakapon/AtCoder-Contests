564d93ffd93eda6d685666c6cd260b481c8a3b1d
Keiho Sakapon <git@saka-pon.net>
2019-09-04 14:58:15 +0900

    Improve code

--- CSharp/Contests2019/TTPC2019/D.cs
+++ CSharp/Contests2019/TTPC2019/D.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class D
@@ -8,20 +7,18 @@ class D
 	{
 		Console.ReadLine();
 		var X = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var ps = Primes(X.Max());
+		var ps = PrimeFlags(X.Max());
 		var d = new int[3];
-		foreach (var i in X.Select(x => x == 7 ? 3 : ps.Contains(x - 2) ? 2 : 1)) d[i - 1]++;
+		foreach (var i in X.Select(x => x == 7 ? 3 : ps[x - 2] ? 2 : 1)) d[i - 1]++;
 		Console.WriteLine(d.Select(x => x % 2).GroupBy(x => x).Count() > 1 ? "An" : "Ai");
 	}
 
-	static HashSet<int> Primes(int M)
+	static bool[] PrimeFlags(int M)
 	{
-		var ps = new List<int>();
-		for (var i = 2; i <= M; i++)
-		{
-			var ri = (int)Math.Sqrt(i);
-			if (ps.TakeWhile(p => p <= ri).All(p => i % p != 0)) ps.Add(i);
-		}
-		return new HashSet<int>(ps);
+		var rM = (int)Math.Sqrt(M);
+		var b = new bool[M + 1]; b[2] = true;
+		for (int i = 3; i <= M; i += 2) b[i] = true;
+		for (int p = 3; p <= rM; p++) if (b[p]) for (var i = 3 * p; i <= M; i += 2 * p) b[i] = false;
+		return b;
 	}
 }
