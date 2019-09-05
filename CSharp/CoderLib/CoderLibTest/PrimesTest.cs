16e29b78c66a294c68b580dfdb1425c516ce4975
Keiho Sakapon <git@saka-pon.net>
2019-09-05 15:15:35 +0900

    Add function for primes

--- CSharp/CoderLib/CoderLibTest/PrimesTest.cs
+++ CSharp/CoderLib/CoderLibTest/PrimesTest.cs
@@ -23,6 +23,26 @@ public class PrimesTest
 		return b;
 	}
 
+	static long[] PrimesF64(long m, long M)
+	{
+		var rM = (long)Math.Sqrt(M);
+		var m2 = Math.Max(m, rM + 1);
+		var b = new bool[rM + 1]; b[2] = true;
+		var b2 = new bool[M - m2 + 1];
+		for (var i = 3; i <= rM; i += 2) b[i] = true;
+		for (var i = m2 % 2 == 1 ? m2 : m2 + 1; i <= M; i += 2) b2[i - m2] = true;
+		for (long p = 3; p <= rM; p++)
+			if (b[p])
+			{
+				for (var i = 3 * p; i <= rM; i += 2 * p) b[i] = false;
+				var s2 = (m2 - 1) / p;
+				s2 = (s2 + 1 + s2 % 2) * p;
+				for (var i = s2; i <= M; i += 2 * p) b2[i - m2] = false;
+			}
+		var ps2 = Range(m2, M).Where(i => b2[i - m2]).ToArray();
+		return m2 == m ? ps2 : Range(m, rM).Where(i => b[i]).Concat(ps2).ToArray();
+	}
+
 	static int[] Primes(int m, int M) => Primes(M).SkipWhile(i => i < m).ToArray();
 	static List<int> Primes(int M)
 	{
@@ -48,16 +68,16 @@ public class PrimesTest
 
 	static List<int> PrimesL(int M)
 	{
+		var rM = (int)Math.Sqrt(M);
 		var ps = new List<int>();
 		var l = Enumerable.Range(2, M - 1).ToList();
-		var rM = (int)Math.Sqrt(M);
-		for (int i = 0; l.Count > 0 && l[0] <= rM; i++)
+		for (var i = 0; l.Count > 0 && l[0] <= rM; i++)
 		{
 			ps.Add(l[0]);
 			l.RemoveAll(j => j % ps[i] == 0);
 		}
-		ps.AddRange(l);
-		return ps;
+		l.InsertRange(0, ps);
+		return l;
 	}
 
 	static List<long> PrimesL64(long m, long M)
@@ -72,7 +92,7 @@ public class PrimesTest
 			ps.Add(l[0]);
 			l.RemoveAll(j => j % ps[i] == 0);
 		}
-		l.InsertRange(0, ps.Where(p => p >= m));
+		l.InsertRange(0, ps.SkipWhile(p => p < m));
 		return l;
 	}
 
@@ -148,6 +168,24 @@ public class PrimesTest
 	public void PrimesF_Large()
 	{
 		Console.WriteLine(string.Join(" ", PrimesF(10000000, 10000100)));
+	}
+
+	[TestMethod]
+	public void PrimesF64()
+	{
+		Console.WriteLine(string.Join(" ", PrimesF64(2, 100)));
+		Assert.AreEqual(25, PrimesF64(2, 100).Length);
+		Assert.AreEqual(143, PrimesF64(100, 1000).Length);
+		Assert.AreEqual(1061, PrimesF64(1000, 10000).Length);
+		Assert.AreEqual(8363, PrimesF64(10000, 100000).Length);
+		Console.WriteLine(string.Join(" ", PrimesF64(998244300, 998244400)));
+		Console.WriteLine(string.Join(" ", PrimesF64(999999900, 1000000100)));
+	}
+
+	[TestMethod]
+	public void PrimesF64_Large()
+	{
+		Console.WriteLine(string.Join(" ", PrimesF64(999999999900, 1000000000100)));
 	}
 
 	[TestMethod]
