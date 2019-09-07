3b6cbaa356fd30558d59a0b49090004112487153
Keiho Sakapon <git@saka-pon.net>
2019-09-07 11:29:52 +0900

    AC if only c=0

---
+++ CSharp/Contests2019/MSOL2019/C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class C
+{
+	static void Main()
+	{
+		var M = 1000000007;
+		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var n = h[0];
+		long _100 = ModInv(100, M), a = h[1] * _100 % M, b = h[2] * _100 % M, c = h[3] * _100 % M;
+
+		if (c > 0) throw new InvalidOperationException();
+
+		long e = 0, ncr = 1, pa = ModPow(a, n, M), pb = ModPow(b, n, M);
+		for (var i = n; i < 2 * n; i++, ncr = ncr * (i - 1) % M * ModInv(i - n, M) % M, pa = pa * b % M, pb = pb * a % M) e = (e + i * ncr % M * (pa + pb)) % M;
+		Console.WriteLine(e);
+	}
+
+	static long ModPow(long b, int i, int mod)
+	{
+		for (var r = 1L; ; b = b * b % mod)
+		{
+			if (i % 2 > 0) r = r * b % mod;
+			if ((i /= 2) < 1) return r;
+		}
+	}
+	static long ModInv(int x, int p) => ModPow(x, p - 2, p);
+}
