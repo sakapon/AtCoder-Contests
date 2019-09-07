5966b6dd72f0d13a2f0be5226af663052f6919f6
Keiho Sakapon <git@saka-pon.net>
2019-09-07 13:05:07 +0900

    Use library

--- CSharp/Contests2019/MSOL2019/C.cs
+++ CSharp/Contests2019/MSOL2019/C.cs
@@ -5,23 +5,26 @@ class C
 {
 	static void Main()
 	{
-		var M = 1000000007;
 		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var n = h[0];
-		long c_ = 100 * ModInv(100 - h[3], M) % M, a = h[1] * c_ % M * ModInv(100, M) % M, b = h[2] * c_ % M * ModInv(100, M) % M;
+		long a = MDiv(h[1], h[1] + h[2]), b = MDiv(h[2], h[1] + h[2]);
 
-		long e = 0, ncr = 1, pa = ModPow(a, n, M), pb = ModPow(b, n, M);
-		for (var i = n; i < 2 * n; i++, ncr = ncr * (i - 1) % M * ModInv(i - n, M) % M, pa = pa * b % M, pb = pb * a % M) e = (e + i * ncr % M * (pa + pb)) % M;
-		Console.WriteLine(c_ * e % M);
+		long e = 0, ncr = 1, pa = MPow(a, n), pb = MPow(b, n);
+		for (var i = n; i < 2 * n; i++, ncr = MMul(ncr, MDiv(i - 1, i - n)), pa = MMul(pa, b), pb = MMul(pb, a)) e = MAdd(e, MMul(MMul(i, ncr), pa + pb));
+		Console.WriteLine(MMul(MDiv(100, 100 - h[3]), e));
 	}
 
-	static long ModPow(long b, int i, int mod)
+	const int M = 1000000007;
+	static long MPow(long b, int i)
 	{
-		for (var r = 1L; ; b = b * b % mod)
+		for (var r = 1L; ; b = b * b % M)
 		{
-			if (i % 2 > 0) r = r * b % mod;
+			if (i % 2 > 0) r = r * b % M;
 			if ((i /= 2) < 1) return r;
 		}
 	}
-	static long ModInv(int x, int p) => ModPow(x, p - 2, p);
+	static long MInv(long x) => MPow(x, M - 2);
+	static long MAdd(long x, long y) => (x + y) % M;
+	static long MMul(long x, long y) => x * y % M;
+	static long MDiv(long x, long y) => x * MInv(y) % M;
 }
