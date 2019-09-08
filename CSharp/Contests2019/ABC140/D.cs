96752ebe005b3f26eac49c2e2c5eb55ca21176a4
Keiho Sakapon <git@saka-pon.net>
2019-09-08 10:00:11 +0900

    AC

--- CSharp/Contests2019/ABC140/D.cs
+++ CSharp/Contests2019/ABC140/D.cs
@@ -9,25 +9,26 @@ class D
 		var s = Console.ReadLine();
 		int n = h[0], k = h[1];
 
+		if (n == 1) { Console.WriteLine(0); return; }
 		var x = Enumerable.Range(1, n - 2).Count(i => s[i] == 'L' ? s[i - 1] == 'L' : s[i + 1] == 'R');
 		if (s.StartsWith("RR")) x++;
 		if (s.EndsWith("LL")) x++;
-		Console.WriteLine(x + Math.Max(Roll(s, n, k, true), Roll(s, n, k, false)));
-	}
 
-	static int Roll(string s, int n, int k, bool isL)
-	{
-		var x = 0;
-		var l = s.IndexOf(isL ? 'R' : 'L'); if (l == -1) l = n;
-		var r = s.LastIndexOf(isL ? 'R' : 'L');
-		var c = isL ? 'L' : 'R';
-		for (; k > 0; k--, x += 2, c = c == 'L' ? 'R' : 'L')
+		var f = Enumerable.Range(0, n - 1).Count(i => s[i] == 'R' && s[i + 1] == 'L');
+		if (s[0] == s[n - 1])
 		{
-			for (; l < r; l++) if (s[l] == c) break;
-			for (; l < r; r--) if (s[r] == c) break;
-			if (l >= r) break;
+			x += 2 * Math.Min(k, f);
 		}
-		if (k > 0 && s[0] != s[n - 1]) x++;
-		return x;
+		else if (s[0] == 'L')
+		{
+			x += 2 * Math.Min(k, f);
+			if (k > f) x++;
+		}
+		else
+		{
+			x += 2 * Math.Min(k, f - 1);
+			if (k >= f) x++;
+		}
+		Console.WriteLine(x);
 	}
 }
