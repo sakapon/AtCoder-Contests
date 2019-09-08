4b091aeb00b20fcec5c7a4381ea6e66acaa37e23
Keiho Sakapon <git@saka-pon.net>
2019-09-08 15:02:56 +0900

    Simplify code

--- CSharp/Contests2019/ABC140/D.cs
+++ CSharp/Contests2019/ABC140/D.cs
@@ -7,28 +7,6 @@ class D
 	{
 		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var s = Console.ReadLine();
-		int n = h[0], k = h[1];
-
-		if (n == 1) { Console.WriteLine(0); return; }
-		var x = Enumerable.Range(1, n - 2).Count(i => s[i] == 'L' ? s[i - 1] == 'L' : s[i + 1] == 'R');
-		if (s.StartsWith("RR")) x++;
-		if (s.EndsWith("LL")) x++;
-
-		var f = Enumerable.Range(0, n - 1).Count(i => s[i] == 'R' && s[i + 1] == 'L');
-		if (s[0] == s[n - 1])
-		{
-			x += 2 * Math.Min(k, f);
-		}
-		else if (s[0] == 'L')
-		{
-			x += 2 * Math.Min(k, f);
-			if (k > f) x++;
-		}
-		else
-		{
-			x += 2 * Math.Min(k, f - 1);
-			if (k >= f) x++;
-		}
-		Console.WriteLine(x);
+		Console.WriteLine(h[0] - 1 + Math.Min(0, 2 * h[1] - Enumerable.Range(0, h[0] - 1).Count(i => s[i] != s[i + 1])));
 	}
 }
