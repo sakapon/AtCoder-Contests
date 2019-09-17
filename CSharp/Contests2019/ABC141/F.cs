2217675106cbd05047737befd8eb04d3e9a731d8
Keiho Sakapon <git@saka-pon.net>
2019-09-18 08:58:09 +0900

    AC

--- CSharp/Contests2019/ABC141/F.cs
+++ CSharp/Contests2019/ABC141/F.cs
@@ -6,7 +6,7 @@ class F
 	static void Main()
 	{
 		var p2 = new long[60];
-		for (long i = 0, v = 1; i < p2.Length; p2[i++] = v, v <<= 1) ;
+		for (long i = 0, v = 1; i < p2.Length; p2[i++] = v, v *= 2) ;
 
 		var n = int.Parse(Console.ReadLine());
 		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
@@ -14,15 +14,13 @@ class F
 
 		for (int p = 59, c = 0; p >= 0; p--)
 		{
+			if ((s & p2[p]) != 0) continue;
 			var j = -1;
-			for (int i = c; i < n; i++) if (a[i] >= p2[p]) { j = i; break; }
+			for (int i = c; i < n; i++) if ((a[i] & p2[p]) != 0) { j = i; break; }
 			if (j == -1) continue;
+
 			Swap(a, c, j);
-			for (int i = 0; i < n; i++)
-			{
-				var v = a[i] ^ a[c];
-				if (v < a[i] && i != c) a[i] = v;
-			}
+			for (int i = 0; i < n; i++) if ((a[i] & p2[p]) != 0 && i != c) a[i] ^= a[c];
 			c++;
 		}
 		var r = a.Aggregate((x, y) => x ^ y);
