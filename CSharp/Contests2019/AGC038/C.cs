3c1d5d6d0542b8de7a029c68e3ec698d289360fe
Keiho Sakapon <git@saka-pon.net>
2019-09-22 02:40:03 +0900

    TLE

---
+++ CSharp/Contests2019/AGC038/C.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class C
+{
+	static void Main()
+	{
+		Console.ReadLine();
+		var a = Console.ReadLine().Split().Select(int.Parse).GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+		var A = a.Max(p => p.Key);
+
+		var c = new double[A + 1];
+		for (var d = 1; d <= A; d++) c[d] = 1.0 / d;
+		for (var d = 1; d <= A / 2; d++)
+			for (var i = 2 * d; i <= A; i += d) c[i] -= c[d];
+
+		var s = 0.0;
+		for (var d = 1; d <= A; d++)
+		{
+			double si = 0, si2 = 0;
+			foreach (var p in a)
+			{
+				if (p.Key % d != 0) continue;
+				si += (double)p.Value * p.Key;
+				si2 += (double)p.Value * p.Key * p.Key;
+			}
+			s += (si * si - si2) / 2 * c[d];
+		}
+		Console.WriteLine(s % 998244353);
+	}
+}
