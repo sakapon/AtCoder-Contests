153673d3aa825a60cb4007a91ad6c03b3985be1b
Keiho Sakapon <git@saka-pon.net>
2019-09-22 03:30:55 +0900

    TLE

--- CSharp/Contests2019/AGC038/C.cs
+++ CSharp/Contests2019/AGC038/C.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 
 class C
 {
@@ -9,23 +10,40 @@ class C
 		var a = Console.ReadLine().Split().Select(int.Parse).GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
 		var A = a.Max(p => p.Key);
 
-		var c = new double[A + 1];
-		for (var d = 1; d <= A; d++) c[d] = 1.0 / d;
+		var c = new long[A + 1];
+		for (var d = 1; d <= A; d++) c[d] = MInv(d);
 		for (var d = 1; d <= A / 2; d++)
-			for (var m = 2 * d; m <= A; m += d) c[m] -= c[d];
+			for (var m = 2 * d; m <= A; m += d) c[m] = MSub(c[m], c[d]);
 
-		var s = 0.0;
+		var h = MInv(2);
+		BigInteger s = 0;
 		for (var d = 1; d <= A; d++)
 		{
-			double si = 0, si2 = 0;
+			BigInteger si = 0, si2 = 0;
 			for (var m = d; m <= A; m += d)
 			{
 				if (!a.ContainsKey(m)) continue;
-				si += (double)a[m] * m;
-				si2 += (double)a[m] * m * m;
+				si += (BigInteger)a[m] * m;
+				si2 += (BigInteger)a[m] * m * m;
 			}
-			s += (si * si - si2) / 2 * c[d];
+			s += (si * si - si2) * h * c[d] % M;
 		}
-		Console.WriteLine(s % 998244353);
+		Console.WriteLine(s % M);
 	}
+
+	const int M = 998244353;
+	static long MPow(long b, int i)
+	{
+		for (var r = 1L; ; b = b * b % M)
+		{
+			if (i % 2 > 0) r = r * b % M;
+			if ((i /= 2) < 1) return r;
+		}
+	}
+	static long MInv(long x) => MPow(x, M - 2);
+	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
+	static long MAdd(long x, long y) => (x + y) % M;
+	static long MSub(long x, long y) => MInt(x - y);
+	static long MMul(long x, long y) => x * y % M;
+	static long MDiv(long x, long y) => x * MInv(y) % M;
 }
