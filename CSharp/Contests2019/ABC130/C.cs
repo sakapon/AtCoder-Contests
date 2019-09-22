94c243bf42aa906b5661fbd425f58120f95d770a
Keiho Sakapon <git@saka-pon.net>
2019-09-22 11:22:59 +0900

    Create projects

---
+++ CSharp/Contests2019/ABC130/C.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class C
+{
+	static void Main()
+	{
+		Console.ReadLine();
+		var s = Console.ReadLine();
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+
+		Console.WriteLine(string.Join(" ", a));
+	}
+}
