6c30a89284ecf3ecfafb7493f88a043070d58631
Keiho Sakapon <git@saka-pon.net>
2019-09-23 18:53:58 +0900

    Other solution

---
+++ CSharp/Contests2019/ABC127/D2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class D2
+{
+	static void Main()
+	{
+		Func<long[]> read = () => Console.ReadLine().Split().Select(long.Parse).ToArray();
+		var h = read();
+		var a = read().GroupBy(x => x).Select(g => new[] { g.Count(), g.Key }).Concat(new int[h[1]].Select(_ => read()))
+			.GroupBy(x => x[1]).OrderByDescending(g => g.Key).Select(g => new { c = g.Key, b = g.Sum(x => x[0]) });
+
+		var M = 0L;
+		foreach (var _ in a)
+		{
+			var b = Math.Min(_.b, h[0]);
+			M += _.c * b;
+			if ((h[0] -= b) == 0) break;
+		}
+		Console.WriteLine(M);
+	}
+}
