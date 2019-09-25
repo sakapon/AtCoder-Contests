4bcdf298567f7af1390060111a8370c2b8ccb556
Keiho Sakapon <git@saka-pon.net>
2019-09-25 16:07:31 +0900

    AC

--- CSharp/Contests2019/AGC038/B.cs
+++ CSharp/Contests2019/AGC038/B.cs
@@ -10,26 +10,37 @@ class B
 		var p = read();
 		int n = h[0], k = h[1];
 
-		var c = n - k + 1;
-		c -= Enumerable.Range(0, n - k).Count(i => p.Skip(i).Take(k + 1).Min() == p[i] && p.Skip(i).Take(k + 1).Max() == p[i + k]);
+		var smin = Slide(p, k + 1, (x, y) => x <= y);
+		var smax = Slide(p, k + 1, (x, y) => x >= y);
+		var c = n - k + 1 - Enumerable.Range(0, n - k).Count(i => smin[i] == p[i] && smax[i] == p[i + k]);
 
-		int c2 = 0, t = 0, v = -1;
-		for (int i = 0; i < n; i++)
+		int c2 = 0, t = 1;
+		for (int i = 1; i < n; i++)
 		{
-			if (p[i] > v)
-			{
-				t++;
-			}
+			if (p[i] > p[i - 1]) t++;
 			else
 			{
 				if (t >= k) c2++;
 				t = 1;
 			}
-			v = p[i];
 		}
 		if (t >= k) c2++;
-		if (c2 > 0) c -= c2 - 1;
 
-		Console.WriteLine(c);
+		Console.WriteLine(c - Math.Max(c2 - 1, 0));
+	}
+
+	static int[] Slide(int[] a, int k, Func<int, int, bool> c)
+	{
+		var r = new int[a.Length - k + 1];
+		var q = new int[a.Length];
+		for (int i = 1 - k, j = 0, s = 0, t = -1; j < a.Length; i++, j++)
+		{
+			while (s <= t && c(a[j], a[q[t]])) t--;
+			q[++t] = j;
+			if (i < 0) continue;
+			r[i] = a[q[s]];
+			if (q[s] == i) s++;
+		}
+		return r;
 	}
 }
