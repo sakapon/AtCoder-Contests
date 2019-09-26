698b8e922e4c1f90ca1704c0062d9393b886d4b0
Keiho Sakapon <git@saka-pon.net>
2019-09-26 23:49:11 +0900

    Improve code

--- CSharp/Contests2019/ABC126/C.cs
+++ CSharp/Contests2019/ABC126/C.cs
@@ -5,21 +5,14 @@ class C
 	static void Main()
 	{
 		var h = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
-		double n = h[0], k = h[1];
-		Console.WriteLine(n < k ? Push(n, k) : (n - k + 1) / n + (k - 1) / n * Push(k - 1, k));
-	}
+		double x = h[0], k = h[1], s = 0, p = 1, c;
 
-	static double Push(double n, double k)
-	{
-		double s = 0.0, p = 1.0, m = k, x = n;
 		while (x > 0)
 		{
-			p /= 2;
-			m /= 2;
-			var c = Math.Max(Math.Floor(x - m + 1), 0);
-			x -= c;
-			s += c / n * p;
+			x -= c = Math.Max(Math.Floor(x - k / p + 1), 0);
+			s += c / p;
+			p *= 2;
 		}
-		return s;
+		Console.WriteLine(s / h[0]);
 	}
 }
