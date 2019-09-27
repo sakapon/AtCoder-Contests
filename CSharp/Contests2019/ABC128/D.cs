e25444fe29a209a1b07ba50a68e1de3c4ffd8749
Keiho Sakapon <git@saka-pon.net>
2019-09-27 17:04:41 +0900

    AC

--- CSharp/Contests2019/ABC128/D.cs
+++ CSharp/Contests2019/ABC128/D.cs
@@ -5,14 +5,25 @@ class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var v = read();
+		int n = h[0], k = h[1], nk = Math.Min(n, k), M = 0;
 
-		Console.WriteLine(string.Join(" ", a));
+		for (int i = 1; i <= nk; i++)
+		{
+			var t = v.Take(i).ToArray();
+			var neg = t.Count(x => x < 0);
+			M = Math.Max(M, t.OrderBy(x => x).Skip(Math.Min(neg, k - i)).Sum());
+
+			for (int j = 1; j <= i; j++)
+			{
+				if (t[i - j] < 0) neg--;
+				if (v[n - j] < 0) neg++;
+				t[i - j] = v[n - j];
+				M = Math.Max(M, t.OrderBy(x => x).Skip(Math.Min(neg, k - i)).Sum());
+			}
+		}
+		Console.WriteLine(M);
 	}
 }
