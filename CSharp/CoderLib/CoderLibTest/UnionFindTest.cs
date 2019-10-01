302b8743611b980fd0a5d9e5793928b9eb9824f7
Keiho Sakapon <git@saka-pon.net>
2019-10-01 23:39:41 +0900

    Add class for Union-Find

---
+++ CSharp/CoderLib/CoderLibTest/UnionFindTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+class UF
+{
+	int[] p;
+	public UF(int n) { p = Enumerable.Range(0, n).ToArray(); }
+
+	public void Unite(int a, int b) { if (!AreUnited(a, b)) p[p[b]] = p[a]; }
+	public bool AreUnited(int a, int b) => GetRoot(a) == GetRoot(b);
+	int GetRoot(int a) => p[a] == a ? a : p[a] = GetRoot(p[a]);
+}
+
+[TestClass]
+public class UnionFindTest
+{
+}
