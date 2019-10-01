c69e85b33c10ee7f4c75aac1fc4d91ad0ae4c54b
Keiho Sakapon <git@saka-pon.net>
2019-10-01 16:05:30 +0900

    Improve code

--- CSharp/Contests/ATC001/A.cs
+++ CSharp/Contests/ATC001/A.cs
@@ -9,20 +9,19 @@ class A
 		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		int h = a[0], w = a[1];
 		var c = new int[h].Select(_ => Console.ReadLine()).ToArray();
+		var u = new bool[h, w];
+		var q = new Queue<P>();
 
 		var si = Array.FindIndex(c, x => x.Contains('s'));
 		var sp = new P(si, c[si].IndexOf('s'));
-
-		var used = new bool[h, w];
-		var q = new Queue<P>();
-		used[sp.i, sp.j] = true;
+		u[sp.i, sp.j] = true;
 		q.Enqueue(sp);
 
+		Func<P, IEnumerable<P>> nexts = p => new[] { new P(p.i, p.j - 1), new P(p.i, p.j + 1), new P(p.i - 1, p.j), new P(p.i + 1, p.j) }.Where(x => 0 <= x.i && x.i < h && 0 <= x.j && x.j < w && !u[x.i, x.j]);
 		while (q.Any())
-			foreach (var np in Nexts(q.Dequeue(), h, w))
+			foreach (var np in nexts(q.Dequeue()))
 			{
-				if (used[np.i, np.j]) continue;
-				used[np.i, np.j] = true;
+				u[np.i, np.j] = true;
 				if (c[np.i][np.j] == 'g') { Console.WriteLine("Yes"); return; }
 				if (c[np.i][np.j] == '.') q.Enqueue(np);
 			}
@@ -34,6 +33,4 @@ class A
 		public int i, j;
 		public P(int _i, int _j) { i = _i; j = _j; }
 	}
-
-	static IEnumerable<P> Nexts(P p, int h, int w) => new[] { new P(p.i, p.j - 1), new P(p.i, p.j + 1), new P(p.i - 1, p.j), new P(p.i + 1, p.j) }.Where(x => 0 <= x.i && x.i < h && 0 <= x.j && x.j < w);
 }
