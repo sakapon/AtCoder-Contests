8daef22d862b5d7dad73a18998d85a9393c94266
Keiho Sakapon <git@saka-pon.net>
2019-10-01 20:22:22 +0900

    Update code

--- CSharp/Contests/ATC001/B.cs
+++ CSharp/Contests/ATC001/B.cs
@@ -19,7 +19,7 @@ class UF
 	int[] p;
 	public UF(int n) { p = Enumerable.Range(0, n).ToArray(); }
 
-	public void Unite(int a, int b) { if (!AreUnited(a, b)) p[p[b]] = p[p[a]]; }
+	public void Unite(int a, int b) { if (!AreUnited(a, b)) p[p[b]] = p[a]; }
 	public bool AreUnited(int a, int b) => GetRoot(a) == GetRoot(b);
 	int GetRoot(int a) => p[a] == a ? a : p[a] = GetRoot(p[a]);
 }
