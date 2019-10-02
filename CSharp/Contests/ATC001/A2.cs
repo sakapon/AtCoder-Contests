ccfa291feba2c98a4a79d6190898fd2bf3015d67
Keiho Sakapon <git@saka-pon.net>
2019-10-02 22:21:23 +0900

    AC

--- CSharp/Contests/ATC001/A2.cs
+++ CSharp/Contests/ATC001/A2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class A
+class A2
 {
 	static void Main()
 	{
@@ -10,20 +10,20 @@ class A
 		int h = a[0], w = a[1];
 		var c = new int[h].Select(_ => Console.ReadLine()).ToArray();
 		var u = new bool[h, w];
-		var q = new Stack<P>();
+		var q = new Queue<P>();
 
 		var si = Array.FindIndex(c, x => x.Contains('s'));
 		var sp = new P(si, c[si].IndexOf('s'));
 		u[sp.i, sp.j] = true;
-		q.Push(sp);
+		q.Enqueue(sp);
 
 		Func<P, P[]> nexts = p => Array.FindAll(new[] { new P(p.i, p.j - 1), new P(p.i, p.j + 1), new P(p.i - 1, p.j), new P(p.i + 1, p.j) }, x => 0 <= x.i && x.i < h && 0 <= x.j && x.j < w && !u[x.i, x.j]);
 		while (q.Any())
-			foreach (var np in nexts(q.Pop()))
+			foreach (var np in nexts(q.Dequeue()))
 			{
 				u[np.i, np.j] = true;
 				if (c[np.i][np.j] == 'g') { Console.WriteLine("Yes"); return; }
-				if (c[np.i][np.j] == '.') q.Push(np);
+				if (c[np.i][np.j] == '.') q.Enqueue(np);
 			}
 		Console.WriteLine("No");
 	}
--- CSharp/Contests/ATC002/A.cs
+++ CSharp/Contests/ATC002/A.cs
@@ -1,16 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var a = read();
+		int h = a[0], w = a[1];
+		var s = read();
+		var g = read();
+		var sp = new P(s[0] - 1, s[1] - 1);
+		var gp = new P(g[0] - 1, g[1] - 1);
+		var c = new int[h].Select(_ => Console.ReadLine()).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var t = new int[h, w];
+		var q = new Queue<P>();
+		t[sp.i, sp.j] = 1;
+		q.Enqueue(sp);
+
+		Func<P, P[]> nexts = p => Array.FindAll(new[] { new P(p.i, p.j - 1), new P(p.i, p.j + 1), new P(p.i - 1, p.j), new P(p.i + 1, p.j) }, x => 0 <= x.i && x.i < h && 0 <= x.j && x.j < w && t[x.i, x.j] == 0);
+		while (q.Any())
+		{
+			var p = q.Dequeue();
+			foreach (var np in nexts(p))
+			{
+				t[np.i, np.j] = c[np.i][np.j] == '#' ? int.MaxValue : t[p.i, p.j] + 1;
+				if (np.i == gp.i && np.j == gp.j) { Console.WriteLine(t[p.i, p.j]); return; }
+				if (c[np.i][np.j] == '.') q.Enqueue(np);
+			}
+		}
+	}
+
+	struct P
+	{
+		public int i, j;
+		public P(int _i, int _j) { i = _i; j = _j; }
 	}
 }
