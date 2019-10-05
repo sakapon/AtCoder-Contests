3f0d7ab38b4b9b306a5e0eeed377b2599a4053f6
Keiho Sakapon <git@saka-pon.net>
2019-10-05 19:54:34 +0900

    AC

---
+++ CSharp/Contests2019/ABC131/E.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class E
+{
+	static void Main()
+	{
+		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		int n = h[0], m = n * (n - 1) / 2 - h[1];
+
+		if (m < n - 1) { Console.WriteLine(-1); return; }
+
+		Console.WriteLine(m);
+		for (int c = 0, i = 1; i <= n; i++)
+			for (int j = i + 1; j <= n && c < m; j++, c++)
+				Console.WriteLine($"{i} {j}");
+	}
+}
