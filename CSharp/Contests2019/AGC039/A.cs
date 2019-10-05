0a98ff61b04cb9ef2c3b3918f3031e367db4e10f
Keiho Sakapon <git@saka-pon.net>
2019-10-05 23:33:54 +0900

    AC

--- CSharp/Contests2019/AGC039/A.cs
+++ CSharp/Contests2019/AGC039/A.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-using System.Linq;
 
 class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
 		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var k = long.Parse(Console.ReadLine());
 
-		Console.WriteLine(string.Join(" ", h));
+		var sum = 0;
+		var c = s[0];
+		for (int i = 1; i < s.Length; i++)
+		{
+			if (c == s[i]) { sum++; c = '!'; }
+			else c = s[i];
+		}
+
+		if (s[0] != s[s.Length - 1])
+		{
+			Console.WriteLine(k * sum);
+		}
+		else
+		{
+			var t1 = s.Length - s.TrimStart(s[0]).Length;
+			if (t1 == s.Length)
+			{
+				Console.WriteLine(k * t1 / 2);
+			}
+			else
+			{
+				var t2 = s.Length - s.TrimEnd(s[0]).Length;
+				Console.WriteLine(k * sum + (t1 % 2 == 1 && t2 % 2 == 1 ? k - 1 : 0));
+			}
+		}
 	}
 }
