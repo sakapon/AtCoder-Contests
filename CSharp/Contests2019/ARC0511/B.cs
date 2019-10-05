7a4909dd8b49c4e240f9998a33a3503dab281736
Keiho Sakapon <git@saka-pon.net>
2019-10-05 12:07:45 +0900

    AC

--- CSharp/Contests2019/ARC0511/B.cs
+++ CSharp/Contests2019/ARC0511/B.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Linq;
 
 class B
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-		Console.WriteLine(string.Join(" ", a));
+		var c = 0;
+		var rM = h[3] / h[0];
+		for (int r = 0; r <= rM; r++)
+		{
+			var n2 = h[3] - r * h[0];
+			var gM = n2 / h[1];
+			for (int g = 0; g <= gM; g++)
+			{
+				var n3 = n2 - g * h[1];
+				if (n3 % h[2] == 0) c++;
+			}
+		}
+		Console.WriteLine(c);
 	}
 }
