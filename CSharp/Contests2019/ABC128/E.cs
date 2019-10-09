2fc2f0e201ec6b59c0c96d84272748fdfc6504f0
Keiho Sakapon <git@saka-pon.net>
2019-10-09 21:53:21 +0900

    AC

--- CSharp/Contests2019/ABC128/E.cs
+++ CSharp/Contests2019/ABC128/E.cs
@@ -7,18 +7,20 @@ class E
 	{
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		var ws = new int[h[0]].Select(_ => read()).OrderByDescending(x => x[2]).ToArray();
-		var d = new int[h[1]].Select(_ => int.Parse(Console.ReadLine())).ToArray();
+		var ws = new int[h[0]].Select(_ => read()).OrderBy(x => x[2]).ToArray();
+		var d = new int[h[1]].Select(_ => int.Parse(Console.ReadLine())).ToList();
+		var map = d.Select((x, i) => new { x, i }).ToDictionary(_ => _.x, _ => _.i);
 		var c = new int[h[1]];
 
 		foreach (var w in ws)
 		{
-			var si = Array.BinarySearch(d, w[0] - w[2]);
+			var si = d.BinarySearch(w[0] - w[2]);
 			if (si < 0) si = ~si;
-			var ti = Array.BinarySearch(d, w[1] - w[2]);
+			var ti = d.BinarySearch(w[1] - w[2]);
 			if (ti < 0) ti = ~ti;
 
-			for (int i = si; i < ti; i++) c[i] = w[2];
+			for (int i = si; i < ti; i++) c[map[d[i]]] = w[2];
+			d.RemoveRange(si, ti - si);
 		}
 		Console.WriteLine(string.Join("\n", c.Select(x => x != 0 ? x : -1)));
 	}
