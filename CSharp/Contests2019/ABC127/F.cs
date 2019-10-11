2c49651e527a1f986ce64cd4f3db7ee87c4656ca
Keiho Sakapon <git@saka-pon.net>
2019-10-11 23:03:29 +0900

    AC

--- CSharp/Contests2019/ABC127/F.cs
+++ CSharp/Contests2019/ABC127/F.cs
@@ -8,27 +8,36 @@ class F
 	{
 		var qs = new int[int.Parse(Console.ReadLine())].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray());
 
-		long m = 0, a = 0, b = 0;
-		var l = new List<long>();
-		var r = new List<string>();
+		long a = 0, b = 0;
+		var l = new PQ<int>(c: (x, y) => y - x);
+		var r = new PQ<int>();
+		var s = new List<string>();
 		foreach (var q in qs)
 		{
-			if (q[0] == 1)
+			if (q[0] == 2) { s.Add($"{l.First} {a + b}"); continue; }
+
+			if (l.Count == r.Count)
 			{
-				var toLeft = l.Count % 2 == 1 && q[1] < m;
-				var i = l.BinarySearch(q[1]);
-				if (i < 0) i = ~i;
-				l.Insert(i, q[1]);
-
-				var m2 = l[(l.Count - 1) / 2];
-				if (toLeft) a += m - m2;
-				a += Math.Abs(m2 - q[1]);
-				m = m2;
-
-				b += q[2];
+				if (r.Any() && q[1] > r.First)
+				{
+					l.Push(r.Pop());
+					r.Push(q[1]);
+				}
+				else l.Push(q[1]);
 			}
-			else r.Add($"{m} {a + b}");
+			else
+			{
+				if (q[1] < l.First)
+				{
+					r.Push(l.Pop());
+					l.Push(q[1]);
+					a += r.First - l.First;
+				}
+				else r.Push(q[1]);
+			}
+			a += Math.Abs(l.First - q[1]);
+			b += q[2];
 		}
-		Console.WriteLine(string.Join("\n", r));
+		Console.WriteLine(string.Join("\n", s));
 	}
 }
