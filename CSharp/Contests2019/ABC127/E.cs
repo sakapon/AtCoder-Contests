f2826875a929c7217af469e535d1ffc9b529c92d
Keiho Sakapon <git@saka-pon.net>
2019-10-12 11:27:04 +0900

    TLE

---
+++ CSharp/Contests2019/ABC127/E.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+class E
+{
+	static void Main()
+	{
+		var a = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		BigInteger h = a[0], w = a[1], k = a[2];
+
+		Func<BigInteger, BigInteger, BigInteger> c = (x, y) => x * (x * x - 1) / 6 * y * y;
+		Console.WriteLine((c(h, w) + c(w, h)) * Ncr(h * w - 2, k - 2) % 1000000007);
+	}
+
+	static BigInteger Factorial(BigInteger n) { for (BigInteger x = 1, i = 1; ; x *= ++i) if (i >= n) return x; }
+	static BigInteger Npr(BigInteger n, BigInteger r)
+	{
+		if (n < r) return 0;
+		for (BigInteger x = 1, i = n - r; ; x *= ++i) if (i >= n) return x;
+	}
+	static BigInteger Ncr(BigInteger n, BigInteger r) => n < r ? 0 : n - r < r ? Ncr(n, n - r) : Npr(n, r) / Factorial(r);
+}
