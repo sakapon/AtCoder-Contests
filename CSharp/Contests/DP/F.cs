45d324126eb36a88e7c6339d465e06aeb6489b43
Keiho Sakapon <git@saka-pon.net>
2019-10-16 00:03:02 +0900

    AC

--- CSharp/Contests/DP/F.cs
+++ CSharp/Contests/DP/F.cs
@@ -6,16 +6,28 @@ class F
 	{
 		var s = Console.ReadLine();
 		var t = Console.ReadLine();
+		int l = s.Length, m = t.Length;
 
-		var dp = new string[s.Length + 1, t.Length + 1];
-		for (int i = 0; i <= s.Length; i++) dp[i, 0] = "";
-		for (int j = 0; j <= t.Length; j++) dp[0, j] = "";
-		for (int i = 1; i <= s.Length; i++)
-			for (int j = 1; j <= t.Length; j++)
+		var dp = new int[l + 1, m + 1];
+		for (int i = 1; i <= l; i++)
+			for (int j = 1; j <= m; j++)
 				if (s[i - 1] == t[j - 1])
-					dp[i, j] = dp[i - 1, j - 1] + s[i - 1];
+					dp[i, j] = dp[i - 1, j - 1] + 1;
 				else
-					dp[i, j] = dp[i, j - 1].Length > dp[i - 1, j].Length ? dp[i, j - 1] : dp[i - 1, j];
-		Console.WriteLine(dp[s.Length, t.Length]);
+					dp[i, j] = dp[i, j - 1] > dp[i - 1, j] ? dp[i, j - 1] : dp[i - 1, j];
+
+		var cs = new char[dp[l, m]];
+		for (int i = l, j = m; i > 0 && j > 0;)
+			if (s[i - 1] == t[j - 1])
+			{
+				i--; j--;
+				cs[dp[i, j]] = s[i];
+			}
+			else
+			{
+				if (dp[i, j - 1] > dp[i - 1, j]) j--;
+				else i--;
+			}
+		Console.WriteLine(new string(cs));
 	}
 }
