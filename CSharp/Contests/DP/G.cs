81c7c702669d99fbd9baa71964e19eb4cd92e1b6
Keiho Sakapon <git@saka-pon.net>
2019-10-16 14:29:37 +0900

    AC

--- CSharp/Contests/DP/G.cs
+++ CSharp/Contests/DP/G.cs
@@ -8,10 +8,12 @@ class G
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
 		var rs = new int[h[1]].Select(_ => read()).ToArray();
-		var map = rs.ToLookup(r => r[0], r => r[1]);
-		var map_r = rs.ToLookup(r => r[1], r => r[0]);
 
-		var q = Enumerable.Range(1, h[0]).Where(i => !map_r.Contains(i)).ToList();
+		var map = rs.ToLookup(r => r[0], r => r[1]);
+		var ins = new int[h[0] + 1];
+		foreach (var r in rs) ins[r[1]]++;
+
+		var q = Enumerable.Range(1, h[0]).Where(i => ins[i] == 0).ToList();
 		var c = -1;
 		for (; q.Any(); c++)
 		{
@@ -20,7 +22,10 @@ class G
 
 			foreach (var p in ps)
 				foreach (var np in map[p])
-					if (!q.Contains(np)) q.Add(np);
+				{
+					ins[np]--;
+					if (ins[np] == 0) q.Add(np);
+				}
 		}
 		Console.WriteLine(c);
 	}
