c6fcfeff0fb167765e59b8d66d95ebc608898141
Keiho Sakapon <git@saka-pon.net>
2019-10-20 00:17:14 +0900

    Add MInt struct

---
+++ CSharp/CoderLib/CoderLibTest/MIntTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+struct MInt
+{
+	const int M = 1000000007;
+	public long V;
+	public MInt(long v) { V = (v %= M) < 0 ? v + M : v; }
+
+	public static implicit operator MInt(long v) => new MInt(v);
+	public static MInt operator +(MInt x) => x;
+	public static MInt operator -(MInt x) => -x.V;
+	public static MInt operator +(MInt x, MInt y) => x.V + y.V;
+	public static MInt operator -(MInt x, MInt y) => x.V - y.V;
+	public static MInt operator *(MInt x, MInt y) => x.V * y.V;
+	public static MInt operator /(MInt x, MInt y) => x * y.Inv();
+
+	public MInt Pow(int i) => MPow(V, i);
+	public MInt Inv() => MPow(V, M - 2);
+
+	static long MPow(long b, int i)
+	{
+		for (var r = 1L; ; b = b * b % M)
+		{
+			if (i % 2 > 0) r = r * b % M;
+			if ((i /= 2) < 1) return r;
+		}
+	}
+}
+
+[TestClass]
+public class MIntTest
+{
+}
