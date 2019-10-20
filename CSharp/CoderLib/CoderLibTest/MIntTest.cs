91eef3c60926c61d78f9367aa410cb16595f6f2a
Keiho Sakapon <git@saka-pon.net>
2019-10-20 14:57:44 +0900

    Add test

--- CSharp/CoderLib/CoderLibTest/MIntTest.cs
+++ CSharp/CoderLib/CoderLibTest/MIntTest.cs
@@ -39,4 +39,23 @@ struct MInt
 [TestClass]
 public class MIntTest
 {
+	[TestMethod]
+	public void Ctor()
+	{
+		Assert.AreEqual(999999999, new MInt(-8).V);
+	}
+
+	[TestMethod]
+	public void Pow()
+	{
+		for (var i = 1; i <= 100000; i++)
+			Assert.AreEqual(1, new MInt(i).Pow(1000000006).V);
+	}
+
+	[TestMethod]
+	public void Inv()
+	{
+		for (var i = 1; i <= 100000; i++)
+			Assert.AreEqual(1, (new MInt(i).Inv() * i).V);
+	}
 }
