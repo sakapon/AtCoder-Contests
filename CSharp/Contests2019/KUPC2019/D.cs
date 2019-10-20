433d1813c2ced6ded39e176937794fce9887fd1e
Keiho Sakapon <git@saka-pon.net>
2019-10-20 14:18:19 +0900

    AC

--- CSharp/Contests2019/KUPC2019/D.cs
+++ CSharp/Contests2019/KUPC2019/D.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 class D
 {
@@ -8,24 +7,43 @@ class D
 		var n = int.Parse(Console.ReadLine());
 		var s = Console.ReadLine();
 
-		var M = 998244353;
-		c = new long[n + 1];
+		var c = new MInt[n + 1];
 		c[0] = 1;
+		for (int i = 1; i <= n; i++) c[i] = c[i - 1] * 2 * (2 * i - 1) / (i + 1);
 
-		var r = 1L;
-		var t = 0;
-		for (int i = 0; i < n; i++)
+		MInt r = 1;
+		for (int t = 0, i = 0; i < n; i++)
 		{
 			t++;
 			if (i == n - 1 || s[i] != s[i + 1])
 			{
-				r = r * ModCatalan(t, M) % M;
+				r *= c[t];
 				t = 0;
 			}
 		}
-		Console.WriteLine(r);
+		Console.WriteLine(r.V);
 	}
+}
 
-	static long[] c;
-	static long ModCatalan(int n, int mod) => c[n] != 0 ? c[n] : (c[n] = Enumerable.Range(0, n).Sum(i => ModCatalan(i, mod) * ModCatalan(n - 1 - i, mod)) % mod);
+struct MInt
+{
+	const int M = 998244353;
+	public long V;
+	public MInt(long v) { V = (v %= M) < 0 ? v + M : v; }
+
+	public static implicit operator MInt(long v) => new MInt(v);
+	public static MInt operator *(MInt x, MInt y) => x.V * y.V;
+	public static MInt operator /(MInt x, MInt y) => x * y.Inv();
+
+	public MInt Pow(int i) => MPow(V, i);
+	public MInt Inv() => MPow(V, M - 2);
+
+	static long MPow(long b, int i)
+	{
+		for (var r = 1L; ; b = b * b % M)
+		{
+			if (i % 2 > 0) r = r * b % M;
+			if ((i /= 2) < 1) return r;
+		}
+	}
 }
