104170c0f372fc7e78c60bb3e3f2d81f2262fa9e
Keiho Sakapon <git@saka-pon.net>
2019-10-20 14:21:17 +0900

    Add functions

--- CSharp/CoderLib/CoderLibTest/MIntTest.cs
+++ CSharp/CoderLib/CoderLibTest/MIntTest.cs
@@ -26,6 +26,14 @@ struct MInt
 			if ((i /= 2) < 1) return r;
 		}
 	}
+
+	public static long MFactorial(int n) { for (long x = 1, i = 1; ; x = x * ++i % M) if (i >= n) return x; }
+	public static long MNpr(int n, int r)
+	{
+		if (n < r) return 0;
+		for (long x = 1, i = n - r; ; x = x * ++i % M) if (i >= n) return x;
+	}
+	public static MInt MNcr(int n, int r) => n < r ? 0 : n - r < r ? MNcr(n, n - r) : (MInt)MNpr(n, r) / MFactorial(r);
 }
 
 [TestClass]
