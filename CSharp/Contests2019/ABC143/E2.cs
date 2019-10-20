01d479aa69421a6651bc18f9e6b0126b39e2c3b3
Keiho Sakapon <git@saka-pon.net>
2019-10-20 09:34:44 +0900

    AC

--- CSharp/Contests2019/ABC143/E2.cs
+++ CSharp/Contests2019/ABC143/E2.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-class E
+class E2
 {
 	static void Main()
 	{
@@ -16,8 +15,6 @@ class E
 		for (int i = 1; i <= n; i++)
 			for (int j = 1; j <= n; j++)
 				d[i, j] = long.MaxValue;
-		for (int i = 1; i <= n; i++)
-			d[i, i] = 0;
 		foreach (var r in rs)
 		{
 			d[r[0], r[1]] = r[2];
@@ -30,32 +27,16 @@ class E
 					if (d[i, k] < long.MaxValue && d[k, j] < long.MaxValue)
 						d[i, j] = Math.Min(d[i, j], d[i, k] + d[k, j]);
 
-		var map = new int[n + 1].Select(_ => new List<int>()).ToArray();
 		for (int i = 1; i <= n; i++)
 			for (int j = 1; j <= n; j++)
-				if (d[i, j] != 0 && d[i, j] <= h[2]) map[i].Add(j);
+				d[i, j] = d[i, j] <= h[2] ? 1 : long.MaxValue;
 
-		var g = new int[n + 1, n + 1];
-		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= n; j++)
-				g[i, j] = -1;
-		var q = new Queue<int>();
-		for (int i = 1; i <= n; i++)
-		{
-			g[i, i] = 0;
-			q.Enqueue(i);
+		for (int k = 1; k <= n; k++)
+			for (int i = 1; i <= n; i++)
+				for (int j = 1; j <= n; j++)
+					if (d[i, k] < long.MaxValue && d[k, j] < long.MaxValue)
+						d[i, j] = Math.Min(d[i, j], d[i, k] + d[k, j]);
 
-			while (q.Any())
-			{
-				var p = q.Dequeue();
-				foreach (var np in map[p])
-				{
-					if (g[i, np] >= 0) continue;
-					g[i, np] = g[i, p] + 1;
-					q.Enqueue(np);
-				}
-			}
-		}
-		Console.WriteLine(string.Join("\n", qs.Select(x => g[x[0], x[1]]).Select(x => x > 0 ? x - 1 : x)));
+		Console.WriteLine(string.Join("\n", qs.Select(x => d[x[0], x[1]]).Select(x => x < long.MaxValue ? x - 1 : -1)));
 	}
 }
