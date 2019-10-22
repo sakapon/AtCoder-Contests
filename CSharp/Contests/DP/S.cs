12da7564c1930330b5787d5ee1c55481e0335224
Keiho Sakapon <git@saka-pon.net>
2019-10-22 11:18:52 +0900

    Refactor

--- CSharp/Contests/DP/S.cs
+++ CSharp/Contests/DP/S.cs
@@ -16,18 +16,16 @@ class S
 		{
 			var c = k[i - 1] - '0';
 
-			for (int x = 0; x <= c; x++)
-				for (int j = 0; j < d; j++)
-					dp[i, (j + x) % d, x == c ? 0 : 1] += dp[i - 1, j, 0];
 			for (int x = 0; x <= 9; x++)
 				for (int j = 0; j < d; j++)
+				{
+					dp[i, (j + x) % d, x == c ? 0 : 1] += x <= c ? dp[i - 1, j, 0] : 0;
 					dp[i, (j + x) % d, 1] += dp[i - 1, j, 1];
+				}
 
 			for (int j = 0; j < d; j++)
-			{
-				dp[i, j, 0] %= M;
-				dp[i, j, 1] %= M;
-			}
+				for (int f = 0; f < 2; f++)
+					dp[i, j, f] %= M;
 		}
 		Console.WriteLine((dp[n, 0, 0] + dp[n, 0, 1] + M - 1) % M);
 	}
