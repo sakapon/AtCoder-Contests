e5cc44c4c19d37408f0cc5c62b523e926b5e6196
Keiho Sakapon <git@saka-pon.net>
2019-10-27 23:45:06 +0900

    Improve code

--- CSharp/Contests2019/ABC144/C.cs
+++ CSharp/Contests2019/ABC144/C.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class C
@@ -7,14 +6,6 @@ class C
 	static void Main()
 	{
 		var n = long.Parse(Console.ReadLine());
-		Console.WriteLine(Divisors(n).Select(x => x + n / x - 2).Min());
-	}
-
-	static long[] Divisors(long v)
-	{
-		var d = new List<long>();
-		for (long i = 1, rv = (long)Math.Sqrt(v); i <= rv; i++)
-			if (v % i == 0) d.Add(i);
-		return d.ToArray();
+		Console.WriteLine(Enumerable.Range(1, (int)Math.Sqrt(n)).Where(x => n % x == 0).Min(x => x + n / x - 2));
 	}
 }
