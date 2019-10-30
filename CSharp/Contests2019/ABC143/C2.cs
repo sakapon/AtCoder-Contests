80bf04f655e51505b68c6ce73245937beff8f5cc
Keiho Sakapon <git@saka-pon.net>
2019-10-30 11:16:16 +0900

    Other solutions

---
+++ CSharp/Contests2019/ABC143/C2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class C2
+{
+	static void Main()
+	{
+		Console.ReadLine();
+		var s = Console.ReadLine();
+
+		for (int i = s.Length - 1; i > 0; i--)
+			if (i < s.Length && s[i - 1] == s[i])
+			{
+				var c = s[i].ToString();
+				s = s.Replace(c + c, c);
+			}
+		Console.WriteLine(s.Length);
+	}
+}
