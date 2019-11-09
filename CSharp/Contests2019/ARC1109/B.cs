8f992742d91110009fb06d7e52583fc64ffc548f
Keiho Sakapon <git@saka-pon.net>
2019-11-10 00:21:30 +0900

    AC

--- CSharp/Contests2019/ARC1109/B.cs
+++ CSharp/Contests2019/ARC1109/B.cs
@@ -5,12 +5,27 @@ class B
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var d = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-		Console.WriteLine(string.Join(" ", h));
+		if (d[0] > 0 || d.Skip(1).Any(x => x == 0)) { Console.WriteLine(0); return; }
+		var c = d.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+		var max = c.Keys.Max();
+		if (c.Count <= max) { Console.WriteLine(0); return; }
+
+		var r = 1L;
+		for (int i = 0; i < max; i++) r = MMul(r, MPow(c[i], c[i + 1]));
+		Console.WriteLine(r);
 	}
+
+	const int M = 998244353;
+	static long MPow(long b, int i)
+	{
+		for (var r = 1L; ; b = b * b % M)
+		{
+			if (i % 2 > 0) r = r * b % M;
+			if ((i /= 2) < 1) return r;
+		}
+	}
+	static long MMul(long x, long y) => x * y % M;
 }
--- CSharp/Contests2019/ARC1109/D.cs
+++ CSharp/Contests2019/ARC1109/D.cs
@@ -5,14 +5,24 @@ class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var qs = new int[h[1]].Select(_ => read()).OrderBy(x => x[1]).ThenBy(x => x[0]).ThenBy(x => x[2]).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var dp = Enumerable.Repeat(-1L, h[0] + 1).ToArray();
+		dp[1] = 0;
+
+		foreach (var q in qs)
+		{
+			if (dp[q[0]] == -1) continue;
+			var d = dp[q[0]] + q[2];
+
+			for (int i = q[1]; i > 0; i--)
+			{
+				if (dp[i] != -1 && dp[i] <= d) break;
+				dp[i] = d;
+			}
+		}
+		Console.WriteLine(dp[h[0]]);
 	}
 }
