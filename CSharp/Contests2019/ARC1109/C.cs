6d9fcb3925ed0644b04d47e4dd7ad385da3eb3fd
Keiho Sakapon <git@saka-pon.net>
2019-11-11 18:58:39 +0900

    WA

--- CSharp/Contests2019/ARC1109/C.cs
+++ CSharp/Contests2019/ARC1109/C.cs
@@ -5,12 +5,22 @@ class C
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var a = read();
+		var b = read();
+		var ab = a.Zip(b, (x, y) => new { x, y }).OrderBy(_ => _.y).ThenBy(_ => _.x).ToArray();
+		var ao = a.OrderBy(x => x).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		if (Enumerable.Range(0, n).Any(i => ao[i] > ab[i].y)) { Console.WriteLine("No"); return; }
+		if (Enumerable.Range(0, n).Any(i => ab[i].x <= ab[i].y && ao[i] <= ab[i].x)) { Console.WriteLine("Yes"); return; }
+
+		int k = 0, c = 0;
+		while (true)
+		{
+			c++;
+			if ((k = Array.BinarySearch(ao, ab[k].x)) == 0) break;
+		}
+		Console.WriteLine(c != n ? "Yes" : "No");
 	}
 }
