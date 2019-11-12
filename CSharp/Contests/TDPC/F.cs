33858a7d48b1df74994b0314df2b78768cd308a3
Keiho Sakapon <git@saka-pon.net>
2019-11-12 11:52:47 +0900

    AC

--- CSharp/Contests/TDPC/F.cs
+++ CSharp/Contests/TDPC/F.cs
@@ -1,18 +1,24 @@
 ﻿using System;
-using System.Linq;
 
 class F
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		int n = h[0], k = h[1];
 
-		Console.WriteLine(string.Join(" ", a));
+		var dp = new long[n + 1, 3];
+		dp[1, 0] = 1;
+
+		for (int i = 1; i < n; i++)
+		{
+			dp[i + 1, 0] = dp[i, 2];
+			dp[i + 1, 1] = MInt(dp[i, 0] + dp[i, 1] - (i > k - 2 ? dp[i - k + 2, 0] : 0));
+			dp[i + 1, 2] = MInt(dp[i, 0] + dp[i, 1] + dp[i, 2]);
+		}
+		Console.WriteLine(MInt(dp[n, 0] + dp[n, 1]));
 	}
+
+	const int M = 1000000007;
+	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
 }
