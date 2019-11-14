194208ca2794a45ceffc653092ad15b1a41fd4d7
Keiho Sakapon <git@saka-pon.net>
2019-11-14 22:50:21 +0900

    TLE

--- CSharp/Contests2019/ABC144/F.cs
+++ CSharp/Contests2019/ABC144/F.cs
@@ -10,12 +10,15 @@ class F
 		var n = h[0];
 		var map = new int[h[1]].Select(_ => read()).ToLookup(r => r[0], r => r[1]);
 
-		var dp = new double[n + 1];
+		var dp = new double[n + 1, n];
 		for (int i = n - 1; i > 0; i--)
-		{
-			var es = map[i].Select(x => dp[x]).ToArray();
-			dp[i] = (es.Length == 1 ? es[0] : (es.Sum() - es.Max()) / (es.Length - 1)) + 1;
-		}
-		Console.WriteLine(dp[1]);
+			for (int j = 0; j < n; j++)
+			{
+				var es = map[i].Select(x => dp[x, j]).ToArray();
+				dp[i, j] = i == j && es.Length > 1 ?
+					(es.Sum() - es.Max()) / (es.Length - 1) + 1 :
+					es.Sum() / es.Length + 1;
+			}
+		Console.WriteLine(Enumerable.Range(1, n - 1).Min(j => dp[1, j]));
 	}
 }
