ac624215091ff47900142c56828bb2667d2dfd3d
Keiho Sakapon <git@saka-pon.net>
2019-11-14 22:26:47 +0900

    WA

--- CSharp/Contests2019/ABC144/F.cs
+++ CSharp/Contests2019/ABC144/F.cs
@@ -5,14 +5,17 @@ class F
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var n = h[0];
+		var map = new int[h[1]].Select(_ => read()).ToLookup(r => r[0], r => r[1]);
 
-		Console.WriteLine(string.Join(" ", a));
+		var dp = new double[n + 1];
+		for (int i = n - 1; i > 0; i--)
+		{
+			var es = map[i].Select(x => dp[x]).ToArray();
+			dp[i] = (es.Length == 1 ? es[0] : (es.Sum() - es.Max()) / (es.Length - 1)) + 1;
+		}
+		Console.WriteLine(dp[1]);
 	}
 }
