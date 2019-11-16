437b7254b2d29d4a672da03abef1585e0161ad55
Keiho Sakapon <git@saka-pon.net>
2019-11-17 01:15:48 +0900

    Shorten code

--- CSharp/Contests2019/ABC145/C.cs
+++ CSharp/Contests2019/ABC145/C.cs
@@ -7,13 +7,6 @@ class C
 	{
 		var n = int.Parse(Console.ReadLine());
 		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
-		Console.WriteLine(ps.SelectMany(p => ps.Select(q => Norm(p, q))).Sum() / n);
-	}
-
-	static double Norm(int[] p, int[] q)
-	{
-		var dx = p[0] - q[0];
-		var dy = p[1] - q[1];
-		return Math.Sqrt(dx * dx + dy * dy);
+		Console.WriteLine(ps.SelectMany(p => ps.Select(q => Math.Sqrt(Math.Pow(p[0] - q[0], 2) + Math.Pow(p[1] - q[1], 2)))).Sum() / n);
 	}
 }
