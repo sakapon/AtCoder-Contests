26811add558626c22701385f5119f57a3ffb00f6
Keiho Sakapon <git@saka-pon.net>
2019-11-24 00:26:45 +0900

    AC

--- CSharp/Contests2019/ARC1123/D.cs
+++ CSharp/Contests2019/ARC1123/D.cs
@@ -3,16 +3,11 @@ using System.Linq;
 
 class D
 {
-	static void Main()
-	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+	static void Main() => Console.WriteLine(Dfs(new int[int.Parse(Console.ReadLine())].Select(_ => Console.ReadLine().Split().Select(long.Parse).ToArray()).ToArray()));
 
-		Console.WriteLine(string.Join(" ", a));
+	static long Dfs(long[][] dc)
+	{
+		var s = dc.Sum(x => x[0] * x[1]);
+		return dc.Sum(x => x[1]) - 1 + (s < 10 ? 0 : Dfs(new[] { new[] { 1, s / 10 }, new[] { s % 10, 1 } }));
 	}
 }
