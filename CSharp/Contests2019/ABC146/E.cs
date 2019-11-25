d554ba39bc3e7c040a75bb3d500ccd2fc41b509d
Keiho Sakapon <git@saka-pon.net>
2019-11-25 10:45:20 +0900

    AC

--- CSharp/Contests2019/ABC146/E.cs
+++ CSharp/Contests2019/ABC146/E.cs
@@ -5,14 +5,23 @@ class E
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var a = read();
+		int n = h[0], k = h[1];
 
-		Console.WriteLine(string.Join(" ", a));
+		var s = new int[n + 1];
+		for (int i = 0; i < n; i++) s[i + 1] = (s[i] + a[i] - 1) % k;
+
+		var r = 0L;
+		var d = new int[0].ToDictionary(_ => _);
+		for (int i = 0; i <= n; i++)
+		{
+			if (i >= k) d[s[i - k]]--;
+			if (!d.ContainsKey(s[i])) d[s[i]] = 0;
+			r += d[s[i]];
+			d[s[i]]++;
+		}
+		Console.WriteLine(r);
 	}
 }
