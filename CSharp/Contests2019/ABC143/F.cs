658c119a7d579b78cf644e8dbe2145d4a88c8562
Keiho Sakapon <git@saka-pon.net>
2019-11-25 21:25:59 +0900

    AC

--- CSharp/Contests2019/ABC143/F.cs
+++ CSharp/Contests2019/ABC143/F.cs
@@ -5,14 +5,24 @@ class F
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var a = Console.ReadLine().Split().Select(int.Parse).GroupBy(x => x).Select(g => g.Count()).OrderBy(x => -x).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var f = new int[n + 1];
+		for (int i = a.Length, c = 1; c <= n; c++)
+		{
+			while (i > 0 && a[i - 1] < c) i--;
+			f[c] = f[c - 1] + i;
+		}
+		f[0] = int.MaxValue;
+		for (int c = 1; c <= n; c++) f[c] /= c;
+
+		var r = new int[n + 1];
+		for (int c = n, i = 1; i <= n; i++)
+		{
+			while (f[c] < i) c--;
+			r[i] = c;
+		}
+		Console.WriteLine(string.Join("\n", r.Skip(1)));
 	}
 }
