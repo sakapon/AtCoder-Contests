685e81a796694dcbfb96e838f87135b39818f2d7
Keiho Sakapon <git@saka-pon.net>
2019-11-30 20:24:00 +0900

    Add functions for matrix

--- CSharp/CoderLib/CoderLibTest/Modulo2Test.cs
+++ CSharp/CoderLib/CoderLibTest/Modulo2Test.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class Modulo2Test
 {
 	const int M = 1000000007;
-	static long MPow(long b, int i)
+	static long MPow(long b, long i)
 	{
 		for (var r = 1L; ; b = b * b % M)
 		{
@@ -19,6 +19,26 @@ public class Modulo2Test
 	static long MSub(long x, long y) => MInt(x - y);
 	static long MMul(long x, long y) => x * y % M;
 	static long MDiv(long x, long y) => x * MInv(y) % M;
+
+	static long[,] MPow3(long[,] b, long i)
+	{
+		for (var r = new long[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } }; ; b = MMul3(b, b))
+		{
+			if (i % 2 > 0) r = MMul3(r, b);
+			if ((i /= 2) < 1) return r;
+		}
+	}
+	static long[,] MMul3(long[,] a, long[,] b)
+	{
+		var r = new long[3, 3];
+		for (var i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+			{
+				for (var k = 0; k < 3; k++) r[i, j] += a[i, k] * b[k, j];
+				r[i, j] %= M;
+			}
+		return r;
+	}
 
 	#region Test Methods
 
