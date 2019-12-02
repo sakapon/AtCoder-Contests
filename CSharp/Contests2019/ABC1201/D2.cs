bd2e3bc9c23b0775fff38d67259af3d13e27d399
Keiho Sakapon <git@saka-pon.net>
2019-12-02 11:13:35 +0900

    Other solution

---
+++ CSharp/Contests2019/ABC1201/D2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class D2
+{
+	static void Main()
+	{
+		Console.ReadLine();
+		var d = Console.ReadLine().Select((c, i) => new { c, i }).GroupBy(_ => _.c).Select(g => g.Select(_ => _.i).ToArray()).ToArray();
+
+		var q = from i1 in d.Select(x => x[0])
+				from i3 in d.Select(x => x.Last()).Where(i3 => i3 - i1 >= 2)
+				select d.Count(x => x.Any(i2 => i1 < i2 && i2 < i3));
+		Console.WriteLine(q.Sum());
+	}
+}
