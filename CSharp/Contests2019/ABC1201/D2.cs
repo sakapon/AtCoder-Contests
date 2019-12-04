5b64d07f5a2692ef1ade1cf460f5d9f5202d3f80
Keiho Sakapon <git@saka-pon.net>
2019-12-04 22:24:58 +0900

    Improve code

--- CSharp/Contests2019/ABC1201/D2.cs
+++ CSharp/Contests2019/ABC1201/D2.cs
@@ -6,11 +6,11 @@ class D2
 	static void Main()
 	{
 		Console.ReadLine();
-		var d = Console.ReadLine().Select((c, i) => new { c, i }).GroupBy(_ => _.c).Select(g => g.Select(_ => _.i).ToArray()).ToArray();
+		var d = Console.ReadLine().Select((c, i) => new { c, i }).ToLookup(_ => _.c, _ => _.i);
 
-		var q = from i1 in d.Select(x => x[0])
-				from i3 in d.Select(x => x.Last()).Where(i3 => i3 - i1 >= 2)
-				select d.Count(x => x.Any(i2 => i1 < i2 && i2 < i3));
+		var q = from i1 in d.Select(g => g.First())
+				from i2 in d.Select(g => g.FirstOrDefault(x => x > i1)).Where(x => x > 0)
+				select d.Count(g => g.Any(x => x > i2));
 		Console.WriteLine(q.Sum());
 	}
 }
