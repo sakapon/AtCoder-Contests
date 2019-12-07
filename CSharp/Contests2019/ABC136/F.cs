88a24669ff114a0c4e367fb948f9af13a8d04e1c
Keiho Sakapon <git@saka-pon.net>
2019-12-07 14:07:04 +0900

    TLE

--- CSharp/Contests2019/ABC136/F.cs
+++ CSharp/Contests2019/ABC136/F.cs
@@ -6,22 +6,35 @@ class F
 	static void Main()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).OrderBy(p => p[0]).ToArray();
+		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var yx = ps.OrderBy(p => p[0]).Select((p, i) => { p[0] = i; return p; }).OrderBy(p => p[1]).ToArray();
+		var xy = yx.Select((p, i) => { p[1] = i; return p; }).OrderBy(p => p[0]).ToArray();
 
-		var rs = ps.Select(p => p[1]).OrderBy(x => x).ToList();
-		var ls = rs.Take(0).ToList();
+		int ss = 64, sc = n / ss + 1;
+		var s = new int[sc, sc];
+		for (int i = 0; i < n; i++)
+			s[xy[i][0] / ss, xy[i][1] / ss]++;
+		for (int i = 0; i < sc; i++)
+			for (int j = 1; j < sc; j++)
+				s[i, j] += s[i, j - 1];
+		for (int j = 0; j < sc; j++)
+			for (int i = 1; i < sc; i++)
+				s[i, j] += s[i - 1, j];
 
 		var c = MInt(n * MPow(2, n) - n);
 		for (int i = 0; i < n; i++)
 		{
-			var v = ps[i][1];
-			var ri = rs.BinarySearch(v);
-			rs.RemoveAt(ri);
-			var li = ~ls.BinarySearch(v);
-			ls.Insert(li, v);
+			var v = xy[i][1];
 
-			c -= MPow(2, i) + MPow(2, n - 1 - i) + MPow(2, li + ri) + MPow(2, n - 1 - li - ri);
-			c += MPow(2, li) + MPow(2, i - li) + MPow(2, ri) + MPow(2, n - 1 - i - ri);
+			var li = i >= ss && v >= ss ? s[i / ss - 1, v / ss - 1] : 0;
+			var ix = i - i % ss;
+			for (int i2 = ix; i2 < i; i2++)
+				if (xy[i2][1] < v) li++;
+			for (int j2 = v - v % ss; j2 < v; j2++)
+				if (yx[j2][0] < ix) li++;
+
+			c -= MPow(2, i) + MPow(2, n - 1 - i) + MPow(2, v) + MPow(2, n - 1 - v);
+			c += MPow(2, li) + MPow(2, i - li) + MPow(2, v - li) + MPow(2, n - 1 + li - i - v);
 			c = MInt(c);
 		}
 		Console.WriteLine(c);
