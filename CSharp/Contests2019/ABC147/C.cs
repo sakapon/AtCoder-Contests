99253b9063d8bc4e7a3d7e194718b3619533e64d
Keiho Sakapon <git@saka-pon.net>
2019-12-09 00:35:36 +0900

    Improve code

--- CSharp/Contests2019/ABC147/C.cs
+++ CSharp/Contests2019/ABC147/C.cs
@@ -10,33 +10,25 @@ class C
 			.Select(a => new int[a].Select(i => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray())
 			.ToArray();
 
-		var m = new int[n, n];
-		for (int i = 0; i < n; i++)
-			foreach (var xy in ps[i])
-				m[i, xy[0] - 1] = xy[1] == 0 ? -1 : 1;
-
 		var p2 = new int[n + 1];
 		p2[0] = 1;
 		for (int i = 1; i <= n; i++) p2[i] = 2 * p2[i - 1];
 
 		var M = 0;
-		for (int x = 0; x < p2[n]; x++)
-		{
-			var ok = true;
-			var c = 0;
-			for (int i = 0; i < n; i++)
-			{
-				if ((x & p2[i]) == 0) continue;
-				c++;
-				for (int j = 0; j < n; j++)
-				{
-					if (m[i, j] == 1 && (x & p2[j]) == 0) { ok = false; break; }
-					if (m[i, j] == -1 && (x & p2[j]) > 0) { ok = false; break; }
-				}
-				if (!ok) break;
-			}
-			if (ok) M = Math.Max(M, c);
-		}
+		for (int f = 0; f < p2[n]; f++) M = Math.Max(M, Assert(f, ps, p2));
 		Console.WriteLine(M);
+	}
+
+	static int Assert(int f, int[][][] ps, int[] p2)
+	{
+		var c = 0;
+		for (int i = 0; i < ps.Length; i++)
+		{
+			if ((f & p2[i]) == 0) continue;
+			c++;
+			foreach (var xy in ps[i])
+				if (((f & p2[xy[0] - 1]) > 0 ? 1 : 0) != xy[1]) return 0;
+		}
+		return c;
 	}
 }
