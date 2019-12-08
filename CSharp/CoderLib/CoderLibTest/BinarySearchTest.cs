18d95d133c2ad8f046cde018adcb306097f3a71b
Keiho Sakapon <git@saka-pon.net>
2019-12-08 15:55:58 +0900

    Add test

--- CSharp/CoderLib/CoderLibTest/BinarySearchTest.cs
+++ CSharp/CoderLib/CoderLibTest/BinarySearchTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
@@ -52,6 +53,66 @@ public class BinarySearchTest
 		Assert.AreEqual(5, IndexForInsert(a, 7));
 		Assert.AreEqual(6, IndexForInsert(a, 8));
 		Assert.AreEqual(6, IndexForInsert(a, 9));
+	}
+
+	static readonly Random random = new Random();
+
+	[TestMethod]
+	public void Index_Random()
+	{
+		for (int k = 0; k < 10; k++)
+		{
+			for (int n = 0; n < 10; n++) Index_Random(n);
+			for (int n = 1000; n < 1010; n++) Index_Random(n);
+		}
+	}
+
+	static void Index_Random(int n)
+	{
+		var a = Enumerable.Range(0, n).Select(_ => random.Next(0, n)).OrderBy(x => x).ToArray();
+		for (int i = -2; i < n + 2; i++)
+		{
+			var expected = Array.BinarySearch(a, i);
+			var actual = Index(a, i);
+			if (expected >= 0)
+			{
+				Assert.AreEqual(i, a[actual]);
+				Assert.IsTrue(actual == 0 || a[actual - 1] < i);
+			}
+			else
+			{
+				Assert.AreEqual(expected, actual);
+			}
+		}
+	}
+
+	[TestMethod]
+	public void IndexForInsert_Random()
+	{
+		for (int k = 0; k < 10; k++)
+		{
+			for (int n = 0; n < 10; n++) IndexForInsert_Random(n);
+			for (int n = 1000; n < 1010; n++) IndexForInsert_Random(n);
+		}
+	}
+
+	static void IndexForInsert_Random(int n)
+	{
+		var a = Enumerable.Range(0, n).Select(_ => random.Next(0, n)).OrderBy(x => x).ToArray();
+		for (int i = -2; i < n + 2; i++)
+		{
+			var expected = Array.BinarySearch(a, i);
+			var actual = IndexForInsert(a, i);
+			if (expected >= 0)
+			{
+				Assert.AreEqual(i, a[actual - 1]);
+				Assert.IsTrue(actual == n || a[actual] > i);
+			}
+			else
+			{
+				Assert.AreEqual(~expected, actual);
+			}
+		}
 	}
 	#endregion
 }
