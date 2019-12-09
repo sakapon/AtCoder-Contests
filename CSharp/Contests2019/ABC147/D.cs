1898be38258c2e10992841924a95b93a62f5f5d7
Keiho Sakapon <git@saka-pon.net>
2019-12-10 01:16:30 +0900

    Improve code

--- CSharp/Contests2019/ABC147/D.cs
+++ CSharp/Contests2019/ABC147/D.cs
@@ -5,23 +5,15 @@ class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+		var M = 1000000007;
+		var t = 1L;
+		var p = Enumerable.Range(0, 60).Select(i => i == 0 ? t : t *= 2).ToArray();
 
-		var p2 = new long[60];
-		p2[0] = 1;
-		for (int i = 1; i < 60; i++) p2[i] = 2 * p2[i - 1];
-
-		var f = new long[60];
-		foreach (var x in a)
+		var b = new long[60];
+		var n = int.Parse(Console.ReadLine());
+		foreach (var x in Console.ReadLine().Split().Select(long.Parse))
 			for (int i = 0; i < 60; i++)
-				if ((x & p2[i]) > 0) f[i]++;
-
-		var s = a.Aggregate((x, y) => (x + y) % M) * (a.Length - 1) % M;
-		var fs = f.Select((x, i) => x * (x - 1) / 2 % M * (p2[i] % M) % M).Aggregate((x, y) => (x + y) % M);
-		Console.WriteLine(MInt(s - 2 * fs));
+				if ((x & p[i]) > 0) b[i]++;
+		Console.WriteLine(b.Select((x, i) => x * (n - x) % M * (p[i] % M)).Sum() % M);
 	}
-
-	const int M = 1000000007;
-	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
 }
