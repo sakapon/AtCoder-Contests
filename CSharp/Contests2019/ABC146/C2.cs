b5a555fabadb34f8368b1653e224893b07465615
Keiho Sakapon <git@saka-pon.net>
2019-12-20 19:22:21 +0900

    Improve code

--- CSharp/Contests2019/ABC146/C2.cs
+++ CSharp/Contests2019/ABC146/C2.cs
@@ -5,13 +5,13 @@ class C2
 	static void Main()
 	{
 		var h = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-		Console.WriteLine(SearchLast(n => h[0] * n + h[1] * n.ToString().Length <= h[2], 0, 1000000000));
+		Console.WriteLine(Last(n => h[0] * n + h[1] * n.ToString().Length <= h[2], 0, 1000000000));
 	}
 
-	static int SearchLast(Func<int, bool> f, int l, int r)
+	static int Last(Func<int, bool> f, int l, int r)
 	{
 		int m;
-		while (l < r) if (f(m = (l + r + 1) / 2)) l = m; else r = m - 1;
-		return r;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
 	}
 }
