2340c623bce4d937f642455b50ff29166d955656
Keiho Sakapon <git@saka-pon.net>
2019-12-21 12:02:46 +0900

    Add functions

--- CSharp/CoderLib/CoderLibTest/BinarySearchTest.cs
+++ CSharp/CoderLib/CoderLibTest/BinarySearchTest.cs
@@ -6,39 +6,91 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 [TestClass]
 public class BinarySearchTest
 {
+	/// <summary>
+	/// 条件 f を満たす最初の値を探索します。
+	/// [l, x) 上で false、[x, r) 上で true となる x を返します。
+	/// f(l) が true のとき、l を返します。
+	/// f(r - 1) が false のとき、r を返します。
+	/// </summary>
+	/// <param name="f">半開区間 [l, r) 上で定義される条件。</param>
+	/// <param name="l">探索範囲の下限。</param>
+	/// <param name="r">探索範囲の上限。</param>
+	/// <returns>条件 f を満たす最初の値。</returns>
+	static int First(Func<int, bool> f, int l, int r)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
+	}
+
+	/// <summary>
+	/// 条件 f を満たす最後の値を探索します。
+	/// (l, x] 上で true、(x, r] 上で false となる x を返します。
+	/// f(r) が true のとき、r を返します。
+	/// f(l + 1) が false のとき、l を返します。
+	/// </summary>
+	/// <param name="f">半開区間 (l, r] 上で定義される条件。</param>
+	/// <param name="l">探索範囲の下限。</param>
+	/// <param name="r">探索範囲の上限。</param>
+	/// <returns>条件 f を満たす最後の値。</returns>
+	static int Last(Func<int, bool> f, int l, int r)
+	{
+		int m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
+	}
+
+	/// <summary>
+	/// 条件 f を満たす最初の値を指定された誤差の範囲内で探索します。
+	/// (l, x) 上で false、[x, r) 上で true となる x を返します。
+	/// l 近傍で true のとき、l を返します。
+	/// r 近傍で false のとき、r を返します。
+	/// </summary>
+	/// <param name="f">開区間 (l, r) 上で定義される条件。</param>
+	/// <param name="l">探索範囲の下限。</param>
+	/// <param name="r">探索範囲の上限。</param>
+	/// <param name="digits">誤差を表す小数部の桁数。</param>
+	/// <returns>条件 f を満たす最初の値。</returns>
+	public static double First(Func<double, bool> f, double l, double r, int digits = 9)
+	{
+		double m;
+		while (Math.Round(r - l, digits) > 0) if (f(m = l + (r - l) / 2)) r = m; else l = m;
+		return r;
+	}
+
+	/// <summary>
+	/// 条件 f を満たす最後の値を指定された誤差の範囲内で探索します。
+	/// (l, x] 上で true、(x, r) 上で false となる x を返します。
+	/// r 近傍で true のとき、r を返します。
+	/// l 近傍で false のとき、l を返します。
+	/// </summary>
+	/// <param name="f">開区間 (l, r) 上で定義される条件。</param>
+	/// <param name="l">探索範囲の下限。</param>
+	/// <param name="r">探索範囲の上限。</param>
+	/// <param name="digits">誤差を表す小数部の桁数。</param>
+	/// <returns>条件 f を満たす最後の値。</returns>
+	public static double Last(Func<double, bool> f, double l, double r, int digits = 9)
+	{
+		double m;
+		while (Math.Round(r - l, digits) > 0) if (f(m = r - (r - l) / 2)) l = m; else r = m;
+		return l;
+	}
+
 	// Array.BinarySearch メソッドと異なる点: 一致する値が複数存在する場合は先頭の番号。
 	static int Index(IList<int> a, int v)
 	{
-		var r = Search(i => a[i] >= v, 0, a.Count);
+		var r = First(i => a[i] >= v, 0, a.Count);
 		return r < a.Count && a[r] == v ? r : ~r;
 	}
 
 	// 挿入先の番号を求めます。値が重複する場合は最後尾に挿入するときの番号です。すべて正の値です。
-	static int IndexForInsert(IList<int> a, int v) => Search(i => a[i] > v, 0, a.Count);
-
-	// 条件を満たす先頭の番号を探索します。
-	// f に r は渡されません。
-	static int Search(Func<int, bool> f, int l, int r)
-	{
-		int m;
-		while (l < r) if (f(m = (l + r - 1) / 2)) r = m; else l = m + 1;
-		return r;
-	}
+	static int IndexForInsert(IList<int> a, int v) => First(i => a[i] > v, 0, a.Count);
 
 	// Array.BinarySearch メソッドと異なる点: 一致する値が複数存在する場合は最後の番号。
 	static int IndexLast(IList<int> a, int v)
 	{
-		var r = SearchLast(i => a[i] <= v, -1, a.Count - 1);
+		var r = Last(i => a[i] <= v, -1, a.Count - 1);
 		return r >= 0 && a[r] == v ? r : ~(r + 1);
-	}
-
-	// 条件を満たす最後の番号を探索します。
-	// f に l は渡されません。
-	static int SearchLast(Func<int, bool> f, int l, int r)
-	{
-		int m;
-		while (l < r) if (f(m = (l + r + 1) / 2)) l = m; else r = m - 1;
-		return r;
 	}
 
 	#region Test Methods
