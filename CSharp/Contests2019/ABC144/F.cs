34b9195646f9579834157fcbcb17d3b9a5231590
Keiho Sakapon <git@saka-pon.net>
2019-12-24 01:56:42 +0900

    AC

--- CSharp/Contests2019/ABC144/F.cs
+++ CSharp/Contests2019/ABC144/F.cs
@@ -8,17 +8,37 @@ class F
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
 		var n = h[0];
-		var map = new int[h[1]].Select(_ => read()).ToLookup(r => r[0], r => r[1]);
+		var map = new int[h[1]].Select(_ => read()).ToLookup(r => r[0], r => r[1]).ToDictionary(g => g.Key, g => g.ToArray());
 
-		var dp = new double[n + 1, n];
-		for (int i = n - 1; i > 0; i--)
-			for (int j = 0; j < n; j++)
+		var m = double.MaxValue;
+		for (int k = 1; k < n; k++)
+		{
+			var dp = new double[n + 1];
+			for (int i = n - 1; i > 0; i--)
 			{
-				var es = map[i].Select(x => dp[x, j]).ToArray();
-				dp[i, j] = i == j && es.Length > 1 ?
-					(es.Sum() - es.Max()) / (es.Length - 1) + 1 :
-					es.Sum() / es.Length + 1;
+				if (i == k && map[i].Length > 1)
+				{
+					var sum = 0.0;
+					var max = 0.0;
+					foreach (var j in map[i])
+					{
+						sum += dp[j];
+						max = Math.Max(max, dp[j]);
+					}
+					dp[i] = (sum - max) / (map[i].Length - 1) + 1;
+				}
+				else
+				{
+					var sum = 0.0;
+					foreach (var j in map[i])
+					{
+						sum += dp[j];
+					}
+					dp[i] = sum / map[i].Length + 1;
+				}
 			}
-		Console.WriteLine(Enumerable.Range(1, n - 1).Min(j => dp[1, j]));
+			m = Math.Min(m, dp[1]);
+		}
+		Console.WriteLine(m);
 	}
 }
