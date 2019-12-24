6c530f7ca3da4aa0f494736b23dafff6c1e61adb
Keiho Sakapon <git@saka-pon.net>
2019-12-25 00:08:35 +0900

    WA

--- CSharp/Contests2019/Xmas2019/A.cs
+++ CSharp/Contests2019/Xmas2019/A.cs
@@ -1,16 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 class A
 {
+	enum Location
+	{
+		Up, Down, Left, Right
+	}
+
+	struct Drawing
+	{
+		public Location Location;
+		public int Begin, End;
+	}
+
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var dirPath = @"signboard_t1\pieces";
+		if (!Directory.Exists(dirPath)) return;
 
-		Console.WriteLine(string.Join(" ", h));
+		var pieces = Enumerable.Range(1, 64)
+			.Select(i => Path.Combine(dirPath, $"{i}.txt"))
+			.Select(p => File.ReadAllLines(p))
+			.ToArray();
+		var drawings = pieces.Select(GetDrawings).ToArray();
+
+		// 左上角
+		var lus = drawings.Where(ds => ds.All(d => d.Location != Location.Up && d.Location != Location.Left)).ToArray();
+	}
+
+	static Drawing[] GetDrawings(string[] lines)
+	{
+		var r = new List<Drawing>();
+		r.AddRange(GetDrawings(lines[0].ToCharArray(), Location.Up));
+		r.AddRange(GetDrawings(lines[31].ToCharArray(), Location.Down));
+		r.AddRange(GetDrawings(Enumerable.Range(0, 32).Select(i => lines[i][0]).ToArray(), Location.Left));
+		r.AddRange(GetDrawings(Enumerable.Range(0, 32).Select(i => lines[i][31]).ToArray(), Location.Right));
+		return r.ToArray();
+	}
+
+	static IEnumerable<Drawing> GetDrawings(char[] cs, Location l)
+	{
+		var begins = Enumerable.Range(0, 32).Where(i => cs[i] == '#' && (i == 0 || cs[i - 1] == '.')).ToArray();
+		var ends = Enumerable.Range(0, 32).Where(i => cs[i] == '#' && (i == 31 || cs[i + 1] == '.')).ToArray();
+		return Enumerable.Range(0, begins.Length).Select(i => new Drawing
+		{
+			Location = l,
+			Begin = begins[i],
+			End = ends[i],
+		});
 	}
 }
--- CSharp/Contests2019/Xmas2019/B.cs
+++ CSharp/Contests2019/Xmas2019/B.cs
@@ -1,16 +1,51 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 class B
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var sw = Stopwatch.StartNew();
 
-		Console.WriteLine(string.Join(" ", h));
+		var h = Console.ReadLine().Split();
+		var n = int.Parse(h[0]);
+		var op = h[1];
+
+		var random = new Random();
+		var empty = new int[2 * n];
+
+		if (op == "=")
+		{
+			Console.WriteLine(string.Join(" ", Enumerable.Range(0, n)));
+		}
+		else if (op == "<")
+		{
+			if (n <= 2) { Console.WriteLine("Merry Christmas!"); return; }
+
+			var n2 = n * 10;
+			while (sw.ElapsedMilliseconds < 1900)
+			{
+				var x = empty.Select(_ => random.Next(n2)).Distinct().Take(n).ToArray();
+				var sl = x.SelectMany(a => x.Select(b => a + b)).Distinct().Count();
+				var dl = x.SelectMany(a => x.Select(b => a - b)).Distinct().Count();
+				if (sl < dl) { Console.WriteLine(string.Join(" ", x.OrderBy(v => v))); return; }
+			}
+			Console.WriteLine("Merry Christmas!");
+		}
+		else
+		{
+			if (n <= 7) { Console.WriteLine("Merry Christmas!"); return; }
+
+			var n2 = n * 3;
+			while (sw.ElapsedMilliseconds < 1800)
+			{
+				var x = empty.Select(_ => random.Next(n2)).Distinct().Take(n).ToArray();
+				var sl = x.SelectMany(a => x.Select(b => a + b)).Distinct().Count();
+				var dl = x.SelectMany(a => x.Select(b => a - b)).Distinct().Count();
+				if (sl > dl) { Console.WriteLine(string.Join(" ", x.OrderBy(v => v))); return; }
+			}
+			Console.WriteLine("Merry Christmas!");
+		}
 	}
 }
