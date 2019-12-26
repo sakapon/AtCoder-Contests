0090feb17828a4ad09760469b48b861cebf92d64
Keiho Sakapon <git@saka-pon.net>
2019-12-26 13:33:23 +0900

    AC

--- CSharp/Contests2019/Paken2019Day3/C.cs
+++ CSharp/Contests2019/Paken2019Day3/C.cs
@@ -5,12 +5,19 @@ class C
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var h = read();
+		int n = h[0], m = h[1];
+		var a = new int[n].Select(_ => read()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var M = 0L;
+		for (int j = 0; j < m; j++)
+			for (int k = j + 1; k < m; k++)
+			{
+				var sum = 0L;
+				for (int i = 0; i < n; i++) sum += Math.Max(a[i][j], a[i][k]);
+				M = Math.Max(M, sum);
+			}
+		Console.WriteLine(M);
 	}
 }
--- CSharp/Contests2019/Paken2019Day3/D.cs
+++ CSharp/Contests2019/Paken2019Day3/D.cs
@@ -5,14 +5,21 @@ class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var s = new int[5].Select(_ => Console.ReadLine()).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var d = new int[n + 1, 3];
+		for (int i = 0; i < n; i++)
+		{
+			d[i + 1, 0] = Enumerable.Range(0, 5).Count(j => s[j][i] != 'B');
+			d[i + 1, 1] = Enumerable.Range(0, 5).Count(j => s[j][i] != 'W');
+			d[i + 1, 2] = Enumerable.Range(0, 5).Count(j => s[j][i] != 'R');
+		}
+
+		var dp = new int[n + 1, 3];
+		for (int i = 1; i <= n; i++)
+			for (int k = 0; k < 3; k++)
+				dp[i, k] = Math.Min(dp[i - 1, (k + 1) % 3], dp[i - 1, (k + 2) % 3]) + d[i, k];
+		Console.WriteLine(Enumerable.Range(0, 3).Min(k => dp[n, k]));
 	}
 }
