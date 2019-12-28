76fba8bc688bcbada6409bc1fe09dfb9241cd029
Keiho Sakapon <git@saka-pon.net>
2019-12-28 23:49:50 +0900

    AC

--- CSharp/Contests2019/AGC041/A.cs
+++ CSharp/Contests2019/AGC041/A.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Linq;
 
 class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
-
-		Console.WriteLine(string.Join(" ", h));
+		var h = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		long n = h[0], a = h[1], b = h[2], d = b - a;
+		Console.WriteLine(d % 2 == 0 ? d / 2 : Math.Min(a, n - b + 1) + (d - 1) / 2);
 	}
 }
--- CSharp/Contests2019/AGC041/B.cs
+++ CSharp/Contests2019/AGC041/B.cs
@@ -5,12 +5,29 @@ class B
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var h = read();
+		int n = h[0], m = h[1], v = h[2], p = h[3];
+		var a = read().OrderBy(x => -x).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var s = new long[n + 1];
+		for (int i = 0; i < n; i++) s[i + 1] = s[i] + a[i];
+
+		Console.WriteLine(1 + Last(0, n - 1, i =>
+		{
+			long x = a[i] + m;
+			long v2 = v - p - n + 1 + i;
+
+			if (x < a[p - 1]) return false;
+			if (v2 <= 0) return true;
+			return s[i] - s[p - 1] + m * v2 <= x * (i - p + 1);
+		}));
+	}
+
+	static int Last(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
 	}
 }
