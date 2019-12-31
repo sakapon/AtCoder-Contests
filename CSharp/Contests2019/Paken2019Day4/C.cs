6ab09ac3aefbb307a8e38b9a87bcf6c7dcf7d484
Keiho Sakapon <git@saka-pon.net>
2020-01-01 03:48:45 +0900

    AC

--- CSharp/Contests2019/Paken2019Day4/C.cs
+++ CSharp/Contests2019/Paken2019Day4/C.cs
@@ -5,12 +5,11 @@ class C
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
-
-		Console.WriteLine(string.Join(" ", h));
+		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var p1 = read();
+		var p2 = read();
+		var p3 = read();
+		int c1 = p1[1] - p1[0] + 1, c2 = p2[1] - p2[0] + 1, c3 = p3[1] - p3[0] + 1;
+		Console.WriteLine(Enumerable.Range(p1[0], c1).Sum(x => (double)Math.Max(0, p2[1] - Math.Max(x, p2[0] - 1)) / c2 * Math.Max(0, p3[1] - Math.Max(x, p3[0] - 1)) / c3) / c1);
 	}
 }
--- CSharp/Contests2019/Paken2019Day4/D.cs
+++ CSharp/Contests2019/Paken2019Day4/D.cs
@@ -5,14 +5,14 @@ class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		int n = h[0], x = h[1];
 
-		Console.WriteLine(string.Join(" ", a));
+		var k = Enumerable.Range(1, n).TakeWhile(i => i * (i + 1) / 2 <= x).LastOrDefault();
+		var q = Enumerable.Repeat(1, k);
+		if (k < n) q = q.Concat(new[] { k + 1 - (x - k * (k + 1) / 2) }).Concat(Enumerable.Repeat(k + 1, n - k - 1));
+
+		Console.WriteLine(k);
+		Console.WriteLine(string.Join(" ", q));
 	}
 }
