4f34d0306d273320aae666460ff9234f7ade6268
Keiho Sakapon <git@saka-pon.net>
2020-01-01 04:18:30 +0900

    Improve code

--- CSharp/Contests2019/Paken2019Day4/C.cs
+++ CSharp/Contests2019/Paken2019Day4/C.cs
@@ -9,7 +9,8 @@ class C
 		var p1 = read();
 		var p2 = read();
 		var p3 = read();
-		int c1 = p1[1] - p1[0] + 1, c2 = p2[1] - p2[0] + 1, c3 = p3[1] - p3[0] + 1;
-		Console.WriteLine(Enumerable.Range(p1[0], c1).Sum(x => (double)Math.Max(0, p2[1] - Math.Max(x, p2[0] - 1)) / c2 * Math.Max(0, p3[1] - Math.Max(x, p3[0] - 1)) / c3) / c1);
+
+		Func<int[], int, double> high = (p, x) => (double)Math.Max(0, p[1] - Math.Max(x, p[0] - 1)) / (p[1] - p[0] + 1);
+		Console.WriteLine(Enumerable.Range(p1[0], p1[1] - p1[0] + 1).Average(x => high(p2, x) * high(p3, x)));
 	}
 }
--- CSharp/Contests2019/Paken2019Day4/D.cs
+++ CSharp/Contests2019/Paken2019Day4/D.cs
@@ -9,10 +9,10 @@ class D
 		int n = h[0], x = h[1];
 
 		var k = Enumerable.Range(1, n).TakeWhile(i => i * (i + 1) / 2 <= x).LastOrDefault();
-		var q = Enumerable.Repeat(1, k);
-		if (k < n) q = q.Concat(new[] { k + 1 - (x - k * (k + 1) / 2) }).Concat(Enumerable.Repeat(k + 1, n - k - 1));
+		var a = Enumerable.Repeat(1, k).Concat(Enumerable.Repeat(k + 1, n - k)).ToArray();
+		if (k < n) a[k] = k + 1 - (x - k * (k + 1) / 2);
 
 		Console.WriteLine(k);
-		Console.WriteLine(string.Join(" ", q));
+		Console.WriteLine(string.Join(" ", a));
 	}
 }
