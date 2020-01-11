f0a7bec96f121e6f7c63a750a892b8f96c0f9ad2
Keiho Sakapon <git@saka-pon.net>
2020-01-11 22:27:25 +0900

    Partial AC

--- CSharp/Contests2020/ARC0111/B.cs
+++ CSharp/Contests2020/ARC0111/B.cs
@@ -5,12 +5,23 @@ class B
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
+		var M = 1000000007;
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var x = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var f = new long[n + 1];
+		f[0] = 1;
+		for (int i = 0; i < n; i++)
+			f[i + 1] = f[i] * (i + 1) % M;
+
+		var dp = new long[n, n];
+		for (int i = 1; i < n; i++)
+			for (int j = 1; j <= i; j++)
+				dp[i, j] = (j * dp[i - 1, j - 1] + (i - j) * dp[i - 1, j] + f[i - 1]) % M;
+
+		var r = 0L;
+		for (int i = 1; i < n; i++)
+			r = (r + dp[n - 1, i] * (x[i] - x[i - 1])) % M;
+		Console.WriteLine(r);
 	}
 }
