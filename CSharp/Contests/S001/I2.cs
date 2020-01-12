866fcfa808d1ab75debc7db07fac33e166431b47
Keiho Sakapon <git@saka-pon.net>
2020-01-12 17:12:23 +0900

    Other solution

---
+++ CSharp/Contests/S001/I2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class I2
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+		int c = 0, s = 0;
+		for (int l = 0, r = -1; l < n; l++)
+		{
+			while (s < n && r < n - 1) s += a[++r];
+			if (s == n) c++;
+			s -= a[l];
+		}
+		Console.WriteLine(c);
+	}
+}
