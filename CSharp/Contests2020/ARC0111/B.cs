e987fb17b370ca3a47ecd157a7f6b934a30c8872
Keiho Sakapon <git@saka-pon.net>
2020-01-12 16:25:54 +0900

    AC

--- CSharp/Contests2020/ARC0111/B.cs
+++ CSharp/Contests2020/ARC0111/B.cs
@@ -1,27 +1,46 @@
 ﻿using System;
-using System.Linq;
 
 class B
 {
 	static void Main()
 	{
-		var M = 1000000007;
 		var n = int.Parse(Console.ReadLine());
-		var x = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		var x = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-		var f = new long[n + 1];
-		f[0] = 1;
-		for (int i = 0; i < n; i++)
-			f[i + 1] = f[i] * (i + 1) % M;
-
-		var dp = new long[n, n];
+		MInt f = MInt.MFactorial(n - 1);
+		var dp = new MInt[n];
 		for (int i = 1; i < n; i++)
-			for (int j = 1; j <= i; j++)
-				dp[i, j] = (j * dp[i - 1, j - 1] + (i - j) * dp[i - 1, j] + f[i - 1]) % M;
+			dp[i] = dp[i - 1] + f / i;
 
-		var r = 0L;
+		MInt r = 0;
 		for (int i = 1; i < n; i++)
-			r = (r + dp[n - 1, i] * (x[i] - x[i - 1])) % M;
-		Console.WriteLine(r);
+			r += dp[i] * (x[i] - x[i - 1]);
+		Console.WriteLine(r.V);
 	}
+}
+
+struct MInt
+{
+	const int M = 1000000007;
+	public long V;
+	public MInt(long v) { V = (v %= M) < 0 ? v + M : v; }
+
+	public static implicit operator MInt(long v) => new MInt(v);
+	public static MInt operator +(MInt x, MInt y) => x.V + y.V;
+	public static MInt operator -(MInt x, MInt y) => x.V - y.V;
+	public static MInt operator *(MInt x, MInt y) => x.V * y.V;
+	public static MInt operator /(MInt x, MInt y) => x * y.Inv();
+
+	public MInt Inv() => MPow(V, M - 2);
+
+	static long MPow(long b, int i)
+	{
+		for (var r = 1L; ; b = b * b % M)
+		{
+			if (i % 2 > 0) r = r * b % M;
+			if ((i /= 2) < 1) return r;
+		}
+	}
+
+	public static long MFactorial(int n) { for (long x = 1, i = 1; ; x = x * ++i % M) if (i >= n) return x; }
 }
