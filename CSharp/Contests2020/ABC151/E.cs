c3d35dbf60a33dc9db343788ff2f716aeda7400b
Keiho Sakapon <git@saka-pon.net>
2020-01-13 10:19:19 +0900

    AC

--- CSharp/Contests2020/ABC151/E.cs
+++ CSharp/Contests2020/ABC151/E.cs
@@ -5,14 +5,45 @@ class E
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var a = read();
+		Array.Sort(a);
+		int n = h[0], k = h[1];
 
-		Console.WriteLine(string.Join(" ", a));
+		MInt M = 0, m = 0, c = 1;
+		for (int i = k; i <= n; i++)
+		{
+			M += c * a[i - 1];
+			m += c * a[n - i];
+
+			c *= i;
+			c /= i - k + 1;
+		}
+		Console.WriteLine((M - m).V);
+	}
+}
+
+struct MInt
+{
+	const int M = 1000000007;
+	public long V;
+	public MInt(long v) { V = (v %= M) < 0 ? v + M : v; }
+
+	public static implicit operator MInt(long v) => new MInt(v);
+	public static MInt operator +(MInt x, MInt y) => x.V + y.V;
+	public static MInt operator -(MInt x, MInt y) => x.V - y.V;
+	public static MInt operator *(MInt x, MInt y) => x.V * y.V;
+	public static MInt operator /(MInt x, MInt y) => x * y.Inv();
+
+	public MInt Inv() => MPow(V, M - 2);
+
+	static long MPow(long b, int i)
+	{
+		for (var r = 1L; ; b = b * b % M)
+		{
+			if (i % 2 > 0) r = r * b % M;
+			if ((i /= 2) < 1) return r;
+		}
 	}
 }
