e2c9a75bc9244da0e663ee8eed2380748c41c563
Keiho Sakapon <git@saka-pon.net>
2020-01-14 00:47:02 +0900

    Partial AC

--- CSharp/Contests2019/JOI2020yo2/D.cs
+++ CSharp/Contests2019/JOI2020yo2/D.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var d = new[]
+		{
+			"1234345456",
+			"2123234345",
+			"3212323434",
+			"4321432543",
+			"3234123234",
+			"4323212323",
+			"5432321432",
+			"4345234123",
+			"5434323212",
+			"6543432321",
+		};
+		map = Enumerable.Range(0, 10).ToDictionary(i => (char)('0' + i), i => Enumerable.Range(0, 10).ToDictionary(j => (char)('0' + j), j => d[i][j] - '0'));
 
-		Console.WriteLine(string.Join(" ", a));
+		var h = Console.ReadLine().Split().Select(long.Parse).ToArray();
+		long m = h[0], r = h[1];
+
+		var c = Count(r);
+		for (int k = 0; k < 1000000; k++)
+		{
+			r += m;
+			c = Math.Min(c, Count(r));
+		}
+		Console.WriteLine(c);
+	}
+
+	static Dictionary<char, Dictionary<char, int>> map;
+
+	static int Count(long n)
+	{
+		var s = $"0{n}";
+		return Enumerable.Range(0, s.Length - 1).Sum(i => map[s[i]][s[i + 1]]);
 	}
 }
