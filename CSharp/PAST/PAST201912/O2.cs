14525e5710416b5f101750e626b3d67559d80e02
Keiho Sakapon <git@saka-pon.net>
2020-01-16 10:37:22 +0900

    Other AC

---
+++ CSharp/PAST/PAST201912/O2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class O2
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Enumerable.Range(0, n).SelectMany(d => Console.ReadLine().Split().Select(s => new { d, x = int.Parse(s) })).OrderBy(_ => _.x).ToArray();
+
+		var e = new double[n];
+		var M = 1.0;
+		for (int i = 6 * n - 1; i >= 0; i--)
+			M = Math.Max(M, (e[a[i].d] += M / 6) + 1);
+		Console.WriteLine(M);
+	}
+}
