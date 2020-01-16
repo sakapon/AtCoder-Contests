f2bffa87eb37cfef49bd7ff4e682fb86145cc5d6
Keiho Sakapon <git@saka-pon.net>
2020-01-16 17:53:26 +0900

    AC

--- CSharp/Contests2019/JOI2020yo2/D.cs
+++ CSharp/Contests2019/JOI2020yo2/D.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class D
@@ -19,25 +18,32 @@ class D
 			"5434323212",
 			"6543432321",
 		};
-		map = Enumerable.Range(0, 10).ToDictionary(i => (char)('0' + i), i => Enumerable.Range(0, 10).ToDictionary(j => (char)('0' + j), j => d[i][j] - '0'));
 
-		var h = Console.ReadLine().Split().Select(long.Parse).ToArray();
-		long m = h[0], r = h[1];
+		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		int m = h[0], r = h[1];
 
-		var c = Count(r);
-		for (int k = 0; k < 1000000; k++)
-		{
-			r += m;
-			c = Math.Min(c, Count(r));
-		}
-		Console.WriteLine(c);
-	}
+		var max = 30;
+		var dp = new int[max, 10, m];
+		for (int i = 0; i < max; i++)
+			for (int j = 0; j < 10; j++)
+				for (int k = 0; k < m; k++)
+					dp[i, j, k] = max;
+		dp[0, 0, 0] = 0;
 
-	static Dictionary<char, Dictionary<char, int>> map;
-
-	static int Count(long n)
-	{
-		var s = $"0{n}";
-		return Enumerable.Range(0, s.Length - 1).Sum(i => map[s[i]][s[i + 1]]);
+		var min = 30;
+		for (int i = 0; i < max - 1; i++)
+			for (int j = 0; j < 10; j++)
+				for (int k = 0; k < m; k++)
+				{
+					if (dp[i, j, k] >= min) continue;
+					for (int nj = 0; nj < 10; nj++)
+					{
+						var nk = (10 * k + nj) % m;
+						var v = dp[i, j, k] + (d[j][nj] - '0');
+						dp[i + 1, nj, nk] = Math.Min(dp[i + 1, nj, nk], v);
+						if (nk == r) min = Math.Min(min, v);
+					}
+				}
+		Console.WriteLine(min);
 	}
 }
