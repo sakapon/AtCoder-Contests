0cf5e741d52b08614dbd260419c5e6fb2045e96f
Keiho Sakapon <git@saka-pon.net>
2020-01-16 12:49:47 +0900

    Shorten code

--- CSharp/PAST/PAST201912/O2.cs
+++ CSharp/PAST/PAST201912/O2.cs
@@ -6,12 +6,10 @@ class O2
 	static void Main()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var a = Enumerable.Range(0, n).SelectMany(d => Console.ReadLine().Split().Select(s => new { d, x = int.Parse(s) })).OrderBy(_ => _.x).ToArray();
-
 		var e = new double[n];
 		var M = 1.0;
-		for (int i = 6 * n - 1; i >= 0; i--)
-			M = Math.Max(M, (e[a[i].d] += M / 6) + 1);
+		foreach (var _ in Enumerable.Range(0, n).SelectMany(d => Console.ReadLine().Split().Select(s => new { d, x = int.Parse(s) })).OrderBy(_ => -_.x))
+			M = Math.Max(M, (e[_.d] += M / 6) + 1);
 		Console.WriteLine(M);
 	}
 }
