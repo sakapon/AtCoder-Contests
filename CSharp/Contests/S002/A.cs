04d4efa6e32ef1c6f405b2b5e2b78ce44e44a8c4
Keiho Sakapon <git@saka-pon.net>
2020-01-19 23:19:46 +0900

    Initialize

---
+++ CSharp/Contests/S002/A.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+class A
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+	}
+}
