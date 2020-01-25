7e6ee4bfceef424b79bc220e496ff7559c0ccb42
Keiho Sakapon <git@saka-pon.net>
2020-01-25 23:51:38 +0900

    Add functions

--- CSharp/CoderLib/CoderLibTest/PriorityQueueTest.cs
+++ CSharp/CoderLib/CoderLibTest/PriorityQueueTest.cs
@@ -5,17 +5,31 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 class PQ<T> : List<T>
 {
+	public static PQ<T> CreateDesc(T[] vs)
+	{
+		var c = Comparer<T>.Default;
+		return new PQ<T>(vs, (x, y) => c.Compare(y, x));
+	}
+
+	public static PQ<T> Create<TKey>(T[] vs, Func<T, TKey> getKey, bool desc = false)
+	{
+		var c = Comparer<TKey>.Default;
+		return desc ?
+			new PQ<T>(vs, (x, y) => c.Compare(getKey(y), getKey(x))) :
+			new PQ<T>(vs, (x, y) => c.Compare(getKey(x), getKey(y)));
+	}
+
 	Comparison<T> _c;
 	public T First => this[0];
 
-	public PQ(IEnumerable<T> vs = null, Comparison<T> c = null)
+	public PQ(T[] vs = null, Comparison<T> c = null)
 	{
 		_c = c ?? Comparer<T>.Default.Compare;
 		if (vs != null) foreach (var v in vs) Push(v);
 	}
 
 	void Swap(int i, int j) { var o = this[i]; this[i] = this[j]; this[j] = o; }
-	void UpHeap(int i) { for (int j; i > 0 && _c(this[(j = (i - 1) / 2)], this[i]) > 0; Swap(i, j), i = j) ; }
+	void UpHeap(int i) { for (int j; i > 0 && _c(this[j = (i - 1) / 2], this[i]) > 0; Swap(i, j), i = j) ; }
 	void DownHeap(int i)
 	{
 		for (int j; (j = 2 * i + 1) < Count; i = j)
@@ -69,13 +83,14 @@ public class PriorityQueueTest
 	public void SortDescending()
 	{
 		var values = Enumerable.Range(0, 100000).Select(i => random.Next(100000)).ToArray();
-		var actual = TestHelper.MeasureTime(() => new PQ<int>(values, (x, y) => -x.CompareTo(y)));
+		var actual = TestHelper.MeasureTime(() => PQ<int>.CreateDesc(values));
 		var a = new int[values.Length];
 		TestHelper.MeasureTime(() => { for (var i = 0; i < a.Length; i++) a[i] = actual.Pop(); });
 		var e = TestHelper.MeasureTime(() => values.OrderByDescending(x => x).ToArray());
 		CollectionAssert.AreEqual(e, a);
 	}
 
+	// 少しだけ取り出す場合、PQ を使うほうが速いです。
 	[TestMethod]
 	public void SortTake()
 	{
