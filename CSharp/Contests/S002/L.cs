406b75f0d90a47123f97ceb71a903e622f1f993e
Keiho Sakapon <git@saka-pon.net>
2020-01-28 23:20:31 +0900

    Improve code

--- CSharp/Contests/S002/L.cs
+++ CSharp/Contests/S002/L.cs
@@ -7,14 +7,14 @@ class L
 	{
 		var n = int.Parse(Console.ReadLine());
 		var q = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray())
-			.GroupBy(p => p.Max(), p => p.Min())
-			.OrderBy(g => g.Key)
-			.SelectMany(g => g.OrderBy(y => -y));
+			.Select(p => p[0] < p[1] ? new[] { p[1], p[0] } : p)
+			.OrderBy(p => p[0])
+			.ThenBy(p => -p[1]);
 
 		var lis = Enumerable.Range(1 << 30, n).ToArray();
 		int j;
-		foreach (var y in q)
-			lis[(j = Array.BinarySearch(lis, y)) < 0 ? ~j : j] = y;
+		foreach (var p in q)
+			lis[(j = Array.BinarySearch(lis, p[1])) < 0 ? ~j : j] = p[1];
 		Console.WriteLine(~Array.BinarySearch(lis, 1 << 30));
 	}
 }
