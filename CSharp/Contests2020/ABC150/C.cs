0bf6e7c7ae0b97b29c4f5db3c19faae2c9b86a6b
Keiho Sakapon <git@saka-pon.net>
2020-01-29 23:59:33 +0900

    AC

--- CSharp/Contests2020/ABC150/C.cs
+++ CSharp/Contests2020/ABC150/C.cs
@@ -3,14 +3,32 @@ using System.Linq;
 
 class C
 {
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		n = int.Parse(Console.ReadLine());
+		f = new int[n + 1];
+		f[0] = 1;
+		for (int i = 1; i <= n; i++)
+			f[i] = i * f[i - 1];
 
-		Console.WriteLine(string.Join(" ", h));
+		var p = GetOrder(Read());
+		var q = GetOrder(Read());
+		Console.WriteLine(Math.Abs(p - q));
+	}
+
+	static int n;
+	static int[] f;
+	static int GetOrder(int[] p)
+	{
+		var pi = 0;
+		var pl = Enumerable.Range(1, n).ToList();
+		for (int i = 0; i < n; i++)
+		{
+			var li = pl.IndexOf(p[i]);
+			pi += li * f[n - 1 - i];
+			pl.RemoveAt(li);
+		}
+		return pi;
 	}
 }
--- CSharp/Contests2020/ABC150/D.cs
+++ CSharp/Contests2020/ABC150/D.cs
@@ -3,16 +3,24 @@ using System.Linq;
 
 class D
 {
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var h = Read();
+		int n = h[0], m = h[1];
+		var a = Read().Distinct().Select(x => x / 2).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		long lcm = a[0];
+		for (int i = 1; i < a.Length; i++)
+		{
+			lcm = Lcm(lcm, a[i]);
+			if (lcm > m) { Console.WriteLine(0); return; }
+		}
+
+		if (a.Any(x => lcm / x % 2 == 0)) { Console.WriteLine(0); return; }
+		Console.WriteLine((int)((m / lcm + 1) / 2));
 	}
+
+	static long Gcd(long x, long y) { for (long r; (r = x % y) > 0; x = y, y = r) ; return y; }
+	static long Lcm(long x, long y) => x / Gcd(x, y) * y;
 }
