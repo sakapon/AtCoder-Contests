db1dda97936ecf2797783eedca9692fd50d53efa
Keiho Sakapon <git@saka-pon.net>
2020-01-30 23:10:27 +0900

    AC

---
+++ CSharp/Contests/S002/K.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class K
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+
+		var d = a.SelectMany(x => x).Distinct().OrderBy(x => x).Select((x, i) => new { x, i }).ToDictionary(_ => _.x, _ => _.i);
+		var u = new int[d.Count];
+		var uf = new UF(d.Count);
+
+		foreach (var x in a)
+		{
+			uf.Unite(d[x[0]], d[x[1]]);
+			u[d[x[0]]]++;
+		}
+		Console.WriteLine(uf.ToGroups().Sum(g => Math.Min(g.Length, g.Sum(x => u[x]))));
+	}
+}
+
+class UF
+{
+	int[] p;
+	public UF(int n) { p = Enumerable.Range(0, n).ToArray(); }
+
+	public void Unite(int a, int b) { if (!AreUnited(a, b)) p[p[b]] = p[a]; }
+	public bool AreUnited(int a, int b) => GetRoot(a) == GetRoot(b);
+	int GetRoot(int a) => p[a] == a ? a : p[a] = GetRoot(p[a]);
+	public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
+}
---
+++ CSharp/Contests/S002/K2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class K2
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+
+		var d = a.SelectMany(x => x).Distinct().OrderBy(x => x).Select((x, i) => new { x, i }).ToDictionary(_ => _.x, _ => _.i);
+		map = a.Concat(a.Select(x => new[] { x[1], x[0] })).GroupBy(x => d[x[0]], x => d[x[1]]).ToDictionary(g => g.Key, g => g.ToArray());
+		u = new bool[d.Count];
+
+		var r = 0;
+		for (int i = 0; i < d.Count; i++)
+		{
+			if (u[i]) continue;
+			c = 0;
+			closed = false;
+			Dfs(i, -1);
+			r += closed ? c : c - 1;
+		}
+		Console.WriteLine(r);
+	}
+
+	static Dictionary<int, int[]> map;
+	static bool[] u;
+	static int c;
+	static bool closed;
+	static void Dfs(int p, int p0)
+	{
+		u[p] = true;
+		++c;
+		if (!map.ContainsKey(p)) return;
+		foreach (var x in map[p])
+		{
+			if (x == p0) continue;
+			if (u[x]) { closed = true; continue; }
+			Dfs(x, p);
+		}
+	}
+}
