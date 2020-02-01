c5c2f49ea7f4584604a389be955064f6b9354f23
Keiho Sakapon <git@saka-pon.net>
2020-02-01 11:22:26 +0900

    AC

--- CSharp/Contests2020/ABC150/E.cs
+++ CSharp/Contests2020/ABC150/E.cs
@@ -3,16 +3,28 @@ using System.Linq;
 
 class E
 {
-	static void Main()
-	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+	static void Main() => Console.WriteLine(((MInt)2).Pow(2 * int.Parse(Console.ReadLine()) - 2) * Console.ReadLine().Split().Select(int.Parse).OrderBy(x => -x).Select((c, i) => (MInt)(i + 2) * c).Aggregate((x, y) => x + y));
+}
 
-		Console.WriteLine(string.Join(" ", a));
+struct MInt
+{
+	const int M = 1000000007;
+	public long V;
+	public MInt(long v) { V = (v %= M) < 0 ? v + M : v; }
+
+	public static implicit operator MInt(long v) => new MInt(v);
+	public static MInt operator +(MInt x, MInt y) => x.V + y.V;
+	public static MInt operator *(MInt x, MInt y) => x.V * y.V;
+
+	public MInt Pow(int i) => MPow(V, i);
+	public override string ToString() => $"{V}";
+
+	static long MPow(long b, int i)
+	{
+		for (var r = 1L; ; b = b * b % M)
+		{
+			if (i % 2 > 0) r = r * b % M;
+			if ((i /= 2) < 1) return r;
+		}
 	}
 }
