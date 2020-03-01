a2062daaf6450cdc1fc8434bd6bcfb6581bb782f
Keiho Sakapon <git@saka-pon.net>
2020-03-02 00:07:19 +0900

    Use factorization

--- CSharp/Contests2019/ABC142/D.cs
+++ CSharp/Contests2019/ABC142/D.cs
@@ -7,25 +7,19 @@ class D
 	static void Main()
 	{
 		var h = Console.ReadLine().Split().Select(long.Parse).ToArray();
-		var g = Gcd(h[0], h[1]);
-
-		var l = new List<long>();
-		foreach (var d in Divisors(g).Skip(1)) if (l.TrueForAll(x => d % x != 0)) l.Add(d);
-		Console.WriteLine(l.Count + 1);
+		Console.WriteLine(Factorize(Gcd(h[0], h[1])).Distinct().Count() + 1);
 	}
 
 	static long Gcd(long x, long y) { for (long r; (r = x % y) > 0; x = y, y = r) ; return y; }
 
-	static long[] Divisors(long v)
+	static long[] Factorize(long n)
 	{
-		var d = new List<long>();
-		var c = 0;
-		for (long i = 1, j, rv = (long)Math.Sqrt(v); i <= rv; i++)
-			if (v % i == 0)
-			{
-				d.Insert(c, i);
-				if ((j = v / i) != i) d.Insert(++c, j);
-			}
-		return d.ToArray();
+		long rn = (long)Math.Ceiling(Math.Sqrt(n)), x = 2;
+		var r = new List<long>();
+		while (n % x == 0) { r.Add(x); n /= x; }
+		for (x++; x <= rn && n > 1; x += 2)
+			while (n % x == 0) { r.Add(x); n /= x; }
+		if (n > 1) r.Add(n);
+		return r.ToArray();
 	}
 }
