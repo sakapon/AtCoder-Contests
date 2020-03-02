63697515d35a0346af885c790c88542b097d3697
Keiho Sakapon <git@saka-pon.net>
2020-03-02 17:59:57 +0900

    Update functions

---
+++ CSharp/CoderLib/CoderLibTest/Maths/PrimesTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoderLibTest.Maths
+{
+	[TestClass]
+	public class PrimesTest
+	{
+		// 素因数分解 O(√n)
+		// n = 1 の場合は空の配列。
+		// √n を超える素因数はたかだか 1 個であり、その次数は 1。
+		// 候補 x を 2 または奇数に限定することで高速化できます。
+		static long[] Factorize(long n)
+		{
+			var r = new List<long>();
+			for (long x = 2; x * x <= n && n > 1; ++x)
+				while (n % x == 0) { r.Add(x); n /= x; }
+			if (n > 1) r.Add(n);
+			return r.ToArray();
+		}
+
+		// すべての約数 O(√n)
+		static long[] Divisors(long n)
+		{
+			var r = new List<long>();
+			for (long x = 1; x * x <= n; ++x)
+				if (n % x == 0) r.Add(x);
+			long v;
+			for (int i = r.Count - 1; i >= 0; --i)
+				if ((v = n / r[i]) > r[i]) r.Add(v);
+			return r.ToArray();
+		}
+
+		// 素数判定 O(√n)
+		// 候補 x を 2 または奇数に限定することで高速化できます。
+		static bool IsPrime(long n)
+		{
+			for (long x = 2; x * x <= n; ++x)
+				if (n % x == 0) return false;
+			return n > 1;
+		}
+
+		#region Test Methods
+
+		[TestMethod]
+		public void Factorize()
+		{
+			CollectionAssert.AreEqual(new long[] { 2, 31 }, Factorize(62));
+			CollectionAssert.AreEqual(new long[] { 2, 2, 2, 3, 5 }, Factorize(120));
+			CollectionAssert.AreEqual(new long[] { 2, 3, 3, 3, 37 }, Factorize(1998));
+			CollectionAssert.AreEqual(new long[] { 3, 23, 29 }, Factorize(2001));
+		}
+
+		[TestMethod]
+		public void Divisors()
+		{
+			CollectionAssert.AreEqual(new long[] { 1 }, Divisors(1));
+			CollectionAssert.AreEqual(new long[] { 1, 2 }, Divisors(2));
+			CollectionAssert.AreEqual(new long[] { 1, 2, 4 }, Divisors(4));
+			CollectionAssert.AreEqual(new long[] { 1, 5 }, Divisors(5));
+			CollectionAssert.AreEqual(new long[] { 1, 3, 7, 21 }, Divisors(21));
+			CollectionAssert.AreEqual(new long[] { 1, 2, 31, 62 }, Divisors(62));
+			CollectionAssert.AreEqual(new long[] { 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120 }, Divisors(120));
+			CollectionAssert.AreEqual(new long[] { 1, 3, 23, 29, 69, 87, 667, 2001 }, Divisors(2001));
+		}
+		#endregion
+	}
+}
--- CSharp/CoderLib/CoderLibTest/PrimesTest.cs
+++ CSharp/CoderLib/CoderLibTest/PrimesTest.cs
@@ -96,75 +96,6 @@ public class PrimesTest
 		return l;
 	}
 
-	// 素因数分解 O(√n)
-	// n = 1 の場合は空の配列。
-	// √n を超える素因数はたかだか 1 個であり、その次数は 1。
-	// 候補 x を 2 または奇数に限定することで高速化します。
-	static long[] Factorize(long n)
-	{
-		long rn = (long)Math.Ceiling(Math.Sqrt(n)), x = 2;
-		var r = new List<long>();
-		while (n % x == 0) { r.Add(x); n /= x; }
-		for (x++; x <= rn && n > 1; x += 2)
-			while (n % x == 0) { r.Add(x); n /= x; }
-		if (n > 1) r.Add(n);
-		return r.ToArray();
-	}
-
-	// すべての約数 O(√n)
-	static long[] Divisors(long n)
-	{
-		long rn = (long)Math.Ceiling(Math.Sqrt(n)), v;
-		var r = new List<long>();
-		for (long x = 1; x <= rn; ++x)
-			if (n % x == 0) r.Add(x);
-		for (int i = r.Count - 1; i >= 0; --i)
-			if ((v = n / r[i]) > rn) r.Add(v);
-		return r.ToArray();
-	}
-
-	#region Lab
-
-	static IEnumerable<int> DivisorsDescending(int v)
-	{
-		var d = new List<int>();
-		for (int i = 1, j, rv = (int)Math.Sqrt(v); i <= rv; i++)
-			if (v % i == 0)
-			{
-				d.Add(i);
-				if ((j = v / i) != i) yield return j;
-			}
-		for (var i = d.Count - 1; i >= 0; i--) yield return d[i];
-	}
-
-	static long[] Divisors2(long v)
-	{
-		if (v == 1) return new[] { 1L };
-		var ps = Factorize(v).GroupBy(p => p).Select(g => new KeyValuePair<long, int>(g.Key, g.Count())).ToArray();
-		var ds = new int[ps.Length];
-		var c = ps.Aggregate(1L, (x, p) => x * (p.Value + 1));
-
-		var d = new List<long> { 1 };
-		var t = 1L;
-		for (var i = 1; i < c; i++) d.Add(t = NextDivisor(ps, ds, t, 0));
-		return d.ToArray();
-	}
-	static long NextDivisor(KeyValuePair<long, int>[] ps, int[] ds, long v, int i)
-	{
-		if (ds[i] < ps[i].Value)
-		{
-			ds[i]++;
-			return v * ps[i].Key;
-		}
-		else
-		{
-			ds[i] = 0;
-			for (int j = 0; j < ps[i].Value; j++) v /= ps[i].Key;
-			return NextDivisor(ps, ds, v, i + 1);
-		}
-	}
-	#endregion
-
 	#region Test Methods
 
 	[TestMethod]
@@ -262,28 +193,6 @@ public class PrimesTest
 		Assert.AreEqual(8363, PrimesL64(10000, 100000).Count);
 		Console.WriteLine(string.Join(" ", PrimesL64(998244300, 998244400)));
 		Console.WriteLine(string.Join(" ", PrimesL64(999999900, 1000000100)));
-	}
-
-	[TestMethod]
-	public void Factorize()
-	{
-		CollectionAssert.AreEqual(new long[] { 2, 31 }, Factorize(62));
-		CollectionAssert.AreEqual(new long[] { 2, 2, 2, 3, 5 }, Factorize(120));
-		CollectionAssert.AreEqual(new long[] { 2, 3, 3, 3, 37 }, Factorize(1998));
-		CollectionAssert.AreEqual(new long[] { 3, 23, 29 }, Factorize(2001));
-	}
-
-	[TestMethod]
-	public void Divisors()
-	{
-		CollectionAssert.AreEqual(new long[] { 1 }, Divisors(1));
-		CollectionAssert.AreEqual(new long[] { 1, 2 }, Divisors(2));
-		CollectionAssert.AreEqual(new long[] { 1, 2, 4 }, Divisors(4));
-		CollectionAssert.AreEqual(new long[] { 1, 5 }, Divisors(5));
-		CollectionAssert.AreEqual(new long[] { 1, 3, 7, 21 }, Divisors(21));
-		CollectionAssert.AreEqual(new long[] { 1, 2, 31, 62 }, Divisors(62));
-		CollectionAssert.AreEqual(new long[] { 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120 }, Divisors(120));
-		CollectionAssert.AreEqual(new long[] { 1, 3, 23, 29, 69, 87, 667, 2001 }, Divisors(2001));
 	}
 	#endregion
 }
