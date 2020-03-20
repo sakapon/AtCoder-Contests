aa7afee81df8c0869ab8ec3ade4a1c93dff19ef4
Keiho Sakapon <git@saka-pon.net>
2020-03-20 23:11:36 +0900

    Update comments

--- CSharp/CoderLib/CoderLibTest/Collections/BinarySearchTest.cs
+++ CSharp/CoderLib/CoderLibTest/Collections/BinarySearchTest.cs
@@ -13,11 +13,8 @@ namespace CoderLibTest.Collections
 		/// [l, x) 上で false、[x, r) 上で true となる x を返します。
 		/// f(l) が true のとき、l を返します。
 		/// f(r - 1) が false のとき、r を返します。
+		/// f(r) は評価されません。
 		/// </summary>
-		/// <param name="l">探索範囲の下限。</param>
-		/// <param name="r">探索範囲の上限。</param>
-		/// <param name="f">半開区間 [l, r) 上で定義される条件。</param>
-		/// <returns>条件 f を満たす最初の値。</returns>
 		static int First(int l, int r, Func<int, bool> f)
 		{
 			int m;
@@ -30,11 +27,8 @@ namespace CoderLibTest.Collections
 		/// (l, x] 上で true、(x, r] 上で false となる x を返します。
 		/// f(r) が true のとき、r を返します。
 		/// f(l + 1) が false のとき、l を返します。
+		/// f(l) は評価されません。
 		/// </summary>
-		/// <param name="l">探索範囲の下限。</param>
-		/// <param name="r">探索範囲の上限。</param>
-		/// <param name="f">半開区間 (l, r] 上で定義される条件。</param>
-		/// <returns>条件 f を満たす最後の値。</returns>
 		static int Last(int l, int r, Func<int, bool> f)
 		{
 			int m;
@@ -45,14 +39,8 @@ namespace CoderLibTest.Collections
 		/// <summary>
 		/// 条件 f を満たす最初の値を指定された誤差の範囲内で探索します。
 		/// (l, x) 上で false、[x, r) 上で true となる x を返します。
-		/// l 近傍で true のとき、l を返します。
 		/// r 近傍で false のとき、r を返します。
 		/// </summary>
-		/// <param name="l">探索範囲の下限。</param>
-		/// <param name="r">探索範囲の上限。</param>
-		/// <param name="f">開区間 (l, r) 上で定義される条件。</param>
-		/// <param name="digits">誤差を表す小数部の桁数。</param>
-		/// <returns>条件 f を満たす最初の値。</returns>
 		static double First(double l, double r, Func<double, bool> f, int digits = 9)
 		{
 			double m;
@@ -63,14 +51,8 @@ namespace CoderLibTest.Collections
 		/// <summary>
 		/// 条件 f を満たす最後の値を指定された誤差の範囲内で探索します。
 		/// (l, x] 上で true、(x, r) 上で false となる x を返します。
-		/// r 近傍で true のとき、r を返します。
 		/// l 近傍で false のとき、l を返します。
 		/// </summary>
-		/// <param name="l">探索範囲の下限。</param>
-		/// <param name="r">探索範囲の上限。</param>
-		/// <param name="f">開区間 (l, r) 上で定義される条件。</param>
-		/// <param name="digits">誤差を表す小数部の桁数。</param>
-		/// <returns>条件 f を満たす最後の値。</returns>
 		static double Last(double l, double r, Func<double, bool> f, int digits = 9)
 		{
 			double m;
