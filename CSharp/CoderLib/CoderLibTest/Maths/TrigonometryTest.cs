11d6e3a2edc0a7e8736eb89e535f71021981a2dd
Keiho Sakapon <git@saka-pon.net>
2020-04-03 14:35:47 +0900

    Add methods

---
+++ CSharp/CoderLib/CoderLibTest/Maths/TrigonometryTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoderLibTest.Maths
+{
+	[TestClass]
+	public class TrigonometryTest
+	{
+		// 余弦定理
+		static double CosA(double a, double b, double c) => (b * b + c * c - a * a) / (2 * b * c);
+		// 正弦定理
+		static double SinA(double a, double r) => a / (2 * r);
+	}
+}
