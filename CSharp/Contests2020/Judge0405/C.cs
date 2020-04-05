8713d71f4a36e9b337aa835eafa4df663a5779cb
Keiho Sakapon <git@saka-pon.net>
2020-04-06 00:15:59 +0900

    AC

--- CSharp/Contests2020/Judge0405/C.cs
+++ CSharp/Contests2020/Judge0405/C.cs
@@ -1,16 +1,33 @@
 ﻿using System;
-using System.Linq;
 
 class C
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
 		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var n = h[0] + h[1] + h[2];
 
-		Console.WriteLine(string.Join(" ", h));
+		var r = 0;
+		var p = new int[3, 3];
+		p[0, 0] = 1;
+
+		Dfs(2);
+		Console.WriteLine(r);
+
+		void Dfs(int v)
+		{
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < h[i]; j++)
+					if (p[i, j] == 0)
+					{
+						if (i > 0 && p[i - 1, j] == 0) break;
+
+						p[i, j] = v;
+						if (v < n) Dfs(v + 1);
+						else r++;
+						p[i, j] = 0;
+						break;
+					}
+		}
 	}
 }
