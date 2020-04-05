5083d7f193354fd4b827a9ede378e24854dc0f89
Keiho Sakapon <git@saka-pon.net>
2020-04-05 18:44:45 +0900

    Add functions

--- CSharp/CoderLib/CoderLibTest/Maths/TrigonometryTest.cs
+++ CSharp/CoderLib/CoderLibTest/Maths/TrigonometryTest.cs
@@ -11,5 +11,24 @@ namespace CoderLibTest.Maths
 		static double CosA(double a, double b, double c) => (b * b + c * c - a * a) / (2 * b * c);
 		// 正弦定理
 		static double SinA(double a, double r) => a / (2 * r);
+		// ヘロンの公式
+		static double Area(double a, double b, double c)
+		{
+			var s = (a + b + c) / 2;
+			return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+		}
+
+		#region Test Methods
+
+		static void AssertNearlyEqual(double expected, double actual) =>
+			Assert.AreEqual(0.0, Math.Round(expected - actual, 12));
+
+		[TestMethod]
+		public void Area()
+		{
+			AssertNearlyEqual(6 * Math.Sqrt(6), Area(5, 6, 7));
+			AssertNearlyEqual(Math.Sqrt(131) / 4, Area(Math.Sqrt(5), Math.Sqrt(7), 3));
+		}
+		#endregion
 	}
 }
