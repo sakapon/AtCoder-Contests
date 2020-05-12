5970d9675afd3303964e012b5e159fe0562c2034
Keiho Sakapon <git@saka-pon.net>
2020-05-12 16:48:17 +0900

    Other solution

---
+++ CSharp/PAST/PAST201912/D3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class D3
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var c = new int[n + 1];
+		c[0] = 1;
+		for (int i = 0; i < n; i++)
+			c[int.Parse(Console.ReadLine())]++;
+		Console.WriteLine(Array.TrueForAll(c, x => x == 1) ? "Correct" : $"{Array.IndexOf(c, 2)} {Array.IndexOf(c, 0)}");
+	}
+}
