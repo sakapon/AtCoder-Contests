fd187c68373e5a97be9ce3a268d6fe2b2c99ec7a
Keiho Sakapon <git@saka-pon.net>
2020-05-18 04:38:44 +0900

    Improve code

--- CSharp/PAST/PAST202004/A.cs
+++ CSharp/PAST/PAST202004/A.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Linq;
 
 class A
 {
 	static void Main()
 	{
-		var r9 = Enumerable.Range(1, 9);
-		var f = r9.Select(i => $"B{i}").Reverse().Concat(r9.Select(i => $"{i}F")).ToArray();
-
-		var h = Console.ReadLine().Split();
-		Console.WriteLine(Math.Abs(Array.IndexOf(f, h[0]) - Array.IndexOf(f, h[1])));
+		var h = Array.ConvertAll(Console.ReadLine().Split(), s => s[1] == 'F' ? s[0] - '1' : '0' - s[1]);
+		Console.WriteLine(Math.Abs(h[0] - h[1]));
 	}
 }
--- CSharp/PAST/PAST202004/E.cs
+++ CSharp/PAST/PAST202004/E.cs
@@ -6,13 +6,13 @@ class E
 	static void Main()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).Select(x => x - 1).ToArray();
+		var a = Console.ReadLine().Split().Select(s => int.Parse(s) - 1).ToArray();
 
 		var uf = new UF(n);
 		for (int i = 0; i < n; i++)
 			uf.Unite(i, a[i]);
-		var q = uf.ToGroups().SelectMany(g => g.Select(x => (x, c: g.Length))).OrderBy(_ => _.x).Select(_ => _.c);
-		Console.WriteLine(string.Join(" ", q));
+		var c = Enumerable.Range(0, n).GroupBy(uf.GetRoot).ToDictionary(g => g.Key, g => g.Count());
+		Console.WriteLine(string.Join(" ", Enumerable.Range(0, n).Select(i => c[uf.GetRoot(i)])));
 	}
 }
 
@@ -24,5 +24,4 @@ class UF
 	public void Unite(int a, int b) { if (!AreUnited(a, b)) p[p[b]] = p[a]; }
 	public bool AreUnited(int a, int b) => GetRoot(a) == GetRoot(b);
 	public int GetRoot(int a) => p[a] == a ? a : p[a] = GetRoot(p[a]);
-	public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
 }
