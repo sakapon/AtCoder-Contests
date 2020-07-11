23e69185d285c4512a3cf958732a10e1f4f4659e
Keiho Sakapon <git@saka-pon.net>
2020-07-12 00:36:46 +0900

    AC

--- CSharp/Contests2020/ABC155/D.cs
+++ CSharp/Contests2020/ABC155/D.cs
@@ -3,16 +3,27 @@ using System.Linq;
 
 class D
 {
+	static long[] Read() => Console.ReadLine().Split().Select(long.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var h = Read();
+		long n = h[0], k = h[1];
+		var a = Read().OrderBy(x => x).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		Console.WriteLine(First(-1L << 60, 1L << 60, x => Enumerable.Range(0, (int)n - 1).Sum(i => a[i] >= 0 ? First(i + 1, n, j => a[i] * a[j] > x) - i - 1 : n - 1 - Last(i, n - 1, j => a[i] * a[j] > x)) >= k));
+	}
+
+	static long First(long l, long r, Func<long, bool> f)
+	{
+		long m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
+	}
+
+	static long Last(long l, long r, Func<long, bool> f)
+	{
+		long m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
 	}
 }
