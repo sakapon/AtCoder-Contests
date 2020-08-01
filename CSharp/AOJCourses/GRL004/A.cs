be9d70859d0133d26bcef3b5beb0303b8065037c
Keiho Sakapon <git@saka-pon.net>
2020-08-01 21:44:04 +0900

    AC

--- CSharp/AOJCourses/GRL004/A.cs
+++ CSharp/AOJCourses/GRL004/A.cs
@@ -1,16 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class A
 {
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var h = Read();
+		var n = h[0];
+		var es = new int[h[1]].Select(_ => Read()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var map = Array.ConvertAll(new int[n + 1], _ => new List<int[]>());
+		var indeg = new int[n + 1];
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e);
+			indeg[e[1]]++;
+		}
+
+		var svs = Enumerable.Range(0, n + 1).Where(v => indeg[v] == 0).ToArray();
+		var u = new bool[n + 1];
+		var q = new Queue<int>();
+
+		foreach (var sv in svs)
+		{
+			u[sv] = true;
+			q.Enqueue(sv);
+
+			while (q.Count > 0)
+			{
+				var v = q.Dequeue();
+				foreach (var e in map[v])
+				{
+					if (--indeg[e[1]] > 0) continue;
+					u[e[1]] = true;
+					q.Enqueue(e[1]);
+				}
+			}
+		}
+		Console.WriteLine(u.Any(b => !b) ? 1 : 0);
 	}
 }
--- CSharp/AOJCourses/GRL004/B.cs
+++ CSharp/AOJCourses/GRL004/B.cs
@@ -1,16 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class B
 {
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var h = Read();
+		var n = h[0];
+		var es = new int[h[1]].Select(_ => Read()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var map = Array.ConvertAll(new int[n], _ => new List<int[]>());
+		var indeg = new int[n];
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e);
+			indeg[e[1]]++;
+		}
+
+		var svs = Enumerable.Range(0, n).Where(v => indeg[v] == 0).ToArray();
+		var seq = new List<int>();
+		var q = new Queue<int>();
+
+		foreach (var sv in svs)
+		{
+			seq.Add(sv);
+			q.Enqueue(sv);
+
+			while (q.Count > 0)
+			{
+				var v = q.Dequeue();
+				foreach (var e in map[v])
+				{
+					if (--indeg[e[1]] > 0) continue;
+					seq.Add(e[1]);
+					q.Enqueue(e[1]);
+				}
+			}
+		}
+		Console.WriteLine(string.Join("\n", seq));
 	}
 }
