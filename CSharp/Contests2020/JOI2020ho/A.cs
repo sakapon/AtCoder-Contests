718bd2d491b06d6f264cec1d95f272dc808d8283
Keiho Sakapon <git@saka-pon.net>
2020-08-01 12:35:48 +0900

    AC

--- CSharp/Contests2020/JOI2020ho/A.cs
+++ CSharp/Contests2020/JOI2020ho/A.cs
@@ -5,12 +5,16 @@ class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var a = Console.ReadLine().Split().Select((s, id) => (id, v: int.Parse(s))).OrderBy(x => x.v).ToArray();
+		var b = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var r = new int[n + 1];
+		int t;
+		r[a[n].id] = t = a.Zip(b, (p, q) => Math.Max(p.v - q, 0)).Max();
+		for (int i = n - 1; i >= 0; i--)
+			r[a[i].id] = t = Math.Max(t, Math.Max(a[i + 1].v - b[i], 0));
+
+		Console.WriteLine(string.Join(" ", r));
 	}
 }
