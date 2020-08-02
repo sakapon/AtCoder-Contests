893d4853ba5c21ae818f350269450d47b1a8eae3
Keiho Sakapon <git@saka-pon.net>
2020-08-02 11:56:18 +0900

    Add functions for spanning tree

---
+++ CSharp/CoderLib/CoderLibTest/Graphs/SpanningTreeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLibTest.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoderLibTest.Graphs
+{
+	[TestClass]
+	public class SpanningTreeTest
+	{
+		static int[][] Kruskal(int n, int[][] es)
+		{
+			var uf = new UF(n);
+			var minEdges = new List<int[]>();
+
+			foreach (var e in es.OrderBy(e => e[2]))
+			{
+				if (uf.AreUnited(e[0], e[1])) continue;
+				uf.Unite(e[0], e[1]);
+				minEdges.Add(e);
+			}
+			return minEdges.ToArray();
+		}
+
+		static int[][] Prim(int n, int[][] es)
+		{
+			var sv = 1;
+			var map = Array.ConvertAll(new int[n + 1], _ => new List<int[]>());
+			foreach (var e in es)
+			{
+				map[e[0]].Add(new[] { e[0], e[1], e[2] });
+				map[e[1]].Add(new[] { e[1], e[0], e[2] });
+			}
+
+			var u = new bool[n + 1];
+			u[sv] = true;
+			var pq = PQ<int[]>.Create(e => e[2], map[sv].ToArray());
+			var minEdges = new List<int[]>();
+
+			while (pq.Count > 0 && minEdges.Count < n)
+			{
+				var e = pq.Pop();
+				if (u[e[1]]) continue;
+				u[e[1]] = true;
+				minEdges.Add(e);
+				foreach (var ne in map[e[1]])
+					if (ne[1] != e[0])
+						pq.Push(ne);
+			}
+			return minEdges.ToArray();
+		}
+	}
+}
