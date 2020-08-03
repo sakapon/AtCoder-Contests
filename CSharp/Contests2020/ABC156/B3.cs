fa6f92d7b93bcf5e8d9085f8fc34a7ddd9752c31
Keiho Sakapon <git@saka-pon.net>
2020-08-03 21:21:31 +0900

    Update code

--- CSharp/Contests2020/ABC156/B3.cs
+++ CSharp/Contests2020/ABC156/B3.cs
@@ -7,8 +7,6 @@ class B3
 	{
 		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		int n = h[0], k = h[1];
-
-		long x = 1;
-		Console.WriteLine(Enumerable.Range(1, 31).First(i => (x *= k) > n));
+		Console.WriteLine(Enumerable.Range(1, 30).First(i => (n /= k) == 0));
 	}
 }
