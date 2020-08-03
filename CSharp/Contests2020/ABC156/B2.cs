9c4a0f970d049f1aa7eb68465be706e9d00070b2
Keiho Sakapon <git@saka-pon.net>
2020-08-03 09:29:23 +0900

    Other AC

---
+++ CSharp/Contests2020/ABC156/B2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class B2
+{
+	static void Main()
+	{
+		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		int n = h[0], k = h[1];
+
+		var p = 0;
+		for (long x = 1; x <= n; x *= k, p++) ;
+		Console.WriteLine(p);
+	}
+}
---
+++ CSharp/Contests2020/ABC156/B3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class B3
+{
+	static void Main()
+	{
+		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		int n = h[0], k = h[1];
+
+		long x = 1;
+		Console.WriteLine(Enumerable.Range(1, 31).First(i => (x *= k) > n));
+	}
+}
