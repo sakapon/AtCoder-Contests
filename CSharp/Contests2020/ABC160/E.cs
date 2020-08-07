0188c15bb760144db491eb71b6ab6f0536a8733b
Keiho Sakapon <git@saka-pon.net>
2020-08-07 11:40:47 +0900

    Improve code

--- CSharp/Contests2020/ABC160/E.cs
+++ CSharp/Contests2020/ABC160/E.cs
@@ -3,17 +3,14 @@ using System.Linq;
 
 class E
 {
-	static long[] Read() => Console.ReadLine().Split().Select(long.Parse).ToArray();
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
 		var h = Read();
-		int x = (int)h[0], y = (int)h[1];
+		int x = h[0], y = h[1];
 		var a = Read();
 		var b = Read();
 		var c = Read();
-
-		var rg = a.OrderBy(v => -v).Take(x).Concat(b.OrderBy(v => -v).Take(y)).OrderBy(v => -v).ToArray();
-		var w = c.OrderBy(v => -v).Take(x + y).Concat(new long[Math.Max(0, x + y - h[4])]).Reverse().ToArray();
-		Console.WriteLine(Enumerable.Range(0, x + y).Sum(i => Math.Max(rg[i], w[i])));
+		Console.WriteLine(a.OrderBy(v => -v).Take(x).Concat(b.OrderBy(v => -v).Take(y)).Concat(c).OrderBy(v => -v).Take(x + y).Sum(v => (long)v));
 	}
 }
