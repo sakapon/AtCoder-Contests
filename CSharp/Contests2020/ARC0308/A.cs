31263d3c72e20591cdc2a0126a65ff22f323c1f7
Keiho Sakapon <git@saka-pon.net>
2020-08-07 19:08:21 +0900

    AC

--- CSharp/Contests2020/ARC0308/A.cs
+++ CSharp/Contests2020/ARC0308/A.cs
@@ -5,12 +5,7 @@ class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
 		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
-
-		Console.WriteLine(string.Join(" ", h));
+		Console.WriteLine(string.Join("", Enumerable.Repeat("hi", s.Length / 2)) == s ? "Yes" : "No");
 	}
 }
--- CSharp/Contests2020/ARC0308/B.cs
+++ CSharp/Contests2020/ARC0308/B.cs
@@ -3,14 +3,10 @@ using System.Linq;
 
 class B
 {
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
-
-		Console.WriteLine(string.Join(" ", h));
+		int[] h = Read(), a = Read(), b = Read();
+		Console.WriteLine(new int[h[2]].Select(_ => Read()).Select(x => a[x[0] - 1] + b[x[1] - 1] - x[2]).Append(a.Min() + b.Min()).Min());
 	}
 }
