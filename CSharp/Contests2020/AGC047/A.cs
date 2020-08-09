178802c9e374e706e761283114375b3527845be6
Keiho Sakapon <git@saka-pon.net>
2020-08-10 00:20:11 +0900

    Improve code

--- CSharp/Contests2020/AGC047/A.cs
+++ CSharp/Contests2020/AGC047/A.cs
@@ -6,62 +6,42 @@ class A
 	static void Main()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var a = new int[n].Select(_ => decimal.Parse(Console.ReadLine())).ToArray();
+		var d = new int[n].Select(_ => Factorize(Console.ReadLine())).GroupBy(x => x).ToDictionary(g => g.Key, g => g.LongCount());
 
-		var d = a.Select(x =>
+		(int p2, int p5) Factorize(string s)
 		{
-			var s = x.ToString().TrimEnd('0');
 			var i = s.IndexOf('.');
-			var p10 = i == -1 ? 0 : s.Length - 1 - i;
-			var d2 = 1M;
-			for (int k = 0; k < p10; k++) d2 *= 10;
-			var d1 = x * d2;
-
-			while (d1 % 2 == 0 && d2 % 2 == 0)
-			{
-				d1 /= 2;
-				d2 /= 2;
-			}
-			while (d1 % 5 == 0 && d2 % 5 == 0)
-			{
-				d1 /= 5;
-				d2 /= 5;
-			}
+			var d1 = long.Parse(s.Replace(".", ""));
+			var d2 = i == -1 ? 1 : long.Parse($"1{new string('0', s.Length - 1 - i)}");
 
 			int p2 = 0, p5 = 0;
 			while (d1 % 2 == 0)
 			{
-				d1 /= 2;
-				p2++;
+				d1 /= 2; p2++;
 			}
 			while (d1 % 5 == 0)
 			{
-				d1 /= 5;
-				p5++;
+				d1 /= 5; p5++;
 			}
 			while (d2 % 2 == 0)
 			{
-				d2 /= 2;
-				p2--;
+				d2 /= 2; p2--;
 			}
 			while (d2 % 5 == 0)
 			{
-				d2 /= 5;
-				p5--;
+				d2 /= 5; p5--;
 			}
 			return (p2, p5);
-		}).GroupBy(x => x).ToDictionary(g => g.Key, g => g.LongCount());
+		}
 
 		var r = 0L;
-		foreach (var (x, c1) in d)
-		{
-			foreach (var (y, c2) in d)
+		foreach (var (x, cx) in d)
+			foreach (var (y, cy) in d)
 			{
 				if (x.p2 + y.p2 < 0 || x.p5 + y.p5 < 0) continue;
-				r += c1 * c2;
-				if (x == y) r -= c1;
+				r += cx * cy;
+				if (x == y) r -= cx;
 			}
-		}
 		Console.WriteLine(r / 2);
 	}
 }
