4133594901a8fb6e4b22c4dd461c693c50c3f90d
Keiho Sakapon <git@saka-pon.net>
2020-08-11 11:16:24 +0900

    AC

--- CSharp/Contests2020/ARC0530/C.cs
+++ CSharp/Contests2020/ARC0530/C.cs
@@ -5,12 +5,19 @@ class C
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var p = a.Select((x, i) => (1L << Math.Min(60, i)) - x).ToArray();
+		if (p.Take(n).Any(x => x <= 0) || p.Last() < 0) { Console.WriteLine(-1); return; }
+
+		p[n] = 0;
+		for (int i = n - 1; i >= 0; i--)
+			p[i] = Math.Min(p[i], p[i + 1] + a[i + 1]);
+		for (int i = 1; i <= n; i++)
+			p[i] = Math.Min(p[i], 2 * p[i - 1] - a[i]);
+
+		if (p.Take(n).Any(x => x <= 0) || p.Last() < 0) { Console.WriteLine(-1); return; }
+		Console.WriteLine(a.Sum() + p.Sum());
 	}
 }
