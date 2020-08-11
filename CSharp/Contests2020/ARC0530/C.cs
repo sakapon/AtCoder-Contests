2b2caf6e09f0826db3fd61c03fb4a78bf01b45f0
Keiho Sakapon <git@saka-pon.net>
2020-08-11 12:11:01 +0900

    Improve code

--- CSharp/Contests2020/ARC0530/C.cs
+++ CSharp/Contests2020/ARC0530/C.cs
@@ -8,16 +8,14 @@ class C
 		var n = int.Parse(Console.ReadLine());
 		var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
 
-		var p = a.Select((x, i) => (1L << Math.Min(60, i)) - x).ToArray();
-		if (p.Take(n).Any(x => x <= 0) || p.Last() < 0) { Console.WriteLine(-1); return; }
-
-		p[n] = 0;
+		var p = new long[n + 1];
+		p[0] = 1 - a[0];
+		for (int i = 0; i < n; i++)
+			p[i + 1] = Math.Min(1L << 60, 2 * p[i] - a[i + 1]);
+		p[n] = Math.Min(p[n], 0);
 		for (int i = n - 1; i >= 0; i--)
 			p[i] = Math.Min(p[i], p[i + 1] + a[i + 1]);
-		for (int i = 1; i <= n; i++)
-			p[i] = Math.Min(p[i], 2 * p[i - 1] - a[i]);
 
-		if (p.Take(n).Any(x => x <= 0) || p.Last() < 0) { Console.WriteLine(-1); return; }
-		Console.WriteLine(a.Sum() + p.Sum());
+		Console.WriteLine(p.Take(n).Any(x => x <= 0) || p[n] < 0 ? -1 : a.Sum() + p.Sum());
 	}
 }
