cd19192bc302647918ca6fe49c353fe4a41a2325
Keiho Sakapon <git@saka-pon.net>
2020-08-13 16:33:16 +0900

    AC

--- CSharp/AOJCourses/CGL001/A.cs
+++ CSharp/AOJCourses/CGL001/A.cs
@@ -28,9 +28,6 @@ struct V : IEquatable<V>
 	public V(double x, double y) { X = x; Y = y; }
 	public override string ToString() => $"{X:F9} {Y:F9}";
 
-	public double Norm => Math.Sqrt(X * X + Y * Y);
-	public double Angle => Math.Atan2(Y, X);
-
 	public bool Equals(V other) => X == other.X && Y == other.Y;
 	public static bool operator ==(V v1, V v2) => v1.Equals(v2);
 	public static bool operator !=(V v1, V v2) => !v1.Equals(v2);
@@ -43,6 +40,16 @@ struct V : IEquatable<V>
 	public static V operator *(double c, V v) => v * c;
 	public static V operator *(V v, double c) => new V(v.X * c, v.Y * c);
 	public static V operator /(V v, double c) => new V(v.X / c, v.Y / c);
+
+	public double Norm => Math.Sqrt(X * X + Y * Y);
+	public double Angle => Math.Atan2(Y, X);
+
+	public V Rotate(double angle)
+	{
+		var cos = Math.Cos(angle);
+		var sin = Math.Sin(angle);
+		return new V(cos * X - sin * Y, sin * X + cos * Y);
+	}
 
 	public static double Dot(V v1, V v2) => v1.X * v2.X + v1.Y * v2.Y;
 	public static double Area(V v1, V v2) => Math.Abs(v1.X * v2.Y - v2.X * v1.Y) / 2;
