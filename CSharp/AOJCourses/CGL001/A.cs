dc6100f646156f9a3f50b82268b4861cb7adb082
Keiho Sakapon <git@saka-pon.net>
2020-08-13 15:47:49 +0900

    Update library for vector

--- CSharp/AOJCourses/CGL001/A.cs
+++ CSharp/AOJCourses/CGL001/A.cs
@@ -22,25 +22,28 @@ class A
 	}
 }
 
-struct V
+struct V : IEquatable<V>
 {
 	public double X, Y;
 	public V(double x, double y) { X = x; Y = y; }
-
-	public static V operator +(V v) => v;
-	public static V operator -(V v) => new V(-v.X, -v.Y);
-	public static V operator +(V v1, V v2) => new V(v1.X + v2.X, v1.Y + v2.Y);
-	public static V operator -(V v1, V v2) => new V(v1.X - v2.X, v1.Y - v2.Y);
-	public static V operator *(double c, V v) => new V(v.X * c, v.Y * c);
-	public static V operator *(V v, double c) => new V(v.X * c, v.Y * c);
-	public static V operator /(V v, double c) => new V(v.X / c, v.Y / c);
-
-	public static bool operator ==(V v1, V v2) => v1.X == v2.X && v1.Y == v2.Y;
-	public static bool operator !=(V v1, V v2) => !(v1 == v2);
-	public override bool Equals(object obj) => obj is V && this == (V)obj;
-	public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
 	public override string ToString() => $"{X:F9} {Y:F9}";
 
 	public double Norm => Math.Sqrt(X * X + Y * Y);
+	public double Angle => Math.Atan2(Y, X);
+
+	public bool Equals(V other) => X == other.X && Y == other.Y;
+	public static bool operator ==(V v1, V v2) => v1.Equals(v2);
+	public static bool operator !=(V v1, V v2) => !v1.Equals(v2);
+	public override bool Equals(object obj) => obj is V && Equals((V)obj);
+	public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
+
+	public static V operator -(V v) => new V(-v.X, -v.Y);
+	public static V operator +(V v1, V v2) => new V(v1.X + v2.X, v1.Y + v2.Y);
+	public static V operator -(V v1, V v2) => new V(v1.X - v2.X, v1.Y - v2.Y);
+	public static V operator *(double c, V v) => v * c;
+	public static V operator *(V v, double c) => new V(v.X * c, v.Y * c);
+	public static V operator /(V v, double c) => new V(v.X / c, v.Y / c);
+
 	public static double Dot(V v1, V v2) => v1.X * v2.X + v1.Y * v2.Y;
+	public static double Area(V v1, V v2) => Math.Abs(v1.X * v2.Y - v2.X * v1.Y) / 2;
 }
