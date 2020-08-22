ee669da3b43c570d30c0892ce890faec6c21e7b7
Keiho Sakapon <git@saka-pon.net>
2020-08-22 10:58:11 +0900

    Update library

--- CSharp/CoderLib/CoderLibTest/Collections/ArrayTest.cs
+++ CSharp/CoderLib/CoderLibTest/Collections/ArrayTest.cs
@@ -24,9 +24,12 @@ namespace CoderLibTest.Collections
 			return a;
 		}
 
-		static double Norm(int[] p, int[] q) => Math.Sqrt(Math.Pow(p[0] - q[0], 2) + Math.Pow(p[1] - q[1], 2));
-		static int KyotoNorm(int[] p, int[] q) => Math.Abs(p[0] - q[0]) + Math.Abs(p[1] - q[1]);
-		//static long KyotoNorm((long x, long y) p, (long x, long y) q) => Math.Abs(q.x - p.x) + Math.Abs(q.y - p.y);
+		static double Norm(int[] p, int[] q) => Math.Sqrt((p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]));
+		static double Norm(double[] p, double[] q) => Math.Sqrt((p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]));
+
+		static int NormL1(int[] p, int[] q) => Math.Abs(p[0] - q[0]) + Math.Abs(p[1] - q[1]);
+		static double NormL1(double[] p, double[] q) => Math.Abs(p[0] - q[0]) + Math.Abs(p[1] - q[1]);
+		//static long NormL1((long x, long y) p, (long x, long y) q) => Math.Abs(q.x - p.x) + Math.Abs(q.y - p.y);
 
 		static int DotProduct(int[] a, int[] b)
 		{
