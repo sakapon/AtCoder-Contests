26c1b315fb0ceef538fad67549710dc0efc28e0b
Keiho Sakapon <git@saka-pon.net>
2020-08-24 15:26:27 +0900

    Use library

--- CSharp/AOJCourses/ITP201/B.cs
+++ CSharp/AOJCourses/ITP201/B.cs
@@ -5,26 +5,19 @@ class B
 {
 	static void Main()
 	{
-		var r = new List<int>();
-		var a = new int[800001];
-		var i0 = 400001;
-		var i1 = 399999;
+		int n = int.Parse(Console.ReadLine());
 
-		for (int k = int.Parse(Console.ReadLine()); k > 0; k--)
+		var r = new List<int>();
+		var a = new int[2 * n];
+		int i0 = n, i1 = n;
+
+		for (int k = n; k > 0; k--)
 		{
 			var q = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 			if (q[0] == 0)
 			{
-				if (i0 > i1)
-				{
-					a[--i0] = q[2];
-					i1++;
-				}
-				else
-				{
-					if (q[1] == 0) a[--i0] = q[2];
-					else a[++i1] = q[2];
-				}
+				if (q[1] == 0 && i0 < i1) a[--i0] = q[2];
+				else a[i1++] = q[2];
 			}
 			else if (q[0] == 1)
 			{
@@ -32,16 +25,8 @@ class B
 			}
 			else
 			{
-				if (i0 == i1)
-				{
-					i0++;
-					i1--;
-				}
-				else
-				{
-					if (q[1] == 0) i0++;
-					else i1--;
-				}
+				if (q[1] == 0 && i0 + 1 < i1) i0++;
+				else i1--;
 			}
 		}
 		Console.WriteLine(string.Join("\n", r));
