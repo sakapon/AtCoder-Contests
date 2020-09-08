550a18f11c9684e96805d1ff6e89dada0bdc9c3a
Keiho Sakapon <git@saka-pon.net>
2020-09-08 15:11:11 +0900

    Add vector structs

---
+++ CSharp/CoderLib/CoderLib6/Values/IntVector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CoderLib6.Values
+{
+	struct IntV : IEquatable<IntV>
+	{
+		public int X, Y;
+		public IntV(int x, int y) { X = x; Y = y; }
+		public override string ToString() => $"{X} {Y}";
+
+		public bool Equals(IntV other) => X == other.X && Y == other.Y;
+		public static bool operator ==(IntV v1, IntV v2) => v1.Equals(v2);
+		public static bool operator !=(IntV v1, IntV v2) => !v1.Equals(v2);
+		public override bool Equals(object obj) => obj is IntV && Equals((IntV)obj);
+		public override int GetHashCode() => Tuple.Create(X, Y).GetHashCode();
+
+		public static IntV operator -(IntV v) => new IntV(-v.X, -v.Y);
+		public static IntV operator +(IntV v1, IntV v2) => new IntV(v1.X + v2.X, v1.Y + v2.Y);
+		public static IntV operator -(IntV v1, IntV v2) => new IntV(v1.X - v2.X, v1.Y - v2.Y);
+
+		public int NormL1 => Math.Abs(X) + Math.Abs(Y);
+		public double NormL2 => Math.Sqrt(X * X + Y * Y);
+	}
+}
---
+++ CSharp/CoderLib/CoderLib6/Values/Vector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CoderLib6.Values
+{
+	struct V : IEquatable<V>
+	{
+		public static V Zero { get; } = new V();
+		public static V UnitX { get; } = new V(1, 0);
+		public static V UnitY { get; } = new V(0, 1);
+
+		public double X, Y;
+		public V(double x, double y) { X = x; Y = y; }
+		public override string ToString() => $"{X} {Y}";
+
+		public bool Equals(V other) => X == other.X && Y == other.Y;
+		public static bool operator ==(V v1, V v2) => v1.Equals(v2);
+		public static bool operator !=(V v1, V v2) => !v1.Equals(v2);
+		public override bool Equals(object obj) => obj is V && Equals((V)obj);
+		public override int GetHashCode() => Tuple.Create(X, Y).GetHashCode();
+
+		public static V operator +(V v) => v;
+		public static V operator -(V v) => new V(-v.X, -v.Y);
+		public static V operator +(V v1, V v2) => new V(v1.X + v2.X, v1.Y + v2.Y);
+		public static V operator -(V v1, V v2) => new V(v1.X - v2.X, v1.Y - v2.Y);
+		public static V operator *(double c, V v) => new V(v.X * c, v.Y * c);
+		public static V operator *(V v, double c) => new V(v.X * c, v.Y * c);
+		public static V operator /(V v, double c) => new V(v.X / c, v.Y / c);
+
+		public double NormL1 => Math.Abs(X) + Math.Abs(Y);
+		public double NormL2 => Math.Sqrt(X * X + Y * Y);
+		public double Angle => Math.Atan2(Y, X);
+
+		public static double Dot(V v1, V v2) => v1.X * v2.X + v1.Y * v2.Y;
+		public static double Area(V v1, V v2) => Math.Abs(v1.X * v2.Y - v2.X * v1.Y) / 2;
+	}
+}
