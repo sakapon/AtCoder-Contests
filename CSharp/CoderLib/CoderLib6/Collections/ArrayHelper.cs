b4d9d4e726e42353d68aa1308c1b6e289a2d3c07
Keiho Sakapon <git@saka-pon.net>
2020-09-08 11:57:10 +0900

    Add helper classes for array

---
+++ CSharp/CoderLib/CoderLib6/Collections/ArrayHelper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CoderLib6.Collections
+{
+	static class ArrayHelper
+	{
+		static void Swap<T>(T[] a, int i, int j) { var o = a[i]; a[i] = a[j]; a[j] = o; }
+	}
+}
---
+++ CSharp/CoderLib/CoderLib6/Values/ArrayVectorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CoderLib6.Values
+{
+	static class ArrayVectorHelper
+	{
+		static int[] Add(int[] v1, int[] v2) => v1.Zip(v2, (x, y) => x + y).ToArray();
+		static int[] Subtract(int[] v1, int[] v2) => v1.Zip(v2, (x, y) => x - y).ToArray();
+
+		static int NormL1(int[] v) => v.Sum(Math.Abs);
+		static double NormL2(int[] v) => Math.Sqrt(v.Sum(x => x * x));
+		// Minkowski Distance
+		static double Norm(int[] v, double p) => Math.Pow(v.Sum(x => Math.Pow(Math.Abs(x), p)), 1 / p);
+		static int NormMax(int[] v) => v.Max(Math.Abs);
+	}
+}
