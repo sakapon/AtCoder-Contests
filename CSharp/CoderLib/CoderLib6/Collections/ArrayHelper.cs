97afcfe075a45e26f2e541654ebd0b79ea1f6ec1
Keiho Sakapon <git@saka-pon.net>
2020-09-09 19:47:45 +0900

    Add functions for array

--- CSharp/CoderLib/CoderLib6/Collections/ArrayHelper.cs
+++ CSharp/CoderLib/CoderLib6/Collections/ArrayHelper.cs
@@ -5,5 +5,35 @@ namespace CoderLib6.Collections
 	static class ArrayHelper
 	{
 		static void Swap<T>(T[] a, int i, int j) { var o = a[i]; a[i] = a[j]; a[j] = o; }
+
+		static T[] CreateArray<T>(int l, T v)
+		{
+			var a = new T[l];
+			if (!Equals(v, default(T)))
+				for (int i = 0; i < l; ++i)
+					a[i] = v;
+			return a;
+		}
+
+		static T[,] CreateArray<T>(int l1, int l2, T v)
+		{
+			var a = new T[l1, l2];
+			if (!Equals(v, default(T)))
+				for (int i = 0; i < l1; ++i)
+					for (int j = 0; j < l2; ++j)
+						a[i, j] = v;
+			return a;
+		}
+
+		static T[,,] CreateArray<T>(int l1, int l2, int l3, T v)
+		{
+			var a = new T[l1, l2, l3];
+			if (!Equals(v, default(T)))
+				for (int i = 0; i < l1; ++i)
+					for (int j = 0; j < l2; ++j)
+						for (int k = 0; k < l3; ++k)
+							a[i, j, k] = v;
+			return a;
+		}
 	}
 }
---
+++ CSharp/CoderLib/CoderLib8/Collections/ArrayHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CoderLib8.Collections
+{
+	static class ArrayHelper
+	{
+		static T[] CreateArray<T>(int l, T v)
+		{
+			var a = new T[l];
+			if (!Equals(v, default(T)))
+				for (int i = 0; i < l; ++i) a[i] = v;
+			return a;
+		}
+
+		static T[][] CreateArray<T>(int l, T[] v)
+		{
+			var a = new T[l][];
+			for (int i = 0; i < l; ++i) a[i] = (T[])v.Clone();
+			return a;
+		}
+
+		static T[][] CreateArray<T>(int l1, int l2, T v) => CreateArray(l1, CreateArray(l2, v));
+		static T[][][] CreateArray<T>(int l1, int l2, int l3, T v) => CreateArray(l1, CreateArray(l2, l3, v));
+	}
+}
