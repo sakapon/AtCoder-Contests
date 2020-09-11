7c7aa90bcd969400f7b75a7e9da250807199f678
Keiho Sakapon <git@saka-pon.net>
2020-09-11 15:13:44 +0900

    Improve code

--- CSharp/AOJCourses/ALDS106/A2.cs
+++ CSharp/AOJCourses/ALDS106/A2.cs
@@ -9,34 +9,29 @@ class A2
 		Console.WriteLine(string.Join(" ", BucketSort(a, 10000)));
 	}
 
-	static int[] Count(int[] a, int max)
+	static int[] Tally(int[] a, int max)
 	{
 		var c = new int[max + 1];
 		foreach (var x in a) ++c[x];
 		return c;
 	}
 
-	static int[] CumSum(int[] a)
-	{
-		var s = new int[a.Length + 1];
-		for (int i = 0; i < a.Length; ++i) s[i + 1] = s[i] + a[i];
-		return s;
-	}
-
 	static int[] BucketSort(int[] a, int max)
 	{
-		var s = CumSum(Count(a, max));
+		var s = Tally(a, max);
+		for (int i = 0; i < max; ++i) s[i + 1] += s[i];
 		var r = new int[a.Length];
-		for (int i = 0; i < a.Length; ++i) r[s[a[i]]++] = a[i];
+		for (int i = a.Length - 1; i >= 0; --i) r[--s[a[i]]] = a[i];
 		return r;
 	}
 
 	static T[] BucketSort<T>(T[] a, Func<T, int> toKey, int max)
 	{
 		var keys = Array.ConvertAll(a, x => toKey(x));
-		var s = CumSum(Count(keys, max));
+		var s = Tally(keys, max);
+		for (int i = 0; i < max; ++i) s[i + 1] += s[i];
 		var r = new T[a.Length];
-		for (int i = 0; i < a.Length; ++i) r[s[keys[i]]++] = a[i];
+		for (int i = a.Length - 1; i >= 0; --i) r[--s[keys[i]]] = a[i];
 		return r;
 	}
 }
