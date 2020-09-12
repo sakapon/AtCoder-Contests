031928d4172a88672c2563e9a620defa9b1711cc
Keiho Sakapon <git@saka-pon.net>
2020-09-13 01:44:38 +0900

    AC

---
+++ CSharp/AOJCourses/ALDS103/C3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class C3
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var qs = new int[n].Select(_ => Console.ReadLine().Split());
+
+		var l = new List<int>();
+		var fi = 0;
+
+		var actions = new Dictionary<string, Action<string[]>>();
+		actions["insert"] = q => l.Add(int.Parse(q[1]));
+		actions["delete"] = q =>
+		{
+			var x = int.Parse(q[1]);
+			for (int i = l.Count - 1; i >= fi; i--)
+				if (l[i] == x) { l.RemoveAt(i); break; }
+		};
+		actions["deleteFirst"] = q => l.RemoveAt(l.Count - 1);
+		actions["deleteLast"] = q => fi++;
+
+		foreach (var q in qs) actions[q[0]](q);
+		l.Reverse();
+		Console.WriteLine(string.Join(" ", l.Take(l.Count - fi)));
+	}
+}
--- CSharp/AOJCourses/ALDS105/D.cs
+++ CSharp/AOJCourses/ALDS105/D.cs
@@ -1,18 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var a = Console.ReadLine().Split().Select(int.Parse).Select((x, i) => new { x, i }).OrderBy(_ => _.x).Select(_ => _.i).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var st = new ST(n);
+		var r = 0L;
+		for (int i = 0; i < n; i++)
+		{
+			r += st.Subsum(a[i], n);
+			st.Add(a[i], 1);
+		}
+		Console.WriteLine(r);
 	}
+}
+
+class ST
+{
+	struct KI
+	{
+		public int k, i;
+	}
+
+	int kMax;
+	List<long[]> vs = new List<long[]> { new long[1] };
+	public ST(int n)
+	{
+		for (int c = 1; c < n; vs.Add(new long[c <<= 1])) ;
+		kMax = vs.Count - 1;
+	}
+
+	KI[] GetLevels(int i)
+	{
+		var r = new List<KI>();
+		for (int k = kMax; k >= 0; --k, i >>= 1) r.Add(new KI { k = k, i = i });
+		return r.ToArray();
+	}
+
+	KI[] GetRange(int minIn, int maxEx)
+	{
+		var r = new List<KI>();
+		for (int k = kMax, f = 1; k >= 0 && minIn < maxEx; --k, f <<= 1)
+		{
+			if ((minIn & f) != 0) r.Add(new KI { k = k, i = (minIn += f) / f - 1 });
+			if ((maxEx & f) != 0) r.Add(new KI { k = k, i = (maxEx -= f) / f });
+		}
+		return r.ToArray();
+	}
+
+	public void Add(int i, long v)
+	{
+		foreach (var x in GetLevels(i)) vs[x.k][x.i] += v;
+	}
+
+	public long Subsum(int minIn, int maxEx) => GetRange(minIn, maxEx).Sum(x => vs[x.k][x.i]);
 }
