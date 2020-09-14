37a0361327c0ade818b25e39a47d575e197b67b5
Keiho Sakapon <git@saka-pon.net>
2020-09-14 18:22:36 +0900

    Copy structs for vector

---
+++ CSharp/CoderLib/CoderLib8/Values/IntVector2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CoderLib8.Values
+{
+	struct IntV : IEquatable<IntV>
+	{
+		public static IntV Zero = new IntV();
+		public static IntV UnitX = new IntV(1, 0);
+		public static IntV UnitY = new IntV(0, 1);
+
+		public int X, Y;
+		public IntV(int x, int y) { X = x; Y = y; }
+		public override string ToString() => $"{X} {Y}";
+		public static IntV Parse(string s) => Array.ConvertAll(s.Split(), int.Parse);
+
+		public static implicit operator IntV(int[] v) => new IntV(v[0], v[1]);
+		public static explicit operator int[](IntV v) => new[] { v.X, v.Y };
+
+		public bool Equals(IntV other) => X == other.X && Y == other.Y;
+		public static bool operator ==(IntV v1, IntV v2) => v1.Equals(v2);
+		public static bool operator !=(IntV v1, IntV v2) => !v1.Equals(v2);
+		public override bool Equals(object obj) => obj is IntV && Equals((IntV)obj);
+		public override int GetHashCode() => Tuple.Create(X, Y).GetHashCode();
+
+		public static IntV operator -(IntV v) => new IntV(-v.X, -v.Y);
+		public static IntV operator +(IntV v1, IntV v2) => new IntV(v1.X + v2.X, v1.Y + v2.Y);
+		public static IntV operator -(IntV v1, IntV v2) => new IntV(v1.X - v2.X, v1.Y - v2.Y);
+
+		public int NormL1 => Math.Abs(X) + Math.Abs(Y);
+		public double Norm => Math.Sqrt(X * X + Y * Y);
+
+		public static int Dot(IntV v1, IntV v2) => v1.X * v2.X + v1.Y * v2.Y;
+		// 菱形の面積
+		public static int Area(IntV v1, IntV v2) => Math.Abs(v1.X * v2.Y - v2.X * v1.Y);
+		public static bool IsParallel(IntV v1, IntV v2) => v1.X * v2.Y == v2.X * v1.Y;
+		public static bool IsOrthogonal(IntV v1, IntV v2) => v1.X * v2.X == -v1.Y * v2.Y;
+	}
+}
---
+++ CSharp/CoderLib/CoderLib8/Values/Vector2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CoderLib8.Values
+{
+	struct V : IEquatable<V>
+	{
+		public static V Zero = new V();
+		public static V UnitX = new V(1, 0);
+		public static V UnitY = new V(0, 1);
+
+		public double X, Y;
+		public V(double x, double y) { X = x; Y = y; }
+		public override string ToString() => $"{X:F9} {Y:F9}";
+		public static V Parse(string s) => Array.ConvertAll(s.Split(), double.Parse);
+
+		public static implicit operator V(double[] v) => new V(v[0], v[1]);
+		public static explicit operator double[](V v) => new[] { v.X, v.Y };
+
+		public bool Equals(V other) => X == other.X && Y == other.Y;
+		public static bool operator ==(V v1, V v2) => v1.Equals(v2);
+		public static bool operator !=(V v1, V v2) => !v1.Equals(v2);
+		public override bool Equals(object obj) => obj is V && Equals((V)obj);
+		public override int GetHashCode() => Tuple.Create(X, Y).GetHashCode();
+
+		public static V operator +(V v) => v;
+		public static V operator -(V v) => new V(-v.X, -v.Y);
+		public static V operator +(V v1, V v2) => new V(v1.X + v2.X, v1.Y + v2.Y);
+		public static V operator -(V v1, V v2) => new V(v1.X - v2.X, v1.Y - v2.Y);
+		public static V operator *(double c, V v) => v * c;
+		public static V operator *(V v, double c) => new V(v.X * c, v.Y * c);
+		public static V operator /(V v, double c) => new V(v.X / c, v.Y / c);
+
+		public double NormL1 => Math.Abs(X) + Math.Abs(Y);
+		public double Norm => Math.Sqrt(X * X + Y * Y);
+		public double Angle => Math.Atan2(Y, X);
+		public double Cos => X / Norm;
+		public double Sin => Y / Norm;
+		public double Tan => Y / X;
+
+		public V Normalize() => this / Norm;
+		public V Rotate(double angle)
+		{
+			var cos = Math.Cos(angle);
+			var sin = Math.Sin(angle);
+			return new V(cos * X - sin * Y, sin * X + cos * Y);
+		}
+
+		public static double Dot(V v1, V v2) => v1.X * v2.X + v1.Y * v2.Y;
+		// 菱形の面積
+		public static double Area(V v1, V v2) => Math.Abs(v1.X * v2.Y - v2.X * v1.Y);
+		public static bool IsParallel(V v1, V v2) => v1.X * v2.Y == v2.X * v1.Y;
+		public static bool IsOrthogonal(V v1, V v2) => v1.X * v2.X == -v1.Y * v2.Y;
+
+		// 分点
+		public static V Divide(V v1, V v2, double m, double n) => (n * v1 + m * v2) / (m + n);
+	}
+}
