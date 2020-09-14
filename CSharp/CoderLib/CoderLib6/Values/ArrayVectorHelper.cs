f86a24dd81e4ddc37e04c934b9dc7209efff349d
Keiho Sakapon <git@saka-pon.net>
2020-09-14 18:00:26 +0900

    Rename files

--- CSharp/CoderLib/CoderLib6/Values/ArrayVectorHelper.cs
+++ CSharp/CoderLib/CoderLib6/Values/ArrayVectorHelper.cs
@@ -3,6 +3,7 @@ using System.Linq;
 
 namespace CoderLib6.Values
 {
+	// 任意次元ベクトル
 	static class ArrayVectorHelper
 	{
 		static int[] Add(int[] v1, int[] v2) => v1.Zip(v2, (x, y) => x + y).ToArray();
@@ -11,6 +12,7 @@ namespace CoderLib6.Values
 		static int NormL1(int[] v) => v.Sum(Math.Abs);
 		static double NormL2(int[] v) => Math.Sqrt(v.Sum(x => x * x));
 		// Minkowski Distance
+		// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/10/ITP1_10_D
 		static double Norm(int[] v, double p) => Math.Pow(v.Sum(x => Math.Pow(Math.Abs(x), p)), 1 / p);
 		static int NormMax(int[] v) => v.Max(Math.Abs);
 	}
