b723a0ef852480ef60315e728f49ce068330088c
Keiho Sakapon <git@saka-pon.net>
2020-09-16 23:18:55 +0900

    Shorten code

--- CSharp/Contests2020/ABC163/B.cs
+++ CSharp/Contests2020/ABC163/B.cs
@@ -4,9 +4,5 @@ using System.Linq;
 class B
 {
 	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
-	static void Main()
-	{
-		var h = Read();
-		Console.WriteLine(Math.Max(h[0] - Read().Sum(), -1));
-	}
+	static void Main() => Console.WriteLine(Math.Max(Read()[0] - Read().Sum(), -1));
 }
