c9b64735994172a8b30fbc0b0c460a44eea36d43
Keiho Sakapon <git@saka-pon.net>
2020-09-19 18:36:57 +0900

    Add functions

---
+++ CSharp/AOJCourses/ALDS110/C2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class C2
+{
+	static void Main()
+	{
+		var q = int.Parse(Console.ReadLine());
+
+		for (int k = 0; k < q; k++)
+		{
+			var s = Console.ReadLine();
+			var t = Console.ReadLine();
+			var n = s.Length;
+			var m = t.Length;
+
+			var dp = NewArray0<int>(n + 1, m + 1);
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < m; j++)
+					dp[i + 1][j + 1] = s[i] == t[j] ? dp[i][j] + 1 : Math.Max(dp[i + 1][j], dp[i][j + 1]);
+			Console.WriteLine(dp[n][m]);
+		}
+	}
+
+	static T[] NewArray<T>(int n, Func<T> newItem)
+	{
+		var a = new T[n];
+		for (int i = 0; i < n; ++i) a[i] = newItem();
+		return a;
+	}
+
+	static T[][] NewArray0<T>(int n1, int n2) => NewArray(n1, () => new T[n2]);
+}
--- CSharp/CoderLib/CoderLib8/Collections/ArrayHelper.cs
+++ CSharp/CoderLib/CoderLib8/Collections/ArrayHelper.cs
@@ -4,14 +4,25 @@ namespace CoderLib8.Collections
 {
 	static class ArrayHelper
 	{
-		static T[] CreateArray<T>(int l, T v)
+		static T[] NewArray<T>(int n, Func<T> newItem)
 		{
-			var a = new T[l];
-			if (!Equals(v, default(T)))
-				for (int i = 0; i < l; ++i) a[i] = v;
+			var a = new T[n];
+			for (int i = 0; i < n; ++i) a[i] = newItem();
 			return a;
 		}
 
+		static T[][] NewArray0<T>(int n1, int n2) => NewArray(n1, () => new T[n2]);
+		static T[][][] NewArray0<T>(int n1, int n2, int n3) => NewArray(n1, () => NewArray0<T>(n2, n3));
+
+		static T[] NewArray<T>(int n, T v)
+		{
+			var a = new T[n];
+			if (!Equals(v, default(T)))
+				for (int i = 0; i < n; ++i) a[i] = v;
+			return a;
+		}
+
+		// 以下は未検証 (Clone を使うかどうか)
 		static T[][] CreateArray<T>(int l, T[] v)
 		{
 			var a = new T[l][];
@@ -19,7 +30,7 @@ namespace CoderLib8.Collections
 			return a;
 		}
 
-		static T[][] CreateArray<T>(int l1, int l2, T v) => CreateArray(l1, CreateArray(l2, v));
+		static T[][] CreateArray<T>(int l1, int l2, T v) => CreateArray(l1, NewArray(l2, v));
 		static T[][][] CreateArray<T>(int l1, int l2, int l3, T v) => CreateArray(l1, CreateArray(l2, l3, v));
 	}
 }
