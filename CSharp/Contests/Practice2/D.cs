48ca8fedcda6ff76e3481ef5e12b57f05059d4a2
Keiho Sakapon <git@saka-pon.net>
2020-09-20 19:20:20 +0900

    Incomplete

--- CSharp/Contests/Practice2/D.cs
+++ CSharp/Contests/Practice2/D.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class D
@@ -6,13 +7,89 @@ class D
 	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = Read();
-		var ps = new int[h[0]].Select(_ => Read()).ToArray();
+		int n = h[0], m = h[1];
+		var s = new int[n].Select(_ => Console.ReadLine()).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var sv = n * m;
+		var ev = sv + 1;
+
+		var vs0 = new List<int>();
+		var vs1 = new HashSet<int>();
+
+		// checker board
+		var dg = new List<long[]>();
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				if (s[i][j] == '.')
+				{
+					var v = n * i + j;
+					if ((i + j) % 2 == 0)
+					{
+						dg.Add(new[] { sv, v, 1L });
+						vs0.Add(v);
+					}
+					else
+					{
+						dg.Add(new[] { v, ev, 1L });
+						vs1.Add(v);
+					}
+				}
+
+		foreach (var v in vs0)
+		{
+			if (vs1.Contains(v - m)) dg.Add(new[] { v, v - m, 1L });
+			if (vs1.Contains(v + m)) dg.Add(new[] { v, v + m, 1L });
+			if (v % m != 0 && vs1.Contains(v - 1)) dg.Add(new[] { v, v - 1, 1L });
+			if (v % m != m - 1 && vs1.Contains(v + 1)) dg.Add(new[] { v, v + 1, 1L });
+		}
+
+		var r = MaxFlow(ev, sv, ev, dg.ToArray());
+		Console.WriteLine(r);
+	}
+
+	// dg: { from, to, capacity }
+	static long MaxFlow(int n, int sv, int ev, long[][] dg)
+	{
+		var map = Array.ConvertAll(new int[n + 1], _ => new List<long[]>());
+		foreach (var e in dg)
+		{
+			map[e[0]].Add(new[] { e[0], e[1], e[2], map[e[1]].Count });
+			map[e[1]].Add(new[] { e[1], e[0], 0, map[e[0]].Count - 1 });
+		}
+
+		long Bfs()
+		{
+			var from = new long[n + 1][];
+			var minFlow = new long[n + 1];
+			Array.Fill(minFlow, long.MaxValue);
+			var q = new Queue<long>();
+			q.Enqueue(sv);
+
+			while (q.TryDequeue(out var v))
+			{
+				if (v == ev) break;
+				foreach (var e in map[v])
+				{
+					if (from[e[1]] != null || e[2] == 0) continue;
+					from[e[1]] = e;
+					minFlow[e[1]] = Math.Min(minFlow[v], e[2]);
+					q.Enqueue(e[1]);
+				}
+			}
+
+			if (from[ev] == null) return 0;
+			for (long v = ev; v != sv; v = from[v][0])
+			{
+				var e = from[v];
+				e[2] -= minFlow[ev];
+				map[e[1]][(int)e[3]][2] += minFlow[ev];
+			}
+			return minFlow[ev];
+		}
+
+		long M = 0, t;
+		while ((t = Bfs()) > 0) M += t;
+		return M;
 	}
 }
--- CSharp/Contests/Practice2/E.cs
+++ CSharp/Contests/Practice2/E.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class E
@@ -6,13 +7,86 @@ class E
 	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		var h = Read();
-		var ps = new int[h[0]].Select(_ => Read()).ToArray();
+		int n = h[0], k = h[1];
+		var a = new int[n].Select(_ => Read()).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var sv = n * n + 2 * n;
+		var ev = sv + 1;
+
+		var dg = new List<long[]>();
+		for (int i = 0; i < n; i++)
+		{
+			dg.Add(new[] { sv, n * n + i, k, 0L });
+			dg.Add(new[] { n * n + n + i, ev, k, 0L });
+		}
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+			{
+				var v = n * i + j;
+				dg.Add(new[] { n * n + i, v, 1L, -a[i][j] });
+				dg.Add(new[] { v, n * n + n + j, 1L, 0 });
+			}
+
+		var r = -MinCostFlow(ev, sv, ev, dg.ToArray(), n * k);
+		Console.WriteLine(r);
+	}
+
+	// dg: { from, to, capacity, cost }
+	static long MinCostFlow(int n, int sv, int ev, long[][] dg, long f)
+	{
+		var map = Array.ConvertAll(new int[n + 1], _ => new List<long[]>());
+		foreach (var e in dg)
+		{
+			map[e[0]].Add(new[] { e[0], e[1], e[2], e[3], map[e[1]].Count });
+			map[e[1]].Add(new[] { e[1], e[0], 0, -e[3], map[e[0]].Count - 1 });
+		}
+
+		long BellmanFord()
+		{
+			var from = new long[n + 1][];
+			var cost = new long[n + 1];
+			Array.Fill(cost, long.MaxValue);
+			var minFlow = new long[n + 1];
+			cost[sv] = 0;
+			minFlow[sv] = f;
+
+			var next = true;
+			while (next)
+			{
+				next = false;
+				for (int v = 0; v <= n; v++)
+				{
+					if (cost[v] == long.MaxValue) continue;
+					foreach (var e in map[v])
+					{
+						if (e[2] == 0 || cost[e[1]] <= cost[v] + e[3]) continue;
+						from[e[1]] = e;
+						cost[e[1]] = cost[v] + e[3];
+						minFlow[e[1]] = Math.Min(minFlow[v], e[2]);
+						next = true;
+					}
+				}
+			}
+
+			if (from[ev] == null) return long.MaxValue;
+			for (long v = ev; v != sv; v = from[v][0])
+			{
+				var e = from[v];
+				e[2] -= minFlow[ev];
+				map[e[1]][(int)e[4]][2] += minFlow[ev];
+			}
+			f -= minFlow[ev];
+			return minFlow[ev] * cost[ev];
+		}
+
+		long r = 0, t;
+		while (f > 0)
+		{
+			if ((t = BellmanFord()) == long.MaxValue) return t;
+			r += t;
+		}
+		return r;
 	}
 }
