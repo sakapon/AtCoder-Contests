7dbeb782f8f39b591c4bfcc88398638061868186
Keiho Sakapon <git@saka-pon.net>
2020-09-23 11:28:06 +0900

    AC

--- CSharp/AOJCourses/ALDS114/C.cs
+++ CSharp/AOJCourses/ALDS114/C.cs
@@ -1,16 +1,74 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 
 class C
 {
+	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		var z = Read();
+		int h = z[0], w = z[1];
+		var s = new int[h].Select(_ => Console.ReadLine()).ToArray();
+		z = Read();
+		int r = z[0], c = z[1];
+		var p = new int[r].Select(_ => Console.ReadLine()).ToArray();
+		if (h < r || w < c) return;
 
-		Console.WriteLine(string.Join(" ", h));
+		long M1 = 1000000007, M2 = 10007;
+		var M1_Pow = Pow(M1, r);
+		var M2_Pow = Pow(M2, c);
+
+		var M2_Pows = new long[c + 1];
+		M2_Pows[0] = 1;
+		for (int j = 0; j < c; j++)
+			M2_Pows[j + 1] = M2_Pows[j] * M2;
+
+		// j に対する i 方向のハッシュ (M2 なし)
+		var t = new long[w];
+		for (int i = 0; i < r; i++)
+			for (int j = 0; j < w; j++)
+				t[j] = M1 * t[j] + s[i][j];
+
+		long sh = 0, ph = 0;
+		for (int j = 0; j < c; j++)
+			sh = M2 * sh + t[j];
+		for (int i = 0; i < r; i++)
+		{
+			long phc = 0;
+			for (int j = 0; j < c; j++)
+				phc = M2 * phc + p[i][j];
+			ph = M1 * ph + phc;
+		}
+
+		for (int i = 0; i + r - 1 < h; i++)
+		{
+			for (int j = 0; j + c - 1 < w; j++)
+			{
+				// 厳密な比較は省略
+				if (sh == ph) Console.WriteLine($"{i} {j}");
+				if (j + c < w) sh = M2 * sh - M2_Pow * t[j] + t[j + c];
+			}
+
+			sh = 0;
+			if (i + r < h)
+			{
+				for (int j = 0; j < w; j++)
+					t[j] = M1 * t[j] - M1_Pow * s[i][j] + s[i + r][j];
+				for (int j = 0; j < c; j++)
+					sh = M2 * sh + t[j];
+			}
+		}
+		Console.Out.Flush();
+	}
+
+	static long Pow(long b, long i)
+	{
+		for (var r = 1L; ; b *= b)
+		{
+			if ((i & 1) != 0) r *= b;
+			if ((i >>= 1) == 0) return r;
+		}
 	}
 }
--- CSharp/AOJCourses/ALDS114/D.cs
+++ CSharp/AOJCourses/ALDS114/D.cs
@@ -5,14 +5,59 @@ class D
 {
 	static void Main()
 	{
-		Console.ReadLine();
 		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		Func<int[]> read = () => Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = read();
-		var ps = new int[h[0]].Select(_ => read()).ToArray();
+		var n = s.Length;
+		var q = int.Parse(Console.ReadLine());
+		var ps = new int[q].Select(_ => Console.ReadLine());
 
-		Console.WriteLine(string.Join(" ", a));
+		var sa = SuffixArray(s);
+
+		Func<string, bool> match = p =>
+		{
+			var order = First(0, n + 1, o => string.CompareOrdinal(s, sa[o], p, 0, p.Length) >= 0);
+			return order <= n && string.CompareOrdinal(s, sa[order], p, 0, p.Length) == 0;
+		};
+		Console.WriteLine(string.Join("\n", ps.Select(p => match(p) ? 1 : 0)));
+	}
+
+	static int[] SuffixArray(string s)
+	{
+		var n = s.Length;
+
+		// order -> index
+		var sa = Enumerable.Range(0, n + 1).ToArray();
+		// index -> order
+		var rank = new int[n + 1];
+		rank[n] = -1;
+		for (int i = 0; i < n; i++) rank[i] = s[i];
+
+		// rank_k(i) と rank_k(i+k) から rank_2k(i) を作ります。
+		var k = 1;
+		Comparison<int> c = (i, j) =>
+		{
+			var r = rank[i].CompareTo(rank[j]);
+			if (r != 0) return r;
+			return rank[Math.Min(i + k, n)].CompareTo(rank[Math.Min(j + k, n)]);
+		};
+		Func<int, int, bool> eq = (i, j) => rank[i] == rank[j] && rank[Math.Min(i + k, n)] == rank[Math.Min(j + k, n)];
+
+		for (; k < n; k <<= 1)
+		{
+			Array.Sort(sa, c);
+
+			// 生成する必要はないか
+			var tr = (int[])rank.Clone();
+			for (int i = 0; i < n; i++)
+				tr[sa[i + 1]] = tr[sa[i]] + (eq(sa[i], sa[i + 1]) ? 0 : 1);
+			rank = tr;
+		}
+		return sa;
+	}
+
+	static int First(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
 	}
 }
