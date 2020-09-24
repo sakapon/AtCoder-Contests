4567856fccd78b875277aad0d00e29fc713039d5
Keiho Sakapon <git@saka-pon.net>
2020-09-24 18:48:23 +0900

    Improve performance (using sort keys)

--- CSharp/CoderLib/CoderLib6/Strings/SuffixArray.cs
+++ CSharp/CoderLib/CoderLib6/Strings/SuffixArray.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace CoderLib6.Strings
@@ -13,9 +12,7 @@ namespace CoderLib6.Strings
 			// order -> index
 			var sa = new int[n + 1];
 			// index -> order
-			// Empty のランクを 0 とします。
 			var rank = new int[n + 1];
-			var tr = new int[n + 1];
 			for (int i = 0; i < n; ++i)
 			{
 				sa[i] = n - i;
@@ -23,25 +20,18 @@ namespace CoderLib6.Strings
 			}
 			if (s.All(c => c == s[0])) return sa;
 
+			// Empty のランクを 0 とします。
 			// rank_k(i) と rank_k(i+k) から rank_2k(i) を作ります。
 			var k = 1;
-			Comparison<int> compare = (i, j) =>
-			{
-				var d = rank[i] - rank[j];
-				if (d != 0) return d;
-				return rank[Math.Min(i + k, n)] - rank[Math.Min(j + k, n)];
-			};
-			Func<int, int, bool> equals = (i, j) => rank[i] == rank[j] && rank[Math.Min(i + k, n)] == rank[Math.Min(j + k, n)];
-
-			var comparer = Comparer<int>.Create(compare);
+			Converter<int, long> toKey = i => rank[i] * 100000000L + rank[Math.Min(i + k, n)];
+			var keys = Array.ConvertAll(sa, toKey);
 
 			// k == 1
 			{
-				Array.Sort(sa, compare);
+				Array.Sort(keys, sa);
 
 				for (int i = 1; i <= n; ++i)
-					tr[sa[i]] = equals(sa[i], sa[i - 1]) ? tr[sa[i - 1]] : i;
-				tr.CopyTo(rank, 0);
+					rank[sa[i]] = keys[i] == keys[i - 1] ? rank[sa[i - 1]] : i;
 			}
 
 			var next = true;
@@ -55,17 +45,17 @@ namespace CoderLib6.Strings
 					var count = j - start + 1;
 
 					// ソートが完了していない部分のみソートします。
-					Array.Sort(sa, start, count, comparer);
+					for (int i = start; i <= j; ++i)
+						keys[i] = toKey(sa[i]);
+					Array.Sort(keys, sa, start, count);
 
-					for (int i = start + 1; i <= j; ++i)
-					{
-						var eq = equals(sa[i], sa[i - 1]);
-						tr[sa[i]] = eq ? tr[sa[i - 1]] : i;
-						if (eq) next = true;
-					}
 					// rank の一部が早く更新されるため、while の回数が少なくなる可能性があります。
 					for (int i = start + 1; i <= j; ++i)
-						rank[sa[i]] = tr[sa[i]];
+					{
+						var eq = keys[i] == keys[i - 1];
+						rank[sa[i]] = eq ? rank[sa[i - 1]] : i;
+						if (eq) next = true;
+					}
 
 					j = start;
 				}
