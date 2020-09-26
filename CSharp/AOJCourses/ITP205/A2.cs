d7203ef4fb6f7e508d6224366a325f8b61e7542f
Keiho Sakapon <git@saka-pon.net>
2020-09-26 18:41:58 +0900

    Add function for sorting by multi-keys

---
+++ CSharp/AOJCourses/ITP205/A2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class A2
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		Sort(a, v => v[0], v => v[1]);
+		Console.WriteLine(string.Join("\n", a.Select(v => $"{v[0]} {v[1]}")));
+	}
+
+	static void Sort<T, TKey>(T[] a, params Func<T, TKey>[] toKeys)
+	{
+		var ec = EqualityComparer<TKey>.Default;
+		var keys = new TKey[a.Length];
+
+		Action<int, int, int> Dfs = null;
+		Dfs = (depth, start, end) =>
+		{
+			for (int i = start; i < end; ++i) keys[i] = toKeys[depth](a[i]);
+			Array.Sort(keys, a, start, end - start);
+
+			if (++depth == toKeys.Length) return;
+			for (int s = start, i = start + 1; i <= end; ++i)
+			{
+				if (i < end && ec.Equals(keys[i - 1], keys[i])) continue;
+				if (s != i - 1) Dfs(depth, s, i);
+				s = i;
+			}
+		};
+		Dfs(0, 0, a.Length);
+	}
+}
--- CSharp/CoderLib/CoderLib8/Collections/SortHelper.cs
+++ CSharp/CoderLib/CoderLib8/Collections/SortHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CoderLib8.Collections
 {
@@ -49,6 +50,30 @@ namespace CoderLib8.Collections
 			for (int b = 8; b < 56; b += 8)
 				BucketSort(a, x => (int)(x >> b) & f, f);
 			BucketSort(a, x => (int)(x >> 56) & f ^ 0x80, f);
+		}
+
+		// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/5/ITP2_5_A
+		// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/5/ITP2_5_B
+		static void Sort<T, TKey>(T[] a, params Func<T, TKey>[] toKeys)
+		{
+			var ec = EqualityComparer<TKey>.Default;
+			var keys = new TKey[a.Length];
+
+			Action<int, int, int> Dfs = null;
+			Dfs = (depth, start, end) =>
+			{
+				for (int i = start; i < end; ++i) keys[i] = toKeys[depth](a[i]);
+				Array.Sort(keys, a, start, end - start);
+
+				if (++depth == toKeys.Length) return;
+				for (int s = start, i = start + 1; i <= end; ++i)
+				{
+					if (i < end && ec.Equals(keys[i - 1], keys[i])) continue;
+					if (s != i - 1) Dfs(depth, s, i);
+					s = i;
+				}
+			};
+			Dfs(0, 0, a.Length);
 		}
 	}
 }
