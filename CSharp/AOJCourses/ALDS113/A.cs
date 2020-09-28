9731aac2a31b0784b49833940f310b9edc1a64a3
Keiho Sakapon <git@saka-pon.net>
2020-09-28 15:06:15 +0900

    AC

--- CSharp/AOJCourses/ALDS113/A.cs
+++ CSharp/AOJCourses/ALDS113/A.cs
@@ -5,12 +5,77 @@ class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var k = int.Parse(Console.ReadLine());
+		var ps = new int[k].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var c = NewArray(8, 8, '.');
+
+		Action<int, int, char, char> Replace = (x, y, before, after) =>
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				if (i == x) continue;
+				if (c[i][y] == before) c[i][y] = after;
+
+				// i+j=x+y
+				var j = x + y - i;
+				if (0 <= j && j < 8 && c[i][j] == before) c[i][j] = after;
+
+				// i-j=x-y
+				j = -x + y + i;
+				if (0 <= j && j < 8 && c[i][j] == before) c[i][j] = after;
+			}
+			for (int j = 0; j < 8; j++)
+			{
+				if (j == y) continue;
+				if (c[x][j] == before) c[x][j] = after;
+			}
+		};
+
+		foreach (var p in ps)
+		{
+			c[p[0]][p[1]] = 'Q';
+			Replace(p[0], p[1], '.', '/');
+		}
+
+		Func<int, bool> Dfs = null;
+		Dfs = d =>
+		{
+			if (d++ == 8) return true;
+			var dc = (char)('0' + d);
+
+			for (int i = 0; i < 8; i++)
+				for (int j = 0; j < 8; j++)
+				{
+					if (c[i][j] != '.') continue;
+
+					c[i][j] = 'Q';
+					Replace(i, j, '.', dc);
+					if (Dfs(d)) return true;
+					c[i][j] = '.';
+					Replace(i, j, dc, '.');
+				}
+			return false;
+		};
+
+		Dfs(k);
+		Console.WriteLine(string.Join("\n", c.Select(cs => string.Join("", cs.Select(x => x == 'Q' ? 'Q' : '.')))));
 	}
+
+	static T[] NewArray<T>(int n, Func<T> newItem)
+	{
+		var a = new T[n];
+		for (int i = 0; i < n; ++i) a[i] = newItem();
+		return a;
+	}
+
+	static T[] NewArray<T>(int n, T v)
+	{
+		var a = new T[n];
+		if (!Equals(v, default(T)))
+			for (int i = 0; i < n; ++i) a[i] = v;
+		return a;
+	}
+
+	static T[][] NewArray<T>(int n1, int n2, T v) => NewArray(n1, () => NewArray(n2, v));
 }
--- CSharp/AOJCourses/ALDS113/B.cs
+++ CSharp/AOJCourses/ALDS113/B.cs
@@ -1,16 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class B
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var a = new int[3].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var sv = 0L;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				sv = (sv << 4) + a[i][j];
+
+		var gp = new P(2, 2);
+		var gv = 0L;
+		var ga = Enumerable.Range(1, 9).ToArray();
+		ga[8] = 0;
+		for (int i = 0; i < 9; i++)
+			gv = (gv << 4) + ga[i];
+
+		var d = new Dictionary<long, int>();
+		var q = new Queue<Tuple<long, P>>();
+		d[gv] = 0;
+		q.Enqueue(Tuple.Create(gv, gp));
+
+		while (q.Any())
+		{
+			var t = q.Dequeue();
+			var id = ToId(t.Item2);
+			foreach (var np in t.Item2.Nexts())
+			{
+				if (!np.IsInRange) continue;
+				var nv = Swap(t.Item1, id, ToId(np));
+				if (d.ContainsKey(nv)) continue;
+				d[nv] = d[t.Item1] + 1;
+				q.Enqueue(Tuple.Create(nv, np));
+			}
+		}
+
+		Console.WriteLine(d[sv]);
+	}
+
+	static int ToId(P p) => 3 * p.i + p.j;
+	static long GetValue(long v, int id) => (v >> 4 * (8 - id)) & 15;
+	static long Swap(long v, int id1, int id2)
+	{
+		var v1 = GetValue(v, id1);
+		var v2 = GetValue(v, id2);
+
+		v -= (v1 - v2) << 4 * (8 - id1);
+		v += (v1 - v2) << 4 * (8 - id2);
+		return v;
+	}
+
+	struct P
+	{
+		const int h = 3, w = 3;
+		public int i, j;
+		public P(int _i, int _j) { i = _i; j = _j; }
+		public bool IsInRange => 0 <= i && i < h && 0 <= j && j < w;
+		public P[] Nexts() => new[] { new P(i, j - 1), new P(i, j + 1), new P(i - 1, j), new P(i + 1, j) };
 	}
 }
