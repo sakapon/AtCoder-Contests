f3d1f40635da280a5076b27d6768e39951b10cf3
Keiho Sakapon <git@saka-pon.net>
2020-09-29 23:18:01 +0900

    Move files for combinatorics

---
+++ CSharp/CoderLib/CoderLib6/Numerics/Combinatorics.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CoderLib6.Numerics
+{
+	// 列挙時に配列の内容が変更されます (同一の配列を参照)。
+	public static class Combinatorics
+	{
+		public static void Permutation<T>(T[] values, int r, Action<T[]> action)
+		{
+			var p = new T[r];
+			var u = new bool[values.Length];
+
+			Action<int> Dfs = null;
+			Dfs = i =>
+			{
+				for (int j = 0; j < values.Length; ++j)
+				{
+					if (u[j]) continue;
+					p[i] = values[j];
+					u[j] = true;
+					if (i + 1 < r) Dfs(i + 1); else action(p);
+					u[j] = false;
+				}
+			};
+			if (r > 0) Dfs(0); else action(p);
+		}
+
+		public static void Combination<T>(T[] values, int r, Action<T[]> action)
+		{
+			var p = new T[r];
+
+			Action<int, int> Dfs = null;
+			Dfs = (i, j0) =>
+			{
+				for (int j = j0; j < values.Length; ++j)
+				{
+					p[i] = values[j];
+					if (i + 1 < r) Dfs(i + 1, j + 1); else action(p);
+				}
+			};
+			if (r > 0) Dfs(0, 0); else action(p);
+		}
+
+		public static void Power<T>(T[] values, int r, Action<T[]> action)
+		{
+			var p = new T[r];
+
+			Action<int> Dfs = null;
+			Dfs = i =>
+			{
+				for (int j = 0; j < values.Length; ++j)
+				{
+					p[i] = values[j];
+					if (i + 1 < r) Dfs(i + 1); else action(p);
+				}
+			};
+			if (r > 0) Dfs(0); else action(p);
+		}
+	}
+}
---
+++ CSharp/CoderLib/CoderLib8/Numerics/Combinatorics.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CoderLib8.Numerics
+{
+	// 列挙時に配列の内容が変更されます (同一の配列を参照)。
+	public static class Combinatorics
+	{
+		public static void Permutation<T>(T[] values, int r, Action<T[]> action)
+		{
+			var n = values.Length;
+			var p = new T[r];
+			var u = new bool[n];
+
+			if (r > 0) Dfs(0);
+			else action(p);
+
+			void Dfs(int i)
+			{
+				var i2 = i + 1;
+				for (int j = 0; j < n; ++j)
+				{
+					if (u[j]) continue;
+					p[i] = values[j];
+					u[j] = true;
+
+					if (i2 < r) Dfs(i2);
+					else action(p);
+
+					u[j] = false;
+				}
+			}
+		}
+
+		public static void Combination<T>(T[] values, int r, Action<T[]> action)
+		{
+			var n = values.Length;
+			var p = new T[r];
+
+			if (r > 0) Dfs(0, 0);
+			else action(p);
+
+			void Dfs(int i, int j0)
+			{
+				var i2 = i + 1;
+				for (int j = j0; j < n; ++j)
+				{
+					p[i] = values[j];
+
+					if (i2 < r) Dfs(i2, j + 1);
+					else action(p);
+				}
+			}
+		}
+
+		public static void Power<T>(T[] values, int r, Action<T[]> action)
+		{
+			var n = values.Length;
+			var p = new T[r];
+
+			if (r > 0) Dfs(0);
+			else action(p);
+
+			void Dfs(int i)
+			{
+				var i2 = i + 1;
+				for (int j = 0; j < n; ++j)
+				{
+					p[i] = values[j];
+
+					if (i2 < r) Dfs(i2);
+					else action(p);
+				}
+			}
+		}
+	}
+}
