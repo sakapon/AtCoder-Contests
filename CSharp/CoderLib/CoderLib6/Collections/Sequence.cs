2d865e91ed3545f09cf1ae9952f3cb2a4ee4f423
Keiho Sakapon <git@saka-pon.net>
2020-10-04 20:11:15 +0900

    Move file

---
+++ CSharp/CoderLib/CoderLib6/Collections/Sequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CoderLib6.Collections
+{
+	class Seq
+	{
+		int[] a;
+		long[] s;
+		public Seq(int[] _a) { a = _a; }
+
+		public long this[int minIn, int maxEx]
+		{
+			get
+			{
+				if (s == null)
+				{
+					s = new long[a.Length + 1];
+					for (int i = 0; i < a.Length; ++i) s[i + 1] = s[i] + a[i];
+				}
+				return s[maxEx] - s[minIn];
+			}
+		}
+
+		// C# 8.0
+		//public long this[Range r] => this[r.Start.GetOffset(a.Length), r.End.GetOffset(a.Length)];
+	}
+}
--- CSharp/CoderLib/CoderLibTest/Collections/SequenceTest.cs
+++ CSharp/CoderLib/CoderLibTest/Collections/SequenceTest.cs
@@ -1,32 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using CoderLib6.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CoderLibTest.Collections
 {
-	class Seq
-	{
-		int[] a;
-		long[] s;
-		public Seq(int[] _a) => a = _a;
-
-		public long this[int minIn, int maxEx]
-		{
-			get
-			{
-				if (s == null)
-				{
-					s = new long[a.Length + 1];
-					for (int i = 0; i < a.Length; ++i) s[i + 1] = s[i] + a[i];
-				}
-				return s[maxEx] - s[minIn];
-			}
-		}
-
-		// C# 8.0
-		//public long this[Range r] => this[r.Start.GetOffset(a.Length), r.End.GetOffset(a.Length)];
-	}
-
 	[TestClass]
 	public class SequenceTest
 	{
