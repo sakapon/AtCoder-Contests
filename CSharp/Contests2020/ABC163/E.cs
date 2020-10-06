1da077ab7308e079959de3ef18435b8eef8feb3f
Keiho Sakapon <git@saka-pon.net>
2020-10-06 11:42:28 +0900

    AC

--- CSharp/Contests2020/ABC163/E.cs
+++ CSharp/Contests2020/ABC163/E.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Linq;
+using static System.Math;
 
 class E
 {
-	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
-		var h = Read();
-		var ps = new int[h[0]].Select(_ => Read()).ToArray();
+		var a = Console.ReadLine().Split().Select(long.Parse).Select((x, i) => (x, i)).OrderBy(_ => -_.x).ToArray();
 
-		Console.WriteLine(string.Join(" ", a));
+		var dp = new long[n + 1, n + 1];
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j <= i; j++)
+			{
+				dp[i + 1, j] = Max(dp[i + 1, j], dp[i, j] + a[i].x * Abs(n - 1 - i + j - a[i].i));
+				dp[i + 1, j + 1] = Max(dp[i + 1, j + 1], dp[i, j] + a[i].x * Abs(a[i].i - j));
+			}
+		Console.WriteLine(Enumerable.Range(0, n + 1).Max(i => dp[n, i]));
 	}
 }
