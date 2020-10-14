e443aeb99b1b703d1203f1afa524e94de1791336
Keiho Sakapon <git@saka-pon.net>
2020-10-14 12:39:27 +0900

    AC

--- CSharp/AOJCourses/DSL004/A.cs
+++ CSharp/AOJCourses/DSL004/A.cs
@@ -5,12 +5,33 @@ class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var rs = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var ys = rs.Select(r => r[1]).Concat(rs.Select(r => r[3])).Distinct().OrderBy(v => v).ToArray();
+		var yd = Enumerable.Range(0, ys.Length).ToDictionary(i => ys[i]);
+
+		var xqs = rs.Select(r => new { x = r[0], d = 1, y1 = yd[r[1]], y2 = yd[r[3]] })
+			.Concat(rs.Select(r => new { x = r[2], d = -1, y1 = yd[r[1]], y2 = yd[r[3]] }))
+			.OrderBy(q => q.x);
+
+		// Segment Tree (Range) を使うと O(n^2 log n)。
+		var c = new int[ys.Length];
+		long sum = 0, xt = -1 << 30;
+
+		foreach (var q in xqs)
+		{
+			if (xt < q.x)
+			{
+				for (int i = 0; i < ys.Length; i++)
+					if (c[i] > 0) sum += (q.x - xt) * (ys[i + 1] - ys[i]);
+
+				xt = q.x;
+			}
+
+			for (int i = q.y1; i < q.y2; i++)
+				c[i] += q.d;
+		}
+		Console.WriteLine(sum);
 	}
 }
--- CSharp/AOJCourses/DSL005/A.cs
+++ CSharp/AOJCourses/DSL005/A.cs
@@ -3,14 +3,41 @@ using System.Linq;
 
 class A
 {
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var h = Read();
+		int n = h[0], t = h[1];
 
-		Console.WriteLine(string.Join(" ", h));
+		var raq = new StaticRAQ(t);
+		for (int i = 0; i < n; i++)
+		{
+			var q = Read();
+			raq.Add(q[0], q[1], 1);
+		}
+		Console.WriteLine(raq.GetAll().Max());
+	}
+}
+
+class StaticRAQ
+{
+	long[] d;
+	public StaticRAQ(int n) { d = new long[n]; }
+
+	// O(1)
+	// l_in < 0, r_ex >= n も可。
+	public void Add(int l_in, int r_ex, long v)
+	{
+		d[Math.Max(0, l_in)] += v;
+		if (r_ex < d.Length) d[r_ex] -= v;
+	}
+
+	// O(n)
+	public long[] GetAll()
+	{
+		var a = new long[d.Length];
+		a[0] = d[0];
+		for (int i = 1; i < d.Length; ++i) a[i] = a[i - 1] + d[i];
+		return a;
 	}
 }
