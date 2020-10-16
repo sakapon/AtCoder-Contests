e5d43199dc1b5b37d1b5e4c62eca2ebe18d1e292
Keiho Sakapon <git@saka-pon.net>
2020-10-16 17:32:20 +0900

    AC

--- CSharp/AOJCourses/DPL002/A.cs
+++ CSharp/AOJCourses/DPL002/A.cs
@@ -1,16 +1,52 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 class A
 {
+	const int max = 1 << 30;
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var h = Read();
+		int n = h[0];
+		var es = Array.ConvertAll(new int[h[1]], _ => Read());
 
-		Console.WriteLine(string.Join(" ", h));
+		var map = Array.ConvertAll(new int[n], _ => new List<int[]>());
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e);
+		}
+
+		var dp = NewArray2(1 << n, n, max);
+		dp[0][0] = 0;
+		for (int x = 0; x < 1 << n; x++)
+		{
+			for (int v = 0; v < n; v++)
+			{
+				if (dp[x][v] == max) continue;
+				foreach (var e in map[v])
+				{
+					if ((x & (1 << e[1])) != 0) continue;
+					dp[x | (1 << e[1])][e[1]] = Math.Min(dp[x | (1 << e[1])][e[1]], dp[x][v] + e[2]);
+				}
+			}
+		}
+		var r = dp[(1 << n) - 1][0];
+		Console.WriteLine(r == max ? -1 : r);
+	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default(T)) => NewArrayF(n1, () => NewArray1(n2, v));
+	static T[] NewArray1<T>(int n, T v = default(T))
+	{
+		var a = new T[n];
+		for (int i = 0; i < n; ++i) a[i] = v;
+		return a;
+	}
+
+	static T[] NewArrayF<T>(int n, Func<T> newItem)
+	{
+		var a = new T[n];
+		for (int i = 0; i < n; ++i) a[i] = newItem();
+		return a;
 	}
 }
--- CSharp/CoderLib/CoderLib8/Collections/ArrayHelper.cs
+++ CSharp/CoderLib/CoderLib8/Collections/ArrayHelper.cs
@@ -4,33 +4,20 @@ namespace CoderLib8.Collections
 {
 	static class ArrayHelper
 	{
-		static T[] NewArray<T>(int n, Func<T> newItem)
+		static T[][][] NewArray3<T>(int n1, int n2, int n3, T v = default(T)) => NewArrayF(n1, () => NewArray2(n2, n3, v));
+		static T[][] NewArray2<T>(int n1, int n2, T v = default(T)) => NewArrayF(n1, () => NewArray1(n2, v));
+		static T[] NewArray1<T>(int n, T v = default(T))
+		{
+			var a = new T[n];
+			for (int i = 0; i < n; ++i) a[i] = v;
+			return a;
+		}
+
+		static T[] NewArrayF<T>(int n, Func<T> newItem)
 		{
 			var a = new T[n];
 			for (int i = 0; i < n; ++i) a[i] = newItem();
 			return a;
 		}
-
-		static T[][] NewArray0<T>(int n1, int n2) => NewArray(n1, () => new T[n2]);
-		static T[][][] NewArray0<T>(int n1, int n2, int n3) => NewArray(n1, () => NewArray0<T>(n2, n3));
-
-		static T[] NewArray<T>(int n, T v)
-		{
-			var a = new T[n];
-			if (!Equals(v, default(T)))
-				for (int i = 0; i < n; ++i) a[i] = v;
-			return a;
-		}
-
-		// 以下は未検証 (Clone を使うかどうか)
-		static T[][] CreateArray<T>(int l, T[] v)
-		{
-			var a = new T[l][];
-			for (int i = 0; i < l; ++i) a[i] = (T[])v.Clone();
-			return a;
-		}
-
-		static T[][] CreateArray<T>(int l1, int l2, T v) => CreateArray(l1, NewArray(l2, v));
-		static T[][][] CreateArray<T>(int l1, int l2, int l3, T v) => CreateArray(l1, CreateArray(l2, l3, v));
 	}
 }
