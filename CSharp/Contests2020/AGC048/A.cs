aa1865d88612ae15b92e1f0fbf25a3eb9a3acf08
Keiho Sakapon <git@saka-pon.net>
2020-10-19 20:33:51 +0900

    AC

--- CSharp/Contests2020/AGC048/A.cs
+++ CSharp/Contests2020/AGC048/A.cs
@@ -3,14 +3,13 @@ using System.Linq;
 
 class A
 {
-	static void Main()
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static int Solve()
 	{
-		Console.ReadLine();
 		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
-
-		Console.WriteLine(string.Join(" ", h));
+		if (s.All(c => c == 'a')) return -1;
+		if ("atcoder".CompareTo(s) < 0) return 0;
+		var si = Enumerable.Range(0, s.Length).First(i => s[i] != 'a');
+		return s[si] < 'u' ? si : si - 1;
 	}
 }
--- CSharp/Contests2020/AGC048/B.cs
+++ CSharp/Contests2020/AGC048/B.cs
@@ -3,14 +3,20 @@ using System.Linq;
 
 class B
 {
+	static long[] Read() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var n2 = n / 2;
+		var a = Read();
+		var b = Read();
 
-		Console.WriteLine(string.Join(" ", h));
+		var d0 = Enumerable.Range(0, n2).Select(i => 2 * i).Select(i => a[i] - b[i]).OrderBy(x => -x).ToArray();
+		var d1 = Enumerable.Range(0, n2).Select(i => 2 * i + 1).Select(i => a[i] - b[i]).OrderBy(x => -x).ToArray();
+
+		long M = 0, t = 0;
+		for (int i = 0; i < n2; i++)
+			M = Math.Max(M, t += d0[i] + d1[i]);
+		Console.WriteLine(b.Sum() + M);
 	}
 }
