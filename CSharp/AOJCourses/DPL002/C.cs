df30f96380121ed8e9f935491f9dc4e63cfc9770
Keiho Sakapon <git@saka-pon.net>
2020-10-20 17:01:38 +0900

    AC

--- CSharp/AOJCourses/DPL002/C.cs
+++ CSharp/AOJCourses/DPL002/C.cs
@@ -5,12 +5,36 @@ class C
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var ps = Array.ConvertAll(new int[n], _ => Array.ConvertAll(Console.ReadLine().Split(), double.Parse));
 
-		Console.WriteLine(string.Join(" ", h));
+		// i in [1, n-2]
+		var dp = NewArray2(n, n, double.MaxValue);
+		dp[1][0] = Distance(ps[0], ps[1]) + Distance(ps[n - 2], ps[n - 1]);
+
+		for (int i = 1; i < n - 2; i++)
+			for (int j = 0; j < i; j++)
+			{
+				dp[i + 1][i] = Math.Min(dp[i + 1][i], dp[i][j] + Distance(ps[i + 1], ps[j]));
+				dp[i + 1][j] = Math.Min(dp[i + 1][j], dp[i][j] + Distance(ps[i + 1], ps[i]));
+			}
+		Console.WriteLine(n == 2 ? dp[1][0] : Enumerable.Range(0, n - 2).Min(i => dp[n - 2][i] + Distance(ps[i], ps[n - 1])));
+	}
+
+	static double Distance(double[] p, double[] q) => Math.Sqrt((p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]));
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default(T)) => NewArrayF(n1, () => NewArray1(n2, v));
+	static T[] NewArray1<T>(int n, T v = default(T))
+	{
+		var a = new T[n];
+		for (int i = 0; i < n; ++i) a[i] = v;
+		return a;
+	}
+
+	static T[] NewArrayF<T>(int n, Func<T> newItem)
+	{
+		var a = new T[n];
+		for (int i = 0; i < n; ++i) a[i] = newItem();
+		return a;
 	}
 }
