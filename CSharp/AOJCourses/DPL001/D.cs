1235a2a17578b861c87b30be947163290e272f01
Keiho Sakapon <git@saka-pon.net>
2020-10-20 15:47:39 +0900

    Improve code

--- CSharp/AOJCourses/DPL001/D.cs
+++ CSharp/AOJCourses/DPL001/D.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 class D
 {
@@ -8,13 +7,10 @@ class D
 		var n = int.Parse(Console.ReadLine());
 		var a = Array.ConvertAll(new int[n], _ => int.Parse(Console.ReadLine()));
 
-		var dp = new int[n];
-		for (int i = 0; i < n; i++)
-			dp[i] = 1 << 30;
-
+		var dp = NewArray1(n + 1, max);
 		for (int i = 0; i < n; i++)
 			dp[First(0, n, x => dp[x] >= a[i])] = a[i];
-		Console.WriteLine(dp.Count(x => x < 1 << 30));
+		Console.WriteLine(Array.IndexOf(dp, max));
 	}
 
 	static int First(int l, int r, Func<int, bool> f)
@@ -22,5 +18,14 @@ class D
 		int m;
 		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
 		return r;
+	}
+
+	const int max = 1 << 30;
+
+	static T[] NewArray1<T>(int n, T v = default(T))
+	{
+		var a = new T[n];
+		for (int i = 0; i < n; ++i) a[i] = v;
+		return a;
 	}
 }
--- CSharp/AOJCourses/DPL003/A.cs
+++ CSharp/AOJCourses/DPL003/A.cs
@@ -8,34 +8,14 @@ class A
 	{
 		var z = Read();
 		int h = z[0], w = z[1];
-		var c = Array.ConvertAll(new int[h], _ => Read());
-
-		var dp = NewArray2<int>(h, w);
-		for (int i = 0; i < h; i++)
-			for (int j = 0; j < w; j++)
-				dp[i][j] = 1 - c[i][j];
+		var dp = Array.ConvertAll(new int[h], _ => Array.ConvertAll(Read(), x => 1 - x));
 
 		for (int i = 1; i < h; i++)
 			for (int j = 1; j < w; j++)
-				if (c[i][j] == 0 && c[i][j - 1] == 0 && c[i - 1][j] == 0 && c[i - 1][j - 1] == 0)
-					dp[i][j] = Math.Min(Math.Min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
+				if (dp[i][j] > 0)
+					dp[i][j] = Math.Min(Math.Min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
 
 		var M = dp.Max(r => r.Max());
 		Console.WriteLine(M * M);
-	}
-
-	static T[][] NewArray2<T>(int n1, int n2, T v = default(T)) => NewArrayF(n1, () => NewArray1(n2, v));
-	static T[] NewArray1<T>(int n, T v = default(T))
-	{
-		var a = new T[n];
-		for (int i = 0; i < n; ++i) a[i] = v;
-		return a;
-	}
-
-	static T[] NewArrayF<T>(int n, Func<T> newItem)
-	{
-		var a = new T[n];
-		for (int i = 0; i < n; ++i) a[i] = newItem();
-		return a;
 	}
 }
