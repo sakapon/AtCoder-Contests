4788b85e4a1f41051960aac0647587914f7f127e
Keiho Sakapon <git@saka-pon.net>
2020-10-21 16:46:37 +0900

    Move struct for modulo integer

---
+++ CSharp/CoderLib/CoderLib6/Values/ModuloInt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CoderLib6.Values
+{
+	// 比較には V を使います。
+	struct MInt
+	{
+		//const long M = 998244353;
+		const long M = 1000000007;
+		public long V;
+		public MInt(long v) { V = (v %= M) < 0 ? v + M : v; }
+		public override string ToString() => $"{V}";
+		public static implicit operator MInt(long v) => new MInt(v);
+
+		public static MInt operator -(MInt x) => -x.V;
+		public static MInt operator +(MInt x, MInt y) => x.V + y.V;
+		public static MInt operator -(MInt x, MInt y) => x.V - y.V;
+		public static MInt operator *(MInt x, MInt y) => x.V * y.V;
+		public static MInt operator /(MInt x, MInt y) => x.V * y.Inv().V;
+
+		static long MPow(long b, long i)
+		{
+			long r = 1;
+			for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+			return r;
+		}
+		public MInt Pow(long i) => MPow(V, i);
+		public MInt Inv() => MPow(V, M - 2);
+	}
+}
