2ceda94c8151821c46613705120767b0421120d6
Keiho Sakapon <git@saka-pon.net>
2020-10-21 09:58:16 +0900

    Improve code

--- CSharp/Contests2020/ABCC1010/E.cs
+++ CSharp/Contests2020/ABCC1010/E.cs
@@ -3,13 +3,13 @@ using System.Linq;
 
 class E
 {
+	const long M = 1000000007;
 	static void Main()
 	{
 		var z = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 		int h = z[0], w = z[1];
 		var s = new int[h].Select(_ => Console.ReadLine()).ToArray();
 
-		const long M = 1000000007;
 		var k = s.Sum(t => t.Count(c => c == '.'));
 		var p2 = new long[k + 1];
 		p2[0] = 1;
@@ -20,84 +20,41 @@ class E
 		for (int i = 0; i < h; i++)
 		{
 			var start = -1;
-			for (int j = 0; j < w; j++)
+			for (int j = 0; j <= w; j++)
 			{
-				if (s[i][j] == '.')
+				if (j < w && s[i][j] == '.')
 				{
-					if (start == -1)
-					{
-						start = j;
-					}
+					if (start == -1) start = j;
 				}
 				else
 				{
-					if (start != -1)
-					{
-						var end = j;
-						for (int x = start; x < end; x++)
-						{
-							u[i, x] += end - start;
-						}
-						start = -1;
-					}
+					if (start != -1) for (int x = start; x < j; x++) u[i, x] += j - start;
+					start = -1;
 				}
-			}
-			if (start != -1)
-			{
-				var end = w;
-				for (int x = start; x < end; x++)
-				{
-					u[i, x] += end - start;
-				}
-				start = -1;
 			}
 		}
-
 		for (int j = 0; j < w; j++)
 		{
 			var start = -1;
-			for (int i = 0; i < h; i++)
+			for (int i = 0; i <= h; i++)
 			{
-				if (s[i][j] == '.')
+				if (i < h && s[i][j] == '.')
 				{
-					if (start == -1)
-					{
-						start = i;
-					}
+					if (start == -1) start = i;
 				}
 				else
 				{
-					if (start != -1)
-					{
-						var end = i;
-						for (int x = start; x < end; x++)
-						{
-							u[x, j] += end - start;
-						}
-						start = -1;
-					}
+					if (start != -1) for (int x = start; x < i; x++) u[x, j] += i - start;
+					start = -1;
 				}
-			}
-			if (start != -1)
-			{
-				var end = h;
-				for (int x = start; x < end; x++)
-				{
-					u[x, j] += end - start;
-				}
-				start = -1;
 			}
 		}
 
 		var r = 0L;
 		for (int i = 0; i < h; i++)
-		{
 			for (int j = 0; j < w; j++)
-			{
-				if (s[i][j] == '#') continue;
-				r = (r + (p2[u[i, j] - 1] - 1 + M) % M * p2[k - u[i, j] + 1]) % M;
-			}
-		}
+				if (u[i, j] > 0)
+					r = (r + (p2[u[i, j] - 1] - 1 + M) % M * p2[k - u[i, j] + 1]) % M;
 		Console.WriteLine(r);
 	}
 }
---
+++ CSharp/Contests2020/ABCC1010/E2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+class E2
+{
+	const long M = 1000000007;
+	static void Main()
+	{
+		var z = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		int h = z[0], w = z[1];
+		var s = new int[h].Select(_ => Console.ReadLine()).ToArray();
+
+		var k = s.Sum(t => t.Count(c => c == '.'));
+		var p2 = new long[k + 1];
+		p2[0] = 1;
+		for (int i = 0; i < k; ++i) p2[i + 1] = p2[i] * 2 % M;
+
+		var raq = new StaticRAQ2(h, w);
+		raq.Add(0, 0, h, w, -1);
+
+		for (int i = 0; i < h; i++)
+		{
+			var start = -1;
+			for (int j = 0; j <= w; j++)
+			{
+				if (j < w && s[i][j] == '.')
+				{
+					if (start == -1) start = j;
+				}
+				else
+				{
+					if (start != -1) raq.Add(i, start, i + 1, j, j - start);
+					start = -1;
+				}
+			}
+		}
+		for (int j = 0; j < w; j++)
+		{
+			var start = -1;
+			for (int i = 0; i <= h; i++)
+			{
+				if (i < h && s[i][j] == '.')
+				{
+					if (start == -1) start = i;
+				}
+				else
+				{
+					if (start != -1) raq.Add(start, j, i, j + 1, i - start);
+					start = -1;
+				}
+			}
+		}
+		var sum = raq.GetAll();
+
+		var r = 0L;
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < w; j++)
+			{
+				if (sum[i, j] == -1) continue;
+				r = (r + (p2[sum[i, j]] - 1 + M) % M * p2[k - sum[i, j]]) % M;
+			}
+		Console.WriteLine(r);
+	}
+}
+
+class StaticRAQ2
+{
+	int nx, ny;
+	int[,] d;
+	public StaticRAQ2(int _nx, int _ny) { nx = _nx; ny = _ny; d = new int[nx, ny]; }
+
+	// O(1)
+	// 範囲外のインデックスも可。
+	public void Add(int x1, int y1, int x2, int y2, int v)
+	{
+		d[Math.Max(0, x1), Math.Max(0, y1)] += v;
+		if (y2 < ny) d[Math.Max(0, x1), y2] -= v;
+		if (x2 < nx) d[x2, Math.Max(0, y1)] -= v;
+		if (x2 < nx && y2 < ny) d[x2, y2] += v;
+	}
+
+	// O(nx ny)
+	public int[,] GetAll()
+	{
+		var a = new int[nx, ny];
+		for (int i = 0; i < nx; ++i) a[i, 0] = d[i, 0];
+		for (int i = 0; i < nx; ++i)
+			for (int j = 1; j < ny; ++j) a[i, j] = a[i, j - 1] + d[i, j];
+		for (int j = 0; j < ny; ++j)
+			for (int i = 1; i < nx; ++i) a[i, j] += a[i - 1, j];
+		return a;
+	}
+}
