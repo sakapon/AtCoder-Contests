5dcc5e84ee4aa9109c0934297e8a07d2eb0cf8f4
Keiho Sakapon <git@saka-pon.net>
2020-10-25 23:24:59 +0900

    Simplify code

--- CSharp/Contests2020/ARC106/C.cs
+++ CSharp/Contests2020/ARC106/C.cs
@@ -1,35 +1,16 @@
 ﻿using System;
+using System.Linq;
 
 class C
 {
 	static void Main()
 	{
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		var h = Console.ReadLine().Split().Select(int.Parse).ToArray();
 		int n = h[0], m = h[1];
 
-		if (m < 0 || n > 1 && m >= n - 1)
-		{
-			Console.WriteLine(-1);
-			return;
-		}
+		if (m < 0 || n > 1 && m >= n - 1) { Console.WriteLine(-1); return; }
 
-		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
-		if (m == 0)
-		{
-			for (int i = 1; i <= n; i++)
-			{
-				Console.WriteLine($"{2 * i} {2 * i + 1}");
-			}
-		}
-		else
-		{
-			for (int i = 1; i < n; i++)
-			{
-				Console.WriteLine($"{3 * i} {3 * i + 2}");
-			}
-			Console.WriteLine($"{1} {3 * m + 4}");
-		}
-
-		Console.Out.Flush();
+		Console.WriteLine(string.Join("\n", Enumerable.Range(1, n - 1).Select(i => $"{3 * i} {3 * i + 2}")));
+		Console.WriteLine($"1 {3 * m + 4}");
 	}
 }
