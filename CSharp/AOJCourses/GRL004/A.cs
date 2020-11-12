04c46f9e0279fabf5270accf57424c68cdc241af
Keiho Sakapon <git@saka-pon.net>
2020-11-12 22:48:34 +0900

    Commonalize topological sort

--- CSharp/AOJCourses/GRL004/A.cs
+++ CSharp/AOJCourses/GRL004/A.cs
@@ -4,28 +4,36 @@ using System.Linq;
 
 class A
 {
-	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main()
 	{
 		var h = Read();
-		var n = h[0];
-		var es = new int[h[1]].Select(_ => Read()).ToArray();
+		var es = Array.ConvertAll(new bool[h[1]], _ => Read());
 
-		var map = Array.ConvertAll(new int[n + 1], _ => new List<int[]>());
-		var indeg = new int[n + 1];
-		foreach (var e in es)
+		var r = DirectedGraph.TopologicalSort(h[0], es);
+		Console.WriteLine(r == null ? 1 : 0);
+	}
+}
+
+class DirectedGraph
+{
+	public static int[] TopologicalSort(int n, int[][] des)
+	{
+		var map = Array.ConvertAll(new bool[n], _ => new List<int[]>());
+		var indeg = new int[n];
+		foreach (var e in des)
 		{
 			map[e[0]].Add(e);
-			indeg[e[1]]++;
+			++indeg[e[1]];
 		}
 
-		var svs = Enumerable.Range(0, n + 1).Where(v => indeg[v] == 0).ToArray();
-		var u = new bool[n + 1];
+		var r = new List<int>();
 		var q = new Queue<int>();
+		var svs = Enumerable.Range(0, n).Where(v => indeg[v] == 0).ToArray();
 
 		foreach (var sv in svs)
 		{
-			u[sv] = true;
+			r.Add(sv);
 			q.Enqueue(sv);
 
 			while (q.Count > 0)
@@ -34,11 +42,12 @@ class A
 				foreach (var e in map[v])
 				{
 					if (--indeg[e[1]] > 0) continue;
-					u[e[1]] = true;
+					r.Add(e[1]);
 					q.Enqueue(e[1]);
 				}
 			}
 		}
-		Console.WriteLine(u.Any(b => !b) ? 1 : 0);
+		if (r.Count < n) return null;
+		return r.ToArray();
 	}
 }
--- CSharp/AOJCourses/GRL004/B.cs
+++ CSharp/AOJCourses/GRL004/B.cs
@@ -4,41 +4,13 @@ using System.Linq;
 
 class B
 {
-	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main()
 	{
 		var h = Read();
-		var n = h[0];
-		var es = new int[h[1]].Select(_ => Read()).ToArray();
+		var es = Array.ConvertAll(new bool[h[1]], _ => Read());
 
-		var map = Array.ConvertAll(new int[n], _ => new List<int[]>());
-		var indeg = new int[n];
-		foreach (var e in es)
-		{
-			map[e[0]].Add(e);
-			indeg[e[1]]++;
-		}
-
-		var svs = Enumerable.Range(0, n).Where(v => indeg[v] == 0).ToArray();
-		var seq = new List<int>();
-		var q = new Queue<int>();
-
-		foreach (var sv in svs)
-		{
-			seq.Add(sv);
-			q.Enqueue(sv);
-
-			while (q.Count > 0)
-			{
-				var v = q.Dequeue();
-				foreach (var e in map[v])
-				{
-					if (--indeg[e[1]] > 0) continue;
-					seq.Add(e[1]);
-					q.Enqueue(e[1]);
-				}
-			}
-		}
-		Console.WriteLine(string.Join("\n", seq));
+		var r = DirectedGraph.TopologicalSort(h[0], es);
+		Console.WriteLine(string.Join("\n", r));
 	}
 }
