011fc07b3b5381b2a3f99be43f3d59dd6d2ba970
Keiho Sakapon <git@saka-pon.net>
2020-11-15 01:30:38 +0900

    AC

--- CSharp/Contests2020/AGC049/B.cs
+++ CSharp/Contests2020/AGC049/B.cs
@@ -11,24 +11,14 @@ class B
 
 		var s1 = Enumerable.Range(0, n).Where(i => s[i] == '1').ToArray();
 		var t1 = Enumerable.Range(0, n).Where(i => t[i] == '1').ToArray();
-		if (s1.Length == 0 && t1.Length == 0) { Console.WriteLine(0); return; }
 
-		//var d1 = s.Count(c => c == '1') - t.Count(c => c == '1');
 		var d1 = s1.Length - t1.Length;
 		if (d1 < 0 || d1 % 2 != 0) { Console.WriteLine(-1); return; }
 		d1 /= 2;
 
 		var r = 0L;
-		// Span?
-		var s1e = s1.AsEnumerable().GetEnumerator();
-		s1e.MoveNext();
-		var i = s1e.Current;
-
-		if (t1.Length == 0)
-		{
-			Console.WriteLine(Enumerable.Range(0, s1.Length / 2).Sum(i => (long)s1[2 * i + 1] - s1[2 * i]));
-			return;
-		}
+		var s1e = s1.AsSpan().GetEnumerator();
+		var i = s1e.MoveNext() ? s1e.Current : n;
 
 		foreach (var j in t1)
 		{
@@ -38,11 +28,16 @@ class B
 				d1--;
 				s1e.MoveNext();
 				r += s1e.Current - i;
-				s1e.MoveNext();
-				i = s1e.Current;
+				i = s1e.MoveNext() ? s1e.Current : n;
 			}
 			r += i - j;
-			if (s1e.MoveNext()) i = s1e.Current;
+			i = s1e.MoveNext() ? s1e.Current : n;
+		}
+		while (i < n)
+		{
+			s1e.MoveNext();
+			r += s1e.Current - i;
+			i = s1e.MoveNext() ? s1e.Current : n;
 		}
 		Console.WriteLine(r);
 	}
