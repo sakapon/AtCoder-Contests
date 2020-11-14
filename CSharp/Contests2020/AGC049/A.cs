9139c2e9675d066dc1f2588b4a3a2c4d1713fbc0
Keiho Sakapon <git@saka-pon.net>
2020-11-15 01:12:23 +0900

    WA

--- CSharp/Contests2020/AGC049/A.cs
+++ CSharp/Contests2020/AGC049/A.cs
@@ -1,16 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class A
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var s = new int[n].Select(_ => Console.ReadLine()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var uf = new UF(n);
+		var map = Array.ConvertAll(new int[n], _ => new List<int>());
+		var map_r = Array.ConvertAll(new int[n], _ => new List<int>());
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (s[i][j] == '1')
+				{
+					uf.Unite(i, j);
+					map[i].Add(j);
+					map_r[j].Add(i);
+				}
+			}
+		}
+
+		var u = new bool[n];
+		var order = new int[n];
+		var c = n;
+		var gs = new List<List<int>>();
+		List<int> lg = null;
+
+		void Dfs(int v)
+		{
+			u[v] = true;
+			foreach (var nv in map[v]) if (!u[nv]) Dfs(nv);
+			order[--c] = v;
+		}
+		for (int v = 0; v < n; v++) if (!u[v]) Dfs(v);
+
+		void Dfs_r(int v)
+		{
+			u[v] = true;
+			foreach (var nv in map_r[v]) if (!u[nv]) Dfs_r(nv);
+			lg.Add(v);
+		}
+		Array.Clear(u, 0, n);
+		foreach (var v in order) if (!u[v]) { gs.Add(lg = new List<int>()); Dfs_r(v); }
+
+		var r = 0.0;
+
+		foreach (var ufg in gs.GroupBy(g => uf.GetRoot(g[0])))
+		{
+			var gcs = ufg.Select(g => g.Count).ToArray();
+			var seq = new Seq(gcs);
+
+			var dp = new double[gcs.Length];
+			dp[0] = 1;
+			for (int i = 1; i < gcs.Length; i++)
+			{
+				var all = seq.Sum(0, i + 1);
+				dp[i] = dp[i - 1] + (double)gcs[i] / all;
+			}
+			r += dp.Last();
+		}
+		Console.WriteLine(r);
 	}
+}
+
+class Seq
+{
+	int[] a;
+	long[] s;
+	public Seq(int[] _a) { a = _a; }
+
+	public long Sum(int minIn, int maxEx)
+	{
+		if (s == null)
+		{
+			s = new long[a.Length + 1];
+			for (int i = 0; i < a.Length; ++i) s[i + 1] = s[i] + a[i];
+		}
+		return s[maxEx] - s[minIn];
+	}
+
+	// C# 8.0
+	//public long Sum(Range r) => Sum(r.Start.GetOffset(a.Length), r.End.GetOffset(a.Length));
+}
+
+class UF
+{
+	int[] p;
+	public UF(int n) { p = Enumerable.Range(0, n).ToArray(); }
+
+	public void Unite(int a, int b) { if (!AreUnited(a, b)) p[p[b]] = p[a]; }
+	public bool AreUnited(int a, int b) => GetRoot(a) == GetRoot(b);
+	public int GetRoot(int a) => p[a] == a ? a : p[a] = GetRoot(p[a]);
+	public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
 }
--- CSharp/Contests2020/AGC049/B.cs
+++ CSharp/Contests2020/AGC049/B.cs
@@ -5,12 +5,45 @@ class B
 {
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var s = Console.ReadLine();
+		var t = Console.ReadLine();
 
-		Console.WriteLine(string.Join(" ", h));
+		var s1 = Enumerable.Range(0, n).Where(i => s[i] == '1').ToArray();
+		var t1 = Enumerable.Range(0, n).Where(i => t[i] == '1').ToArray();
+		if (s1.Length == 0 && t1.Length == 0) { Console.WriteLine(0); return; }
+
+		//var d1 = s.Count(c => c == '1') - t.Count(c => c == '1');
+		var d1 = s1.Length - t1.Length;
+		if (d1 < 0 || d1 % 2 != 0) { Console.WriteLine(-1); return; }
+		d1 /= 2;
+
+		var r = 0L;
+		// Span?
+		var s1e = s1.AsEnumerable().GetEnumerator();
+		s1e.MoveNext();
+		var i = s1e.Current;
+
+		if (t1.Length == 0)
+		{
+			Console.WriteLine(Enumerable.Range(0, s1.Length / 2).Sum(i => (long)s1[2 * i + 1] - s1[2 * i]));
+			return;
+		}
+
+		foreach (var j in t1)
+		{
+			while (i < j)
+			{
+				if (d1 == 0) { Console.WriteLine(-1); return; }
+				d1--;
+				s1e.MoveNext();
+				r += s1e.Current - i;
+				s1e.MoveNext();
+				i = s1e.Current;
+			}
+			r += i - j;
+			if (s1e.MoveNext()) i = s1e.Current;
+		}
+		Console.WriteLine(r);
 	}
 }
