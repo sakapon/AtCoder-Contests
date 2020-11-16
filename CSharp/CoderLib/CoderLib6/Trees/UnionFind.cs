cf56f1ffb69eb576281692dd5dfb28e1e44c3d3c
Keiho Sakapon <git@saka-pon.net>
2020-11-16 12:29:40 +0900

    Add class for Union-Find tree with data

--- CSharp/CoderLib/CoderLib6/Trees/UnionFind.cs
+++ CSharp/CoderLib/CoderLib6/Trees/UnionFind.cs
@@ -17,4 +17,30 @@ namespace CoderLib6.Trees
 		public int GetRoot(int a) => p[a] == a ? a : p[a] = GetRoot(p[a]);
 		public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
 	}
+
+	class UF<T>
+	{
+		int[] p;
+		T[] a;
+		public Func<T, T, T> Merge;
+
+		// (parent, child) -> result
+		public UF(int n, Func<T, T, T> merge, T[] a)
+		{
+			p = Enumerable.Range(0, n).ToArray();
+			this.a = a;
+			Merge = merge;
+		}
+
+		public void Unite(int x, int y)
+		{
+			if (AreUnited(x, y)) return;
+			a[p[x]] = Merge(a[p[x]], a[p[y]]);
+			p[p[y]] = p[x];
+		}
+		public bool AreUnited(int x, int y) => GetRoot(x) == GetRoot(y);
+		public int GetRoot(int x) => p[x] == x ? x : p[x] = GetRoot(p[x]);
+		public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
+		public T GetValue(int x) => a[GetRoot(x)];
+	}
 }
