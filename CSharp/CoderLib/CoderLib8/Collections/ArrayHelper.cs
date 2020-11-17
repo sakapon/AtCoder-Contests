fd86ce5b5afaa4cb2e835eb77f59b5dbdaf105c6
Keiho Sakapon <git@saka-pon.net>
2020-11-17 16:22:12 +0900

    Update code

--- CSharp/CoderLib/CoderLib8/Collections/ArrayHelper.cs
+++ CSharp/CoderLib/CoderLib8/Collections/ArrayHelper.cs
@@ -11,21 +11,9 @@ namespace CoderLib8.Collections
 		const int max = 1 << 30;
 		const int min = -1 << 30;
 
-		static T[][][] NewArray3<T>(int n1, int n2, int n3, T v = default(T)) => NewArrayF(n1, () => NewArray2(n2, n3, v));
-		static T[][] NewArray2<T>(int n1, int n2, T v = default(T)) => NewArrayF(n1, () => NewArray1(n2, v));
-		static T[] NewArray1<T>(int n, T v = default(T))
-		{
-			var a = new T[n];
-			for (int i = 0; i < n; ++i) a[i] = v;
-			return a;
-		}
-
-		static T[] NewArrayF<T>(int n, Func<T> newItem)
-		{
-			var a = new T[n];
-			for (int i = 0; i < n; ++i) a[i] = newItem();
-			return a;
-		}
+		static T[] NewArray1<T>(int n, T v = default) => Array.ConvertAll(new bool[n], _ => v);
+		static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+		static T[][][] NewArray3<T>(int n1, int n2, int n3, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => Array.ConvertAll(new bool[n3], ___ => v)));
 
 		static int[] Range(int start, int count)
 		{
@@ -43,5 +31,14 @@ namespace CoderLib8.Collections
 
 		static (T, T) ToTuple2<T>(T[] a) => (a[0], a[1]);
 		static (T, T, T) ToTuple3<T>(T[] a) => (a[0], a[1], a[2]);
+	}
+
+	static class ArrayHelperLab
+	{
+		static T[] NewArrayByFunc<T>(int n, Func<T> newItem) => Array.ConvertAll(new bool[n], _ => newItem());
+		static T[] NewArray1<T>(int n, T v = default) => Array.ConvertAll(new bool[n], _ => v);
+		static T[][] NewArray2<T>(int n1, int n2, T v = default) => NewArrayByFunc(n1, () => NewArray1(n2, v));
+		static T[][][] NewArray3<T>(int n1, int n2, int n3, T v = default) => NewArrayByFunc(n1, () => NewArray2(n2, n3, v));
+		static T[][][][] NewArray4<T>(int n1, int n2, int n3, int n4, T v = default) => NewArrayByFunc(n1, () => NewArray3(n2, n3, n4, v));
 	}
 }
--- CSharp/CoderLib/CoderLib8/Collections/CollectionHelper.cs
+++ CSharp/CoderLib/CoderLib8/Collections/CollectionHelper.cs
@@ -5,8 +5,10 @@ namespace CoderLib8.Collections
 {
 	static class CollectionHelper
 	{
+		public static TV GetValue<TK, TV>(this Dictionary<TK, TV> d, TK k, TV v0 = default) => d.ContainsKey(k) ? d[k] : v0;
+
 		// 項目数が多いほうにマージします。
-		static Dictionary<TK, int> Merge<TK>(Dictionary<TK, int> d1, Dictionary<TK, int> d2)
+		public static Dictionary<TK, int> Merge<TK>(Dictionary<TK, int> d1, Dictionary<TK, int> d2)
 		{
 			if (d1.Count < d2.Count) (d1, d2) = (d2, d1);
 			foreach (var (k, v) in d2)
