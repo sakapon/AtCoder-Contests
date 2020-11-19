26e8f30c685d3b7006ddab6b8409c4077b5346ec
Keiho Sakapon <git@saka-pon.net>
2020-11-19 10:39:04 +0900

    Update library

--- CSharp/Contests2020/ARC105/C.cs
+++ CSharp/Contests2020/ARC105/C.cs
@@ -15,12 +15,12 @@ class C
 		if (lvs.Min(lv => lv.v) < w.Max()) { Console.WriteLine(-1); return; }
 
 		lvs = lvs.OrderBy(lv => lv.v).ToArray();
-		var lmaxs = Seq.GetMaxs(lvs.Select(lv => lv.l).ToArray(), 0);
+		var lmaxs = lvs.Select(lv => lv.l).ToArray().CumMax(0);
 
 		int GetAllLength(int[] w)
 		{
 			var dp = new int[n];
-			var wsum = Seq.GetCumSum(w);
+			var wsum = new CumSum(w);
 
 			for (int i = 0; i < n; i++)
 				for (int j = i + 1; j < n; j++)
@@ -71,24 +71,26 @@ class C
 	}
 }
 
+class CumSum
+{
+	long[] s;
+	public CumSum(int[] a)
+	{
+		s = new long[a.Length + 1];
+		for (int i = 0; i < a.Length; ++i) s[i + 1] = s[i] + a[i];
+	}
+	public long Sum(int l_in, int r_ex) => s[r_ex] - s[l_in];
+}
+
 static class Seq
 {
-	public static TR[] Aggregate<TS, TR>(TS[] a, TR r0, Func<TR, TS, TR> func)
+	public static TR[] Cumulate<TS, TR>(this TS[] a, TR r0, Func<TR, TS, TR> func)
 	{
 		var r = new TR[a.Length + 1];
 		r[0] = r0;
 		for (int i = 0; i < a.Length; ++i) r[i + 1] = func(r[i], a[i]);
 		return r;
 	}
-	public static CumSum GetCumSum(int[] a) => new CumSum(Aggregate(a, 0L, (x, y) => x + y));
-	public static int[] GetMaxs(int[] a, int v0 = int.MinValue) => Aggregate(a, v0, Math.Max);
-	public static int[] GetMins(int[] a, int v0 = int.MaxValue) => Aggregate(a, v0, Math.Min);
-}
-
-class CumSum
-{
-	long[] s;
-	public CumSum(long[] _s) { s = _s; }
-	public long Sum(int l_in, int r_ex) => s[r_ex] - s[l_in];
-	//public long Sum(Range r) => Sum(r.Start.GetOffset(s.Length - 1), r.End.GetOffset(s.Length - 1));
+	public static int[] CumMax(this int[] a, int v0 = int.MinValue) => Cumulate(a, v0, Math.Max);
+	public static int[] CumMin(this int[] a, int v0 = int.MaxValue) => Cumulate(a, v0, Math.Min);
 }
