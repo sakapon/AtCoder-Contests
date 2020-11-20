48c85c71c596001b8bae2603191e8fe6575d3cb6
Keiho Sakapon <git@saka-pon.net>
2020-11-20 19:06:09 +0900

    Initialize

---
+++ CSharp/Contests2020/ARC109/A.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class A
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main()
+	{
+		Console.ReadLine();
+		var s = Console.ReadLine();
+		//var h = Read();
+		//int n = h[0], m = h[1];
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+		var ps = Array.ConvertAll(new bool[n], _ => Read());
+
+		Console.WriteLine(string.Join(" ", a));
+	}
+}
