e1e70b658fb8d3756305443764cbda3ee41a6e21
Keiho Sakapon <git@saka-pon.net>
2020-11-20 23:44:25 +0900

    Add extension methods

--- CSharp/CoderLib/CoderLib8/Linq/Grouping.cs
+++ CSharp/CoderLib/CoderLib8/Linq/Grouping.cs
@@ -4,8 +4,39 @@ using System.Linq;
 
 namespace CoderLib8.Linq
 {
+	// Test: https://atcoder.jp/contests/past202010-open/tasks/past202010_d
+	// Test: https://atcoder.jp/contests/past202010-open/tasks/past202010_f
 	static class GE
 	{
+		public static Dictionary<TK, int> GroupCounts<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
+		{
+			var d = new Dictionary<TK, int>();
+			TK k;
+			foreach (var o in source)
+				if (d.ContainsKey(k = toKey(o))) ++d[k];
+				else d[k] = 1;
+			return d;
+		}
+
+		public static IEnumerable<KeyValuePair<TK, int>> GroupCountsBySeq<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
+		{
+			var c = EqualityComparer<TK>.Default;
+			TK k = default(TK), kt;
+			var count = 0;
+
+			foreach (var o in source)
+			{
+				if (!c.Equals(k, kt = toKey(o)))
+				{
+					if (count > 0) yield return new KeyValuePair<TK, int>(k, count);
+					k = kt;
+					count = 0;
+				}
+				++count;
+			}
+			if (count > 0) yield return new KeyValuePair<TK, int>(k, count);
+		}
+
 		public static IEnumerable<IGrouping<TK, TS>> GroupBySeq<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
 		{
 			var c = EqualityComparer<TK>.Default;
