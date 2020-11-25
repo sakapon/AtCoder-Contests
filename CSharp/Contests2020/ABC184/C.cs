e57463ea2291d791259e66320b70b61e1f5dd6d7
Keiho Sakapon <git@saka-pon.net>
2020-11-25 23:37:59 +0900

    Improve code

--- CSharp/Contests2020/ABC184/C.cs
+++ CSharp/Contests2020/ABC184/C.cs
@@ -4,31 +4,23 @@ using static System.Math;
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static (T, T) ToTuple2<T>(T[] a) => (a[0], a[1]);
-	static void Main()
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
 	{
-		(int x, int y) p1 = ToTuple2(Read());
-		(int x, int y) p2 = ToTuple2(Read());
-		var (a, b) = p1;
-		var (c, d) = p2;
+		var (a, b) = Read2();
+		var (c, d) = Read2();
 
-		if (p1 == p2)
-		{
-			Console.WriteLine(0);
-		}
-		else if (a + b == c + d || a - b == c - d || Abs(a - c) + Abs(b - d) <= 3)
-		{
-			Console.WriteLine(1);
-		}
-		else if (Abs(a - b) % 2 == Abs(c - d) % 2 || Abs(a - c) + Abs(b - d) <= 6 ||
+		if ((a, b) == (c, d))
+			return 0;
+
+		if (a + b == c + d || a - b == c - d || Abs(a - c) + Abs(b - d) <= 3)
+			return 1;
+
+		if (Abs(a - b) % 2 == Abs(c - d) % 2 || Abs(a - c) + Abs(b - d) <= 6 ||
 			Abs(Abs(a - b) - Abs(c - d)) <= 3 || Abs(Abs(a + b) - Abs(c + d)) <= 3)
-		{
-			Console.WriteLine(2);
-		}
-		else
-		{
-			Console.WriteLine(3);
-		}
+			return 2;
+
+		return 3;
 	}
 }
