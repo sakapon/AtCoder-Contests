4ce9050fbeff192ea469ea7c25cc1ad111649a14
Keiho Sakapon <git@saka-pon.net>
2020-11-25 16:08:41 +0900

    Add helper class for tree

---
+++ CSharp/CoderLib/CoderLib6/Graphs/TreeHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CoderLib6.Trees;
+
+namespace CoderLib6.Graphs
+{
+	static class TreeHelper
+	{
+		// 連結な無向グラフから木を取り出します。
+		// 結果は undirected
+		// Test: https://atcoder.jp/contests/arc108/tasks/arc108_c
+		static int[][] GetTree(int n, int[][] ues)
+		{
+			var uf = new UF(n);
+			var res = new List<int[]>();
+
+			foreach (var e in ues)
+			{
+				if (uf.AreUnited(e[0], e[1])) continue;
+				uf.Unite(e[0], e[1]);
+				res.Add(e);
+			}
+			return res.ToArray();
+		}
+	}
+}
