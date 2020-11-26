84b682c1dfa339892d21188fe0857b88c35b0195
Keiho Sakapon <git@saka-pon.net>
2020-11-26 15:52:19 +0900

    Improve code

--- CSharp/Contests2020/ABC184/D.cs
+++ CSharp/Contests2020/ABC184/D.cs
@@ -3,17 +3,15 @@
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main()
 	{
-		var h = Read();
-		int a = h[0], b = h[1], c = h[2];
+		var (a, b, c) = Read3();
 
 		var dp = NewArray3<double>(101, 101, 101);
 
 		for (int i = 99; i >= 0; i--)
-		{
 			for (int j = 99; j >= 0; j--)
-			{
 				for (int k = 99; k >= 0; k--)
 				{
 					var all = i + j + k;
@@ -22,12 +20,8 @@ class D
 					dp[i][j][k] += (dp[i][j + 1][k] + 1) * j / all;
 					dp[i][j][k] += (dp[i][j][k + 1] + 1) * k / all;
 				}
-			}
-		}
 		Console.WriteLine(dp[a][b][c]);
 	}
 
-	static T[] NewArray1<T>(int n, T v = default) => Array.ConvertAll(new bool[n], _ => v);
-	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 	static T[][][] NewArray3<T>(int n1, int n2, int n3, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => Array.ConvertAll(new bool[n3], ___ => v)));
 }
