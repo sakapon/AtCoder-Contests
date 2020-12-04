1d31ee57e98e4e829036dfbdbbf6595daa26da1e
Keiho Sakapon <git@saka-pon.net>
2020-12-04 17:09:51 +0900

    Update library

--- CSharp/Contests/ATC002/AD.cs
+++ CSharp/Contests/ATC002/AD.cs
@@ -8,20 +8,26 @@ class AD
 	static void Main()
 	{
 		var (h, w) = Read2();
-		var sv = Read2() - new P(1, 1);
-		var gv = Read2() - new P(1, 1);
+		var (i, j) = Read2();
+		var sv = (i - 1, j - 1);
+		(i, j) = Read2();
+		var gv = (i - 1, j - 1);
 		var c = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
 
 		var dp = new DP2<int>(h, w, MergeOp.Min(1 << 30));
-		dp.a.SetByP(sv, 0);
-		dp.AddTransition(p => c[p.i][p.j] == '#' ? 1 << 30 : dp.GetValue(p, new P(-1, 0)) + 1);
-		dp.AddTransition(p => c[p.i][p.j] == '#' ? 1 << 30 : dp.GetValue(p, new P(1, 0)) + 1);
-		dp.AddTransition(p => c[p.i][p.j] == '#' ? 1 << 30 : dp.GetValue(p, new P(0, -1)) + 1);
-		dp.AddTransition(p => c[p.i][p.j] == '#' ? 1 << 30 : dp.GetValue(p, new P(0, 1)) + 1);
+		dp[sv] = 0;
+		dp.AddTransition(p =>
+		{
+			if (c[p.i][p.j] == '#') return;
+			p.MergeValue(-1, 0, p.GetValue() + 1);
+			p.MergeValue(1, 0, p.GetValue() + 1);
+			p.MergeValue(0, -1, p.GetValue() + 1);
+			p.MergeValue(0, 1, p.GetValue() + 1);
+		});
 
 		for (int k = 0; k < h * w; k++)
 			dp.Execute(0, h, 0, w);
-		Console.WriteLine(dp.a.GetByP(gv));
+		Console.WriteLine(dp[gv]);
 	}
 }
 
@@ -46,33 +52,42 @@ struct MergeOp<T>
 
 class DP2<T>
 {
-	public T[][] a;
 	int n1, n2;
 	MergeOp<T> MergeOp;
-	List<Func<P, T>> Transitions = new List<Func<P, T>>();
+	public T[][] a;
+	List<Action<P>> Transitions = new List<Action<P>>();
 
 	public DP2(int n1, int n2, MergeOp<T> mergeOp)
 	{
-		a = Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => mergeOp.V0));
 		this.n1 = n1;
 		this.n2 = n2;
 		MergeOp = mergeOp;
+		a = Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => mergeOp.V0));
 	}
 	public T[] this[int i] => a[i];
-
-	public T GetValue(P p, P delta)
+	public T this[int i, int j]
 	{
-		p += delta;
+		get => a[i][j];
+		set => a[i][j] = value;
+	}
+	public T this[(int i, int j) p]
+	{
+		get => a[p.i][p.j];
+		set => a[p.i][p.j] = value;
+	}
+
+	internal T GetValue(P p)
+	{
 		if (!p.IsInRange(n1, n2)) return MergeOp.V0;
 		return a[p.i][p.j];
 	}
-	public void PushValue(P p, P delta, T value)
+	internal void MergeValue(P p, T value)
 	{
-		p += delta;
+		if (!p.IsInRange(n1, n2)) return;
 		a[p.i][p.j] = MergeOp.Merge(value, a[p.i][p.j]);
 	}
 
-	public void AddTransition(Func<P, T> getNewValue) => Transitions.Add(getNewValue);
+	public void AddTransition(Action<P> transition) => Transitions.Add(transition);
 
 	public void Execute(int si, int ei, int sj, int ej)
 	{
@@ -81,7 +96,27 @@ class DP2<T>
 
 		for (int i = si; i != ei; i += di)
 			for (int j = sj; j != ej; j += dj)
-				foreach (var getNewValue in Transitions)
-					a[i][j] = MergeOp.Merge(getNewValue(new P(i, j)), a[i][j]);
+				foreach (var transition in Transitions)
+					transition(new P(i, j, this));
+	}
+
+	public struct P
+	{
+		public int i, j;
+		DP2<T> dp;
+		public P(int _i, int _j, DP2<T> _dp) { i = _i; j = _j; dp = _dp; }
+		public override string ToString() => $"{i} {j}";
+
+		public static explicit operator (int, int)(P v) => (v.i, v.j);
+
+		public static P operator +(P v1, (int i, int j) v2) => new P(v1.i + v2.i, v1.j + v2.j, v1.dp);
+		public static P operator -(P v1, (int i, int j) v2) => new P(v1.i - v2.i, v1.j - v2.j, v1.dp);
+
+		public bool IsInRange(int h, int w) => 0 <= i && i < h && 0 <= j && j < w;
+
+		public T GetValue() => dp.GetValue(this);
+		public T GetValue(int di, int dj) => dp.GetValue(this + (di, dj));
+		public void MergeValue(T value) => dp.MergeValue(this, value);
+		public void MergeValue(int di, int dj, T value) => dp.MergeValue(this + (di, dj), value);
 	}
 }
