20030243b560e083d9b4a902be1e1141dd79b310
Keiho Sakapon <git@saka-pon.net>
2020-12-10 23:51:55 +0900

    Add class for inverse functions

--- CSharp/Contests2020/ARC109/B.cs
+++ CSharp/Contests2020/ARC109/B.cs
@@ -5,11 +5,21 @@ class B
 	static void Main()
 	{
 		var n = long.Parse(Console.ReadLine());
-		Console.WriteLine(n + 1 - InverseFloor_NaturalSum()(n + 1));
+		Console.WriteLine(n + 1 - InverseFunc.FloorNaturalSum()(n + 1));
 	}
+}
 
-	static Func<long, long> InverseFloor(long l, long r, Func<long, long> f) => y => Last(l, r, x => f(x) <= y);
-	static Func<long, long> InverseFloor_NaturalSum() => InverseFloor(-1, 1L << 31, n => n * (n + 1) / 2);
+static class InverseFunc
+{
+	// floor(f^{-1} (x))
+	public static Func<long, long> Floor(long l, long r, Func<long, long> f) => y => Last(l, r, x => f(x) <= y);
+	public static Func<long, long> FloorNaturalSum() => Floor(-1, 1L << 31, n => n * (n + 1) / 2);
+	public static Func<long, long> FloorSqrt() => Floor(-1, 1L << 31, x => x * x);
+
+	// floor(f^{-1} (0))
+	public static long FloorEquation(long l, long r, Func<long, long> f, bool increasing) => Last(l, r, x => increasing ? f(x) <= 0 : f(x) >= 0);
+	public static Func<long, long> FloorNaturalSum2() => s => FloorEquation(-1, 1L << 31, n => n * (n + 1) - 2 * s, true);
+	public static Func<long, long> FloorSqrt2() => y => FloorEquation(-1, 1L << 31, x => x * x - y, true);
 
 	static long Last(long l, long r, Func<long, bool> f)
 	{
