7e19441652bd3a0ba532c578b6489bc8cb4b5444
Keiho Sakapon <git@saka-pon.net>
2020-12-10 14:51:15 +0900

    Update templates

--- CSharp/Contests2020/ABC000/C.cs
+++ CSharp/Contests2020/ABC000/C.cs
@@ -3,14 +3,13 @@
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		//var h = Read();
-		//int n = h[0], m = h[1];
 		var n = int.Parse(Console.ReadLine());
+		//var (n, m) = Read2();
+		var s = Console.ReadLine();
 		var a = Read();
 		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
