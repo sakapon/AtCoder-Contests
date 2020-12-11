7d08f2c579492955e354ae3c64b794842ea6a8ba
Keiho Sakapon <git@saka-pon.net>
2020-12-11 16:32:04 +0900

    Add functions for large long integer

--- CSharp/CoderLib/CoderLib8/Numerics/InverseFunc.cs
+++ CSharp/CoderLib/CoderLib8/Numerics/InverseFunc.cs
@@ -2,6 +2,7 @@
 
 namespace CoderLib8.Numerics
 {
+	// Test: https://atcoder.jp/contests/arc108/tasks/arc108_a
 	// Test: https://atcoder.jp/contests/arc109/tasks/arc109_b
 	static class InverseFunc
 	{
@@ -15,11 +16,31 @@ namespace CoderLib8.Numerics
 		public static Func<long, long> FloorNaturalSum2() => s => FloorEquation(-1, 1L << 31, n => n * (n + 1) - 2 * s, true);
 		public static Func<long, long> FloorSqrt2() => y => FloorEquation(-1, 1L << 31, x => x * x - y, true);
 
+		public static bool IsSquareNumber(long x)
+		{
+			var r = FloorSqrt()(x);
+			return r * r == x;
+		}
+
 		static long Last(long l, long r, Func<long, bool> f)
 		{
 			long m;
 			while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
 			return l;
 		}
+
+		#region For Large long Integer
+
+		// decimal < 2^96
+		// floor(f^{-1} (x))
+		public static Func<decimal, long> FloorDec(long l, long r, Func<decimal, decimal> f) => y => Last(l, r, x => f(x) <= y);
+		public static Func<decimal, long> FloorSqrtDec() => FloorDec(-1, 1L << 45, x => x * x);
+
+		public static bool IsSquareNumberDec(decimal x)
+		{
+			decimal r = FloorSqrtDec()(x);
+			return r * r == x;
+		}
+		#endregion
 	}
 }
---
+++ CSharp/Contests2020/ARC109/B2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class B2
+{
+	static void Main()
+	{
+		var n = long.Parse(Console.ReadLine());
+		var x = (FloorSqrtDec()(9 + 8 * n) - 1) / 2;
+		Console.WriteLine(n + 1 - x);
+	}
+
+	static Func<decimal, long> FloorDec(long l, long r, Func<decimal, decimal> f) => y => Last(l, r, x => f(x) <= y);
+	static Func<decimal, long> FloorSqrtDec() => FloorDec(-1, 1L << 45, x => x * x);
+
+	static long Last(long l, long r, Func<long, bool> f)
+	{
+		long m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
+	}
+}
