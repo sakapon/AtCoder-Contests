88be9941dba779a0b8030ef80e0d63f2beb05720
Keiho Sakapon <git@saka-pon.net>
2020-12-13 09:52:23 +0900

    Update code

--- CSharp/Contests2020/ABC161/D3.cs
+++ CSharp/Contests2020/ABC161/D3.cs
@@ -5,12 +5,12 @@ class D3
 {
 	static void Main()
 	{
-		var k = int.Parse(Console.ReadLine());
+		var k = int.Parse(Console.ReadLine()) - 1;
 
 		long[] Next(long[] a) => a.SelectMany(x => Enumerable.Range(0, 10).Where(d => Math.Abs(x % 10 - d) <= 1).Select(d => x * 10 + d)).ToArray();
 
 		var lun = new long[10][];
-		lun[0] = Enumerable.Range(1, 9).Select(i => (long)i).ToArray();
-		Console.WriteLine(Enumerable.Range(0, 10).SelectMany(i => i > 0 ? lun[i] = Next(lun[i - 1]) : lun[i]).ElementAt(k - 1));
+		var lun0 = Enumerable.Range(1, 9).Select(i => (long)i).ToArray();
+		Console.WriteLine(Enumerable.Range(0, 10).SelectMany(i => lun[i] = i > 0 ? Next(lun[i - 1]) : lun0).ElementAt(k));
 	}
 }
