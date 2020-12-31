f260a9fbfd3f8ddf959311764d7f522286f1376d
Keiho Sakapon <git@saka-pon.net>
2020-12-31 19:43:12 +0900

    Other AC

---
+++ CSharp/PAST/PAST202012/B2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class B2
+{
+	static void Main()
+	{
+		var h = "".ToHashSet();
+		Console.ReadLine();
+		Console.WriteLine(string.Join("", Console.ReadLine().Reverse().Where(h.Add).Reverse()));
+	}
+}
---
+++ CSharp/PAST/PAST202012/B3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class B3
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine();
+
+		var d = new Dictionary<char, int>();
+		for (int i = 0; i < n; i++)
+			d[s[i]] = i;
+		Console.WriteLine(string.Join("", d.OrderBy(p => p.Value).Select(p => p.Key)));
+	}
+}
