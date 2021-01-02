7d57de3be52639a24d42be4c9ac2a44dbcd99e62
Keiho Sakapon <git@saka-pon.net>
2021-01-02 12:50:41 +0900

    AC

--- CSharp/PAST/PAST202012/L.cs
+++ CSharp/PAST/PAST202012/L.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class L
 {
@@ -11,74 +9,42 @@ class L
 		var s = Console.ReadLine();
 		var t = Console.ReadLine();
 
-		var dc = t.Distinct().Count();
-		if (dc == 2 && t[0] == t[2])
+		if (t[0] != t[1] && t[0] == t[2])
 		{
 			if (!s.Contains(t)) return 0;
 
-			var map = Array.ConvertAll(new bool[n / 3 + 1], _ => new HashSet<string>());
-			var all = new Stack<string>();
-			map[1].Add(t);
-			all.Push(t);
+			var dp = NewArray2<int>(n + 1, n + 1);
 
-			var (s11, s12) = (t[..1], t[1..]);
-			var (s21, s22) = (t[..2], t[2..]);
-
-			void Add(int i, string nx)
+			for (int d = 3; d <= n; d++)
 			{
-				if (!map[i].Contains(nx) && s.Contains(nx))
+				for (int l = 0, r = d; r <= n; l++, r++)
 				{
-					map[i].Add(nx);
-					all.Push(nx);
-				}
-			}
+					dp[l][r] = Math.Max(dp[l + 1][r], dp[l][r - 1]);
 
-			for (int i = 2; i < map.Length; i++)
-			{
-				foreach (var x in map[i - 1])
-				{
-					Add(i, $"{s11}{x}{s12}");
-					Add(i, $"{s21}{x}{s22}");
-				}
+					if (s[l] != t[0] || s[r - 1] != t[2]) continue;
+					for (int m = l + 1; m < r; m++)
+						dp[l][r] = Math.Max(dp[l][r], dp[l][m] + dp[m][r]);
 
-				for (int j = 1; j < i - j; j++)
-					foreach (var x in map[j])
-						foreach (var y in map[i - j])
+					if (d % 3 != 0) continue;
+					for (int m = l + 1; m < r - 1; m++)
+					{
+						if (s[m] == t[1] && 3 * dp[l + 1][m] == m - l - 1 && 3 * dp[m + 1][r - 1] == r - m - 2)
 						{
-							Add(i, x + y);
-							Add(i, y + x);
+							dp[l][r] = d / 3;
+							break;
 						}
-
-				if (i % 2 == 0)
-				{
-					var a = map[i / 2].ToArray();
-					for (int xi = 0; xi < a.Length; xi++)
-						for (int yi = xi; yi < a.Length; yi++)
-						{
-							Add(i, a[xi] + a[yi]);
-							Add(i, a[yi] + a[xi]);
-						}
-				}
-
-				if (map[i].Count == 0) break;
-			}
-
-			foreach (var x in all)
-			{
-				for (int i; (i = s.IndexOf(x)) != -1;)
-				{
-					s = s.Remove(i, x.Length);
+					}
 				}
 			}
+			return dp[0][n];
 		}
 		else
 		{
 			for (int i; (i = s.IndexOf(t)) != -1;)
-			{
 				s = s.Remove(i, 3);
-			}
+			return (n - s.Length) / 3;
 		}
-
-		return (n - s.Length) / 3;
 	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 }
