617e34d82ddf70c01ec3c70842c627fba9c9adff
Keiho Sakapon <git@saka-pon.net>
2021-01-07 12:00:12 +0900

    Initialize

---
+++ CSharp/Contests2021/Paken2020Day2/F.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class F
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		//var (n, m) = Read2();
+		var s = Console.ReadLine();
+		var a = Read();
+		var ps = Array.ConvertAll(new bool[n], _ => Read());
+
+		Console.WriteLine(string.Join(" ", a));
+	}
+}
