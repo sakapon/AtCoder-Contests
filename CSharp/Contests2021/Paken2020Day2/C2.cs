28cf17d45fa8370e254403f01b6482a552794882
Keiho Sakapon <git@saka-pon.net>
2021-01-07 18:07:52 +0900

    Other AC

---
+++ CSharp/Contests2021/Paken2020Day2/C2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class C2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
+	static object Solve()
+	{
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+		var c = new int[n + 1];
+		foreach (var (a, b) in es)
+		{
+			if (c[a] < 0) m--;
+			c[a]++;
+			if (c[b] > 0) m--;
+			c[b]--;
+		}
+		return m;
+	}
+}
