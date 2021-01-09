6652c002e5df66e7e9db50761652f71d4f053281
Keiho Sakapon <git@saka-pon.net>
2021-01-09 11:40:42 +0900

    Improve code

--- CSharp/Yukicoder/YCC277/B.cs
+++ CSharp/Yukicoder/YCC277/B.cs
@@ -4,7 +4,6 @@ using System.Linq;
 class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main()
@@ -12,11 +11,10 @@ class B
 		var (n, m, p) = Read3();
 		var a = ReadL();
 
-		var amax = a.Max();
+		var a_max = a.Max();
 
-		// p^j を含む a_i
-		var pa = new long[32];
 		// a_i / p^j
+		// j + 1 回でこの値による乗算
 		var da = new long[32];
 
 		foreach (var v in a)
@@ -28,17 +26,16 @@ class B
 				t /= p;
 				j++;
 			}
-			pa[j] = Math.Max(pa[j], v);
 			da[j] = Math.Max(da[j], t);
 		}
 
-		var dp = new long[200];
+		var dp = new long[1 << 10];
 		dp[0] = 1;
 
-		for (int i = 0; i < dp.Length - da.Length - 20; i++)
+		for (int i = 0; i < dp.Length - da.Length; i++)
 		{
 			var x = dp[i];
-			if (x * amax > m)
+			if (x * a_max > m)
 			{
 				Console.WriteLine(i + 1);
 				return;
