8656c19a801f431a92930d289564ebb2f3ad84fc
Keiho Sakapon <git@saka-pon.net>
2021-01-10 23:56:26 +0900

    AC

---
+++ CSharp/Contests2021/Paken2020Day1/J.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class J
+{
+	static void Main()
+	{
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		Solve();
+		Console.Out.Flush();
+	}
+
+	static void Solve()
+	{
+		int count = 0;
+		for (int p = 1; ; p++)
+			for (int q = 1; q < p; q++)
+			{
+				var (a, b, c) = (2 * p * q, p * p - q * q, p * p + q * q);
+				if (Gcd(a, b) != 1) continue;
+				if (a > b) (a, b) = (b, a);
+				Console.WriteLine($"{a} {b} {c}");
+				if (++count == 100000) return;
+			}
+	}
+
+	static long Gcd(long a, long b) { for (long r; (r = a % b) > 0; a = b, b = r) ; return b; }
+}
