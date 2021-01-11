d13c4a142d709f7b2504856f1213be20e76e11bb
Keiho Sakapon <git@saka-pon.net>
2021-01-11 15:27:13 +0900

    AC

---
+++ CSharp/Contests2021/Paken2020Day1/L.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class L
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+	static void Main()
+	{
+		var (q, x) = Read2L();
+		var a = Array.ConvertAll(new bool[q], _ => int.Parse(Console.ReadLine()));
+
+		const long M = 1000000007;
+		var p10 = PowsL(10, 10);
+		var p2 = 1L;
+
+		foreach (var v in a)
+		{
+			p2 *= 2;
+			p2 %= M;
+
+			x += x * (p10[v.ToString().Length] % M);
+			x += v * p2;
+			x %= M;
+		}
+		Console.WriteLine(x);
+	}
+
+	public static long[] PowsL(long b, int n)
+	{
+		var p = new long[n + 1];
+		p[0] = 1;
+		for (int i = 0; i < n; ++i) p[i + 1] = p[i] * b;
+		return p;
+	}
+}
--- CSharp/PAST/PAST202012/O.cs
+++ CSharp/PAST/PAST202012/O.cs
@@ -8,29 +8,54 @@ class O
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static void Main()
 	{
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
 		var (n, m) = Read2();
 		var es = Array.ConvertAll(new bool[m], _ => Read());
 		var qc = int.Parse(Console.ReadLine());
 		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
+		const int c = 1000;
 		var map = EdgesToMap1(n + 1, es, false);
+		var stars = Enumerable.Range(1, n).Where(v => map[v].Count > c).ToArray();
+
+		var hs = new HashSet<long>();
+		foreach (var e in es)
+		{
+			hs.Add((1L << 30) * e[0] + e[1]);
+			hs.Add((1L << 30) * e[1] + e[0]);
+		}
+
+		var lazy = new int[n + 1];
 		var notes = new int[n + 1];
 		var ends = new int[n + 1];
 
-		foreach (var q in qs)
+		foreach (var (t, x) in qs)
 		{
-			var x = q.Item2;
-			if (q.Item1 == 1)
+			if (t == 1)
 			{
-				notes[x]++;
+				if (map[x].Count <= c)
+				{
+					foreach (var y in map[x])
+						notes[y]++;
+				}
+				else
+				{
+					lazy[x]++;
+				}
 			}
 			else
 			{
-				var sum = map[x].Sum(y => notes[y]);
-				Console.WriteLine(sum - ends[x]);
+				var sum = 0;
+				foreach (var y in stars)
+					if (hs.Contains((1L << 30) * x + y))
+						sum += lazy[y];
+				Console.WriteLine(notes[x] + sum - ends[x]);
+
+				notes[x] = 0;
 				ends[x] = sum;
 			}
 		}
+		Console.Out.Flush();
 	}
 
 	static List<int>[] EdgesToMap1(int n, int[][] es, bool directed)
