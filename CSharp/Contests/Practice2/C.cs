89cb52de32a37e36a09d272fe7f2d7d2bea10486
Keiho Sakapon <git@saka-pon.net>
2021-01-23 18:16:06 +0900

    AC

--- CSharp/Contests/Practice2/C.cs
+++ CSharp/Contests/Practice2/C.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-using System.Linq;
 
 class C
 {
-	static void Main()
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int, int, int) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+	static void Main() => Console.WriteLine(string.Join("\n", Array.ConvertAll(new bool[int.Parse(Console.ReadLine())], _ => Solve())));
+	static object Solve()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
-		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
+		var (n, m, a, b) = Read4();
+		return FloorSum(n, m, a, b);
+	}
 
-		Console.WriteLine(string.Join(" ", h));
+	// https://github.com/atcoder/ac-library/blob/master/atcoder/internal_math.hpp
+	static long FloorSum(long n, long m, long a, long b)
+	{
+		var r = 0L;
+		while (true)
+		{
+			if (a >= m)
+			{
+				r += n * (n - 1) / 2 * (a / m);
+				a %= m;
+			}
+			if (b >= m)
+			{
+				r += n * (b / m);
+				b %= m;
+			}
+
+			var yMax = a * n + b;
+			if (yMax < m) return r;
+			(n, m, a, b) = (yMax / m, a, m, yMax % m);
+		}
 	}
 }
