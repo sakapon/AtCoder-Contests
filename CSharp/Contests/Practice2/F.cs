e4077dde3f226b9f8f7927bf7d2ff6f7a322ae68
Keiho Sakapon <git@saka-pon.net>
2021-01-27 23:29:42 +0900

    Add class for NTT

--- CSharp/Contests/Practice2/F.cs
+++ CSharp/Contests/Practice2/F.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 class F
 {
@@ -12,15 +11,16 @@ class F
 		var a = ReadL();
 		var b = ReadL();
 
-		var c = Convolution(a, b);
-		Console.WriteLine(string.Join(" ", c.Take(n + m - 1)));
+		var c = Ntt.Convolution(a, b);
+		Console.WriteLine(string.Join(" ", c[..(n + m - 1)]));
 	}
+}
 
-	const long p = 998244353;
-	const long g = 3;
+public class Ntt
+{
+	const long p = 998244353, g = 3;
 	const long half = (p + 1) / 2;
 
-	static long MInt(long x, long M) => (x %= M) < 0 ? x + M : x;
 	static long MPow(long b, long i)
 	{
 		long r = 1;
@@ -28,19 +28,33 @@ class F
 		return r;
 	}
 
-	static long NthRoot(int n, int i)
+	static long[] NthRoots(int n)
 	{
-		return MPow(g, (p - 1) / n * MInt(i, n));
+		var z = MPow(g, (p - 1) / n);
+		var r = new long[n + 1];
+		r[0] = 1;
+		for (int i = 0; i < n; ++i) r[i + 1] = r[i] * z % p;
+		return r;
 	}
 
 	// n: Power of 2
-	// { f(z^i) }
-	public static long[] Fft(long[] c, bool inverse = false)
+	int n;
+	long[] roots;
+	public Ntt(int n)
 	{
-		var n = c.Length;
-		if (n == 1) return c;
+		this.n = n;
+		roots = NthRoots(n);
+	}
 
-		var n2 = n / 2;
+	long MthRoot(int m, int i) => roots[n / m * i];
+
+	// { f(z^i) }
+	public void Transform(long[] c, bool inverse = false)
+	{
+		var m = c.Length;
+		if (m == 1) return;
+
+		var n2 = m / 2;
 		var c1 = new long[n2];
 		var c2 = new long[n2];
 		for (int i = 0; i < n2; ++i)
@@ -49,31 +63,30 @@ class F
 			c2[i] = c[2 * i + 1];
 		}
 
-		var f1 = Fft(c1, inverse);
-		var f2 = Fft(c2, inverse);
+		Transform(c1, inverse);
+		Transform(c2, inverse);
 
-		var r = new long[n];
 		for (int i = 0; i < n2; ++i)
 		{
-			var z = f2[i] * NthRoot(n, inverse ? -i : i) % p;
-			r[i] = (f1[i] + z) % p;
-			r[n2 + i] = (f1[i] - z + p) % p;
+			var z = c2[i] * MthRoot(m, inverse ? m - i : i) % p;
+			c[i] = (c1[i] + z) % p;
+			c[n2 + i] = (c1[i] - z + p) % p;
 			if (inverse)
 			{
-				r[i] = r[i] * half % p;
-				r[n2 + i] = r[n2 + i] * half % p;
+				c[i] = c[i] * half % p;
+				c[n2 + i] = c[n2 + i] * half % p;
 			}
 		}
-		return r;
 	}
 
 	// n: Power of 2
-	static long[] Convolution_In(long[] a, long[] b)
+	long[] Convolution_In(long[] a, long[] b)
 	{
-		var fa = Fft(a);
-		var fb = Fft(b);
-		for (int i = 0; i < a.Length; ++i) fa[i] = fa[i] * fb[i] % p;
-		return Fft(fa, true);
+		Transform(a);
+		Transform(b);
+		for (int i = 0; i < n; ++i) a[i] = a[i] * b[i] % p;
+		Transform(a, true);
+		return a;
 	}
 
 	public static long[] Convolution(long[] a, long[] b)
@@ -85,7 +98,6 @@ class F
 		var bc = new long[n];
 		a.CopyTo(ac, 0);
 		b.CopyTo(bc, 0);
-
-		return Convolution_In(ac, bc);
+		return new Ntt(n).Convolution_In(ac, bc);
 	}
 }
