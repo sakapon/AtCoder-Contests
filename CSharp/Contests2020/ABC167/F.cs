c71c9c2a6283387b162a0f0c12bf15863cdcc685
Keiho Sakapon <git@saka-pon.net>
2021-02-02 10:01:10 +0900

    AC

--- CSharp/Contests2020/ABC167/F.cs
+++ CSharp/Contests2020/ABC167/F.cs
@@ -1,18 +1,40 @@
 ﻿using System;
+using System.Linq;
 
 class F
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main()
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		//var (n, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var s = Array.ConvertAll(new bool[n], _ => Console.ReadLine());
 
-		Console.WriteLine(string.Join(" ", a));
+		var count = 0;
+		foreach (var si in s)
+			foreach (var c in si)
+				if (c == '(') count--;
+				else count++;
+		if (count != 0) return false;
+
+		var lrs = new (int l, int r)[n];
+		for (int i = 0; i < n; i++)
+		{
+			var (l, r) = (0, 0);
+			foreach (var c in s[i])
+				if (c == '(') l++;
+				else if (l > 0) l--;
+				else r++;
+			lrs[i] = (l, r);
+		}
+
+		var al = 0;
+		var q1 = lrs.Where(lr => lr.l >= lr.r).OrderBy(lr => lr.r);
+		var q2 = lrs.Where(lr => lr.l < lr.r).OrderBy(lr => -lr.l);
+		foreach (var (l, r) in q1.Concat(q2))
+		{
+			if (al < r) return false;
+			al += l - r;
+		}
+		return al == 0;
 	}
 }
