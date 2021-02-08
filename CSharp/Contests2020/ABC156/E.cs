5a666b12392965f8b077ac70b36741abdbd3b5e4
Keiho Sakapon <git@saka-pon.net>
2021-02-08 11:42:33 +0900

    Update library

--- CSharp/Contests2020/ABC156/E.cs
+++ CSharp/Contests2020/ABC156/E.cs
@@ -25,3 +25,27 @@ class E
 		return c;
 	}
 }
+
+struct MInt
+{
+	const long M = 1000000007;
+	public long V;
+	public MInt(long v) { V = (v %= M) < 0 ? v + M : v; }
+	public override string ToString() => $"{V}";
+	public static implicit operator MInt(long v) => new MInt(v);
+
+	public static MInt operator -(MInt x) => -x.V;
+	public static MInt operator +(MInt x, MInt y) => x.V + y.V;
+	public static MInt operator -(MInt x, MInt y) => x.V - y.V;
+	public static MInt operator *(MInt x, MInt y) => x.V * y.V;
+	public static MInt operator /(MInt x, MInt y) => x.V * y.Inv().V;
+
+	public static long MPow(long b, long i)
+	{
+		long r = 1;
+		for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+		return r;
+	}
+	public MInt Pow(long i) => MPow(V, i);
+	public MInt Inv() => MPow(V, M - 2);
+}
