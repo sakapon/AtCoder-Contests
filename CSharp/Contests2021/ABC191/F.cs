f8e75f413773f9a5ce09b23904045357897e7536
Keiho Sakapon <git@saka-pon.net>
2021-02-23 14:08:38 +0900

    AC

--- CSharp/Contests2021/ABC191/F.cs
+++ CSharp/Contests2021/ABC191/F.cs
@@ -10,75 +10,25 @@ class F
 		var n = int.Parse(Console.ReadLine());
 		var a = Read();
 
+		var map = new Dictionary<int, int>();
+		foreach (var x in a)
+			foreach (int d in Divisors(x))
+				if (map.ContainsKey(d)) map[d] = Gcd(map[d], x);
+				else map[d] = x;
+
 		var min = a.Min();
-		var set = new HashSet<int>(a);
-		var q = PQ<int>.Create(true);
-		q.PushRange(a);
-
-		while (q.Count > 0)
-		{
-			var v = q.Pop();
-			var l = new List<int>();
-
-			foreach (var u in q)
-			{
-				var gcd = Gcd(v, u);
-				if (set.Add(gcd)) l.Add(gcd);
-			}
-			q.PushRange(l.ToArray());
-		}
-		Console.WriteLine(set.Count(v => v <= min));
+		Console.WriteLine(map.Count(p => p.Key == p.Value && p.Key <= min));
 	}
 
 	static int Gcd(int a, int b) { for (int r; (r = a % b) > 0; a = b, b = r) ; return b; }
-}
 
-class PQ<T> : List<T>
-{
-	public static PQ<T> Create(bool desc = false)
+	static long[] Divisors(long n)
 	{
-		var c = Comparer<T>.Default;
-		return desc ?
-			new PQ<T>((x, y) => c.Compare(y, x)) :
-			new PQ<T>(c.Compare);
-	}
-
-	public static PQ<T> Create<TKey>(Func<T, TKey> toKey, bool desc = false)
-	{
-		var c = Comparer<TKey>.Default;
-		return desc ?
-			new PQ<T>((x, y) => c.Compare(toKey(y), toKey(x))) :
-			new PQ<T>((x, y) => c.Compare(toKey(x), toKey(y)));
-	}
-
-	Comparison<T> c;
-	public T First => this[0];
-	internal PQ(Comparison<T> _c) { c = _c; }
-
-	void Swap(int i, int j) { var o = this[i]; this[i] = this[j]; this[j] = o; }
-	void UpHeap(int i) { for (int j; i > 0 && c(this[j = (i - 1) / 2], this[i]) > 0; Swap(i, i = j)) ; }
-	void DownHeap(int i)
-	{
-		for (int j; (j = 2 * i + 1) < Count;)
-		{
-			if (j + 1 < Count && c(this[j], this[j + 1]) > 0) j++;
-			if (c(this[i], this[j]) > 0) Swap(i, i = j); else break;
-		}
-	}
-
-	public void Push(T v)
-	{
-		Add(v);
-		UpHeap(Count - 1);
-	}
-	public void PushRange(T[] vs) { foreach (var v in vs) Push(v); }
-
-	public T Pop()
-	{
-		var r = this[0];
-		this[0] = this[Count - 1];
-		RemoveAt(Count - 1);
-		DownHeap(0);
-		return r;
+		var r = new List<long>();
+		for (long x = 1; x * x <= n; ++x) if (n % x == 0) r.Add(x);
+		var i = r.Count - 1;
+		if (r[i] * r[i] == n) --i;
+		for (; i >= 0; --i) r.Add(n / r[i]);
+		return r.ToArray();
 	}
 }
