283e8be6c7fd28c92c76dc6a599825b306be9f1c
Keiho Sakapon <git@saka-pon.net>
2021-02-23 11:56:46 +0900

    Improve code

--- CSharp/Contests2021/ABC191/B.cs
+++ CSharp/Contests2021/ABC191/B.cs
@@ -4,11 +4,9 @@ using System.Linq;
 class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static void Main()
 	{
-		var (n, x) = Read2();
-		var a = Read();
-		Console.WriteLine(string.Join(" ", a.Where(v => v != x)));
+		var x = Read()[1];
+		Console.WriteLine(string.Join(" ", Read().Where(v => v != x)));
 	}
 }
--- CSharp/Contests2021/ABC191/C.cs
+++ CSharp/Contests2021/ABC191/C.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class C
+static class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
@@ -15,21 +15,8 @@ class C
 		var c2 = Enumerable.Range(0, w - 1).Sum(j => Enumerable.Range(0, h).GroupCountsBySeq(i => s[i][j] != s[i][j + 1]).Count(g => g.Key));
 		Console.WriteLine(c1 + c2);
 	}
-}
 
-static class GE
-{
-	public static Dictionary<TK, int> GroupCounts<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
-	{
-		var d = new Dictionary<TK, int>();
-		TK k;
-		foreach (var o in source)
-			if (d.ContainsKey(k = toKey(o))) ++d[k];
-			else d[k] = 1;
-		return d;
-	}
-
-	public static IEnumerable<KeyValuePair<TK, int>> GroupCountsBySeq<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
+	static IEnumerable<KeyValuePair<TK, int>> GroupCountsBySeq<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
 	{
 		var c = EqualityComparer<TK>.Default;
 		TK k = default(TK), kt;
@@ -46,35 +33,5 @@ static class GE
 			++count;
 		}
 		if (count > 0) yield return new KeyValuePair<TK, int>(k, count);
-	}
-
-	public static IEnumerable<IGrouping<TK, TS>> GroupBySeq<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
-	{
-		var c = EqualityComparer<TK>.Default;
-		var k = default(TK);
-		var l = new List<TS>();
-
-		foreach (var o in source)
-		{
-			var kt = toKey(o);
-			if (!c.Equals(kt, k))
-			{
-				if (l.Count > 0) yield return new G<TK, TS>(k, l.ToArray());
-				k = kt;
-				l.Clear();
-			}
-			l.Add(o);
-		}
-		if (l.Count > 0) yield return new G<TK, TS>(k, l.ToArray());
-	}
-
-	class G<TK, TE> : IGrouping<TK, TE>
-	{
-		public TK Key { get; }
-		IEnumerable<TE> Values;
-		public G(TK key, TE[] values) { Key = key; Values = values; }
-
-		public IEnumerator<TE> GetEnumerator() => Values.GetEnumerator();
-		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
 	}
 }
