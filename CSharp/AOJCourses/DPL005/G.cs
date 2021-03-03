a4cf2c6f6e001eb8fb850e88f71de6df8d57367b
Keiho Sakapon <git@saka-pon.net>
2021-03-03 16:13:29 +0900

    AC

---
+++ CSharp/AOJCourses/DPL005/G.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class G
+{
+	static void Main()
+	{
+		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		int n = h[0], k = h[1];
+
+		var f = new MInt[k + 1];
+		var t = new MInt[k + 1];
+		f[0] = 1;
+		t[0] = 1;
+		for (int i = 1; i <= k; ++i)
+		{
+			f[i] = f[i - 1] / i;
+			t[i] = t[i - 1] + f[i] * (i % 2 == 0 ? 1 : -1);
+		}
+
+		MInt r = 0;
+		for (int i = 1; i <= k; ++i)
+		{
+			r += MInt.MPow(i, n) * f[i] * t[k - i];
+		}
+		Console.WriteLine(r);
+	}
+}
---
+++ CSharp/AOJCourses/DPL005/I.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class I
+{
+	static void Main()
+	{
+		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		int n = h[0], k = h[1];
+
+		MInt r = 0, c = 1, f = 1;
+		for (int i = 0; i < k; i++)
+		{
+			r += c * MInt.MPow(k - i, n);
+			c *= -(k - i);
+			c /= i + 1;
+			f *= i + 1;
+		}
+		Console.WriteLine(r / f);
+	}
+}
