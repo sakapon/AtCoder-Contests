dab66218cac7b8add44b7a22c7c59a046e578cb1
Keiho Sakapon <git@saka-pon.net>
2021-03-04 10:35:36 +0900

    Add helper class for string

---
+++ CSharp/CoderLib/CoderLib6/Strings/StringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CoderLib6.Strings
+{
+	static class StringHelper
+	{
+		static string Reverse(string s)
+		{
+			var c = s.ToCharArray();
+			Array.Reverse(c);
+			return new string(c);
+		}
+
+		static bool IsPalindrome(string s)
+		{
+			for (int i = 0; i < s.Length; ++i) if (s[i] != s[s.Length - 1 - i]) return false;
+			return true;
+		}
+	}
+}
