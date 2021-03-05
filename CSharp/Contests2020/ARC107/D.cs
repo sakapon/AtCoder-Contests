d5398af2d213e14789e37f13fdf04f71c94fab4c
Keiho Sakapon <git@saka-pon.net>
2021-03-05 09:27:06 +0900

    AC

--- CSharp/Contests2020/ARC107/D.cs
+++ CSharp/Contests2020/ARC107/D.cs
@@ -2,25 +2,25 @@
 
 class D
 {
+	const long M = 998244353;
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static void Main()
 	{
 		var (n, k) = Read2();
-		Console.WriteLine(PartitionNonZero(n, k));
-	}
 
-	const long M = 998244353;
-	static long[,] PartitionDP(int n, int k)
-	{
-		var dp = new long[n + 1, k + 1];
-		for (int j = 1; j <= k; ++j) dp[0, j] = 1;
+		var dp = new long[n + 1, n + 1];
+		dp[0, 0] = 1;
 
-		for (int i = 1; i <= n; ++i)
-			for (int j = 1; j <= k; ++j)
-				dp[i, j] = (dp[i, j - 1] + (j <= i ? dp[i - j, j] : 0)) % M;
-		return dp;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = i; j > 0; j--)
+			{
+				dp[i, j] = dp[i - 1, j - 1];
+				if (2 * j <= i) dp[i, j] += dp[i, 2 * j];
+				dp[i, j] %= M;
+			}
+		}
+		Console.WriteLine(dp[n, k]);
 	}
-	static long Partition(int n, int k) => PartitionDP(n, k)[n, k];
-	static long PartitionNonZero(int n, int k) => n < k ? 0 : Partition(n - k, k);
 }
