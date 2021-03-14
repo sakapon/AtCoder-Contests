219751e58033eca890706f011f319da92e048607
Keiho Sakapon <git@saka-pon.net>
2021-03-14 23:44:24 +0900

    Add helper class for greedy

---
+++ CSharp/CoderLib/CoderLib8/Extra/GreedyHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoderLib8.Extra
+{
+	public static class GreedyHelper
+	{
+		// k 回目に true となる日を求めます。
+		// k, day: 0-indexed
+		// 0 <= trueDay < period
+		public static long KthTrueDay(long k, long period, long[] trueDays)
+		{
+			var turns = Math.DivRem(k, trueDays.Length, out k);
+			return turns * period + trueDays[k];
+		}
+
+		// Test: https://codeforces.com/contest/1501/problem/D
+		// k 回目に false となる日を求めます。
+		// k, day: 0-indexed
+		// 0 <= trueDay < period
+		public static long KthFalseDay(long k, long period, long[] trueDays)
+		{
+			var turns = Math.DivRem(k, period - trueDays.Length, out k);
+			for (int i = 0; i < trueDays.Length; ++i)
+				if (k + i < trueDays[i])
+					return turns * period + k + i;
+			return turns * period + k + trueDays.Length;
+		}
+	}
+}
---
+++ CSharp/CoderLib/CoderLibTest/Extra/GreedyTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using CoderLib8.Extra;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoderLibTest.Extra
+{
+	[TestClass]
+	public class GreedyTest
+	{
+		[TestMethod]
+		public void KthTrueDay_1()
+		{
+			var period = 9L;
+			var trueDays = new long[] { 0, 2, 6 };
+
+			void Test(long expected, long k)
+			{
+				Assert.AreEqual(expected, GreedyHelper.KthTrueDay(k, period, trueDays));
+			}
+
+			var es = new long[] { 0, 2, 6, 9, 11, 15, 18 };
+			for (int i = 0; i < es.Length; i++)
+				Test(es[i], i);
+		}
+
+		[TestMethod]
+		public void KthTrueDay_2()
+		{
+			var period = 9L;
+			var trueDays = new long[] { 1, 5, 8 };
+
+			void Test(long expected, long k)
+			{
+				Assert.AreEqual(expected, GreedyHelper.KthTrueDay(k, period, trueDays));
+			}
+
+			var es = new long[] { 1, 5, 8, 10, 14, 17, 19 };
+			for (int i = 0; i < es.Length; i++)
+				Test(es[i], i);
+		}
+
+		[TestMethod]
+		public void KthFalseDay_1()
+		{
+			var period = 9L;
+			var trueDays = new long[] { 0, 2, 6 };
+
+			void Test(long expected, long k)
+			{
+				Assert.AreEqual(expected, GreedyHelper.KthFalseDay(k, period, trueDays));
+			}
+
+			var es = new long[] { 1, 3, 4, 5, 7, 8, 10, 12, 13, 14, 16, 17, 19, 21 };
+			for (int i = 0; i < es.Length; i++)
+				Test(es[i], i);
+		}
+
+		[TestMethod]
+		public void KthFalseDay_2()
+		{
+			var period = 9L;
+			var trueDays = new long[] { 1, 5, 8 };
+
+			void Test(long expected, long k)
+			{
+				Assert.AreEqual(expected, GreedyHelper.KthFalseDay(k, period, trueDays));
+			}
+
+			var es = new long[] { 0, 2, 3, 4, 6, 7, 9, 11, 12, 13, 15, 16, 18, 20 };
+			for (int i = 0; i < es.Length; i++)
+				Test(es[i], i);
+		}
+	}
+}
