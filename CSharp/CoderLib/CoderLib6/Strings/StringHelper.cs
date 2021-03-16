72647dd8f911965f1ed9dae294a439ee19e47f04
Keiho Sakapon <git@saka-pon.net>
2021-03-16 15:42:06 +0900

    Add functions

--- CSharp/CoderLib/CoderLib6/Strings/StringHelper.cs
+++ CSharp/CoderLib/CoderLib6/Strings/StringHelper.cs
@@ -5,6 +5,15 @@ namespace CoderLib6.Strings
 	// Test: https://codeforces.com/contest/1304/problem/B
 	static class StringHelper
 	{
+		static int ToInt(this char c) => c - 48;
+		static char ToNumber(this int i) => (char)(i + 48);
+
+		static int ToIndexForUpper(this char c) => c - 65;
+		static char ToUpper(this int i) => (char)(i + 65);
+
+		static int ToIndexForLower(this char c) => c - 97;
+		static char ToLower(this int i) => (char)(i + 97);
+
 		static string Reverse(string s)
 		{
 			var c = s.ToCharArray();
