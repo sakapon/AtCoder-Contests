cc013d30bf676fcdc4c9445d010a1d51b999baac
Keiho Sakapon <git@saka-pon.net>
2021-04-03 12:50:52 +0900

    Add functions

--- CSharp/CoderLib/CoderLib8/Extra/PrimesHelper.cs
+++ CSharp/CoderLib/CoderLib8/Extra/PrimesHelper.cs
@@ -13,5 +13,41 @@ namespace CoderLib8.Extra
 			for (int p = 2; p <= n; ++p) if (t[p] == 0) for (int x = p; x <= n; x += p) ++t[x];
 			return t;
 		}
+
+		// n 以下のすべての数に対する、素因数の個数 O(n)?
+		// 10_000_000 程度までなら速い
+		static int[] GetFactorCounts(int n)
+		{
+			var d = new int[n + 1];
+			for (int p = 2; p * p <= n; ++p)
+				if (d[p] == 0)
+					for (int x = p * p; x <= n; x += p)
+						d[x] = p;
+
+			var c = new int[n + 1];
+			for (int x = 2; x <= n; ++x)
+				c[x] = d[x] == 0 ? 1 : c[x / d[x]] + 1;
+			return c;
+		}
+
+		// Naive
+		static int[] GetFactorCounts0(int n)
+		{
+			var a = new int[n + 1];
+			for (int i = 1; i <= n; ++i) a[i] = i;
+			var c = new int[n + 1];
+
+			for (int q = 2; q <= n; ++q)
+				if (a[q] != 1)
+				{
+					var p = a[q];
+					for (int x = q; x <= n; x += q)
+					{
+						a[x] /= p;
+						++c[x];
+					}
+				}
+			return c;
+		}
 	}
 }
