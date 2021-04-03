085f31a370d7ed8b07cb7e97bfce88b4607739c9
Keiho Sakapon <git@saka-pon.net>
2021-04-03 11:33:13 +0900

    Add PrimesHelper class

---
+++ CSharp/CoderLib/CoderLib8/Extra/PrimesHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoderLib8.Extra
+{
+	static class PrimesHelper
+	{
+		// n = 20000000 の例:
+		// Test: https://codeforces.com/contest/1499/problem/D
+		// n 以下のすべての数に対する、素因数の種類の数 O(n)?
+		static int[] GetPrimeTypes(int n)
+		{
+			var t = new int[n + 1];
+			for (int p = 2; p <= n; ++p) if (t[p] == 0) for (int x = p; x <= n; x += p) ++t[x];
+			return t;
+		}
+	}
+}
