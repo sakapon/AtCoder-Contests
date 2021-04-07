5ad4d3d7dc7f321ab92ae2327781297a142f719a
Keiho Sakapon <git@saka-pon.net>
2021-04-07 17:20:40 +0900

    Other AC

---
+++ CSharp/Contests2021/ARC115/C2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class C2
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+
+		var a = GetFactorCounts(n);
+		Console.WriteLine(string.Join(" ", a[1..].Select(x => x + 1)));
+	}
+
+	static int[] GetFactorCounts(int n)
+	{
+		var d = new int[n + 1];
+		for (int p = 2; p * p <= n; ++p)
+			if (d[p] == 0)
+				for (int x = p * p; x <= n; x += p)
+					d[x] = p;
+
+		var c = new int[n + 1];
+		for (int x = 2; x <= n; ++x)
+			c[x] = d[x] == 0 ? 1 : c[x / d[x]] + 1;
+		return c;
+	}
+}
