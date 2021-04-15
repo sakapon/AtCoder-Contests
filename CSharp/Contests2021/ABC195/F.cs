207aff6ef51bb437ff58b0b9151e68919e3816d3
Keiho Sakapon <git@saka-pon.net>
2021-04-16 03:37:17 +0900

    AC

--- CSharp/Contests2021/ABC195/F.cs
+++ CSharp/Contests2021/ABC195/F.cs
@@ -1,18 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class F
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main()
 	{
-		var n = int.Parse(Console.ReadLine());
-		//var (n, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (a, b) = Read2L();
 
-		Console.WriteLine(string.Join(" ", a));
+		var n = (int)(b - a + 1);
+		var rn = Enumerable.Range(0, n).Select(i => a + i).ToArray();
+		var pn = GetPrimes(n);
+		var fn = new int[n];
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < pn.Length; j++)
+				if (rn[i] % pn[j] == 0)
+					fn[i] |= 1 << j;
+
+		var dp = new long[1 << pn.Length];
+		dp[0] = 1;
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int x = 0; x < dp.Length; x++)
+			{
+				if ((x & fn[i]) == 0)
+				{
+					// つねに coprime である数 (fn[i] == 0) の場合は 2 倍になります。
+					dp[x | fn[i]] += dp[x];
+				}
+			}
+		}
+		Console.WriteLine(dp.Sum());
+	}
+
+	static int[] GetPrimes(int n)
+	{
+		var b = new bool[n + 1];
+		for (int p = 2; p * p <= n; ++p) if (!b[p]) for (int x = p * p; x <= n; x += p) b[x] = true;
+		var r = new List<int>();
+		for (int x = 2; x <= n; ++x) if (!b[x]) r.Add(x);
+		return r.ToArray();
 	}
 }
