fb008a61f425f006ba8f62951171b388146b46c4
Keiho Sakapon <git@saka-pon.net>
2021-04-19 18:26:42 +0900

    Improve code

--- CSharp/Contests2020/ABC184/F2.cs
+++ CSharp/Contests2020/ABC184/F2.cs
@@ -6,11 +6,10 @@ class F2
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main()
 	{
 		var (n, t) = Read2();
-		var a = ReadL();
+		var a = Read();
 
 		var n2 = n / 2;
 
@@ -30,32 +29,19 @@ class F2
 		Console.WriteLine(r);
 	}
 
-	static long[] CreateSums(long[] a, int t)
+	static long[] CreateSums(int[] a, int t)
 	{
+		var n = a.Length;
 		var l = new List<long>();
 
-		AllCombination(a, p =>
+		for (int x = 0; x < 1 << n; ++x)
 		{
-			var sum = p.Sum();
+			var sum = 0L;
+			for (int i = 0; i < n; ++i)
+				if ((x & (1 << i)) != 0)
+					sum += a[i];
 			if (sum <= t) l.Add(sum);
-			return false;
-		});
-		return l.ToArray();
-	}
-
-	static void AllCombination<T>(T[] values, Func<T[], bool> action)
-	{
-		var n = values.Length;
-		if (n > 30) throw new InvalidOperationException();
-		var pn = 1 << n;
-
-		var rn = new int[n];
-		for (int i = 0; i < n; ++i) rn[i] = i;
-
-		for (int x = 0; x < pn; ++x)
-		{
-			var indexes = Array.FindAll(rn, i => (x & (1 << i)) != 0);
-			if (action(Array.ConvertAll(indexes, i => values[i]))) break;
 		}
+		return l.ToArray();
 	}
 }
