ddf02e93cdbdd7910ef30fe3cf810c5d2350b752
Keiho Sakapon <git@saka-pon.net>
2021-04-21 21:28:45 +0900

    Add class for positional notation

---
+++ CSharp/CoderLib/CoderLib8/Numerics/PositionalNotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderLib8.Numerics
+{
+	public static class PositionalNotation
+	{
+		public static int[] Convert(long x, int b)
+		{
+			var r = new List<int>();
+			for (; x > 0; x /= b) r.Add((int)(x % b));
+			return r.ToArray();
+		}
+
+		const string AN = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		public static string ConvertAsString(long x, int b)
+		{
+			if (x == 0) return "0";
+			if (x < 0) return "-" + ConvertAsString(-x, b);
+			var r = Convert(x, b);
+			Array.Reverse(r);
+			return new string(Array.ConvertAll(r, d => AN[d]));
+		}
+	}
+}
