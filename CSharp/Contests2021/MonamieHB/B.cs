5ae18d4bce164dab57c272a0e6c1a5d0fb90ad41
Keiho Sakapon <git@saka-pon.net>
2021-04-21 16:56:28 +0900

    AC

--- CSharp/Contests2021/MonamieHB/B.cs
+++ CSharp/Contests2021/MonamieHB/B.cs
@@ -4,18 +4,35 @@ using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, k) = ((long, int))Read2L();
 
-		return string.Join(" ", a);
+		var a = Convert(n - 1, k);
+		Array.Reverse(a);
+
+		// 0: <, 1: =
+		var dp = NewArray2<long>(a.Length + 1, 2);
+		dp[0][1] = 1;
+
+		for (int i = 0; i < a.Length; i++)
+		{
+			dp[i + 1][0] += dp[i][0] * (k - 1);
+			dp[i + 1][0] += dp[i][1] * a[i];
+			if (a[i] < k - 1) dp[i + 1][1] += dp[i][1];
+		}
+		return dp[^1].Sum();
 	}
+
+	static int[] Convert(long x, int b)
+	{
+		var r = new List<int>();
+		for (; x > 0; x /= b) r.Add((int)(x % b));
+		return r.ToArray();
+	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 }
--- CSharp/Contests2021/MonamieHB/E.cs
+++ CSharp/Contests2021/MonamieHB/E.cs
@@ -5,17 +5,31 @@ using System.Linq;
 class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var p = Read().Select(x => x - 1).ToArray();
 
-		return string.Join(" ", a);
+		var r = new List<(int i, int j)>();
+
+		for (int x = 0; x < n; x++)
+		{
+			var i = Array.IndexOf(p, x, x);
+
+			for (int j = i - 1; j >= x; j--)
+			{
+				if (p[j] >= i)
+				{
+					(p[i], p[j]) = (p[j], p[i]);
+					r.Add((j, i));
+					i = j;
+				}
+			}
+		}
+
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		Console.WriteLine(r.Count);
+		foreach (var (i, j) in r) Console.WriteLine($"{i + 1} {j + 1}");
+		Console.Out.Flush();
 	}
 }
