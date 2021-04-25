098335d6b07cd9cfc225913d68cb909fb643f2ed
Keiho Sakapon <git@saka-pon.net>
2021-04-25 22:02:38 +0900

    AC

---
+++ CSharp/Contests/Typical90/Q016.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Q016
+{
+	const int max = 10000;
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+		Array.Sort(a);
+
+		var r = max;
+		for (int i = 0; i < max; i++)
+		{
+			var n0 = n - i * a[0];
+
+			for (int j = 0; i + j < max && n0 >= 0; j++, n0 -= a[1])
+			{
+				if (n0 % a[2] == 0)
+				{
+					r = Math.Min(r, i + j + n0 / a[2]);
+				}
+			}
+		}
+		return r;
+	}
+}
