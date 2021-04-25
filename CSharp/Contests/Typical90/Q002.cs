ec6dbe431463834957b7eb495c58e9325af49c88
Keiho Sakapon <git@saka-pon.net>
2021-04-25 13:18:04 +0900

    AC

---
+++ CSharp/Contests/Typical90/Q002.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Q002
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		if (n % 2 != 0) return;
+
+		AllBoolCombination(n, b =>
+		{
+			var cs = Array.ConvertAll(b, f => f ? ')' : '(');
+			Array.Reverse(cs);
+			var s = new string(cs);
+			if (IsRegularBracket(s)) Console.WriteLine(s);
+			return false;
+		});
+	}
+
+	static void AllBoolCombination(int n, Func<bool[], bool> action)
+	{
+		if (n > 30) throw new InvalidOperationException();
+		var pn = 1 << n;
+		var b = new bool[n];
+
+		for (int x = 0; x < pn; ++x)
+		{
+			for (int i = 0; i < n; ++i) b[i] = (x & (1 << i)) != 0;
+			if (action(b)) break;
+		}
+	}
+
+	static bool IsRegularBracket(string s)
+	{
+		var t = 0;
+		foreach (var c in s)
+			if (c == '(') ++t;
+			else if (c == ')') if (--t < 0) return false;
+		return t == 0;
+	}
+}
---
+++ CSharp/Contests/Typical90/Q019L.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Q019L
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+
+		var dp2 = new MemoDP2<int>(2 * n + 1, 2 * n + 1, -1, (dp, l, r) =>
+		{
+			var v = Math.Abs(a[l] - a[r - 1]) + dp[l + 1, r - 1];
+
+			for (int c = l + 2; c < r; c += 2)
+			{
+				v = Math.Min(v, dp[l, c] + dp[c, r]);
+			}
+			return v;
+		});
+
+		for (int i = 0; i <= 2 * n; i++)
+		{
+			dp2[i, i] = 0;
+		}
+		return dp2[0, 2 * n];
+	}
+}
+
+class MemoDP2<T>
+{
+	static readonly Func<T, T, bool> TEquals = EqualityComparer<T>.Default.Equals;
+	public T[,] Raw { get; }
+	T iv;
+	Func<MemoDP2<T>, int, int, T> rec;
+
+	public MemoDP2(int n1, int n2, T iv, Func<MemoDP2<T>, int, int, T> rec)
+	{
+		Raw = new T[n1, n2];
+		for (int i1 = 0; i1 < n1; ++i1)
+			for (int i2 = 0; i2 < n2; ++i2)
+				Raw[i1, i2] = iv;
+		this.iv = iv;
+		this.rec = rec;
+	}
+
+	public T this[int i1, int i2]
+	{
+		get => TEquals(Raw[i1, i2], iv) ? Raw[i1, i2] = rec(this, i1, i2) : Raw[i1, i2];
+		set => Raw[i1, i2] = value;
+	}
+}
