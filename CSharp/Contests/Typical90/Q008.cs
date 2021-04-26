83a01b531bd6677a6807497a23dae36db948b43a
Keiho Sakapon <git@saka-pon.net>
2021-04-26 21:54:04 +0900

    AC

---
+++ CSharp/Contests/Typical90/Q008.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Q008
+{
+	const string AtCoder = "atcoder";
+	const long M = 1000000007;
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine();
+
+		var dp = new long[AtCoder.Length + 1];
+		dp[0] = 1;
+
+		foreach (var c in s)
+		{
+			var i = AtCoder.IndexOf(c);
+			if (i == -1) continue;
+
+			dp[i + 1] += dp[i];
+			dp[i + 1] %= M;
+		}
+		return dp[^1];
+	}
+}
---
+++ CSharp/Contests/Typical90/Q014.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Q014
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = ReadL();
+		var b = ReadL();
+
+		Array.Sort(a);
+		Array.Sort(b);
+		return a.Zip(b, (x, y) => Math.Abs(x - y)).Sum();
+	}
+}
