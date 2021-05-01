6c91c0f6d0a366f25deaf30d48e0ce7ec0d346f6
Keiho Sakapon <git@saka-pon.net>
2021-05-01 11:43:01 +0900

    AC

---
+++ CSharp/Contests/Typical90/Q025.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class Q025
+{
+	const long max = 100000000000;
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, b) = Read2L();
+
+		static long f(long x) => x.ToString().Select(c => (long)(c - '0')).Aggregate((x, y) => x * y);
+		int Count(long v)
+		{
+			var m = v + b;
+			if (m > n) return 0;
+			return f(m) == v ? 1 : 0;
+		}
+
+		var r = Count(0);
+
+		for (var (i2, v2) = (0, 1L); i2 < 33 && v2 < max; i2++, v2 *= 2)
+		{
+			for (var (i3, v3) = (0, v2); i3 < 22 && v3 < max; i3++, v3 *= 3)
+			{
+				for (var (i5, v5) = (0, v3); i5 < 11 && v5 < max; i5++, v5 *= 5)
+				{
+					for (var (i7, v7) = (0, v5); i7 < 11 && v7 < max; i7++, v7 *= 7)
+					{
+						r += Count(v7);
+					}
+				}
+			}
+		}
+		return r;
+	}
+}
---
+++ CSharp/Contests/Typical90/Q026.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Q026
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var es = Array.ConvertAll(new bool[n - 1], _ => Read());
+
+		var tree = new Tree(n + 1, 1, es);
+		var gs = Enumerable.Range(1, n)
+			.GroupBy(v => tree.Depths[v] % 2)
+			.Select(g => g.ToArray())
+			.ToArray();
+
+		var r = gs[0].Length >= n / 2 ? gs[0] : gs[1];
+		return string.Join(" ", r[..(n / 2)]);
+	}
+}
+
+public class Tree
+{
+	static List<int>[] ToMap(int n, int[][] es, bool directed)
+	{
+		var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e[1]);
+			if (!directed) map[e[1]].Add(e[0]);
+		}
+		return map;
+	}
+
+	public List<int>[] Map { get; }
+	public int[] Depths { get; }
+	public int[] Parents { get; }
+
+	public Tree(int n, int root, int[][] ues) : this(n, root, ToMap(n, ues, false)) { }
+	public Tree(int n, int root, List<int>[] map)
+	{
+		Map = map;
+		Depths = new int[n];
+		Parents = new int[n];
+		Parents[root] = -1;
+
+		Dfs(root, -1);
+
+		void Dfs(int v, int pv)
+		{
+			foreach (var nv in Map[v])
+			{
+				if (nv == pv) continue;
+				Depths[nv] = Depths[v] + 1;
+				Parents[nv] = v;
+				Dfs(nv, v);
+			}
+		}
+	}
+}
