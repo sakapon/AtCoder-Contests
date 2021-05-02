7a9ff2bc489418cfeb2d141cc64040fef3a4c658
Keiho Sakapon <git@saka-pon.net>
2021-05-02 15:02:19 +0900

    Shorten code

--- CSharp/GCJ/GCJ2021R1C/C.cs
+++ CSharp/GCJ/GCJ2021R1C/C.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class C
@@ -10,8 +9,8 @@ class C
 		var h = Console.ReadLine().Split();
 		if (h[0] == h[1]) return 0;
 
-		var s = h[0].ConvertFrom(2);
-		var e = h[1].ConvertFrom(2);
+		var s = Convert.ToInt32(h[0], 2);
+		var e = Convert.ToInt32(h[1], 2);
 
 		var dp = Array.ConvertAll(new bool[1 << 16], _ => 1 << 30);
 		dp[s] = 0;
@@ -27,7 +26,6 @@ class C
 				fs[x] = f;
 			}
 		}
-		//fs[0] = 0;
 
 		for (int k = 0; k < 1024; k++)
 		{
@@ -49,42 +47,5 @@ class C
 		}
 
 		return "IMPOSSIBLE";
-	}
-}
-
-public static class PositionalNotation
-{
-	public static int[] Convert(this long x, int b)
-	{
-		var r = new List<int>();
-		for (; x > 0; x /= b) r.Add((int)(x % b));
-		return r.ToArray();
-	}
-
-	public static long ConvertFrom(this int[] a, int b)
-	{
-		var r = 0L;
-		for (int i = a.Length - 1; i >= 0; --i) r = r * b + a[i];
-		return r;
-	}
-
-	const string AN = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	static readonly Dictionary<char, int> ANMap = AN.Select((c, i) => new { c, i }).ToDictionary(_ => _.c, _ => _.i);
-
-	public static string ConvertAsString(this long x, int b)
-	{
-		if (x == 0) return "0";
-		if (x < 0) return "-" + ConvertAsString(-x, b);
-		var r = Convert(x, b);
-		Array.Reverse(r);
-		return new string(Array.ConvertAll(r, d => AN[d]));
-	}
-
-	public static long ConvertFrom(this string s, int b)
-	{
-		if (s.StartsWith('-')) return -ConvertFrom(s.Substring(1), b);
-		var a = Array.ConvertAll(s.ToCharArray(), c => ANMap[c]);
-		Array.Reverse(a);
-		return ConvertFrom(a, b);
 	}
 }
