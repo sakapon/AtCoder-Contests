db53901b020ee394f940b763f837136c7d1e0b18
Keiho Sakapon <git@saka-pon.net>
2021-05-02 19:31:44 +0900

    Add TreeHelper class

---
+++ CSharp/CoderLib/CoderLib8/Graphs/Arrays/TreeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CoderLib8.Graphs.Arrays
+{
+	public static class TreeHelper
+	{
+		public static int Diameter(int n, int root, int[][] ues)
+		{
+			var tree = new Tree(n, root, ues);
+
+			var (mv, md) = (-1, -1);
+			for (int v = 0; v < n; v++)
+			{
+				var d = tree.Depths[v];
+				if (md < d) (mv, md) = (v, d);
+			}
+			return new Tree(n, mv, tree.Map).Depths.Max();
+		}
+	}
+}
--- CSharp/Contests/Typical90/Q003.cs
+++ CSharp/Contests/Typical90/Q003.cs
@@ -11,42 +11,19 @@ class Q003
 		var n = int.Parse(Console.ReadLine());
 		var es = Array.ConvertAll(new bool[n - 1], _ => Read());
 
-		return Diameter(n, 1, es.Select(e => new[] { e[0], e[1], 1 }).ToArray()) + 1;
+		return Diameter(n + 1, 1, es) + 1;
 	}
 
-	static long Diameter(int n, int sv, int[][] es)
+	static int Diameter(int n, int root, int[][] ues)
 	{
-		var map = Array.ConvertAll(new int[n + 1], _ => new List<int[]>());
-		foreach (var e in es)
+		var tree = new Tree(n, root, ues);
+
+		var (mv, md) = (-1, -1);
+		for (int v = 0; v < n; v++)
 		{
-			map[e[0]].Add(new[] { e[1], e[2] });
-			map[e[1]].Add(new[] { e[0], e[2] });
+			var d = tree.Depths[v];
+			if (md < d) (mv, md) = (v, d);
 		}
-
-		var d = Distances(n, sv, map);
-		var ev = Enumerable.Range(0, n + 1).OrderBy(v => -d[v]).First();
-		return Distances(n, ev, map).Max();
-	}
-
-	static long[] Distances(int n, int sv, List<int[]>[] map)
-	{
-		var from = new int[n + 1];
-		var d = new long[n + 1];
-		var q = new Queue<int>();
-		from[sv] = -1;
-		q.Enqueue(sv);
-
-		while (q.Count > 0)
-		{
-			var v = q.Dequeue();
-			foreach (var e in map[v])
-			{
-				if (e[0] == from[v]) continue;
-				from[e[0]] = v;
-				d[e[0]] = d[v] + e[1];
-				q.Enqueue(e[0]);
-			}
-		}
-		return d;
+		return new Tree(n, mv, tree.Map).Depths.Max();
 	}
 }
