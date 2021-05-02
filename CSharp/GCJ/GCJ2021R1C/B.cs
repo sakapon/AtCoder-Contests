470bb8b196bb38105cd5416c270ee2cf73fc15b4
Keiho Sakapon <git@saka-pon.net>
2021-05-02 12:52:28 +0900

    AC

--- CSharp/GCJ/GCJ2021R1C/B.cs
+++ CSharp/GCJ/GCJ2021R1C/B.cs
@@ -4,18 +4,56 @@ using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select((_, i) => $"Case #{i + 1}: {Solve()}")));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
+		//var n = long.Parse(Console.ReadLine());
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		if (s.Length == 1) return 12;
+
+		var comp = Comparer<string>.Create((x, y) =>
+		{
+			if (x.Length != y.Length) return x.Length.CompareTo(y.Length);
+			return x.CompareTo(y);
+		});
+
+		var kmax = s.Length / 2 + 1;
+		var l = new List<string>();
+
+		for (int k = 1; k <= kmax; k++)
+		{
+			var x = long.Parse(s.Substring(0, k));
+
+			for (int i = 0; i < 10; i++)
+			{
+				l.Add(GetRoaring(x).SkipWhile(t => comp.Compare(t, s) <= 0).First());
+				x++;
+				if (x.ToString().Length > k)
+					x /= 10;
+			}
+		}
+		l.Sort(comp);
+
+		return l[0];
+	}
+
+	static IEnumerable<string> GetRoaring(long x)
+	{
+		var s = x.ToString();
+		for (var i = x + 1; ; i++)
+		{
+			s += i;
+			yield return s;
+		}
+	}
+
+	static IEnumerable<string> GetRoaring(string s)
+	{
+		for (var i = long.Parse(s) + 1; ; i++)
+		{
+			s += i;
+			yield return s;
+		}
 	}
 }
--- CSharp/GCJ/GCJ2021R1C/C.cs
+++ CSharp/GCJ/GCJ2021R1C/C.cs
@@ -4,18 +4,87 @@ using System.Linq;
 
 class C
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select((_, i) => $"Case #{i + 1}: {Solve()}")));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var h = Console.ReadLine().Split();
+		if (h[0] == h[1]) return 0;
 
-		return string.Join(" ", a);
+		var s = h[0].ConvertFrom(2);
+		var e = h[1].ConvertFrom(2);
+
+		var dp = Array.ConvertAll(new bool[1 << 16], _ => 1 << 30);
+		dp[s] = 0;
+		var fmax = (1 << 16) - 1;
+
+		var fs = new int[1 << 16];
+		for (int k = 16; k > 0; k--)
+		{
+			var f = (1 << k) - 1;
+
+			for (int x = 0; x <= f; x++)
+			{
+				fs[x] = f;
+			}
+		}
+		//fs[0] = 0;
+
+		for (int k = 0; k < 1024; k++)
+		{
+			var nk = k + 1;
+
+			for (int x = 0; x < 1 << 16; x++)
+			{
+				if (dp[x] != k) continue;
+
+				var nx = x << 1;
+				if (nx <= fmax)
+					dp[nx] = Math.Min(dp[nx], nk);
+
+				nx = ~x & fs[x];
+				dp[nx] = Math.Min(dp[nx], nk);
+			}
+
+			if (dp[e] == nk) return nk;
+		}
+
+		return "IMPOSSIBLE";
+	}
+}
+
+public static class PositionalNotation
+{
+	public static int[] Convert(this long x, int b)
+	{
+		var r = new List<int>();
+		for (; x > 0; x /= b) r.Add((int)(x % b));
+		return r.ToArray();
+	}
+
+	public static long ConvertFrom(this int[] a, int b)
+	{
+		var r = 0L;
+		for (int i = a.Length - 1; i >= 0; --i) r = r * b + a[i];
+		return r;
+	}
+
+	const string AN = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	static readonly Dictionary<char, int> ANMap = AN.Select((c, i) => new { c, i }).ToDictionary(_ => _.c, _ => _.i);
+
+	public static string ConvertAsString(this long x, int b)
+	{
+		if (x == 0) return "0";
+		if (x < 0) return "-" + ConvertAsString(-x, b);
+		var r = Convert(x, b);
+		Array.Reverse(r);
+		return new string(Array.ConvertAll(r, d => AN[d]));
+	}
+
+	public static long ConvertFrom(this string s, int b)
+	{
+		if (s.StartsWith('-')) return -ConvertFrom(s.Substring(1), b);
+		var a = Array.ConvertAll(s.ToCharArray(), c => ANMap[c]);
+		Array.Reverse(a);
+		return ConvertFrom(a, b);
 	}
 }
