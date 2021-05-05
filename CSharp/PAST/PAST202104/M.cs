5e7e36166eb65d8c32b93a4fe6917df929d91332
Keiho Sakapon <git@saka-pon.net>
2021-05-05 12:47:58 +0900

    WA

--- CSharp/PAST/PAST202104/M.cs
+++ CSharp/PAST/PAST202104/M.cs
@@ -19,26 +19,106 @@ class M
 		// 各グループ内の個数
 		var rn = (int)Math.Sqrt(n);
 		var gc = (n - 1) / rn + 1;
-		var gs = Enumerable.Range(0, gc).Select(i => new Group(a[(i * rn)..Math.Min(n, (i + 1) * rn)])).ToArray();
+
+		var nodes = new LinkedListNode<(int l, int x)>[gc];
+		LinkedListNode<(int l, int x)> GetNode(int i) => nodes[i / rn];
+
+		var ll = new LinkedList<(int l, int x)>();
+		ll.AddLast((-1, -1));
+
+		for (int i = 0; i < n; i++)
+		{
+			if (ll.Last.Value.x != a[i])
+			{
+				ll.AddLast((i, a[i]));
+			}
+
+			if (i % rn == 0)
+			{
+				nodes[i / rn] = ll.Last;
+			}
+		}
+		ll.AddLast((n, -1));
+
+		Dictionary<int, int> Set(int l, int r, int x)
+		{
+			var lr = r - l;
+			var d = new Dictionary<int, int> { [x] = lr };
+
+			var ln = GetNode(l);
+			while (ln.Value.l < l)
+			{
+				ln = ln.Next;
+			}
+
+			if (r < ln.Value.l)
+			{
+				var x0 = ln.Previous.Value.x;
+				d[x0] = -lr;
+				ll.AddBefore(ln, (l, x));
+				ll.AddBefore(ln, (r, x0));
+
+				ln = ln.Previous;
+				for (int gi = (ln.Next.Value.l - 1) / rn; r <= gi * rn; gi--)
+				{
+					nodes[gi] = ln;
+				}
+
+				ln = ln.Previous;
+				for (int gi = (r - 1) / rn; l <= gi * rn; gi--)
+				{
+					nodes[gi] = ln;
+				}
+			}
+			else
+			{
+				// Partitions left
+				if (l < ln.Value.l)
+				{
+					var x0 = ln.Previous.Value.x;
+					d[x0] = d.GetValueOrDefault(x0) - (ln.Value.l - l);
+				}
+
+				// Inserts
+				ln = ll.AddBefore(ln, (l, x));
+
+				for (ln = ln.Next; ln.Value.l < r;)
+				{
+					var (l0, x0) = ln.Value;
+					if (r < ln.Next.Value.l)
+					{
+						d[x0] = d.GetValueOrDefault(x0) - (r - l0);
+						ln.Value = (r, x0);
+					}
+					else
+					{
+						d[x0] = d.GetValueOrDefault(x0) - (ln.Next.Value.l - l0);
+						var t = ln;
+						ln = ln.Next;
+						ll.Remove(t);
+					}
+				}
+
+				ln = ln.Previous;
+				for (int gi = (r - 1) / rn; l <= gi * rn; gi--)
+				{
+					nodes[gi] = ln;
+				}
+			}
+
+			return d;
+		}
 
 		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
-		foreach (var (l0, r, x) in qs)
+		foreach (var (l, r, x) in qs)
 		{
-			var l = l0 - 1;
-
-			var delta = new Dictionary<int, int>();
-			for (int gl = l / rn * rn; gl < r; gl += rn)
-			{
-				var li = Math.Max(gl, l);
-				var ri = Math.Min(gl + rn, r);
-				delta = CollectionHelper.Merge(delta, gs[gl / rn].Set(li - gl, ri - gl, x));
-			}
+			var delta = Set(l - 1, r, x);
 
 			foreach (var (k, dc) in delta)
 			{
 				if (dc == 0) continue;
 
-				var c = d.GetValue(k, 0);
+				var c = d.GetValueOrDefault(k);
 				sum -= c * (c - 1) / 2;
 				c += dc;
 				sum += c * (c - 1) / 2;
@@ -49,81 +129,5 @@ class M
 			Console.WriteLine(sum);
 		}
 		Console.Out.Flush();
-	}
-
-	class Group
-	{
-		int n;
-		int[] a;
-		int x_all;
-
-		public bool AreAllSame => x_all > 0;
-
-		public Group(int[] a)
-		{
-			n = a.Length;
-			this.a = a;
-
-			if (a.Distinct().Count() == 1)
-			{
-				x_all = a[0];
-			}
-		}
-
-		public Dictionary<int, int> Set(int l, int r, int x)
-		{
-			var lr = r - l;
-			var d = new Dictionary<int, int> { [x] = lr };
-
-			if (AreAllSame)
-			{
-				d[x_all] = -lr;
-
-				if (lr == n)
-				{
-					x_all = x;
-				}
-				else
-				{
-					Array.Fill(a, x_all);
-					Array.Fill(a, x, l, lr);
-					x_all = 0;
-				}
-			}
-			else
-			{
-				for (int i = l; i < r; i++)
-				{
-					var v = a[i];
-					if (d.ContainsKey(v)) d[v]--;
-					else d[v] = -1;
-				}
-
-				if (lr == n)
-				{
-					x_all = x;
-				}
-				else
-				{
-					Array.Fill(a, x, l, lr);
-				}
-			}
-			return d;
-		}
-	}
-}
-
-static class CollectionHelper
-{
-	public static TV GetValue<TK, TV>(this Dictionary<TK, TV> d, TK k, TV v0 = default) => d.ContainsKey(k) ? d[k] : v0;
-
-	// 項目数が大きいほうにマージします。
-	public static Dictionary<TK, int> Merge<TK>(Dictionary<TK, int> d1, Dictionary<TK, int> d2)
-	{
-		if (d1.Count < d2.Count) (d1, d2) = (d2, d1);
-		foreach (var (k, v) in d2)
-			if (d1.ContainsKey(k)) d1[k] += v;
-			else d1[k] = v;
-		return d1;
 	}
 }
