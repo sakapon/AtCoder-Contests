bf83db2cded023df59fb1b1e7111e9f27c3e1c41
Keiho Sakapon <git@saka-pon.net>
2021-05-10 16:43:53 +0900

    AC

---
+++ CSharp/Contests/Typical90/L7/Q035.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib8.Graphs.Arrays;
+
+class Q035
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var es = Array.ConvertAll(new bool[n - 1], _ => Read());
+		var qc = int.Parse(Console.ReadLine());
+
+		var tree = new Tree(n + 1, 1, es);
+		var bll = new BLLca(tree);
+
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		for (int qi = 0; qi < qc; qi++)
+		{
+			var vs = Read().Skip(1).OrderBy(v => tree.TourMap[v][0]).ToArray();
+
+			var r = 0;
+			var tv = bll.GetLca(vs[0], vs[^1]);
+
+			foreach (var v in vs)
+			{
+				var lca = bll.GetLca(tv, v);
+				r += tree.Depths[v] - tree.Depths[lca];
+				tv = v;
+			}
+			Console.WriteLine(r);
+		}
+		Console.Out.Flush();
+	}
+}
---
+++ CSharp/Contests/Typical90/Lib/Tree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderLib8.Graphs.Arrays
+{
+	// Test: https://atcoder.jp/contests/typical90/tasks/typical90_c
+	// Test: https://atcoder.jp/contests/typical90/tasks/typical90_z
+	public class Tree
+	{
+		static List<int>[] ToMap(int n, int[][] es, bool directed)
+		{
+			var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+			foreach (var e in es)
+			{
+				map[e[0]].Add(e[1]);
+				if (!directed) map[e[1]].Add(e[0]);
+			}
+			return map;
+		}
+
+		public int Count { get; }
+		public int Root { get; }
+		public List<int>[] Map { get; }
+		public int[] Depths { get; }
+		public int[] Parents { get; }
+
+		// この Euler Tour では方向を記録しません。
+		// order -> vertex
+		public int[] Tour { get; }
+		// vertex -> orders
+		public List<int>[] TourMap { get; }
+
+		public Tree(int n, int root, int[][] ues) : this(n, root, ToMap(n, ues, false)) { }
+		public Tree(int n, int root, List<int>[] map)
+		{
+			Count = n;
+			Root = root;
+			Map = map;
+			Depths = Array.ConvertAll(Map, _ => -1);
+			Parents = Array.ConvertAll(Map, _ => -1);
+
+			var tour = new List<int>();
+			TourMap = Array.ConvertAll(Map, _ => new List<int>());
+
+			Depths[root] = 0;
+			Dfs(root, -1);
+
+			Tour = tour.ToArray();
+
+			void Dfs(int v, int pv)
+			{
+				TourMap[v].Add(tour.Count);
+				tour.Add(v);
+
+				foreach (var nv in Map[v])
+				{
+					if (nv == pv) continue;
+					Depths[nv] = Depths[v] + 1;
+					Parents[nv] = v;
+					Dfs(nv, v);
+
+					TourMap[v].Add(tour.Count);
+					tour.Add(v);
+				}
+			}
+		}
+	}
+}
