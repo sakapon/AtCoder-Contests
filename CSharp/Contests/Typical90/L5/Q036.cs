ac9d58560621e389dabafeab1a2a2398e38daaac
Keiho Sakapon <git@saka-pon.net>
2021-05-14 12:17:41 +0900

    AC

---
+++ CSharp/Contests/Typical90/L5/Q036.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+class Q036
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int x, int y) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, qc) = Read2();
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
+		var qs = Array.ConvertAll(new bool[qc], _ => int.Parse(Console.ReadLine()));
+
+		var xy1 = Array.ConvertAll(ps, p => p.x + p.y);
+		var xy2 = Array.ConvertAll(ps, p => p.x - p.y);
+
+		long max1 = xy1.Max();
+		long min1 = xy1.Min();
+		long max2 = xy2.Max();
+		long min2 = xy2.Min();
+
+		long Query(int i)
+		{
+			var d1 = Math.Max(max1 - xy1[i], xy1[i] - min1);
+			var d2 = Math.Max(max2 - xy2[i], xy2[i] - min2);
+			return Math.Max(d1, d2);
+		}
+
+		return string.Join("\n", qs.Select(q => Query(q - 1)));
+	}
+}
---
+++ CSharp/Contests/Typical90/L5/Q037.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CoderLib8.Trees;
+
+class Q037
+{
+	const long min = -1L << 60;
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (w, n) = Read2();
+		var ps = Array.ConvertAll(new bool[n], _ => Read3());
+
+		var st = new ST1<long>(w + 1, Math.Max, min);
+		st.Set(0, 0);
+
+		foreach (var (l, r, v) in ps)
+		{
+			for (int x = w; x > 0; x--)
+			{
+				var m = st.Get(Math.Max(0, x - r), Math.Max(0, x - l + 1));
+				if (m == min) continue;
+				st.Set(x, Math.Max(st.Get(x), m + v));
+			}
+		}
+		var mv = st.Get(w);
+		return mv == min ? -1 : mv;
+	}
+}
