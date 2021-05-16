f25a1b349dc93a9ef6f0eb6ceed8a6e5a1936b49
Keiho Sakapon <git@saka-pon.net>
2021-05-16 14:05:27 +0900

    AC

--- CSharp/Contests2021/ABC201/E.cs
+++ CSharp/Contests2021/ABC201/E.cs
@@ -1,21 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class E
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	const long M = 1000000007;
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var es = Array.ConvertAll(new bool[n - 1], _ => ReadL());
 
-		return string.Join(" ", a);
+		var map = ToMap(n + 1, es, false);
+		var xor = new long[n + 1];
+
+		Dfs(1, -1);
+
+		void Dfs(long v, long pv)
+		{
+			foreach (var e in map[v])
+			{
+				var nv = e[1];
+				if (nv == pv) continue;
+
+				xor[nv] = xor[v] ^ e[2];
+				Dfs(nv, v);
+			}
+		}
+
+		var r = 0L;
+
+		for (int i = 0; i < 60; i++)
+		{
+			var c = 0L;
+			for (int v = 1; v <= n; v++)
+			{
+				if ((xor[v] & (1L << i)) != 0)
+				{
+					c++;
+				}
+			}
+
+			r += (1L << i) % M * (c * (n - c) % M);
+			r %= M;
+		}
+		return r;
+	}
+
+	static List<long[]>[] ToMap(int n, long[][] es, bool directed)
+	{
+		var map = Array.ConvertAll(new bool[n], _ => new List<long[]>());
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e);
+			if (!directed) map[e[1]].Add(new[] { e[1], e[0], e[2] });
+		}
+		return map;
 	}
 }
