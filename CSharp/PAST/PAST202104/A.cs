bac81159610cc7776dc709ca9ed40484a2aa9a92
Keiho Sakapon <git@saka-pon.net>
2021-05-22 20:56:21 +0900

    Improve code

--- CSharp/PAST/PAST202104/A.cs
+++ CSharp/PAST/PAST202104/A.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class A
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
 	static bool Solve()
 	{
 		var s = Console.ReadLine();
-
-		var s3 = s.Remove(3, 1);
-		return s[3] == '-' && s3.All(c => c != '-');
+		return s[3] == '-' && s.Remove(3, 1).All(c => c != '-');
 	}
 }
---
+++ CSharp/PAST/PAST202104/A2.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class A2
+{
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve() => Regex.IsMatch(Console.ReadLine(), @"\d{3}-\d{4}");
+}
