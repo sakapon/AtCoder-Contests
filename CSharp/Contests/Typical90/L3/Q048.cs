4b1f3c882f56493132c51a312ccfedd8578c923c
Keiho Sakapon <git@saka-pon.net>
2021-05-25 18:25:55 +0900

    AC

---
+++ CSharp/Contests/Typical90/L3/Q048.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Q048
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int a, int b) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, k) = Read2();
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
+
+		var r = 0L;
+		var q = PQ<(int i, int x)>.Create(_ => _.x, true);
+		q.PushRange(ps.Select((_, i) => (i, _.b)).ToArray());
+
+		for (int j = 0; j < k; j++)
+		{
+			var (i, x) = q.Pop();
+			r += x;
+			if (i != -1)
+			{
+				var (a, b) = ps[i];
+				q.Push((-1, a - b));
+			}
+		}
+		return r;
+	}
+}
---
+++ CSharp/Contests/Typical90/L6/Q045.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Q045
+{
+	const long max = 1L << 60;
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int x, int y) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, k) = Read2();
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
+
+		// グループ内での最大値
+		var d = new long[1 << n];
+		for (int x = 0; x < 1 << n; x++)
+		{
+			var m = 0L;
+
+			for (int i = 0; i < n; i++)
+			{
+				if ((x & (1 << i)) == 0) continue;
+
+				for (int j = i + 1; j < n; j++)
+				{
+					if ((x & (1 << j)) == 0) continue;
+
+					long dx = ps[i].x - ps[j].x;
+					long dy = ps[i].y - ps[j].y;
+					m = Math.Max(m, dx * dx + dy * dy);
+				}
+			}
+			d[x] = m;
+		}
+
+		var dp = NewArray2(1 << n, k + 1, max);
+		dp[0][0] = 0;
+
+		for (int x = 0; x < 1 << n; x++)
+		{
+			for (int y = x + 1; y < 1 << n; y++)
+			{
+				if ((x & y) != 0) continue;
+				var nx = x | y;
+
+				for (int j = 0; j < k; j++)
+				{
+					if (dp[x][j] == max) continue;
+
+					dp[nx][j + 1] = Math.Min(dp[nx][j + 1], Math.Max(dp[x][j], d[y]));
+				}
+			}
+		}
+		return dp[^1][k];
+	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+}
