2bb553dd40847ed9f85adf5d785a91251ae61432
Keiho Sakapon <git@saka-pon.net>
2021-05-25 22:55:30 +0900

    Add functions

--- CSharp/CoderLib/CoderLib8/Numerics/Math2.cs
+++ CSharp/CoderLib/CoderLib8/Numerics/Math2.cs
@@ -37,8 +37,25 @@ namespace CoderLib8.Numerics
 			return r * r == x;
 		}
 
+		// 階乗も含まれます。
+		// 20_P_20 = 20! は Int64 の範囲内です。
+		// n を大きい値に変更できます。O(n^2)
+		public static long[,] GetNprs()
+		{
+			var n = 20;
+			var dp = new long[n + 1, n + 1];
+			for (int i = 0; i <= n; i++)
+			{
+				dp[i, 0] = 1;
+				for (int j = 0; j < i; j++)
+					dp[i, j + 1] = dp[i, j] * (i - j);
+			}
+			return dp;
+		}
+
 		// 66_C_33 は Int64 の範囲内です。
-		static long[,] GetNcrs()
+		// n を大きい値に変更できます。O(n^2)
+		public static long[,] GetNcrs()
 		{
 			var n = 66;
 			var dp = new long[n + 1, n + 1];
