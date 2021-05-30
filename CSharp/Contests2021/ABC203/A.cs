5d2bb51bda7905e1ee8516837133672b82215ae3
Keiho Sakapon <git@saka-pon.net>
2021-05-30 23:27:14 +0900

    AC

--- CSharp/Contests2021/ABC203/A.cs
+++ CSharp/Contests2021/ABC203/A.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class A
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (a, b, c) = Read3();
 
-		return string.Join(" ", a);
+		if (a == b) return c;
+		if (b == c) return a;
+		if (c == a) return b;
+		return 0;
 	}
 }
