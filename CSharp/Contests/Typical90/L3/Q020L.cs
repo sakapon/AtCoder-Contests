b48b6047f58be92897e1a6998210d953e95d5644
Keiho Sakapon <git@saka-pon.net>
2021-06-01 10:43:32 +0900

    Other AC

---
+++ CSharp/Contests/Typical90/L3/Q020L.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Q020L
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long, long) Read3L() { var a = ReadL(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
+	{
+		var (a, b, c) = Read3L();
+		return a < Enumerable.Repeat(c, (int)b).Aggregate((x, y) => x * y);
+	}
+}
