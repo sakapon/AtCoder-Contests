d968ed9072468556ac5b74158f2326d43006efd7
Keiho Sakapon <git@saka-pon.net>
2021-06-01 10:03:40 +0900

    AC

---
+++ CSharp/Contests/Typical90/L2/Q004.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Q004
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main()
+	{
+		var (h, w) = Read2();
+		var a = Array.ConvertAll(new bool[h], _ => Read());
+
+		var si = new int[h];
+		var sj = new int[w];
+
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < w; j++)
+			{
+				si[i] += a[i][j];
+				sj[j] += a[i][j];
+			}
+
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < w; j++)
+				a[i][j] = si[i] + sj[j] - a[i][j];
+
+		foreach (var r in a)
+			Console.WriteLine(string.Join(" ", r));
+	}
+}
---
+++ CSharp/Contests/Typical90/L2/Q022.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Q022
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var a = ReadL();
+
+		var g = a.Aggregate(Gcd);
+		return a.Sum(x => x / g - 1);
+	}
+
+	static long Gcd(long a, long b) { for (long r; (r = a % b) > 0; a = b, b = r) ; return b; }
+}
