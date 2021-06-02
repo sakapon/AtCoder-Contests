e6b0d584e430d7bcfa6137ded17a60ab2641f3a9
Keiho Sakapon <git@saka-pon.net>
2021-06-02 23:30:42 +0900

    AC

--- CSharp/Contests2021/ABC203/F.cs
+++ CSharp/Contests2021/ABC203/F.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class F
 {
+	const int max = 1 << 30;
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
@@ -22,24 +24,22 @@ class F
 			next[i] = j;
 		}
 
-		var jump = (int[])next.Clone();
+		var dp = NewArray2(n + 1, 31, max);
+		dp[0][0] = 0;
 
-		for (int mt = 1; ; mt++)
+		for (int i = 0; i < n; i++)
 		{
-			var ma = 1 << 30;
-
-			for (int i = 0; i < n; i++)
+			for (int j = 0; j < 30; j++)
 			{
-				var ca = n - (jump[i] - i);
-				ma = Math.Min(ma, ca);
-			}
-			if (ma <= k) return $"{mt} {ma}";
+				if (dp[i][j] == max) continue;
 
-			for (int i = 0; i < n; i++)
-			{
-				jump[i] = next[jump[i]];
+				dp[i + 1][j] = Math.Min(dp[i + 1][j], dp[i][j] + 1);
+				dp[next[i]][j + 1] = Math.Min(dp[next[i]][j + 1], dp[i][j]);
 			}
 		}
+
+		var mt = Enumerable.Range(0, 31).First(j => dp[n][j] <= k);
+		return $"{mt} {dp[n][mt]}";
 	}
 
 	static IEnumerable<(int i, int j)> TwoPointers(int n1, int n2, Func<int, int, bool> predicate)
@@ -48,4 +48,6 @@ class F
 			for (; j < n2; ++j)
 				if (predicate(i, j)) { yield return (i, j); break; }
 	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 }
