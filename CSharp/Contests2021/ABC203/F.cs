c9139ac8f3394a8b123f8b1cdc56bba8fc589239
Keiho Sakapon <git@saka-pon.net>
2021-06-02 22:49:59 +0900

    WA

--- CSharp/Contests2021/ABC203/F.cs
+++ CSharp/Contests2021/ABC203/F.cs
@@ -1,21 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, k) = Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		if (n == k) return $"0 {k}";
+
+		Array.Sort(a);
+		Array.Reverse(a);
+
+		var next = new int[n + 1];
+		foreach (var (i, j) in TwoPointers(n + 1, n + 1, (i, j) => j == n || a[i] >= a[j] * 2))
+		{
+			next[i] = j;
+		}
+
+		var jump = (int[])next.Clone();
+
+		for (int mt = 1; ; mt++)
+		{
+			var ma = 1 << 30;
+
+			for (int i = 0; i < n; i++)
+			{
+				var ca = n - (jump[i] - i);
+				ma = Math.Min(ma, ca);
+			}
+			if (ma <= k) return $"{mt} {ma}";
+
+			for (int i = 0; i < n; i++)
+			{
+				jump[i] = next[jump[i]];
+			}
+		}
+	}
+
+	static IEnumerable<(int i, int j)> TwoPointers(int n1, int n2, Func<int, int, bool> predicate)
+	{
+		for (int i = 0, j = 0; i < n1 && j < n2; ++i)
+			for (; j < n2; ++j)
+				if (predicate(i, j)) { yield return (i, j); break; }
 	}
 }
