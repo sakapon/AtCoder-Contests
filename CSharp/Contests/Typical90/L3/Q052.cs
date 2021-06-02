fdaf5b22fcd8ed2a274d9670b552d094ee27ca1d
Keiho Sakapon <git@saka-pon.net>
2021-06-02 13:55:47 +0900

    AC

---
+++ CSharp/Contests/Typical90/L3/Q052.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Q052
+{
+	const long M = 1000000007;
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Array.ConvertAll(new bool[n], _ => ReadL().Sum());
+
+		return a.Aggregate((x, y) => x * y % M);
+	}
+}
