2f1687ecaed51e4a5d26885e40f77fd3b30c23f9
Keiho Sakapon <git@saka-pon.net>
2021-06-03 15:59:01 +0900

    AC

--- CSharp/Contests2021/ABC203/E.cs
+++ CSharp/Contests2021/ABC203/E.cs
@@ -5,17 +5,35 @@ using System.Linq;
 class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int x, int y) Read2() { var a = Read(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var ps = Array.ConvertAll(new bool[m], _ => Read2());
 
-		return string.Join(" ", a);
+		var u = new bool[2 * m + 3];
+		u[m + 1] = true;
+		var d = new Dictionary<int, bool>();
+
+		var q = ps.GroupBy(p => p.x).OrderBy(g => g.Key);
+		foreach (var g in q)
+		{
+			d.Clear();
+			foreach (var (_, y) in g)
+			{
+				var j = y - n + m + 1;
+				if (j < 1 || 2 * m + 1 < j) continue;
+
+				d[j] = u[j - 1] || u[j + 1];
+			}
+
+			foreach (var (j, b) in d)
+			{
+				u[j] = b;
+			}
+		}
+
+		return u.Count(b => b);
 	}
 }
