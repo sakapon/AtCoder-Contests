9972ae5e42e43ed0529e7116b9d7fe65b012326d
Keiho Sakapon <git@saka-pon.net>
2021-06-06 22:53:47 +0900

    AC

--- CSharp/Contests2021/ABC204/D.cs
+++ CSharp/Contests2021/ABC204/D.cs
@@ -1,21 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var sum = a.Sum();
+
+		var dp = new bool[sum + 1];
+		dp[0] = true;
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = sum; j >= 0; j--)
+			{
+				if (!dp[j]) continue;
+				dp[j + a[i]] = true;
+			}
+		}
+		return Enumerable.Range(0, sum).Where(x => dp[x]).Min(x => Math.Max(x, sum - x));
 	}
 }
