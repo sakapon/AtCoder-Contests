3c3e0146002350541a22417dd4881690f493e7e9
Keiho Sakapon <git@saka-pon.net>
2021-06-07 11:56:32 +0900

    Update parameter

--- CSharp/Contests/Typical90/L7/Q059.cs
+++ CSharp/Contests/Typical90/L7/Q059.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 class Q059
 {
-	const int Buffer = 8192;
+	const int Buffer = 1 << 14;
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
