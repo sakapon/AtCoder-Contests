741d0a9862ed8be1d10e61fcc5c1fef686e1a5ba
Keiho Sakapon <git@saka-pon.net>
2021-06-07 15:49:26 +0900

    AC

--- CSharp/CafeCoder/Break001/B.cs
+++ CSharp/CafeCoder/Break001/B.cs
@@ -1,21 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-class B
+static class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void Main()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, m, qc) = Read3();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var b = Read();
+		var qs = Array.ConvertAll(new bool[qc], _ => Read3());
 
-		return string.Join(" ", a);
+		var dp = NewArray2<int>(m + 1, n);
+		dp[0] = Enumerable.Range(1, n).ToArray();
+
+		for (int j = 0; j < m; j++)
+		{
+			var map = dp[j].ToInverseMap(n);
+			for (int i = 0; i < n; i++)
+			{
+				dp[j + 1][map[i + 1]] = b[(i + a[j]) % n];
+			}
+		}
+
+		foreach (var (s, t, x) in qs)
+		{
+			var i = Array.IndexOf(dp[s - 1], x);
+			Console.WriteLine(dp[t][i]);
+		}
+	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+
+	static int[] ToInverseMap(this int[] a, int max)
+	{
+		var d = Array.ConvertAll(new bool[max + 1], _ => -1);
+		for (int i = 0; i < a.Length; ++i) d[a[i]] = i;
+		return d;
 	}
 }
