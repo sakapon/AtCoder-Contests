b1716bc347b07de3717c09462654024224d4b9df
Keiho Sakapon <git@saka-pon.net>
2021-06-13 23:50:44 +0900

    AC

---
+++ CSharp/Contests/Typical90/L5/Q060.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+class Q060
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+
+		var l1 = LisMax(a);
+		Array.Reverse(a);
+		var l2 = LisMax(a);
+		Array.Reverse(l2);
+
+		return Enumerable.Range(0, n).Max(i => l1[i] + l2[i] - 1);
+	}
+
+	// index i で最大となるときの個数
+	static int[] LisMax(int[] a)
+	{
+		var n = a.Length;
+		var r = new int[n];
+
+		// 部分列の i 番目となりうる最小値 (0-indexed) (LIS)。
+		var l = Array.ConvertAll(new bool[n + 1], _ => int.MaxValue);
+
+		for (int i = 0; i < n; ++i)
+		{
+			var li = Min(0, n, x => l[x] >= a[i]);
+			l[li] = a[i];
+			r[i] = li + 1;
+		}
+		return r;
+	}
+
+	static int Min(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
+	}
+}
---
+++ CSharp/Contests/Typical90/L6/Q062.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Q062
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var es = Array.ConvertAll(new bool[n], _ => Read2());
+
+		var map = Array.ConvertAll(new bool[n + 1], _ => new List<int>());
+
+		for (int i = 1; i <= n; i++)
+		{
+			var (a, b) = es[i - 1];
+			map[i == a ? 0 : a].Add(i);
+			map[i == b ? 0 : b].Add(i);
+		}
+
+		var r = new List<int>();
+		var u = new bool[n + 1];
+		var q = new Queue<int>();
+		u[0] = true;
+		q.Enqueue(0);
+
+		while (q.TryDequeue(out var v))
+		{
+			foreach (var nv in map[v])
+			{
+				if (u[nv]) continue;
+				r.Add(nv);
+				u[nv] = true;
+				q.Enqueue(nv);
+			}
+		}
+
+		if (r.Count < n) return -1;
+		r.Reverse();
+		return string.Join("\n", r);
+	}
+}
