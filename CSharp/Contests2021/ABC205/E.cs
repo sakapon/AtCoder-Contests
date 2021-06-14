606e92aca5eaa235f407f3b7aa9b6167b14db96a
Keiho Sakapon <git@saka-pon.net>
2021-06-14 12:13:14 +0900

    AC

--- CSharp/Contests2021/ABC205/E.cs
+++ CSharp/Contests2021/ABC205/E.cs
@@ -10,27 +10,11 @@ class E
 		var (n, m, k) = Read3();
 
 		if (n - m > k) return 0;
+		if (n == k) return MNcr(n + m, n);
 
-		// 余事象
-		var r = 0L;
-		var mc = new MCombination(n + m);
-
-		for (int i = 0; i < n - k; i++)
-		{
-			var all1 = k + 2 * i - 1;
-			if (all1 < i) all1 = i;
-			r += mc.MNcr(all1, i) * mc.MNcr(n + m - k - 2 * i - 1, m - i);
-			r %= M;
-		}
-		return MInt(mc.MNcr(n + m, n) - r);
+		return MInt(MNcr(n + m, n) - MNcr(n + m, n - k - 1));
 	}
 
-	const long M = 1000000007;
-	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
-}
-
-public class MCombination
-{
 	const long M = 1000000007;
 	static long MPow(long b, long i)
 	{
@@ -40,24 +24,14 @@ public class MCombination
 	}
 	static long MInv(long x) => MPow(x, M - 2);
 
-	static long[] MFactorials(int n)
+	// n >= 0
+	static long MFactorial(int n) { for (long x = 1, i = 1; ; x = x * ++i % M) if (i >= n) return x; }
+	static long MNpr(int n, int r)
 	{
-		var f = new long[n + 1];
-		f[0] = 1;
-		for (int i = 1; i <= n; ++i) f[i] = f[i - 1] * i % M;
-		return f;
+		if (n < r) return 0;
+		for (long x = 1, i = n - r; ; x = x * ++i % M) if (i >= n) return x;
 	}
+	static long MNcr(int n, int r) => n < r ? 0 : n - r < r ? MNcr(n, n - r) : MNpr(n, r) * MInv(MFactorial(r)) % M;
 
-	// nPr, nCr を O(1) で求めるため、階乗を O(n) で求めておきます。
-	long[] f, f_;
-	public MCombination(int nMax)
-	{
-		f = MFactorials(nMax);
-		f_ = Array.ConvertAll(f, MInv);
-	}
-
-	public long MFactorial(int n) => f[n];
-	public long MInvFactorial(int n) => f_[n];
-	public long MNpr(int n, int r) => n < r ? 0 : f[n] * f_[n - r] % M;
-	public long MNcr(int n, int r) => n < r ? 0 : f[n] * f_[n - r] % M * f_[r] % M;
+	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
 }
