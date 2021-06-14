6f1c2e2f0b1ea1c069fd738e7c34f4f94db818b2
Keiho Sakapon <git@saka-pon.net>
2021-06-14 10:04:12 +0900

    AC

--- CSharp/Contests2021/ABC205/C.cs
+++ CSharp/Contests2021/ABC205/C.cs
@@ -1,21 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (a, b, c) = Read3();
 
-		return string.Join(" ", a);
+		if (c % 2 == 0)
+		{
+			a = Math.Abs(a);
+			b = Math.Abs(b);
+		}
+		return a == b ? '=' : a < b ? '<' : '>';
 	}
 }
--- CSharp/Contests2021/ABC205/D2.cs
+++ CSharp/Contests2021/ABC205/D2.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-class D
+class D2
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
@@ -13,20 +11,15 @@ class D
 		var a = ReadL();
 		var qs = Array.ConvertAll(new bool[qc], _ => long.Parse(Console.ReadLine()));
 
-		var b = a.Select((v, i) => v - i - 1).ToArray();
-
 		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
 		foreach (var k in qs)
 		{
-			if (k > b[^1])
+			var r = First(0, 1L << 60, x =>
 			{
-				Console.WriteLine(a[^1] + k - b[^1]);
-			}
-			else
-			{
-				var i = First(0, n, x => b[x] >= k);
-				Console.WriteLine(a[i] + k - b[i] - 1);
-			}
+				var c = First(0, n, i => a[i] > x);
+				return x - c >= k;
+			});
+			Console.WriteLine(r);
 		}
 		Console.Out.Flush();
 	}
@@ -38,10 +31,10 @@ class D
 		return r;
 	}
 
-	static int Last(int l, int r, Func<int, bool> f)
+	static long First(long l, long r, Func<long, bool> f)
 	{
-		int m;
-		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
-		return l;
+		long m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
 	}
 }
