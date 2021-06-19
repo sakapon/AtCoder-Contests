6455d36720bb7368916099193f69312b7f450d9a
Keiho Sakapon <git@saka-pon.net>
2021-06-19 20:23:55 +0900

    AC

---
+++ CSharp/Contests/Typical90/L3/Q069.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Q069
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, k) = Read2L();
+
+		if (n == 1) return k;
+		if (k == 1) return 0;
+		return k * (k - 1) % M * MPow(k - 2, n - 2) % M;
+	}
+
+	const long M = 1000000007;
+	static long MPow(long b, long i)
+	{
+		long r = 1;
+		for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+		return r;
+	}
+}
---
+++ CSharp/Contests/Typical90/L5/Q068.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using CoderLib6.Trees;
+
+class Q068
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int t, int x, int y, int v) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var qc = int.Parse(Console.ReadLine());
+		var qs = Array.ConvertAll(new bool[qc], _ => Read4());
+
+		var roots = new int[n + 1];
+		var d = new long[n + 1];
+
+		foreach (var (_, x, y, v) in qs.Where(q => q.t == 0).OrderBy(q => q.x))
+		{
+			if (roots[x] == -1) roots[x] = x;
+
+			roots[y] = roots[x];
+			d[y] = v - d[x];
+		}
+
+		var uf = new UF(n + 1);
+
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		foreach (var (t, x, y, v) in qs)
+		{
+			if (t == 0)
+			{
+				uf.Unite(x, y);
+			}
+			else
+			{
+				if (uf.AreUnited(x, y))
+				{
+					var v0 = (x - roots[x]) % 2 == 0 ? v - d[x] : d[x] - v;
+					var vy = (y - roots[y]) % 2 == 0 ? d[y] + v0 : d[y] - v0;
+					Console.WriteLine(vy);
+				}
+				else
+				{
+					Console.WriteLine("Ambiguous");
+				}
+			}
+		}
+		Console.Out.Flush();
+	}
+}
