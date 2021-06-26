f0773476a4f185decfe3fe1bbb19ad6fecd634f3
Keiho Sakapon <git@saka-pon.net>
2021-06-26 23:23:21 +0900

    AC

--- CSharp/Contests2021/ABC207/C.cs
+++ CSharp/Contests2021/ABC207/C.cs
@@ -1,21 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => Read3());
 
-		return string.Join(" ", a);
+		var ps2 = ps.Select(p =>
+		{
+			var (t, l, r) = p;
+			l *= 2;
+			r *= 2;
+			if (t >= 3) l++;
+			if (t % 2 == 1) r++;
+			return (l, r);
+		}).OrderBy(p => p.l).ToArray();
+
+		var r = 0;
+		for (int i = 0; i < n; i++)
+			for (int j = i + 1; j < n; j++)
+				if (ps2[j].l < ps2[i].r) r++;
+		return r;
 	}
 }
