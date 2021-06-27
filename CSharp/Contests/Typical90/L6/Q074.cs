23210a6808cf920da93563a9480f35f8ba95b4f0
Keiho Sakapon <git@saka-pon.net>
2021-06-27 20:59:32 +0900

    AC

---
+++ CSharp/Contests/Typical90/L6/Q074.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Q074
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine();
+
+		return s.Select((c, i) => (c - 'a') * (1L << i)).Sum();
+	}
+}
