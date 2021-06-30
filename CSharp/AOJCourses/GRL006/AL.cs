434b9d3612dfe101452f78a90588121cbf971ed1
Keiho Sakapon <git@saka-pon.net>
2021-06-30 12:26:05 +0900

    Add class for Dinic's algorithm

--- CSharp/AOJCourses/GRL006/AL.cs
+++ CSharp/AOJCourses/GRL006/AL.cs
@@ -10,13 +10,13 @@ class AL
 		var n = h[0];
 		var es = Array.ConvertAll(new bool[h[1]], _ => Read());
 
-		var mf = new MaxFlowFF(n);
+		var mf = new MaxFlow(n);
 		mf.AddEdges(es);
-		Console.WriteLine(mf.FordFulkersonByDfs(0, n - 1));
+		Console.WriteLine(mf.Dinic(0, n - 1));
 	}
 }
 
-public class MaxFlowFF
+public class MaxFlow
 {
 	public class Edge
 	{
@@ -26,12 +26,13 @@ public class MaxFlowFF
 	}
 
 	List<Edge>[] map;
-	bool[] u;
+	int[] depth;
+	Queue<int> q = new Queue<int>();
 
-	public MaxFlowFF(int n)
+	public MaxFlow(int n)
 	{
 		map = Array.ConvertAll(new bool[n], _ => new List<Edge>());
-		u = new bool[n];
+		depth = new int[n];
 	}
 
 	public void AddEdge(int from, int to, long capacity)
@@ -50,30 +51,54 @@ public class MaxFlowFF
 		foreach (var e in des) AddEdge((int)e[0], (int)e[1], e[2]);
 	}
 
+	void Bfs(int sv)
+	{
+		depth = Array.ConvertAll(depth, _ => int.MaxValue);
+		depth[sv] = 0;
+		q.Enqueue(sv);
+
+		while (q.Count > 0)
+		{
+			var v = q.Dequeue();
+			var nd = depth[v] + 1;
+
+			foreach (var e in map[v])
+			{
+				if (e.Capacity == 0) continue;
+				if (depth[e.To] <= nd) continue;
+				depth[e.To] = nd;
+				q.Enqueue(e.To);
+			}
+		}
+	}
+
 	long Dfs(int v, int ev, long fMin)
 	{
 		if (v == ev) return fMin;
-		u[v] = true;
 		foreach (var e in map[v])
 		{
-			if (u[e.To] || e.Capacity == 0) continue;
+			if (e.Capacity == 0) continue;
+			if (depth[v] >= depth[e.To]) continue;
 			var delta = Dfs(e.To, ev, Math.Min(fMin, e.Capacity));
 			if (delta > 0)
 			{
 				e.Capacity -= delta;
 				map[e.To][e.RevIndex].Capacity += delta;
-				u[v] = false;
 				return delta;
 			}
 		}
-		u[v] = false;
 		return 0;
 	}
 
-	public long FordFulkersonByDfs(int sv, int ev)
+	public long Dinic(int sv, int ev)
 	{
 		long M = 0, t;
-		while ((t = Dfs(sv, ev, long.MaxValue)) > 0) M += t;
+		while (true)
+		{
+			Bfs(sv);
+			if (depth[ev] == int.MaxValue) break;
+			while ((t = Dfs(sv, ev, long.MaxValue)) > 0) M += t;
+		}
 		return M;
 
 		// パスの復元が必要となる場合
---
+++ CSharp/CoderLib/CoderLib8/Graphs/Int/MaxFlow.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderLib8.Graphs.Int
+{
+	public class MaxFlow
+	{
+		public class Edge
+		{
+			public int From, To, RevIndex;
+			public long Capacity;
+			public Edge(int from, int to, long capacity, int revIndex) { From = from; To = to; Capacity = capacity; RevIndex = revIndex; }
+		}
+
+		List<Edge>[] map;
+		int[] depth;
+		Queue<int> q = new Queue<int>();
+
+		public MaxFlow(int n)
+		{
+			map = Array.ConvertAll(new bool[n], _ => new List<Edge>());
+			depth = new int[n];
+		}
+
+		public void AddEdge(int from, int to, long capacity)
+		{
+			map[from].Add(new Edge(from, to, capacity, map[to].Count));
+			map[to].Add(new Edge(to, from, 0, map[from].Count - 1));
+		}
+
+		// { from, to, capacity }
+		public void AddEdges(int[][] des)
+		{
+			foreach (var e in des) AddEdge(e[0], e[1], e[2]);
+		}
+		public void AddEdges(long[][] des)
+		{
+			foreach (var e in des) AddEdge((int)e[0], (int)e[1], e[2]);
+		}
+
+		void Bfs(int sv)
+		{
+			Array.Fill(depth, int.MaxValue);
+			depth[sv] = 0;
+			q.Enqueue(sv);
+
+			while (q.Count > 0)
+			{
+				var v = q.Dequeue();
+				var nd = depth[v] + 1;
+
+				foreach (var e in map[v])
+				{
+					if (e.Capacity == 0) continue;
+					if (depth[e.To] <= nd) continue;
+					depth[e.To] = nd;
+					q.Enqueue(e.To);
+				}
+			}
+		}
+
+		long Dfs(int v, int ev, long fMin)
+		{
+			if (v == ev) return fMin;
+			foreach (var e in map[v])
+			{
+				if (e.Capacity == 0) continue;
+				if (depth[v] >= depth[e.To]) continue;
+				var delta = Dfs(e.To, ev, Math.Min(fMin, e.Capacity));
+				if (delta > 0)
+				{
+					e.Capacity -= delta;
+					map[e.To][e.RevIndex].Capacity += delta;
+					return delta;
+				}
+			}
+			return 0;
+		}
+
+		public long Dinic(int sv, int ev)
+		{
+			long M = 0, t;
+			while (true)
+			{
+				Bfs(sv);
+				if (depth[ev] == int.MaxValue) break;
+				while ((t = Dfs(sv, ev, long.MaxValue)) > 0) M += t;
+			}
+			return M;
+
+			// パスの復元が必要となる場合
+			//return (M, map);
+		}
+	}
+}
