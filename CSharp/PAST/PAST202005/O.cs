12500d6e66a80b889d307127bc7015c785458157
Keiho Sakapon <git@saka-pon.net>
2021-07-02 15:32:09 +0900

    Use new library

--- CSharp/PAST/PAST202005/O.cs
+++ CSharp/PAST/PAST202005/O.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class O
 {
-	static int[] Read() => Console.ReadLine().Split().Select(int.Parse).ToArray();
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main()
 	{
 		var h = Read();
@@ -13,73 +12,106 @@ class O
 		var b = Read();
 		var r = Read();
 
-		var dag = new List<long[]>();
-		dag.AddRange(Enumerable.Range(1, 3).Select(i => new[] { 0, i, m, 0L }));
+		var mf = new MinCostFlow(n + 10);
+		for (int i = 1; i <= 3; i++)
+			mf.AddEdge(0, i, m, 0);
+
 		for (int j = 0; j < n; j++)
 		{
 			long p = a[j] * b[j];
 			for (int i = 1; i <= 3; i++, p *= b[j])
-				dag.Add(new[] { i, j + 10, 1, -(p % r[i - 1]) });
+				mf.AddEdge(i, j + 10, 1, -(p % r[i - 1]));
 
-			dag.Add(new[] { j + 10, 4, 1, p = a[j] * b[j] });
-			dag.Add(new[] { j + 10, 4, 1, p *= b[j] - 1 });
-			dag.Add(new[] { j + 10, 4, 1, p *= b[j] });
+			mf.AddEdge(j + 10, 4, 1, p = a[j] * b[j]);
+			mf.AddEdge(j + 10, 4, 1, p *= b[j] - 1);
+			mf.AddEdge(j + 10, 4, 1, p *= b[j]);
 		}
 
-		Console.WriteLine(-MinCostFlow(n + 9, 0, 4, dag.ToArray(), 3 * m));
+		Console.WriteLine(-mf.GetMinCost(0, 4, 3 * m));
+	}
+}
+
+public class MinCostFlow
+{
+	public class Edge
+	{
+		public int From, To, RevIndex;
+		public long Capacity, Cost;
+		public Edge(int from, int to, long capacity, long cost, int revIndex) { From = from; To = to; Capacity = capacity; Cost = cost; RevIndex = revIndex; }
 	}
 
-	static long MinCostFlow(int n, int sv, int ev, long[][] dag, long f)
+	int n;
+	List<Edge>[] map;
+	public Edge[][] Map;
+
+	public MinCostFlow(int n)
 	{
-		var map = Array.ConvertAll(new int[n + 1], _ => new List<long[]>());
-		foreach (var e in dag)
-		{
-			map[e[0]].Add(new[] { e[0], e[1], e[2], e[3], map[e[1]].Count });
-			map[e[1]].Add(new[] { e[1], e[0], 0, -e[3], map[e[0]].Count - 1 });
-		}
+		this.n = n;
+		map = Array.ConvertAll(new bool[n], _ => new List<Edge>());
+	}
 
-		long BellmanFord()
-		{
-			var from = new long[n + 1][];
-			var cost = new long[n + 1];
-			Array.Fill(cost, long.MaxValue);
-			var minFlow = new long[n + 1];
-			cost[sv] = 0;
-			minFlow[sv] = f;
+	public void AddEdge(int from, int to, long capacity, long cost)
+	{
+		map[from].Add(new Edge(from, to, capacity, cost, map[to].Count));
+		map[to].Add(new Edge(to, from, 0, -cost, map[from].Count - 1));
+	}
 
-			var next = true;
-			while (next)
+	// { from, to, capacity, cost }
+	public void AddEdges(int[][] des)
+	{
+		foreach (var e in des) AddEdge(e[0], e[1], e[2], e[3]);
+	}
+	public void AddEdges(long[][] des)
+	{
+		foreach (var e in des) AddEdge((int)e[0], (int)e[1], e[2], e[3]);
+	}
+
+	long BellmanFord(int sv, int ev, ref long f)
+	{
+		var from = new Edge[n];
+		var cost = Array.ConvertAll(from, _ => long.MaxValue);
+		var minFlow = new long[n];
+		cost[sv] = 0;
+		minFlow[sv] = f;
+
+		var next = true;
+		while (next)
+		{
+			next = false;
+			for (int v = 0; v < n; ++v)
 			{
-				next = false;
-				for (int v = 0; v <= n; v++)
+				if (cost[v] == long.MaxValue) continue;
+				foreach (var e in Map[v])
 				{
-					if (cost[v] == long.MaxValue) continue;
-					foreach (var e in map[v])
-					{
-						if (e[2] == 0 || cost[e[1]] <= cost[v] + e[3]) continue;
-						from[e[1]] = e;
-						cost[e[1]] = cost[v] + e[3];
-						minFlow[e[1]] = Math.Min(minFlow[v], e[2]);
-						next = true;
-					}
+					if (e.Capacity == 0 || cost[e.To] <= cost[v] + e.Cost) continue;
+					from[e.To] = e;
+					cost[e.To] = cost[v] + e.Cost;
+					minFlow[e.To] = Math.Min(minFlow[v], e.Capacity);
+					next = true;
 				}
 			}
-
-			if (from[ev] == null) return long.MaxValue;
-			for (long v = ev; v != sv; v = from[v][0])
-			{
-				var e = from[v];
-				e[2] -= minFlow[ev];
-				map[e[1]][(int)e[4]][2] += minFlow[ev];
-			}
-			f -= minFlow[ev];
-			return minFlow[ev] * cost[ev];
 		}
+
+		if (from[ev] == null) return long.MaxValue;
+		for (var v = ev; v != sv; v = from[v].From)
+		{
+			var e = from[v];
+			e.Capacity -= minFlow[ev];
+			Map[e.To][e.RevIndex].Capacity += minFlow[ev];
+		}
+		f -= minFlow[ev];
+		return minFlow[ev] * cost[ev];
+	}
+
+	// 到達不可能の場合、MaxValue を返します。
+	public long GetMinCost(int sv, int ev, long f)
+	{
+		Map = Array.ConvertAll(map, l => l.ToArray());
 
 		long r = 0, t;
 		while (f > 0)
 		{
-			if ((t = BellmanFord()) == long.MaxValue) return t;
+			if ((t = BellmanFord(sv, ev, ref f)) == long.MaxValue) return t;
 			r += t;
 		}
 		return r;
