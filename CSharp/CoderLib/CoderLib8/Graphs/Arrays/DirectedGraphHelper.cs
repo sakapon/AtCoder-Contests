3da1a67111eec5eff97cab4a8e0bc0d04602b08c
Keiho Sakapon <git@saka-pon.net>
2021-07-05 18:29:28 +0900

    Add old function

--- CSharp/CoderLib/CoderLib8/Graphs/Arrays/DirectedGraphHelper.cs
+++ CSharp/CoderLib/CoderLib8/Graphs/Arrays/DirectedGraphHelper.cs
@@ -46,5 +46,44 @@ namespace CoderLib8.Graphs.Arrays
 			if (r.Count < n) return null;
 			return r.ToArray();
 		}
+
+		// 連結されたグループ順になるとは限りません。
+		// テンプレートとして使えます。
+		[Obsolete]
+		public static int[] TopologicalSort0(int n, int[][] des)
+		{
+			var map = Array.ConvertAll(new bool[n], _ => new List<int[]>());
+			var indeg = new int[n];
+			foreach (var e in des)
+			{
+				map[e[0]].Add(e);
+				++indeg[e[1]];
+			}
+
+			var r = new List<int>();
+			var q = new Queue<int>();
+			for (int v = 0; v < n; ++v)
+			{
+				if (indeg[v] == 0)
+				{
+					r.Add(v);
+					q.Enqueue(v);
+				}
+			}
+
+			while (q.Count > 0)
+			{
+				var v = q.Dequeue();
+				foreach (var e in map[v])
+				{
+					if (--indeg[e[1]] > 0) continue;
+					r.Add(e[1]);
+					q.Enqueue(e[1]);
+				}
+			}
+
+			if (r.Count < n) return null;
+			return r.ToArray();
+		}
 	}
 }
