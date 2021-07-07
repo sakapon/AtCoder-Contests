42c4808bafc236b9f1726b5077c3cfb62fdfff99
Keiho Sakapon <git@saka-pon.net>
2021-07-07 14:28:57 +0900

    AC

---
+++ CSharp/Contests/Typical90/L3/Q082.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Q082
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var lr = Console.ReadLine().Split();
+		var l = long.Parse(lr[0]);
+		return MInt(f(lr[1]) - f((l - 1).ToString()));
+
+		long Sum(long n)
+		{
+			n %= M;
+			var r = n * (n + 1) % M;
+			return r * MHalf % M;
+		}
+
+		long f(string s)
+		{
+			var v = long.Parse(s);
+			if (s.Length == 1) return Sum(v);
+
+			var s9 = new string('9', s.Length - 1);
+			var v9 = long.Parse(s9);
+
+			return MInt((Sum(v) - Sum(v9)) * s.Length + f(s9));
+		}
+	}
+
+	const long M = 1000000007;
+	const long MHalf = (M + 1) / 2;
+	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
+}
---
+++ CSharp/Contests/Typical90/L4/Q085.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Q085
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var k = long.Parse(Console.ReadLine());
+
+		var r = 0;
+		var ds = Divisors(k);
+
+		foreach (var a in ds)
+		{
+			var q = k / a;
+			var ds2 = Divisors(q);
+
+			foreach (var b in ds2)
+			{
+				if (a > b) continue;
+				var c = q / b;
+				if (b > c) continue;
+				r++;
+			}
+		}
+		return r;
+	}
+
+	static long[] Divisors(long n)
+	{
+		var r = new List<long>();
+		for (long x = 1; x * x <= n; ++x) if (n % x == 0) r.Add(x);
+		var i = r.Count - 1;
+		if (r[i] * r[i] == n) --i;
+		for (; i >= 0; --i) r.Add(n / r[i]);
+		return r.ToArray();
+	}
+}
