b23cbc6d505928a54196f69dd89601031f806c09
Keiho Sakapon <git@saka-pon.net>
2021-07-14 14:34:43 +0900

    Add function for inversion number

---
+++ CSharp/AOJCourses/ALDS105/DB2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class DB2
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split().Select(int.Parse).Select((x, i) => new { x, i }).OrderBy(_ => _.x).Select(_ => _.i + 1).ToArray();
+
+		Console.WriteLine(InversionNumber(n, a));
+	}
+
+	static long InversionNumber(int n, int[] a)
+	{
+		var r = 0L;
+		var bit = new BIT(n);
+		for (int i = 0; i < n; ++i)
+		{
+			r += i - bit.Sum(a[i]);
+			bit.Add(a[i], 1);
+		}
+		return r;
+	}
+}
--- CSharp/CoderLib/CoderLib8/Extra/SequenceHelper.cs
+++ CSharp/CoderLib/CoderLib8/Extra/SequenceHelper.cs
@@ -41,5 +41,47 @@ namespace CoderLib8.Extra
 			// 最大長を求める場合。
 			//return dp[n, m];
 		}
+
+		// for permutation (1..n)
+		// 任意の数列に対しては、座標圧縮してから呼び出します。
+		public static long InversionNumber(int n, int[] a)
+		{
+			var r = 0L;
+			var bit = new BIT(n);
+			for (int i = 0; i < n; ++i)
+			{
+				r += i - bit.Sum(a[i]);
+				bit.Add(a[i], 1);
+			}
+			return r;
+		}
+	}
+
+	// 機能限定版
+	class BIT
+	{
+		// Power of 2
+		int n2 = 1;
+		long[] a;
+
+		public BIT(int n)
+		{
+			while (n2 < n) n2 <<= 1;
+			a = new long[n2 + 1];
+		}
+
+		public long this[int i] => Sum(i) - Sum(i - 1);
+
+		public void Add(int i, long v)
+		{
+			for (; i <= n2; i += i & -i) a[i] += v;
+		}
+
+		public long Sum(int r_in)
+		{
+			var r = 0L;
+			for (var i = r_in; i > 0; i -= i & -i) r += a[i];
+			return r;
+		}
 	}
 }
