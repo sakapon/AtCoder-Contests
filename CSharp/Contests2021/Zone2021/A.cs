37be8096fb7e5a01ac37486c7d86a06989055e41
Keiho Sakapon <git@saka-pon.net>
2021-07-22 00:04:57 +0900

    AC

--- CSharp/Contests2021/Zone2021/A.cs
+++ CSharp/Contests2021/Zone2021/A.cs
@@ -10,12 +10,9 @@ class A
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var t = s.Replace("ZONe", "...");
+		return s.Length - t.Length;
 	}
 }
--- CSharp/Contests2021/Zone2021/C.cs
+++ CSharp/Contests2021/Zone2021/C.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 class C
 {
+	const int max = 1 << 30;
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
@@ -11,11 +12,47 @@ class C
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
 		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		int ToBits(int i, int min)
+		{
+			var p = ps[i];
+			var r = 0;
+
+			for (int f = 0; f < 5; f++)
+			{
+				if (p[f] >= min)
+				{
+					r |= 1 << f;
+				}
+			}
+			return r;
+		}
+
+		return Max(-1, max, v =>
+		{
+			var dp = Array.ConvertAll(new bool[1 << 5], _ => max);
+			dp[0] = 0;
+
+			for (int i = 0; i < n; i++)
+			{
+				var y = ToBits(i, v);
+
+				for (int x = dp.Length - 1; x >= 0; x--)
+				{
+					if (dp[x] == max) continue;
+					var nx = x | y;
+					dp[nx] = Math.Min(dp[nx], dp[x] + 1);
+				}
+			}
+			return dp[^1] <= 3;
+		});
+	}
+
+	static int Max(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
 	}
 }
