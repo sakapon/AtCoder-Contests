c6e3bd9d4d73f605e6170466f1017c3c55fe3deb
Keiho Sakapon <git@saka-pon.net>
2021-07-23 01:15:31 +0900

    AC

---
+++ CSharp/Yukicoder/YCC20210722/B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class B
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var s = Console.ReadLine();
+		var n = s.Length;
+
+		if (!IsPalindrome(s)) return n;
+		if (n == 3) return -1;
+		if (s.Distinct().Count() > 1) return n - 2;
+		return -n % 2;
+	}
+
+	static bool IsPalindrome(string s)
+	{
+		for (int i = 0; i < s.Length; ++i) if (s[i] != s[s.Length - 1 - i]) return false;
+		return true;
+	}
+}
---
+++ CSharp/Yukicoder/YCC20210722/E.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class E
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var s = Console.ReadLine();
+		var n = s.Length;
+
+		var r = 0L;
+		var p = MPow(2, n - 1);
+		var M11_2 = MHalf * 11 % M;
+
+		for (int i = n - 1; i >= 0; i--)
+		{
+			var d = s[i] - '0';
+
+			r += p * d;
+			r %= M;
+
+			p *= M11_2;
+			p %= M;
+		}
+
+		return r;
+	}
+
+	const long M = 998244353;
+	const long MHalf = (M + 1) / 2;
+	static long MPow(long b, long i)
+	{
+		long r = 1;
+		for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+		return r;
+	}
+}
