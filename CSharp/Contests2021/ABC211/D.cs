073771562ce0d81447ca61746075fd71332c0542
Keiho Sakapon <git@saka-pon.net>
2021-07-25 19:34:14 +0900

    Improve code

--- CSharp/Contests2021/ABC211/D.cs
+++ CSharp/Contests2021/ABC211/D.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class D
 {
 	const long M = 1000000007;
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
@@ -44,16 +42,16 @@ class D
 				{
 					dp[nv] = dp[v];
 					costs[nv] = nc;
-					//if (nv == ev) return costs;
 					q.Enqueue(nv);
 				}
 			}
 		}
 
-		return dp[n];
+		return dp[ev];
 	}
 
-	static List<int>[] ToMap(int n, int[][] es, bool directed)
+	public static int[][] ToMap(int n, int[][] es, bool directed) => Array.ConvertAll(ToMapList(n, es, directed), l => l.ToArray());
+	public static List<int>[] ToMapList(int n, int[][] es, bool directed)
 	{
 		var map = Array.ConvertAll(new bool[n], _ => new List<int>());
 		foreach (var e in es)
