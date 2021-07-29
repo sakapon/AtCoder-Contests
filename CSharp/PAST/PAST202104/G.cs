80ab4c68c2bc77be6a7a372d00ed2d114c8245da
Keiho Sakapon <git@saka-pon.net>
2021-07-29 20:23:03 +0900

    Improve code

--- CSharp/PAST/PAST202104/G.cs
+++ CSharp/PAST/PAST202104/G.cs
@@ -15,67 +15,54 @@ class G
 		var es = Array.ConvertAll(new bool[m], _ => Read());
 		var x = Read();
 
-		var xMax = x.Max();
-		es = Array.FindAll(es, e => e[2] <= xMax);
-		var map = EdgesToMap2(n + 1, es, false);
-
-		var cumMax = new int[qc];
-		cumMax[^1] = x[^1];
-		for (int i = qc - 2; i >= 0; i--)
-		{
-			cumMax[i] = Math.Max(cumMax[i + 1], x[i]);
-		}
-
-		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		var map = ToMap(n + 1, es, false);
+		var sv = 1;
 
 		var r = 1;
-		var d = Array.ConvertAll(new bool[n + 1], _ => int.MaxValue);
+		var u = new bool[n + 1];
 		var q = PQ<int[]>.Create(e => e[2]);
+		var l = new List<int>();
 
-		d[1] = -1;
-		foreach (var e in map[1])
-		{
-			q.Push(e);
-		}
+		u[sv] = true;
+		q.PushRange(map[sv]);
 
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
 		for (int i = 0; i < qc; i++)
 		{
-			var l = new List<int[]>();
-
 			while (q.Count > 0 && q.First[2] <= x[i])
 			{
 				var e = q.Pop();
-				var nv = e[1];
-				var c = e[2];
+				var to = e[1];
+				if (u[to]) continue;
 
-				if (d[nv] <= i) continue;
-				//if (x[i] < c)
-				//{
-				//	if (i + 1 < qc && cumMax[i + 1] >= c) q.Push(e);
-				//	continue;
-				//}
 				r++;
-				d[nv] = i;
-				foreach (var ne in map[nv])
-				{
-					l.Add(ne);
-				}
+				u[to] = true;
+				l.Add(to);
 			}
 
-			q.PushRange(l.ToArray());
+			foreach (var v in l)
+			{
+				foreach (var e in map[v])
+				{
+					var to = e[1];
+					if (u[to]) continue;
+					q.Push(e);
+				}
+			}
+			l.Clear();
+
 			Console.WriteLine(r);
 		}
-
 		Console.Out.Flush();
 	}
 
-	// weighted
-	static List<int[]>[] EdgesToMap2(int n, int[][] es, bool directed)
+	public static int[][][] ToMap(int n, int[][] es, bool directed) => Array.ConvertAll(ToMapList(n, es, directed), l => l.ToArray());
+	public static List<int[]>[] ToMapList(int n, int[][] es, bool directed)
 	{
 		var map = Array.ConvertAll(new bool[n], _ => new List<int[]>());
 		foreach (var e in es)
 		{
-			map[e[0]].Add(new[] { e[0], e[1], e[2] });
+			map[e[0]].Add(e);
 			if (!directed) map[e[1]].Add(new[] { e[1], e[0], e[2] });
 		}
 		return map;
