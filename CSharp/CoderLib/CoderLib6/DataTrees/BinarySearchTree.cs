b1e34b821bc5fb25cc3d3ef2881f3cd930e16602
Keiho Sakapon <git@saka-pon.net>
2021-08-03 17:17:24 +0900

    Add class for binary search tree

---
+++ CSharp/CoderLib/CoderLib6/DataTrees/BinarySearchTree.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderLib6.DataTrees
+{
+	public class BSTree<T>
+	{
+		public static BSTree<T> Create<TKey>(Func<T, TKey> keySelector, bool descending = false)
+		{
+			if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
+			var c = Comparer<TKey>.Default;
+			return descending ?
+				new BSTree<T>((x, y) => c.Compare(keySelector(y), keySelector(x))) :
+				new BSTree<T>((x, y) => c.Compare(keySelector(x), keySelector(y)));
+		}
+
+		[System.Diagnostics.DebuggerDisplay(@"\{{Value}\}")]
+		class Node
+		{
+			public T Value;
+			public Node Parent;
+
+			Node _left;
+			public Node Left
+			{
+				get { return _left; }
+				set
+				{
+					_left = value;
+					if (value != null) value.Parent = this;
+				}
+			}
+
+			Node _right;
+			public Node Right
+			{
+				get { return _right; }
+				set
+				{
+					_right = value;
+					if (value != null) value.Parent = this;
+				}
+			}
+		}
+
+		Node _root;
+		Node Root
+		{
+			get { return _root; }
+			set
+			{
+				_root = value;
+				if (value != null) value.Parent = null;
+			}
+		}
+
+		Comparison<T> compare;
+		public int Count { get; private set; }
+
+		public BSTree(Comparison<T> comparison = null)
+		{
+			compare = comparison ?? Comparer<T>.Default.Compare;
+		}
+
+		static Node SearchMinNode(Node node)
+		{
+			if (node == null) return null;
+			return SearchMinNode(node.Left) ?? node;
+		}
+
+		static Node SearchMaxNode(Node node)
+		{
+			if (node == null) return null;
+			return SearchMaxNode(node.Right) ?? node;
+		}
+
+		static Node SearchMinNode(Node node, Func<T, bool> f)
+		{
+			if (node == null) return null;
+			if (f(node.Value)) return SearchMinNode(node.Left, f) ?? node;
+			else return SearchMinNode(node.Right, f);
+		}
+
+		static Node SearchMaxNode(Node node, Func<T, bool> f)
+		{
+			if (node == null) return null;
+			if (f(node.Value)) return SearchMaxNode(node.Right, f) ?? node;
+			else return SearchMaxNode(node.Left, f);
+		}
+
+		static Node SearchPreviousAncestor(Node node)
+		{
+			if (node == null) return null;
+			if (node.Parent == null) return null;
+			if (node.Parent.Right == node) return node.Parent;
+			else return SearchPreviousAncestor(node.Parent);
+		}
+
+		static Node SearchNextAncestor(Node node)
+		{
+			if (node == null) return null;
+			if (node.Parent == null) return null;
+			if (node.Parent.Left == node) return node.Parent;
+			else return SearchNextAncestor(node.Parent);
+		}
+
+		static Node SearchPreviousNode(Node node)
+		{
+			if (node == null) return null;
+			return SearchMaxNode(node.Left) ?? SearchPreviousAncestor(node);
+		}
+
+		static Node SearchNextNode(Node node)
+		{
+			if (node == null) return null;
+			return SearchMinNode(node.Right) ?? SearchNextAncestor(node);
+		}
+
+		Node SearchNode(Node node, T value)
+		{
+			if (node == null) return null;
+			var d = compare(value, node.Value);
+			if (d == 0) return node;
+			if (d < 0) return SearchNode(node.Left, value);
+			else return SearchNode(node.Right, value);
+		}
+
+		public T GetMin()
+		{
+			if (Root == null) throw new InvalidOperationException("The tree is empty.");
+			return SearchMinNode(Root).Value;
+		}
+
+		public T GetMax()
+		{
+			if (Root == null) throw new InvalidOperationException("The tree is empty.");
+			return SearchMaxNode(Root).Value;
+		}
+
+		public T GetNextValue(T value, T defaultValue = default(T))
+		{
+			var node = SearchNode(Root, value);
+			if (node == null) throw new InvalidOperationException("The value does not exist.");
+			node = SearchNextNode(node);
+			if (node == null) return defaultValue;
+			return node.Value;
+		}
+
+		public T GetPreviousValue(T value, T defaultValue = default(T))
+		{
+			var node = SearchNode(Root, value);
+			if (node == null) throw new InvalidOperationException("The value does not exist.");
+			node = SearchPreviousNode(node);
+			if (node == null) return defaultValue;
+			return node.Value;
+		}
+
+		public IEnumerable<T> GetValues()
+		{
+			for (var n = SearchMinNode(Root); n != null; n = SearchNextNode(n))
+			{
+				yield return n.Value;
+			}
+		}
+
+		public IEnumerable<T> GetValues(Func<T, bool> predicateForMin, Func<T, bool> predicateForMax)
+		{
+			for (var n = SearchMinNode(Root, predicateForMin); n != null && predicateForMax(n.Value); n = SearchNextNode(n))
+			{
+				yield return n.Value;
+			}
+		}
+
+		public bool Contains(T value)
+		{
+			return SearchNode(Root, value) != null;
+		}
+
+		public bool Add(T value)
+		{
+			bool r;
+			if (Root == null)
+			{
+				Root = new Node { Value = value };
+				r = true;
+			}
+			else
+			{
+				r = Add(Root, value);
+			}
+
+			if (r) ++Count;
+			return r;
+		}
+
+		// Suppose t != null.
+		bool Add(Node t, T value)
+		{
+			var d = compare(value, t.Value);
+			if (d == 0) return false;
+			if (d < 0)
+			{
+				if (t.Left != null) return Add(t.Left, value);
+				t.Left = new Node { Value = value };
+			}
+			else
+			{
+				if (t.Right != null) return Add(t.Right, value);
+				t.Right = new Node { Value = value };
+			}
+			return true;
+		}
+
+		public bool Remove(T value)
+		{
+			var node = SearchNode(Root, value);
+			if (node == null) return false;
+
+			Remove(node);
+			--Count;
+			return true;
+		}
+
+		// Suppose t != null.
+		void Remove(Node t)
+		{
+			if (t.Left == null || t.Right == null)
+			{
+				var c = t.Left ?? t.Right;
+
+				if (t.Parent == null)
+				{
+					Root = c;
+				}
+				else if (t.Parent.Left == t)
+				{
+					t.Parent.Left = c;
+				}
+				else
+				{
+					t.Parent.Right = c;
+				}
+			}
+			else
+			{
+				var t2 = SearchNextNode(t);
+				t.Value = t2.Value;
+				Remove(t2);
+			}
+		}
+
+		// Suppose values are distinct and sorted.
+		public void SetValues(T[] values)
+		{
+			Root = CreateSubtree(values, 0, values.Length);
+		}
+
+		Node CreateSubtree(T[] values, int l, int r)
+		{
+			if (r - l == 0) return null;
+			if (r - l == 1) return new Node { Value = values[l] };
+
+			var m = (l + r) / 2;
+			return new Node
+			{
+				Value = values[m],
+				Left = CreateSubtree(values, l, m),
+				Right = CreateSubtree(values, m + 1, r),
+			};
+		}
+	}
+}
