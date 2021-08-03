d7e9d9b5e354532dd7e4a182779fcef299f004b7
Keiho Sakapon <git@saka-pon.net>
2021-08-03 23:31:21 +0900

    AC

--- CSharp/AOJCourses/ALDS111/B.cs
+++ CSharp/AOJCourses/ALDS111/B.cs
@@ -3,14 +3,41 @@ using System.Linq;
 
 class B
 {
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main()
 	{
-		Console.ReadLine();
-		var s = Console.ReadLine();
 		var n = int.Parse(Console.ReadLine());
-		var h = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		var ps = new int[n].Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
 
-		Console.WriteLine(string.Join(" ", h));
+		var map = new int[n + 1][];
+		for (int i = 0; i < n; i++)
+		{
+			var a = Read();
+			map[a[0]] = a.Skip(2).ToArray();
+		}
+
+		var pre = new int[n + 1];
+		var post = new int[n + 1];
+		var u = new bool[n + 1];
+		var t = 0;
+
+		Action<int> Dfs = null;
+		Dfs = v =>
+		{
+			if (u[v]) return;
+			u[v] = true;
+
+			pre[v] = ++t;
+			foreach (var nv in map[v])
+			{
+				Dfs(nv);
+			}
+			post[v] = ++t;
+		};
+
+		for (int v = 1; v <= n; v++)
+			Dfs(v);
+
+		for (int v = 1; v <= n; v++)
+			Console.WriteLine($"{v} {pre[v]} {post[v]}");
 	}
 }
