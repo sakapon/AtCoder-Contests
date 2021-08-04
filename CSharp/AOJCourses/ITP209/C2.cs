733c6471acb9ccbd9984d6907e067cff9e6b2934
Keiho Sakapon <git@saka-pon.net>
2021-08-04 17:33:18 +0900

    Other AC

---
+++ CSharp/AOJCourses/ITP209/C2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class C2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+		var m = int.Parse(Console.ReadLine());
+		var b = Read();
+
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		for (int i = 0, j = 0; i < n;)
+		{
+			if (j == m)
+			{
+				Console.WriteLine(a[i++]);
+			}
+			else
+			{
+				if (a[i] < b[j])
+				{
+					Console.WriteLine(a[i++]);
+				}
+				else if (a[i] > b[j])
+				{
+					j++;
+				}
+				else
+				{
+					i++;
+					j++;
+				}
+			}
+		}
+		Console.Out.Flush();
+	}
+}
