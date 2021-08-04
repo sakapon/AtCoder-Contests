88559f11aebb6f8b2f121fff741606e80f1ccf42
Keiho Sakapon <git@saka-pon.net>
2021-08-04 15:29:14 +0900

    AC

---
+++ CSharp/AOJCourses/ITP209/A.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class A
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main()
+	{
+		Console.ReadLine();
+		var a = Read();
+		Console.ReadLine();
+		var b = Read();
+
+		var set = new SortedSet<int>(a.Concat(b));
+		Console.WriteLine(string.Join("\n", set));
+	}
+}
---
+++ CSharp/AOJCourses/ITP209/D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class D
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main()
+	{
+		Console.ReadLine();
+		var a = Read();
+		Console.ReadLine();
+		var b = Read();
+
+		var set = new SortedSet<int>(a);
+		foreach (var x in b)
+			if (!set.Add(x))
+				set.Remove(x);
+		if (set.Any()) Console.WriteLine(string.Join("\n", set));
+	}
+}
