81be77090344cddd6bd828e344cd1f4f17acff31
Keiho Sakapon <git@saka-pon.net>
2021-08-05 23:06:13 +0900

    Other AC (AVL tree)

---
+++ CSharp/Contests/Typical90/L3/Q007T.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib6.DataTrees;
+
+class Q007T
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+		var qc = int.Parse(Console.ReadLine());
+		var qs = Array.ConvertAll(new bool[qc], _ => int.Parse(Console.ReadLine()));
+
+		var set = new AvlTree<int>();
+		foreach (var v in a) set.Add(v);
+		set.Add(-1 << 30);
+		set.Add(int.MaxValue);
+
+		return string.Join("\n", qs.Select(GetMin));
+
+		int GetMin(int bv)
+		{
+			var av2 = set.GetValues(x => x >= bv, x => true).First();
+			var av1 = set.GetPreviousValue(av2);
+			return Math.Min(av2 - bv, bv - av1);
+		}
+	}
+}
---
+++ CSharp/Contests/Typical90/L5/Q006T.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CoderLib6.DataTrees;
+
+class Q006T
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, k) = Read2();
+		var s = Console.ReadLine();
+
+		var r = new List<char>();
+
+		var t = -1;
+		int ti;
+		var set = AvlTree<int>.Create(i => s[i] * 100000L + i);
+		for (int i = 0; i < n - k; i++)
+		{
+			set.Add(i);
+		}
+
+		for (int i = n - k; i < n; i++)
+		{
+			set.Add(i);
+			while ((ti = set.GetMin()) < t) set.Remove(ti);
+
+			set.Remove(ti);
+			t = ti;
+			r.Add(s[t]);
+		}
+
+		return string.Join("", r);
+	}
+}
