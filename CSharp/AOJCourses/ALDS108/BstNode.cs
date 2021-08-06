53f48c903ccd6744cbbf421f01b116c3547a606d
Keiho Sakapon <git@saka-pon.net>
2021-08-06 15:14:30 +0900

    Add class for tree node

---
+++ CSharp/AOJCourses/ALDS108/BstNode.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+[System.Diagnostics.DebuggerDisplay(@"\{{Key}\}")]
+public class BstNode<TKey>
+{
+	public TKey Key { get; set; }
+	public BstNode<TKey> Parent { get; private set; }
+
+	BstNode<TKey> _left;
+	public BstNode<TKey> Left
+	{
+		get { return _left; }
+		set
+		{
+			_left = value;
+			if (value != null) value.Parent = this;
+		}
+	}
+
+	BstNode<TKey> _right;
+	public BstNode<TKey> Right
+	{
+		get { return _right; }
+		set
+		{
+			_right = value;
+			if (value != null) value.Parent = this;
+		}
+	}
+}
+
+public static class BstNode
+{
+	public static BstNode<TKey> SearchNode<TKey>(this BstNode<TKey> node, TKey key, Comparison<TKey> compare)
+	{
+		if (node == null) return null;
+		var d = compare(key, node.Key);
+		if (d == 0) return node;
+		if (d < 0) return SearchNode(node.Left, key, compare);
+		else return SearchNode(node.Right, key, compare);
+	}
+
+	public static BstNode<TKey> SearchMinNode<TKey>(this BstNode<TKey> node)
+	{
+		if (node == null) return null;
+		return SearchMinNode(node.Left) ?? node;
+	}
+
+	public static BstNode<TKey> SearchMaxNode<TKey>(this BstNode<TKey> node)
+	{
+		if (node == null) return null;
+		return SearchMaxNode(node.Right) ?? node;
+	}
+
+	public static BstNode<TKey> SearchMinNode<TKey>(this BstNode<TKey> node, Func<TKey, bool> predicate)
+	{
+		if (node == null) return null;
+		if (predicate(node.Key)) return SearchMinNode(node.Left, predicate) ?? node;
+		else return SearchMinNode(node.Right, predicate);
+	}
+
+	public static BstNode<TKey> SearchMaxNode<TKey>(this BstNode<TKey> node, Func<TKey, bool> predicate)
+	{
+		if (node == null) return null;
+		if (predicate(node.Key)) return SearchMaxNode(node.Right, predicate) ?? node;
+		else return SearchMaxNode(node.Left, predicate);
+	}
+
+	public static BstNode<TKey> SearchPreviousNode<TKey>(this BstNode<TKey> node)
+	{
+		if (node == null) return null;
+		return SearchMaxNode(node.Left) ?? SearchPreviousAncestor(node);
+	}
+
+	public static BstNode<TKey> SearchNextNode<TKey>(this BstNode<TKey> node)
+	{
+		if (node == null) return null;
+		return SearchMinNode(node.Right) ?? SearchNextAncestor(node);
+	}
+
+	static BstNode<TKey> SearchPreviousAncestor<TKey>(this BstNode<TKey> node)
+	{
+		if (node?.Parent == null) return null;
+		if (node.Parent.Right == node) return node.Parent;
+		else return SearchPreviousAncestor(node.Parent);
+	}
+
+	static BstNode<TKey> SearchNextAncestor<TKey>(this BstNode<TKey> node)
+	{
+		if (node?.Parent == null) return null;
+		if (node.Parent.Left == node) return node.Parent;
+		else return SearchNextAncestor(node.Parent);
+	}
+
+	public static IEnumerable<TKey> GetKeys<TKey>(this BstNode<TKey> node)
+	{
+		for (var n = SearchMinNode(node); n != null; n = SearchNextNode(n))
+		{
+			yield return n.Key;
+		}
+	}
+
+	public static IEnumerable<TKey> GetKeys<TKey>(this BstNode<TKey> node, Func<TKey, bool> predicateForMin, Func<TKey, bool> predicateForMax)
+	{
+		for (var n = SearchMinNode(node, predicateForMin); n != null && predicateForMax(n.Key); n = SearchNextNode(n))
+		{
+			yield return n.Key;
+		}
+	}
+}
