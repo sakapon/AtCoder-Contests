ecc0c4f57e048d2f121d8306687aadad63f7b877
Keiho Sakapon <git@saka-pon.net>
2021-08-09 01:28:05 +0900

    Other AC

---
+++ CSharp/Contests2021/ABC213/B2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class B2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+
+		// ソートしない方法
+		var amax1 = a.Max();
+		var amax2 = a.Max(x => x == amax1 ? 0 : x);
+		return Array.IndexOf(a, amax2) + 1;
+	}
+}
