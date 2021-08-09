4223176be5eb457272d231b7147454d51e47fbb2
Keiho Sakapon <git@saka-pon.net>
2021-08-09 10:11:20 +0900

    Other AC

---
+++ CSharp/Contests2021/ABC213/DL.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DL
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var es = Array.ConvertAll(new bool[n - 1], _ => Read());
+
+		var tree = new Tree(n + 1, 1, es);
+		return string.Join(" ", tree.Tour);
+	}
+}
+
+public class Tree
+{
+	static List<int>[] ToMap(int n, int[][] es, bool directed)
+	{
+		var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e[1]);
+			if (!directed) map[e[1]].Add(e[0]);
+		}
+		for (int v = 0; v < n; v++)
+		{
+			map[v].Sort();
+		}
+		return map;
+	}
+
+	public int Count { get; }
+	public int Root { get; }
+	public List<int>[] Map { get; }
+	public int[] Depths { get; }
+	public int[] Parents { get; }
+
+	// この Euler Tour では方向を記録しません。
+	// order -> vertex
+	public int[] Tour { get; }
+	// vertex -> orders
+	public List<int>[] TourMap { get; }
+
+	public Tree(int n, int root, int[][] ues) : this(n, root, ToMap(n, ues, false)) { }
+	public Tree(int n, int root, List<int>[] map)
+	{
+		Count = n;
+		Root = root;
+		Map = map;
+		Depths = Array.ConvertAll(Map, _ => -1);
+		Parents = Array.ConvertAll(Map, _ => -1);
+
+		var tour = new List<int>();
+		TourMap = Array.ConvertAll(Map, _ => new List<int>());
+
+		Depths[root] = 0;
+		Dfs(root, -1);
+
+		Tour = tour.ToArray();
+
+		void Dfs(int v, int pv)
+		{
+			TourMap[v].Add(tour.Count);
+			tour.Add(v);
+
+			foreach (var nv in Map[v])
+			{
+				if (nv == pv) continue;
+				Depths[nv] = Depths[v] + 1;
+				Parents[nv] = v;
+				Dfs(nv, v);
+
+				TourMap[v].Add(tour.Count);
+				tour.Add(v);
+			}
+		}
+	}
+}
