a0f55cb6fb76fef9000c8d2efb56c8c2eec73d4a
Keiho Sakapon <git@saka-pon.net>
2021-08-11 18:31:36 +0900

    Other AC

--- CSharp/AOJCourses/ITP208/CA.cs
+++ CSharp/AOJCourses/ITP208/CA.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using CoderLib6.DataTrees;
 
 class CA
@@ -9,21 +8,22 @@ class CA
 	{
 		var n = int.Parse(Console.ReadLine());
 
-		var r = new List<string>();
 		var map = new AvlMap<string, string>();
 
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
 		for (int i = 0; i < n; i++)
 		{
 			var q = Console.ReadLine().Split();
 			if (q[0] == "0")
 				map[q[1]] = q[2];
 			else if (q[0] == "1")
-				r.Add(map.ContainsKey(q[1]) ? map[q[1]] : "0");
+				Console.WriteLine(map.ContainsKey(q[1]) ? map[q[1]] : "0");
 			else if (q[0] == "2")
 				map.Remove(q[1]);
 			else
-				r.AddRange(map.GetItems(x => x.CompareTo(q[1]) >= 0, x => x.CompareTo(q[2]) <= 0).Select(p => $"{p.Key} {p.Value}"));
+				foreach (var p in map.GetItems(x => x.CompareTo(q[1]) >= 0, x => x.CompareTo(q[2]) <= 0))
+					Console.WriteLine($"{p.Key} {p.Value}");
 		}
-		Console.WriteLine(string.Join("\n", r));
+		Console.Out.Flush();
 	}
 }
---
+++ CSharp/AOJCourses/ITP208/CA2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib6.DataTrees;
+
+class CA2
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var qs = Array.ConvertAll(new bool[n], _ => Console.ReadLine().Split());
+
+		var keys = qs.Select(q => q[1]).Concat(qs.Where(q => q[0] == "3").Select(q => q[2])).Distinct().ToArray();
+		Array.Sort(keys);
+		var keymap = Enumerable.Range(0, keys.Length).ToDictionary(i => keys[i], i => i);
+
+		var map = new AvlMap<int, string>();
+
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		foreach (var q in qs)
+		{
+			var key = keymap[q[1]];
+			if (q[0] == "0")
+				map[key] = q[2];
+			else if (q[0] == "1")
+				Console.WriteLine(map.ContainsKey(key) ? map[key] : "0");
+			else if (q[0] == "2")
+				map.Remove(key);
+			else
+				foreach (var p in map.GetItems(x => x >= key, x => x <= keymap[q[2]]))
+					Console.WriteLine($"{keys[p.Key]} {p.Value}");
+		}
+		Console.Out.Flush();
+	}
+}
