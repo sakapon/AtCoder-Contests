0be8bd07297151f0181580b70664d50f56273fa6
Keiho Sakapon <git@saka-pon.net>
2021-08-12 15:38:11 +0900

    Re-design AvlMap class

---
+++ CSharp/CoderLib/CoderLib6/DataTrees/AvlMap.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderLib6.DataTrees
+{
+	public class AvlMap<TKey, TValue> : BstMapBase<TKey, TValue, AvlSetNode<KeyValuePair<TKey, TValue>>>
+	{
+		public static AvlMap<TKey, TValue> Create(bool descending = false) =>
+			new AvlMap<TKey, TValue>(ComparisonHelper.Create<TKey>(descending));
+
+		public AvlMap(Comparison<TKey> comparison = null) : base(comparison) { }
+
+		public override bool Add(TKey key, TValue value)
+		{
+			var c = Count;
+			Root = Add(Root, key, value);
+			return Count != c;
+		}
+
+		AvlSetNode<KeyValuePair<TKey, TValue>> Add(AvlSetNode<KeyValuePair<TKey, TValue>> node, TKey key, TValue value)
+		{
+			if (node == null)
+			{
+				++Count;
+				return new AvlSetNode<KeyValuePair<TKey, TValue>> { Key = new KeyValuePair<TKey, TValue>(key, value) };
+			}
+
+			var d = compare(key, node.Key.Key);
+			if (d == 0) return node;
+
+			if (d < 0)
+			{
+				node.TypedLeft = Add(node.TypedLeft, key, value);
+			}
+			else
+			{
+				node.TypedRight = Add(node.TypedRight, key, value);
+			}
+
+			var lrh = node.LeftHeight - node.RightHeight;
+			if (lrh > 2 || lrh == 2 && node.TypedLeft.LeftHeight >= node.TypedLeft.RightHeight)
+			{
+				node = node.RotateToRight() as AvlSetNode<KeyValuePair<TKey, TValue>>;
+				node.TypedRight.UpdateHeight();
+			}
+			else if (lrh < -2 || lrh == -2 && node.TypedRight.LeftHeight <= node.TypedRight.RightHeight)
+			{
+				node = node.RotateToLeft() as AvlSetNode<KeyValuePair<TKey, TValue>>;
+				node.TypedLeft.UpdateHeight();
+			}
+
+			node.UpdateHeight();
+			return node;
+		}
+
+		public override bool Remove(TKey key)
+		{
+			var node = Root.SearchNode(key, compare) as AvlSetNode<KeyValuePair<TKey, TValue>>;
+			if (node == null) return false;
+
+			RemoveTarget(node);
+			--Count;
+			return true;
+		}
+
+		// Suppose t != null.
+		void RemoveTarget(AvlSetNode<KeyValuePair<TKey, TValue>> t)
+		{
+			if (t.TypedLeft == null || t.TypedRight == null)
+			{
+				var c = t.TypedLeft ?? t.TypedRight;
+
+				if (t.TypedParent == null)
+				{
+					Root = c;
+				}
+				else if (t.TypedParent.TypedLeft == t)
+				{
+					t.TypedParent.TypedLeft = c;
+				}
+				else
+				{
+					t.TypedParent.TypedRight = c;
+				}
+
+				t.TypedParent?.UpdateHeight(true);
+			}
+			else
+			{
+				var t2 = t.SearchNextNode() as AvlSetNode<KeyValuePair<TKey, TValue>>;
+				t.Key = t2.Key;
+				RemoveTarget(t2);
+			}
+		}
+	}
+}
--- CSharp/CoderLib/CoderLib6/DataTrees/BstBase.cs
+++ CSharp/CoderLib/CoderLib6/DataTrees/BstBase.cs
@@ -23,6 +23,15 @@ namespace CoderLib6.DataTrees
 			else return SearchNode(node.Right, key, comparison);
 		}
 
+		public static BstNodeBase<KeyValuePair<TKey, TValue>> SearchNode<TKey, TValue>(this BstNodeBase<KeyValuePair<TKey, TValue>> node, TKey key, Comparison<TKey> comparison)
+		{
+			if (node == null) return null;
+			var d = comparison(key, node.Key.Key);
+			if (d == 0) return node;
+			if (d < 0) return SearchNode(node.Left, key, comparison);
+			else return SearchNode(node.Right, key, comparison);
+		}
+
 		public static BstNodeBase<TKey> SearchMinNode<TKey>(this BstNodeBase<TKey> node)
 		{
 			if (node == null) return null;
@@ -46,6 +55,20 @@ namespace CoderLib6.DataTrees
 		{
 			if (node == null) return null;
 			if (predicate(node.Key)) return SearchMaxNode(node.Right, predicate) ?? node;
+			else return SearchMaxNode(node.Left, predicate);
+		}
+
+		public static BstNodeBase<KeyValuePair<TKey, TValue>> SearchMinNode<TKey, TValue>(this BstNodeBase<KeyValuePair<TKey, TValue>> node, Func<TKey, bool> predicate)
+		{
+			if (node == null) return null;
+			if (predicate(node.Key.Key)) return SearchMinNode(node.Left, predicate) ?? node;
+			else return SearchMinNode(node.Right, predicate);
+		}
+
+		public static BstNodeBase<KeyValuePair<TKey, TValue>> SearchMaxNode<TKey, TValue>(this BstNodeBase<KeyValuePair<TKey, TValue>> node, Func<TKey, bool> predicate)
+		{
+			if (node == null) return null;
+			if (predicate(node.Key.Key)) return SearchMaxNode(node.Right, predicate) ?? node;
 			else return SearchMaxNode(node.Left, predicate);
 		}
 
@@ -86,6 +109,14 @@ namespace CoderLib6.DataTrees
 		public static IEnumerable<TKey> GetKeys<TKey>(this BstNodeBase<TKey> node, Func<TKey, bool> predicateForMin, Func<TKey, bool> predicateForMax)
 		{
 			for (var n = SearchMinNode(node, predicateForMin); n != null && predicateForMax(n.Key); n = SearchNextNode(n))
+			{
+				yield return n.Key;
+			}
+		}
+
+		public static IEnumerable<KeyValuePair<TKey, TValue>> GetKeys<TKey, TValue>(this BstNodeBase<KeyValuePair<TKey, TValue>> node, Func<TKey, bool> predicateForMin, Func<TKey, bool> predicateForMax)
+		{
+			for (var n = SearchMinNode(node, predicateForMin); n != null && predicateForMax(n.Key.Key); n = SearchNextNode(n))
 			{
 				yield return n.Key;
 			}
@@ -195,6 +226,107 @@ namespace CoderLib6.DataTrees
 
 		public abstract bool Add(T item);
 		public abstract bool Remove(T item);
+	}
+
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public abstract class BstMapBase<TKey, TValue, TNode> : IEnumerable<KeyValuePair<TKey, TValue>> where TNode : BstNodeBase<KeyValuePair<TKey, TValue>>
+	{
+		TNode _root;
+		public TNode Root
+		{
+			get { return _root; }
+			protected set
+			{
+				_root = value;
+				if (value != null) value.Parent = null;
+			}
+		}
+
+		protected Comparison<TKey> compare;
+		public int Count { get; protected set; }
+
+		protected BstMapBase(Comparison<TKey> comparison = null)
+		{
+			compare = comparison ?? Comparer<TKey>.Default.Compare;
+		}
+
+		public bool ContainsKey(TKey key)
+		{
+			return Root.SearchNode(key, compare) != null;
+		}
+
+		public KeyValuePair<TKey, TValue> GetMin()
+		{
+			if (Root == null) throw new InvalidOperationException("The tree is empty.");
+			return Root.SearchMinNode().Key;
+		}
+
+		public KeyValuePair<TKey, TValue> GetMax()
+		{
+			if (Root == null) throw new InvalidOperationException("The tree is empty.");
+			return Root.SearchMaxNode().Key;
+		}
+
+		public KeyValuePair<TKey, TValue> GetMin(Func<TKey, bool> predicate)
+		{
+			if (Root == null) throw new InvalidOperationException("The tree is empty.");
+			return Root.SearchMinNode(predicate).Key;
+		}
+
+		public KeyValuePair<TKey, TValue> GetMax(Func<TKey, bool> predicate)
+		{
+			if (Root == null) throw new InvalidOperationException("The tree is empty.");
+			return Root.SearchMaxNode(predicate).Key;
+		}
+
+		public KeyValuePair<TKey, TValue> GetPrevious(TKey key)
+		{
+			var node = Root.SearchNode(key, compare);
+			if (node == null) throw new InvalidOperationException("The item is not found.");
+			node = node.SearchPreviousNode();
+			if (node == null) throw new InvalidOperationException("The target item is not found.");
+			return node.Key;
+		}
+
+		public KeyValuePair<TKey, TValue> GetNext(TKey key)
+		{
+			var node = Root.SearchNode(key, compare);
+			if (node == null) throw new InvalidOperationException("The item is not found.");
+			node = node.SearchNextNode();
+			if (node == null) throw new InvalidOperationException("The target item is not found.");
+			return node.Key;
+		}
+
+		public IEnumerable<KeyValuePair<TKey, TValue>> GetItems() => Root.GetKeys();
+		public IEnumerable<KeyValuePair<TKey, TValue>> GetItems(Func<TKey, bool> predicateForMin, Func<TKey, bool> predicateForMax) => Root.GetKeys(predicateForMin, predicateForMax);
+
+		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => GetItems().GetEnumerator();
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetItems().GetEnumerator();
+
+		public abstract bool Add(TKey key, TValue value);
+		public abstract bool Remove(TKey key);
+
+		public TValue this[TKey key]
+		{
+			get
+			{
+				var node = Root.SearchNode(key, compare);
+				if (node == null) throw new InvalidOperationException("The item is not found.");
+				return node.Key.Value;
+			}
+			set
+			{
+				var node = Root.SearchNode(key, compare);
+				if (node == null) Add(key, value);
+				else node.Key = new KeyValuePair<TKey, TValue>(key, value);
+			}
+		}
+
+		public TValue GetValueOrDefault(TKey key, TValue defaultValue = default(TValue))
+		{
+			var node = Root.SearchNode(key, compare);
+			return node == null ? defaultValue : node.Key.Value;
+		}
 	}
 
 	public static class ComparisonHelper
