948a40ba721dfa692115fe7082c68c480a58fcf8
Keiho Sakapon <git@saka-pon.net>
2021-08-12 16:45:32 +0900

    Use AvlMap

--- CSharp/PAST/PAST202104/MT.cs
+++ CSharp/PAST/PAST202104/MT.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CoderLib6.DataTrees;
 
 class MT
 {
@@ -24,13 +25,13 @@ class MT
 			d[x] = c;
 		}
 
-		var set = Treap<(int l, int r)>.Create(_ => _.l);
+		var map = new AvlMap<int, int>();
 
 		for (var (l, r) = (0, 1); r <= n; r++)
 		{
 			if (r == n || a[r] != a[r - 1])
 			{
-				set.Add((l, r));
+				map.Add(l, r);
 				l = r;
 			}
 		}
@@ -41,7 +42,7 @@ class MT
 			var (L, R, X) = Read3();
 			L--;
 
-			var ranges = set.GetValues(_ => L < _.r, _ => _.l < R).ToArray();
+			var ranges = map.GetItems(p => L < p.Value, p => p.Key < R).ToArray();
 			var (ll, _) = ranges[0];
 			var (rl, rr) = ranges[^1];
 			var lx = a[ll];
@@ -51,7 +52,7 @@ class MT
 			foreach (var (l, r) in ranges)
 			{
 				Add(a[l], -(r - l));
-				set.Remove((l, r));
+				map.Remove(l);
 			}
 
 			Add(lx, L - ll);
@@ -61,9 +62,9 @@ class MT
 			a[L] = X;
 			if (R < rr) a[R] = rx;
 
-			if (ll < L) set.Add((ll, L));
-			set.Add((L, R));
-			if (R < rr) set.Add((R, rr));
+			if (ll < L) map.Add(ll, L);
+			map.Add(L, R);
+			if (R < rr) map.Add(R, rr);
 
 			Console.WriteLine(sum);
 		}
@@ -71,302 +72,20 @@ class MT
 	}
 }
 
-public class Treap<T>
+// Dummy
+namespace CoderLib6.DataTrees
 {
-	public static Treap<T> Create<TKey>(Func<T, TKey> keySelector, bool descending = false)
+	public class AvlMap<TKey, TValue>
 	{
-		if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+		public IEnumerable<KeyValuePair<TKey, TValue>> GetItems(Func<KeyValuePair<TKey, TValue>, bool> predicateForMin, Func<KeyValuePair<TKey, TValue>, bool> predicateForMax) => throw new NotImplementedException();
+		public IEnumerable<KeyValuePair<TKey, TValue>> GetItems(Func<TKey, bool> predicateForMin, Func<TKey, bool> predicateForMax) => throw new NotImplementedException();
+		public bool Add(TKey key, TValue value) => throw new NotImplementedException();
+		public bool Remove(TKey key) => throw new NotImplementedException();
 
-		var c = Comparer<TKey>.Default;
-		return descending ?
-			new Treap<T>((x, y) => c.Compare(keySelector(y), keySelector(x))) :
-			new Treap<T>((x, y) => c.Compare(keySelector(x), keySelector(y)));
-	}
-
-	[System.Diagnostics.DebuggerDisplay(@"\{{Value}\}")]
-	class Node
-	{
-		public T Value;
-		public Node Parent;
-		public int Priority;
-
-		Node _left;
-		public Node Left
+		public TValue this[TKey key]
 		{
-			get { return _left; }
-			set
-			{
-				_left = value;
-				if (value != null) value.Parent = this;
-			}
-		}
-
-		Node _right;
-		public Node Right
-		{
-			get { return _right; }
-			set
-			{
-				_right = value;
-				if (value != null) value.Parent = this;
-			}
-		}
-	}
-
-	Node _root;
-	Node Root
-	{
-		get { return _root; }
-		set
-		{
-			_root = value;
-			if (value != null) value.Parent = null;
-		}
-	}
-
-	HashSet<int> prioritySet = new HashSet<int>();
-	Random random = new Random();
-	int CreatePriority()
-	{
-		int v;
-		while (!prioritySet.Add(v = random.Next())) ;
-		return v;
-	}
-	void RemovePriority(int v) => prioritySet.Remove(v);
-
-	Comparison<T> compare;
-	public int Count { get; private set; }
-
-	public Treap(Comparison<T> comparison = null)
-	{
-		compare = comparison ?? Comparer<T>.Default.Compare;
-	}
-
-	static Node SearchMinNode(Node node)
-	{
-		if (node == null) return null;
-		return SearchMinNode(node.Left) ?? node;
-	}
-
-	static Node SearchMaxNode(Node node)
-	{
-		if (node == null) return null;
-		return SearchMaxNode(node.Right) ?? node;
-	}
-
-	static Node SearchMinNode(Node node, Func<T, bool> f)
-	{
-		if (node == null) return null;
-		if (f(node.Value)) return SearchMinNode(node.Left, f) ?? node;
-		else return SearchMinNode(node.Right, f);
-	}
-
-	static Node SearchMaxNode(Node node, Func<T, bool> f)
-	{
-		if (node == null) return null;
-		if (f(node.Value)) return SearchMaxNode(node.Right, f) ?? node;
-		else return SearchMaxNode(node.Left, f);
-	}
-
-	static Node SearchPreviousAncestor(Node node)
-	{
-		if (node == null) return null;
-		if (node.Parent == null) return null;
-		if (node.Parent.Right == node) return node.Parent;
-		else return SearchPreviousAncestor(node.Parent);
-	}
-
-	static Node SearchNextAncestor(Node node)
-	{
-		if (node == null) return null;
-		if (node.Parent == null) return null;
-		if (node.Parent.Left == node) return node.Parent;
-		else return SearchNextAncestor(node.Parent);
-	}
-
-	static Node SearchPreviousNode(Node node)
-	{
-		if (node == null) return null;
-		return SearchMaxNode(node.Left) ?? SearchPreviousAncestor(node);
-	}
-
-	static Node SearchNextNode(Node node)
-	{
-		if (node == null) return null;
-		return SearchMinNode(node.Right) ?? SearchNextAncestor(node);
-	}
-
-	Node SearchNode(Node node, T value)
-	{
-		if (node == null) return null;
-		var d = compare(value, node.Value);
-		if (d == 0) return node;
-		if (d < 0) return SearchNode(node.Left, value);
-		else return SearchNode(node.Right, value);
-	}
-
-	public T GetMin()
-	{
-		if (Root == null) throw new InvalidOperationException("The tree is empty.");
-		return SearchMinNode(Root).Value;
-	}
-
-	public T GetMax()
-	{
-		if (Root == null) throw new InvalidOperationException("The tree is empty.");
-		return SearchMaxNode(Root).Value;
-	}
-
-	public T GetNextValue(T value, T defaultValue = default(T))
-	{
-		var node = SearchNode(Root, value);
-		if (node == null) throw new InvalidOperationException("The value does not exist.");
-		node = SearchNextNode(node);
-		if (node == null) return defaultValue;
-		return node.Value;
-	}
-
-	public T GetPreviousValue(T value, T defaultValue = default(T))
-	{
-		var node = SearchNode(Root, value);
-		if (node == null) throw new InvalidOperationException("The value does not exist.");
-		node = SearchPreviousNode(node);
-		if (node == null) return defaultValue;
-		return node.Value;
-	}
-
-	public IEnumerable<T> GetValues()
-	{
-		for (var n = SearchMinNode(Root); n != null; n = SearchNextNode(n))
-		{
-			yield return n.Value;
-		}
-	}
-
-	public IEnumerable<T> GetValues(Func<T, bool> predicateForMin, Func<T, bool> predicateForMax)
-	{
-		for (var n = SearchMinNode(Root, predicateForMin); n != null && predicateForMax(n.Value); n = SearchNextNode(n))
-		{
-			yield return n.Value;
-		}
-	}
-
-	public bool Contains(T value)
-	{
-		return SearchNode(Root, value) != null;
-	}
-
-	public bool Add(T value)
-	{
-		Node node;
-		if (Root == null)
-		{
-			node = Root = new Node { Value = value, Priority = CreatePriority() };
-		}
-		else
-		{
-			node = Add(Root, value);
-		}
-
-		if (node != null)
-		{
-			Rotate(node);
-			++Count;
-		}
-		return node != null;
-	}
-
-	// Suppose t != null.
-	Node Add(Node t, T value)
-	{
-		var d = compare(value, t.Value);
-		if (d == 0) return null;
-		if (d < 0)
-		{
-			if (t.Left != null) return Add(t.Left, value);
-			return t.Left = new Node { Value = value, Priority = CreatePriority() };
-		}
-		else
-		{
-			if (t.Right != null) return Add(t.Right, value);
-			return t.Right = new Node { Value = value, Priority = CreatePriority() };
-		}
-	}
-
-	// Suppose t != null.
-	void Rotate(Node t)
-	{
-		if (t.Parent == null) return;
-		if (t.Parent.Priority > t.Priority) return;
-
-		var p = t.Parent;
-		var pp = p.Parent;
-
-		if (p.Left == t)
-		{
-			// to right
-			p.Left = t.Right;
-			t.Right = p;
-		}
-		else
-		{
-			// to left
-			p.Right = t.Left;
-			t.Left = p;
-		}
-
-		if (pp == null)
-		{
-			Root = t;
-		}
-		else if (pp.Left == p)
-		{
-			pp.Left = t;
-		}
-		else
-		{
-			pp.Right = t;
-		}
-
-		Rotate(t);
-	}
-
-	public bool Remove(T value)
-	{
-		var node = SearchNode(Root, value);
-		if (node == null) return false;
-
-		Remove(node);
-		--Count;
-		return true;
-	}
-
-	// Suppose t != null.
-	void Remove(Node t)
-	{
-		if (t.Left == null || t.Right == null)
-		{
-			var c = t.Left ?? t.Right;
-
-			if (t.Parent == null)
-			{
-				Root = c;
-			}
-			else if (t.Parent.Left == t)
-			{
-				t.Parent.Left = c;
-			}
-			else
-			{
-				t.Parent.Right = c;
-			}
-			RemovePriority(t.Priority);
-		}
-		else
-		{
-			var t2 = SearchNextNode(t);
-			t.Value = t2.Value;
-			Remove(t2);
+			get => throw new NotImplementedException();
+			set => throw new NotImplementedException();
 		}
 	}
 }
