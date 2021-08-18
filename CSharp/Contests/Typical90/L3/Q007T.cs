eff82c5e966a68861186553234a9694de4035c7d
Keiho Sakapon <git@saka-pon.net>
2021-08-18 20:01:32 +0900

    Update code

--- CSharp/Contests/Typical90/L3/Q007T.cs
+++ CSharp/Contests/Typical90/L3/Q007T.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CoderLib6.DataTrees;
+using CoderLib6.DataTrees.Bsts;
 
 class Q007T
 {
@@ -14,7 +14,7 @@ class Q007T
 		var qc = int.Parse(Console.ReadLine());
 		var qs = Array.ConvertAll(new bool[qc], _ => int.Parse(Console.ReadLine()));
 
-		var set = new AvlSet<int>();
+		var set = new IndexedSet<int>();
 		foreach (var v in a) set.Add(v);
 		set.Add(-1 << 30);
 		set.Add(int.MaxValue);
@@ -23,9 +23,31 @@ class Q007T
 
 		int GetMin(int bv)
 		{
-			var av2 = set.GetMin(x => x >= bv);
-			var av1 = set.GetPrevious(av2);
-			return Math.Min(av2 - bv, bv - av1);
+			var an2 = set.Root.SearchFirstNode(x => x >= bv);
+			var an1 = an2.SearchPreviousNode();
+			return Math.Min(an2.Item - bv, bv - an1.Item);
 		}
+	}
+}
+
+namespace CoderLib6.DataTrees.Bsts
+{
+	public class IndexedSet<T>
+	{
+		public class Node
+		{
+			public T Item { get; set; }
+			public Node SearchFirstNode(Func<T, bool> predicate) => throw new NotImplementedException();
+			public Node SearchLastNode(Func<T, bool> predicate) => throw new NotImplementedException();
+			public Node SearchPreviousNode() => throw new NotImplementedException();
+			public Node SearchNextNode() => throw new NotImplementedException();
+		}
+
+		public Node Root { get; }
+		public T GetFirst() => throw new NotImplementedException();
+		public T GetLast() => throw new NotImplementedException();
+		public bool Contains(T item) => throw new NotImplementedException();
+		public bool Add(T item) => throw new NotImplementedException();
+		public bool Remove(T item) => throw new NotImplementedException();
 	}
 }
