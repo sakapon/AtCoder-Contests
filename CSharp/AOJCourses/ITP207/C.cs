0c73ccb3131cedfe0e51bfeb4ddaca2ad6cb49e2
Keiho Sakapon <git@saka-pon.net>
2021-08-18 17:37:40 +0900

    Update code

--- CSharp/AOJCourses/ITP207/C.cs
+++ CSharp/AOJCourses/ITP207/C.cs
@@ -7,24 +7,25 @@ class C
 	{
 		var n = int.Parse(Console.ReadLine());
 
-		var r = new List<int>();
 		var set = new SortedSet<int>();
 
-		for (int i = 0; i < n; i++)
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+		while (n-- > 0)
 		{
 			var q = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 			if (q[0] == 0)
 			{
 				set.Add(q[1]);
-				r.Add(set.Count);
+				Console.WriteLine(set.Count);
 			}
 			else if (q[0] == 1)
-				r.Add(set.Contains(q[1]) ? 1 : 0);
+				Console.WriteLine(set.Contains(q[1]) ? 1 : 0);
 			else if (q[0] == 2)
 				set.Remove(q[1]);
 			else
-				r.AddRange(set.GetViewBetween(q[1], q[2]));
+				foreach (var x in set.GetViewBetween(q[1], q[2]))
+					Console.WriteLine(x);
 		}
-		Console.WriteLine(string.Join("\n", r));
+		Console.Out.Flush();
 	}
 }
