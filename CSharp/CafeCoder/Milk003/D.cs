7cfa31e328131d73dd2727badedfee308043a1a2
Keiho Sakapon <git@saka-pon.net>
2021-09-20 00:34:13 +0900

    AC

--- CSharp/CafeCoder/Milk003/D.cs
+++ CSharp/CafeCoder/Milk003/D.cs
@@ -1,21 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var es = Array.ConvertAll(new bool[n], _ => Console.ReadLine().Split());
 
-		return string.Join(" ", a);
+		var names = es.SelectMany(e => e).Distinct().ToArray();
+		var map = Enumerable.Range(0, names.Length).ToDictionary(i => names[i]);
+
+		var uf = new UF(names.Length);
+
+		foreach (var e in es)
+		{
+			uf.Unite(map[e[0]], map[e[1]]);
+		}
+
+		Console.WriteLine(uf.GroupsCount);
+		Console.WriteLine(string.Join(" ", uf.ToGroups().Select(g => g.Length).OrderBy(x => x)));
 	}
+}
+
+class UF
+{
+	int[] p, sizes;
+	public int GroupsCount;
+	public UF(int n)
+	{
+		p = Enumerable.Range(0, n).ToArray();
+		sizes = Array.ConvertAll(p, _ => 1);
+		GroupsCount = n;
+	}
+
+	public int GetRoot(int x) => p[x] == x ? x : p[x] = GetRoot(p[x]);
+	public int GetSize(int x) => sizes[GetRoot(x)];
+
+	public bool AreUnited(int x, int y) => GetRoot(x) == GetRoot(y);
+	public bool Unite(int x, int y)
+	{
+		if ((x = GetRoot(x)) == (y = GetRoot(y))) return false;
+
+		// 要素数が大きいほうのグループにマージします。
+		if (sizes[x] < sizes[y]) Merge(y, x);
+		else Merge(x, y);
+		return true;
+	}
+	protected virtual void Merge(int x, int y)
+	{
+		p[y] = x;
+		sizes[x] += sizes[y];
+		--GroupsCount;
+	}
+	public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
 }
--- CSharp/CafeCoder/Milk003/E.cs
+++ CSharp/CafeCoder/Milk003/E.cs
@@ -1,21 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void WriteYesNo(bool b) => Console.WriteLine(b ? "Yes" : "No");
+	static void Main()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m, c) = Read3();
+		var s = Console.ReadLine().Replace(" ", "");
+		var l0 = Convert.ToInt32(string.Join("", s.Reverse()), 2);
+		var b = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var rc = Enumerable.Range(0, c).ToArray();
+
+		var off = false;
+		var on = false;
+		var allOn = (1 << m) - 1;
+
+		Power(rc, n, p =>
+		{
+			var l = l0;
+
+			for (int j = 0; j < n; j++)
+			{
+				var i = b[j][p[j]] - 1;
+				l ^= 1 << i;
+			}
+
+			if (l == 0) off = true;
+			if (l == allOn) on = true;
+		});
+
+		WriteYesNo(off);
+		WriteYesNo(on);
+	}
+
+	public static void Power<T>(T[] values, int r, Action<T[]> action)
+	{
+		var n = values.Length;
+		var p = new T[r];
+
+		if (r > 0) Dfs(0);
+		else action(p);
+
+		void Dfs(int i)
+		{
+			var i2 = i + 1;
+			for (int j = 0; j < n; ++j)
+			{
+				p[i] = values[j];
+
+				if (i2 < r) Dfs(i2);
+				else action(p);
+			}
+		}
 	}
 }
