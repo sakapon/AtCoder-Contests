e2b9e15f8cf4566396f4aff43ed839697b00faa1
Keiho Sakapon <git@saka-pon.net>
2021-09-23 14:37:38 +0900

    Update library

--- CSharp/Contests/ATC001/C.cs
+++ CSharp/Contests/ATC001/C.cs
@@ -66,6 +66,26 @@ public class FFT
 		roots = NthRoots(n);
 	}
 
+	// c の長さは 2 の冪とします。
+	// h: 更新対象の長さの半分
+	void TransformRecursive(Complex[] c, int l, int h)
+	{
+		if (h == 0) return;
+		var d = (n >> 1) / h;
+
+		TransformRecursive(c, l, h >> 1);
+		TransformRecursive(c, l + h, h >> 1);
+
+		for (int k = 0; k < h; ++k)
+		{
+			var v0 = c[l + k];
+			var v1 = c[l + k + h] * roots[d * k];
+			c[l + k] = v0 + v1;
+			c[l + k + h] = v0 - v1;
+		}
+	}
+
+	// f が整数でも f^ は整数になるとは限りません。
 	public Complex[] Transform(Complex[] c, bool inverse)
 	{
 		if (c == null) throw new ArgumentNullException(nameof(c));
@@ -74,19 +94,7 @@ public class FFT
 		for (int k = 0; k < c.Length; ++k)
 			t[br[k]] = c[k];
 
-		for (int p = 1, d = n >> 1; p < n; p <<= 1, d >>= 1)
-		{
-			for (int l = 0; l < n; l += p << 1)
-			{
-				for (int k = 0; k < p; ++k)
-				{
-					var v0 = t[l + k];
-					var v1 = t[l + k + p] * roots[d * k];
-					t[l + k] = v0 + v1;
-					t[l + k + p] = v0 - v1;
-				}
-			}
-		}
+		TransformRecursive(t, 0, n >> 1);
 
 		if (inverse && n > 1)
 		{
@@ -96,6 +104,7 @@ public class FFT
 		return t;
 	}
 
+	// 戻り値の長さは |a| + |b| - 1 となります。
 	public static Complex[] Convolution(Complex[] a, Complex[] b)
 	{
 		if (a == null) throw new ArgumentNullException(nameof(a));
@@ -111,8 +120,8 @@ public class FFT
 		{
 			fa[k] *= fb[k];
 		}
-
 		var c = fft.Transform(fa, true);
+
 		if (n < c.Length) Array.Resize(ref c, n);
 		return c;
 	}
