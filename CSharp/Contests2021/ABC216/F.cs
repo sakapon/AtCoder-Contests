ed9faa47ed1d201a3f9de4a58d0d046beaee95a7
Keiho Sakapon <git@saka-pon.net>
2021-09-25 10:05:35 +0900

    AC

--- CSharp/Contests2021/ABC216/F.cs
+++ CSharp/Contests2021/ABC216/F.cs
@@ -1,21 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class F
 {
+	const long M = 998244353;
+	const int max = 5000;
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var b = Read();
 
-		return string.Join(" ", a);
+		(int a, int b)[] ab = a.Zip(b).OrderBy(_ => -_.First).ToArray();
+
+		var dp = new long[max + 1];
+
+		for (int i = 0; i < n; i++)
+		{
+			var (s, t) = ab[i];
+
+			for (int j = 0; j <= max; j++)
+			{
+				var nj = j - t;
+				if (nj >= 0)
+				{
+					dp[nj] += dp[j];
+					dp[nj] %= M;
+				}
+			}
+
+			var ns = s - t;
+			if (ns >= 0)
+			{
+				dp[ns]++;
+				dp[ns] %= M;
+			}
+		}
+
+		return dp.Sum() % M;
 	}
 }
