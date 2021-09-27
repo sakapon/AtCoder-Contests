2b4fe738ce7d2bc9b416bca64a7487ecc6fdb6c0
Keiho Sakapon <git@saka-pon.net>
2021-09-27 14:38:47 +0900

    Other AC

---
+++ CSharp/Contests2021/ABC220/C2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class C2
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = ReadL();
+		var x = long.Parse(Console.ReadLine());
+
+		var q = Math.DivRem(x, a.Sum(), out x);
+		return n * q + Enumerable.Range(0, n).First(i => (x -= a[i]) < 0) + 1;
+	}
+}
