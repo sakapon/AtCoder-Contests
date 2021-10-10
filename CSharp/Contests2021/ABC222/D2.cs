e55a9ee72e7fd041d8a35b112d761c8f53090b86
Keiho Sakapon <git@saka-pon.net>
2021-10-11 01:06:34 +0900

    Other AC

---
+++ CSharp/Contests2021/ABC222/D2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class D2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+		var b = Read();
+
+		var dp = NewArray2<long>(n + 1, 3000 + 1);
+		dp[0][0] = 1;
+
+		for (int i = 0; i < n; i++)
+		{
+			var av = a[i];
+			var bv = b[i];
+
+			for (int j = 0; j < bv; j++)
+			{
+				dp[i][j + 1] += dp[i][j];
+			}
+
+			for (int j = av; j <= bv; j++)
+			{
+				dp[i + 1][j] = dp[i][j] % M;
+			}
+		}
+
+		return dp[n].Sum() % M;
+	}
+
+	const long M = 998244353;
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+}
---
+++ CSharp/Contests2021/ABC222/D3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+class D3
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+		var b = Read();
+
+		var dp = new long[max + 1];
+		dp[0] = 1;
+
+		for (int i = 0; i < n; i++)
+		{
+			var av = a[i];
+			var bv = b[i];
+
+			for (int j = 0; j < bv; j++)
+			{
+				dp[j + 1] += dp[j];
+				dp[j + 1] %= M;
+
+				if (j < av) dp[j] = 0;
+			}
+		}
+
+		return dp.Sum() % M;
+	}
+
+	const int max = 3000;
+	const long M = 998244353;
+}
