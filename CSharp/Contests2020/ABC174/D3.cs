62de7faf0630ab54e654b497c615bb7fa8b21b12
Keiho Sakapon <git@saka-pon.net>
2021-10-18 12:23:41 +0900

    Other AC

---
+++ CSharp/Contests2020/ABC174/D3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class D3
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine();
+
+		var rn = Enumerable.Range(0, n).ToArray();
+		var wis = Array.FindAll(rn, i => s[i] == 'W');
+		Array.Reverse(rn);
+		var ris = Array.FindAll(rn, i => s[i] == 'R');
+		return wis.Zip(ris).Count(p => p.First < p.Second);
+	}
+}
