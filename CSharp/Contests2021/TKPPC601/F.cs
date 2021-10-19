18a9b0f77a5e08f62af53f279af6dbd40b72d848
Keiho Sakapon <git@saka-pon.net>
2021-10-19 10:16:27 +0900

    AC

--- CSharp/Contests2021/TKPPC601/F.cs
+++ CSharp/Contests2021/TKPPC601/F.cs
@@ -1,21 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, k) = Read2();
 
-		return string.Join(" ", a);
+		var p2k = MPow(2, k);
+
+		var dp = new long[n + 1, 2];
+		dp[1, 0] = 1;
+
+		for (int i = 2; i <= n; i++)
+		{
+			dp[i, 0] = dp[i - 1, 0] + dp[i - 1, 1];
+			dp[i, 0] %= M;
+
+			dp[i, 1] = dp[i, 0];
+			dp[i, 1] *= 2;
+			if (i - k > 0)
+				dp[i, 1] -= dp[i - k, 0] * p2k;
+			dp[i, 1] = MInt(dp[i, 1]);
+		}
+
+		return dp[n, 0];
 	}
+
+	const long M = 998244353;
+	static long MPow(long b, long i)
+	{
+		long r = 1;
+		for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+		return r;
+	}
+	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
 }
