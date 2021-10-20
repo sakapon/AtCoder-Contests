0fc450c90d692c8b3f32ee914ebb653127c7f242
Keiho Sakapon <git@saka-pon.net>
2021-10-20 16:02:30 +0900

    Update code

--- CSharp/Contests2020/ABCC0725/E.cs
+++ CSharp/Contests2020/ABCC0725/E.cs
@@ -12,49 +12,48 @@ class E
 
 		// distances
 		// i: town, j: combination
-		var nsMin = NewArray2(n, 1 << n, 1L << 30);
-		var ewMin = NewArray2(n, 1 << n, 1L << 30);
+		var nsMin = new long[n][];
+		var ewMin = new long[n][];
 
 		for (int i = 0; i < n; i++)
 		{
-			nsMin[i][0] = Math.Abs(ps[i].x);
-			ewMin[i][0] = Math.Abs(ps[i].y);
+			nsMin[i] = NewArray1(1 << n, Math.Abs(ps[i].x));
+			ewMin[i] = NewArray1(1 << n, Math.Abs(ps[i].y));
 		}
 
-		for (int k = 0; k < n; k++)
+		for (int i = 0; i < n; i++)
 		{
-			var nsk = nsMin[k];
-			var ewk = ewMin[k];
-			var (xk, yk, _) = ps[k];
+			var nsi = nsMin[i];
+			var ewi = ewMin[i];
 
-			for (int x = 0; x < 1 << n; x++)
+			for (int j = 0; j < n; j++)
 			{
-				for (int i = 0; i < n; i++)
+				var f = 1 << j;
+				var ns_ij = Math.Abs(ps[i].x - ps[j].x);
+				var ew_ij = Math.Abs(ps[i].y - ps[j].y);
+
+				for (int x = 0; x < 1 << n; x++)
 				{
-					var f = 1 << i;
-					if ((x & f) == 0)
-					{
-						var n_ns = Math.Min(nsk[x], Math.Abs(ps[i].x - xk));
-						var n_ew = Math.Min(ewk[x], Math.Abs(ps[i].y - yk));
-						nsk[x | f] = Math.Min(nsk[x | f], n_ns);
-						ewk[x | f] = Math.Min(ewk[x | f], n_ew);
-					}
+					if ((x & f) == 0) x |= f;
+
+					nsi[x] = Math.Min(nsi[x], ns_ij);
+					ewi[x] = Math.Min(ewi[x], ew_ij);
 				}
 			}
 		}
 
 		var r = NewArray1(n + 1, 1L << 60);
-		var k3 = 0;
+		var k = 0;
 		var x3 = new int[3];
 
 		Power(new[] { 0, 1, 2 }, n, p =>
 		{
-			k3 = 0;
+			k = 0;
 			Array.Clear(x3, 0, 3);
 
 			for (int i = 0; i < n; i++)
 			{
-				if (p[i] != 0) k3++;
+				if (p[i] != 0) k++;
 				x3[p[i]] |= 1 << i;
 			}
 
@@ -63,14 +62,13 @@ class E
 			{
 				sum += Math.Min(nsMin[i][x3[1]], ewMin[i][x3[2]]) * ps[i].p;
 			}
-			r[k3] = Math.Min(r[k3], sum);
+			r[k] = Math.Min(r[k], sum);
 		});
 
 		return string.Join("\n", r);
 	}
 
 	static T[] NewArray1<T>(int n, T v = default) => Array.ConvertAll(new bool[n], _ => v);
-	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 
 	public static void Power<T>(T[] values, int r, Action<T[]> action)
 	{
