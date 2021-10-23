87167c439e182b6e5169422490975530c78c8262
Keiho Sakapon <git@saka-pon.net>
2021-10-23 19:14:11 +0900

    AC

--- CSharp/Contests2021/ABC210/E.cs
+++ CSharp/Contests2021/ABC210/E.cs
@@ -7,15 +7,31 @@ class E
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long a, long c) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = ((long, int))Read2();
+		var ps = Array.ConvertAll(new bool[m], _ => Read2L());
 
-		return string.Join(" ", a);
+		ps = ps.OrderBy(p => p.c).ThenBy(p => p.a).ToArray();
+
+		var r = 0L;
+		var p = 1L;
+
+		foreach (var (a, c) in ps)
+		{
+			var d0 = Gcd(p, a);
+			var d = Gcd(n, a / d0);
+
+			r += (n / d - 1) * d * p * c;
+			n = d;
+			p *= n / d;
+
+			if (n == 1) return r;
+		}
+		return -1;
 	}
+
+	static long Gcd(long a, long b) { for (long r; (r = a % b) > 0; a = b, b = r) ; return b; }
 }
