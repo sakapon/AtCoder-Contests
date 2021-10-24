818190148a7f643901b68b6bc7d457536476c24e
Keiho Sakapon <git@saka-pon.net>
2021-10-24 11:05:37 +0900

    AC

--- CSharp/Contests2021/ABC224/B.cs
+++ CSharp/Contests2021/ABC224/B.cs
@@ -7,15 +7,32 @@ class B
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w) = Read2();
+		var a = Array.ConvertAll(new bool[h], _ => Read());
 
-		return string.Join(" ", a);
+		var l = new List<(int, int)>();
+
+		for (int i = 0; i < h; i++)
+		{
+			for (int j = 0; j < w; j++)
+			{
+				l.Add((i, j));
+			}
+		}
+
+		for (int s = 0; s < l.Count; s++)
+		{
+			var (i1, j1) = l[s];
+			for (int t = s + 1; t < l.Count; t++)
+			{
+				var (i2, j2) = l[t];
+				if (i1 >= i2 || j1 >= j2) continue;
+				if (a[i1][j1] + a[i2][j2] > a[i2][j1] + a[i1][j2]) return false;
+			}
+		}
+		return true;
 	}
 }
--- CSharp/Contests2021/ABC224/C.cs
+++ CSharp/Contests2021/ABC224/C.cs
@@ -11,11 +11,51 @@ class C
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => IntV.Parse(Console.ReadLine()));
 
-		return string.Join(" ", a);
+		var r = 0;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				for (int k = j + 1; k < n; k++)
+				{
+					var v1 = ps[i] - ps[j];
+					var v2 = ps[i] - ps[k];
+					if (!IntV.IsParallel(v1, v2)) r++;
+				}
+			}
+		}
+		return r;
 	}
+}
+
+public struct IntV : IEquatable<IntV>
+{
+	public static IntV Zero = (0, 0);
+	public static IntV UnitX = (1, 0);
+	public static IntV UnitY = (0, 1);
+
+	public long X, Y;
+	public IntV(long x, long y) => (X, Y) = (x, y);
+	public void Deconstruct(out long x, out long y) => (x, y) = (X, Y);
+	public override string ToString() => $"{X} {Y}";
+	public static IntV Parse(string s) => Array.ConvertAll(s.Split(), long.Parse);
+
+	public static implicit operator IntV(long[] v) => (v[0], v[1]);
+	public static explicit operator long[](IntV v) => new[] { v.X, v.Y };
+	public static implicit operator IntV((long x, long y) v) => new IntV(v.x, v.y);
+	public static explicit operator (long, long)(IntV v) => (v.X, v.Y);
+
+	public bool Equals(IntV other) => X == other.X && Y == other.Y;
+	public static bool operator ==(IntV v1, IntV v2) => v1.Equals(v2);
+	public static bool operator !=(IntV v1, IntV v2) => !v1.Equals(v2);
+	public override bool Equals(object obj) => obj is IntV v && Equals(v);
+	public override int GetHashCode() => (X, Y).GetHashCode();
+
+	public static IntV operator -(IntV v) => (-v.X, -v.Y);
+	public static IntV operator +(IntV v1, IntV v2) => (v1.X + v2.X, v1.Y + v2.Y);
+	public static IntV operator -(IntV v1, IntV v2) => (v1.X - v2.X, v1.Y - v2.Y);
+
+	public static bool IsParallel(IntV v1, IntV v2) => v1.X * v2.Y == v2.X * v1.Y;
 }
