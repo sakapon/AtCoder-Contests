5f913c918500a9c6ca90fdafd8fdb097fe4230b5
Keiho Sakapon <git@saka-pon.net>
2021-10-24 23:14:19 +0900

    AC

--- CSharp/Contests2021/ABC224/E.cs
+++ CSharp/Contests2021/ABC224/E.cs
@@ -5,17 +5,50 @@ using System.Linq;
 class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int r, int c, int a) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w, n) = Read3();
+		var ps = Array.ConvertAll(new bool[n], _ => Read3());
 
-		return string.Join(" ", a);
+		const int max = 1 << 30;
+		var rn = Enumerable.Range(0, n).ToArray();
+
+		var dp = new int[n];
+		var rmap = Array.ConvertAll(new bool[h + 1], _ => new List<(int a, int v)> { (max, -1) });
+		var cmap = Array.ConvertAll(new bool[w + 1], _ => new List<(int a, int v)> { (max, -1) });
+
+		foreach (var id in rn.OrderBy(id => -ps[id].a))
+		{
+			var (r, c, a) = ps[id];
+
+			var rl = rmap[r];
+			var cl = cmap[c];
+
+			var rli = rl[^1].a == a ? 2 : 1;
+			var cli = cl[^1].a == a ? 2 : 1;
+			dp[id] = Math.Max(rl[^rli].v, cl[^cli].v) + 1;
+
+			if (rli == 2)
+			{
+				rl[^1] = (a, Math.Max(rl[^1].v, dp[id]));
+			}
+			else
+			{
+				rl.Add((a, dp[id]));
+			}
+
+			if (cli == 2)
+			{
+				cl[^1] = (a, Math.Max(cl[^1].v, dp[id]));
+			}
+			else
+			{
+				cl.Add((a, dp[id]));
+			}
+		}
+
+		return string.Join("\n", dp);
 	}
 }
---
+++ CSharp/Contests2021/ABC224/F2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class F2
+{
+	const long M = 998244353;
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var s = Console.ReadLine().Select(c => c - '0').ToArray();
+		var n = s.Length;
+
+		long x = s[0];
+		long y = 0;
+		var p2 = 1L;
+
+		for (int i = 1; i < n; i++)
+		{
+			p2 = p2 * 2 % M;
+			var t = (x * 10 + s[i] * p2) % M;
+			var u = (x + y * 2) % M;
+			(x, y) = (t, u);
+		}
+		return (x + y) % M;
+	}
+}
