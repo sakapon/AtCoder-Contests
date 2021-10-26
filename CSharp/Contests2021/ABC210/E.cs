c7729679bc29d8d6875d440c9c0b58b31ab219f4
Keiho Sakapon <git@saka-pon.net>
2021-10-26 21:00:04 +0900

    Improve code

--- CSharp/Contests2021/ABC210/E.cs
+++ CSharp/Contests2021/ABC210/E.cs
@@ -1,37 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static (long a, long c) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+	static (int a, int c) Read2() { var a = Read(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var (n, m) = ((long, int))Read2();
-		var ps = Array.ConvertAll(new bool[m], _ => Read2L());
-
-		ps = ps.OrderBy(p => p.c).ThenBy(p => p.a).ToArray();
+		var (n, m) = Read2();
+		var ps = Array.ConvertAll(new bool[m], _ => Read2());
 
 		var r = 0L;
-		var p = 1L;
 
-		foreach (var (a, c) in ps)
+		foreach (var (a, c) in ps.OrderBy(p => p.c))
 		{
-			var d0 = Gcd(p, a);
-			var d = Gcd(n, a / d0);
-
-			r += (n / d - 1) * d * p * c;
-			n = d;
-			p *= n / d;
+			var g = Gcd(n, a);
+			r += (n - g) * (long)c;
+			n = g;
 
 			if (n == 1) return r;
 		}
 		return -1;
 	}
 
-	static long Gcd(long a, long b) { for (long r; (r = a % b) > 0; a = b, b = r) ; return b; }
+	static int Gcd(int a, int b) { for (int r; (r = a % b) > 0; a = b, b = r) ; return b; }
 }
