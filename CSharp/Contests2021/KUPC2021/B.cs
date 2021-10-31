d5b7b91d84e35931f38fa469a5c2cb085e1e929b
Keiho Sakapon <git@saka-pon.net>
2021-10-31 18:26:54 +0900

    AC

--- CSharp/Contests2021/KUPC2021/B.cs
+++ CSharp/Contests2021/KUPC2021/B.cs
@@ -1,21 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var c = NewArray2(n, n, '.');
+
+		IntV p = (0, -1);
+		IntV v = (0, 1);
+		for (int k = n - 1; k > 0; k--)
+		{
+			for (int i = 0; i < k; i++)
+			{
+				p += v;
+				c[p.X][p.Y] = '#';
+			}
+			v = v.Rotate270();
+		}
+
+		foreach (var r in c)
+			Console.WriteLine(string.Join("", r));
 	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+}
+
+public struct IntV : IEquatable<IntV>
+{
+	public static IntV Zero = (0, 0);
+	public static IntV UnitX = (1, 0);
+	public static IntV UnitY = (0, 1);
+
+	public long X, Y;
+	public IntV(long x, long y) => (X, Y) = (x, y);
+	public void Deconstruct(out long x, out long y) => (x, y) = (X, Y);
+	public override string ToString() => $"{X} {Y}";
+	public static IntV Parse(string s) => Array.ConvertAll(s.Split(), long.Parse);
+
+	public static implicit operator IntV(long[] v) => (v[0], v[1]);
+	public static explicit operator long[](IntV v) => new[] { v.X, v.Y };
+	public static implicit operator IntV((long x, long y) v) => new IntV(v.x, v.y);
+	public static explicit operator (long, long)(IntV v) => (v.X, v.Y);
+
+	public bool Equals(IntV other) => X == other.X && Y == other.Y;
+	public static bool operator ==(IntV v1, IntV v2) => v1.Equals(v2);
+	public static bool operator !=(IntV v1, IntV v2) => !v1.Equals(v2);
+	public override bool Equals(object obj) => obj is IntV v && Equals(v);
+	public override int GetHashCode() => (X, Y).GetHashCode();
+
+	public static IntV operator -(IntV v) => (-v.X, -v.Y);
+	public static IntV operator +(IntV v1, IntV v2) => (v1.X + v2.X, v1.Y + v2.Y);
+	public static IntV operator -(IntV v1, IntV v2) => (v1.X - v2.X, v1.Y - v2.Y);
+
+	public long NormL1 => Math.Abs(X) + Math.Abs(Y);
+	public double Norm => Math.Sqrt(X * X + Y * Y);
+	public double Angle => Math.Atan2(Y, X);
+	public double Cos => X / Norm;
+	public double Sin => Y / Norm;
+	public double Tan => Y / (double)X;
+
+	public IntV Rotate90() => new IntV(-Y, X);
+	public IntV Rotate180() => new IntV(-X, -Y);
+	public IntV Rotate270() => new IntV(Y, -X);
+
+	public static long Dot(IntV v1, IntV v2) => v1.X * v2.X + v1.Y * v2.Y;
+	// 菱形の面積
+	public static long Area(IntV v1, IntV v2) => Math.Abs(v1.X * v2.Y - v2.X * v1.Y);
+	public static bool IsParallel(IntV v1, IntV v2) => v1.X * v2.Y == v2.X * v1.Y;
+	public static bool IsOrthogonal(IntV v1, IntV v2) => Dot(v1, v2) == 0;
 }
--- CSharp/Contests2021/KUPC2021/C.cs
+++ CSharp/Contests2021/KUPC2021/C.cs
@@ -5,17 +5,57 @@ using System.Linq;
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var b = Read();
 
-		return string.Join(" ", a);
+		var max = Math.Max(a.Max(), b.Max());
+
+		var l = new List<(int gacha, int coin)>();
+
+		for (int i = 0; i < n; i++)
+		{
+			var g = a[i];
+			var c = b[i];
+
+			if (c <= g) continue;
+
+			if (l.Count == 0)
+			{
+				l.Add((g, c));
+			}
+			else
+			{
+				var (g0, c0) = l.Last();
+				if (c0 < g)
+				{
+					l.Add((g, c));
+				}
+				else
+				{
+					l[l.Count - 1] = (g0, c);
+				}
+			}
+		}
+
+		if (l.Count == 0) return max;
+
+		long r = 2 * max - l[0].gacha;
+		var t = r;
+
+		for (int i = 1; i < l.Count; i++)
+		{
+			var d1 = l[i - 1].coin - l[i - 1].gacha;
+			var d2 = l[i].gacha - l[i - 1].coin;
+
+			t += d1;
+			t -= d2;
+			r = Math.Min(r, t);
+		}
+
+		return r;
 	}
 }
