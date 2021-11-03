cc03e07701a1e91ccbd54bb1bc5999e1dfd92acf
Keiho Sakapon <git@saka-pon.net>
2021-11-03 16:35:16 +0900

    AC

--- CSharp/Contests/TypicalAlgo/E.cs
+++ CSharp/Contests/TypicalAlgo/E.cs
@@ -1,21 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read());
 
-		return string.Join(" ", a);
+		var (d, _) = WarshallFloyd(n, es, true);
+		return d.Sum(r => r.Sum());
+	}
+
+	public static Tuple<long[][], int[][]> WarshallFloyd(int n, int[][] es, bool directed)
+	{
+		var cs = Array.ConvertAll(new bool[n], i => Array.ConvertAll(new bool[n], _ => long.MaxValue));
+		var inters = Array.ConvertAll(new bool[n], i => Array.ConvertAll(new bool[n], _ => -1));
+		for (int i = 0; i < n; ++i) cs[i][i] = 0;
+
+		foreach (var e in es)
+		{
+			cs[e[0]][e[1]] = Math.Min(cs[e[0]][e[1]], e[2]);
+			if (!directed) cs[e[1]][e[0]] = Math.Min(cs[e[1]][e[0]], e[2]);
+		}
+
+		for (int k = 0; k < n; ++k)
+			for (int i = 0; i < n; ++i)
+				for (int j = 0; j < n; ++j)
+				{
+					if (cs[i][k] == long.MaxValue || cs[k][j] == long.MaxValue) continue;
+					var nc = cs[i][k] + cs[k][j];
+					if (cs[i][j] <= nc) continue;
+					cs[i][j] = nc;
+					inters[i][j] = k;
+				}
+		for (int i = 0; i < n; ++i) if (cs[i][i] < 0) return Tuple.Create<long[][], int[][]>(null, null);
+		return Tuple.Create(cs, inters);
 	}
 }
--- CSharp/Contests/TypicalAlgo/F.cs
+++ CSharp/Contests/TypicalAlgo/F.cs
@@ -6,16 +6,62 @@ class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read());
 
-		return string.Join(" ", a);
+		return Kruskal(n, es).Sum(e => (long)e[2]);
 	}
+
+	public static int[][] Kruskal(int n, int[][] ues)
+	{
+		var uf = new UF(n);
+		var mes = new List<int[]>();
+
+		foreach (var e in ues.OrderBy(e => e[2]))
+		{
+			if (uf.AreUnited(e[0], e[1])) continue;
+			uf.Unite(e[0], e[1]);
+			mes.Add(e);
+			// 実際の頂点数に注意。
+			// あまり実行速度に影響しないようです。
+			//if (mes.Count == n - 1) break;
+		}
+		return mes.ToArray();
+	}
+}
+
+class UF
+{
+	int[] p, sizes;
+	public int GroupsCount;
+	public UF(int n)
+	{
+		p = Enumerable.Range(0, n).ToArray();
+		sizes = Array.ConvertAll(p, _ => 1);
+		GroupsCount = n;
+	}
+
+	public int GetRoot(int x) => p[x] == x ? x : p[x] = GetRoot(p[x]);
+	public int GetSize(int x) => sizes[GetRoot(x)];
+
+	public bool AreUnited(int x, int y) => GetRoot(x) == GetRoot(y);
+	public bool Unite(int x, int y)
+	{
+		if ((x = GetRoot(x)) == (y = GetRoot(y))) return false;
+
+		// 要素数が大きいほうのグループにマージします。
+		if (sizes[x] < sizes[y]) Merge(y, x);
+		else Merge(x, y);
+		return true;
+	}
+	protected virtual void Merge(int x, int y)
+	{
+		p[y] = x;
+		sizes[x] += sizes[y];
+		--GroupsCount;
+	}
+	public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
 }
