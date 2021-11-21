5e6fcc46f18a6f4a2af1565be5de0e5ecb4ddbcd
Keiho Sakapon <git@saka-pon.net>
2021-11-21 18:35:15 +0900

    AC

--- CSharp/Contests2021/ABC228/A.cs
+++ CSharp/Contests2021/ABC228/A.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class A
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (s, t, x) = Read3();
 
-		return string.Join(" ", a);
+		if (s > t) t += 24;
+		if (s > x) x += 24;
+		return s <= x && x < t;
 	}
 }
--- CSharp/Contests2021/ABC228/E.cs
+++ CSharp/Contests2021/ABC228/E.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class E
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long, long) Read3L() { var a = ReadL(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, k, m) = Read3L();
 
-		return string.Join(" ", a);
+		k %= M - 1;
+		m %= M;
+		if (m == 0) return 0;
+
+		var kn = MPow(k, n, M - 1);
+		return MPow(m, kn, M);
+	}
+
+	const long M = 998244353;
+	static long MPow(long b, long i, long mod)
+	{
+		long r = 1;
+		for (; i != 0; b = b * b % mod, i >>= 1) if ((i & 1) != 0) r = r * b % mod;
+		return r;
 	}
 }
