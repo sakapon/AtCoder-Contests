67ebbe273b5ffb4620eb743b3ef984c4257eaf26
Keiho Sakapon <git@saka-pon.net>
2021-11-25 23:55:02 +0900

    Other AC

---
+++ CSharp/PAST/PAST202109/I3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class I3
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = ReadL();
+
+		// 3^30
+		const long max = 205891132094649;
+		var l = new List<long>();
+		var c = 0;
+
+		foreach (var v in a)
+		{
+			var x = v;
+			while ((x & 1) == 0)
+			{
+				x >>= 1;
+				c++;
+			}
+
+			while (x < max)
+			{
+				l.Add(x);
+				x *= 3;
+			}
+		}
+
+		a = l.ToArray();
+		Array.Sort(a);
+		return a[c];
+	}
+}
---
+++ CSharp/PAST/PAST202109/I4.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class I4
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = ReadL();
+
+		var c = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			while ((a[i] & 1) == 0)
+			{
+				a[i] >>= 1;
+				c++;
+			}
+		}
+
+		Array.Sort(a);
+
+		var a2 = (long[])a.Clone();
+		var c2 = c;
+
+		var max = a2[^1];
+		var l = new List<long>();
+
+		for (int i = 0; i < n; i++)
+		{
+			while (3 * a2[i] <= max)
+			{
+				l.Add(a2[i]);
+				a2[i] *= 3;
+				c2--;
+			}
+		}
+
+		if (c2 >= 0)
+		{
+			Array.Sort(a2);
+			var r = a2[c2 % n];
+			c2 /= n;
+			while (c2-- > 0) r *= 3;
+			return r;
+		}
+		else
+		{
+			a = l.ToArray();
+			Array.Sort(a);
+			return a[c];
+		}
+	}
+}
