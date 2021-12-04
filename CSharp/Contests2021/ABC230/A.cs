cd0c44f1817234b4eeff1a7745f70b293f7773db
Keiho Sakapon <git@saka-pon.net>
2021-12-04 23:35:53 +0900

    Other AC

--- CSharp/Contests2021/ABC230/A.cs
+++ CSharp/Contests2021/ABC230/A.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class A
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		return "AGC" + (n < 42 ? n : n + 1).ToString().PadLeft(3, '0');
+		return $"AGC{(n < 42 ? n : n + 1):D3}";
 	}
 }
