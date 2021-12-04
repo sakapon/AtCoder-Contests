ef89107766e67e2dfce7056800422e832e6d79c1
Keiho Sakapon <git@saka-pon.net>
2021-12-05 01:40:38 +0900

    AC

--- CSharp/Contests2021/ABC229/G.cs
+++ CSharp/Contests2021/ABC229/G.cs
@@ -1,21 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class G
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var n = s.Length;
+		var k = long.Parse(Console.ReadLine());
 
-		return string.Join(" ", a);
+		var indexes = Enumerable.Range(0, n).Where(i => s[i] == 'Y').ToArray();
+		var m = indexes.Length;
+		var cs = CumSumL(indexes);
+
+		return Last(0, m, x =>
+		{
+			var xl = x / 2;
+			var xr = x - xl;
+
+			for (int i = 0; i < m - x + 1; i++)
+			{
+				var c = 0L;
+				c += cs[i + x] - cs[i + xl];
+				c -= (indexes[i + xl] * 2 + xr - 1) * (long)xr / 2;
+				c += (indexes[i + xl] * 2 - xl - 1) * (long)xl / 2;
+				c -= cs[i + xl] - cs[i];
+
+				if (c <= k) return true;
+			}
+			return false;
+		});
+	}
+
+	public static long[] CumSumL(int[] a)
+	{
+		var s = new long[a.Length + 1];
+		for (int i = 0; i < a.Length; ++i) s[i + 1] = s[i] + a[i];
+		return s;
+	}
+
+	static int Last(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
 	}
 }
