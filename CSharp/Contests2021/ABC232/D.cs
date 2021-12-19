3ae560ef017808a7d332a2855396fd0f70e1f8b3
Keiho Sakapon <git@saka-pon.net>
2021-12-19 22:51:46 +0900

    AC

--- CSharp/Contests2021/ABC232/D.cs
+++ CSharp/Contests2021/ABC232/D.cs
@@ -1,21 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w) = Read2();
+		var s = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
 
-		return string.Join(" ", a);
+		var dp = NewArray2<int>(h, w);
+		dp[0][0] = 1;
+
+		for (int i = 0; i < h; i++)
+		{
+			for (int j = 0; j < w; j++)
+			{
+				if (s[i][j] == '#') continue;
+				if (dp[i][j] == 0) continue;
+
+				if (i < h - 1 && s[i + 1][j] == '.')
+					dp[i + 1][j] = Math.Max(dp[i + 1][j], dp[i][j] + 1);
+				if (j < w - 1 && s[i][j + 1] == '.')
+					dp[i][j + 1] = Math.Max(dp[i][j + 1], dp[i][j] + 1);
+			}
+		}
+
+		return dp.Max(r => r.Max());
 	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 }
