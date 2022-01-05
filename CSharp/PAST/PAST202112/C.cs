8827800155a268e5aeaf4a4ad56ad52aa993c99b
Keiho Sakapon <git@saka-pon.net>
2022-01-05 22:20:28 +0900

    Improve code

--- CSharp/PAST/PAST202112/C.cs
+++ CSharp/PAST/PAST202112/C.cs
@@ -1,26 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class C
 {
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
 		var ps = Array.ConvertAll(new bool[n], _ => Console.ReadLine().Split());
-
-		var r = new int[6];
-
-		for (int id = 1; id <= n; id++)
-		{
-			var p = ps[id - 1];
-			if (p[1] != "AC") continue;
-
-			var pid = p[0][0] - 'A';
-			if (r[pid] != 0) continue;
-
-			r[pid] = id;
-		}
-
-		return string.Join("\n", r);
+		return string.Join("\n", Enumerable.Range(1, n).Where(i => ps[i - 1][1] == "AC").GroupBy(i => ps[i - 1][0]).OrderBy(g => g.Key).Select(g => g.First()));
 	}
 }
---
+++ CSharp/PAST/PAST202112/D2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class D2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+		var b = Read();
+
+		var r = Enumerable.Range(1, n).Select(i => (-a[i - 1] - b[i - 1], -a[i - 1], i)).ToArray();
+		Array.Sort(r);
+		return string.Join(" ", r.Select(t => t.i));
+	}
+}
