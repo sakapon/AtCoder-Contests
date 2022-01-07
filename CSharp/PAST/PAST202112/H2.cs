e708f852a681af676ae2dd802c0c3f9cf8970706
Keiho Sakapon <git@saka-pon.net>
2022-01-07 16:17:36 +0900

    Use library

--- CSharp/PAST/PAST202112/H2.cs
+++ CSharp/PAST/PAST202112/H2.cs
@@ -5,27 +5,21 @@ class H2
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var s = Console.ReadLine();
-		var t = Console.ReadLine();
-		var n = s.Length;
-		var m = t.Length;
-
-		var dp = NewArray2<int>(n + 1, m + 1);
-
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= m; j++)
-			{
-				var nv = dp[i - 1][j - 1];
-				if (s[i - 1] != t[j - 1]) nv++;
-
-				dp[i][j] = Math.Max(dp[i - 1][j], dp[i][j - 1]);
-				dp[i][j] = Math.Max(dp[i][j], nv);
-			}
-		}
-
-		return dp[n][m];
+		var s = Console.ReadLine().ToCharArray();
+		var t = Console.ReadLine().ToCharArray();
+		return Lcs(s, t);
 	}
 
-	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+	public static int Lcs<T>(T[] a, T[] b)
+	{
+		var n = a.Length;
+		var m = b.Length;
+		var c = System.Collections.Generic.EqualityComparer<T>.Default;
+
+		var dp = new int[n + 1, m + 1];
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < m; ++j)
+				dp[i + 1, j + 1] = !c.Equals(a[i], b[j]) ? dp[i, j] + 1 : Math.Max(dp[i + 1, j], dp[i, j + 1]);
+		return dp[n, m];
+	}
 }
