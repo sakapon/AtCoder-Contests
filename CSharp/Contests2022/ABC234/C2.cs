cf5f79bb952d7dfb86c7a4d435407d91ee4940ac
Keiho Sakapon <git@saka-pon.net>
2022-01-12 00:43:02 +0900

    Other AC

---
+++ CSharp/Contests2022/ABC234/C2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class C2
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var k = long.Parse(Console.ReadLine());
+		return string.Join("", Enumerable.Range(0, 60).Select(i => (k & (1L << i)) == 0 ? 0 : 2).Reverse()).TrimStart('0');
+	}
+}
