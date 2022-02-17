a9402daa05de19a5e8682fa04ec2a4e7e8544eb7
Keiho Sakapon <git@saka-pon.net>
2022-02-17 23:23:25 +0900

    AC

--- CSharp/Contests2022/ABC236/F.cs
+++ CSharp/Contests2022/ABC236/F.cs
@@ -4,18 +4,46 @@ using System.Linq;
 
 class F
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var c = ReadL();
 
-		return string.Join(" ", a);
+		var q = new Queue<int>(Enumerable.Range(1, (1 << n) - 1).OrderBy(x => c[x - 1]));
+		var a = new List<int>();
+		var es = new List<int>();
+
+		while (es.Count < n)
+		{
+			var x0 = q.Dequeue();
+			var x = x0;
+
+			foreach (var e in es)
+			{
+				var f = GetFirstBit(e);
+				if ((x & f) != 0) x ^= e;
+			}
+
+			if (x == 0) continue;
+			var g = GetFirstBit(x);
+
+			for (int i = 0; i < es.Count; i++)
+			{
+				if ((es[i] & g) != 0) es[i] ^= x;
+			}
+
+			a.Add(x0);
+			es.Add(x);
+		}
+
+		return a.Sum(x => c[x - 1]);
+	}
+
+	static int GetFirstBit(int x)
+	{
+		for (int f = 1; ; f <<= 1)
+			if ((x & f) != 0) return f;
 	}
 }
