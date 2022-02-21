647495a87823243c24e00fd1a2e3da6e88369cd1
Keiho Sakapon <git@saka-pon.net>
2022-02-21 20:04:14 +0900

    WA

--- CSharp/Contests2022/ABC235/F.cs
+++ CSharp/Contests2022/ABC235/F.cs
@@ -12,60 +12,54 @@ class F
 		var m = int.Parse(Console.ReadLine());
 		var c = Read();
 
-		// 0: =
-		// 1: <
 		var pm = 1 << 10;
-		var dp = NewArray3(n + 1, pm, 2, min);
-		dp[0][0][0] = 0;
+		var dp = NewArray2(n + 1, pm, min);
 
+		var sx = 0;
+		var sv = 0L;
 		long v;
+
 		for (int i = 0; i < n; i++)
 		{
 			for (int x = 0; x < pm; x++)
 			{
-				if ((v = dp[i][x][0]) != min)
-				{
-					int k = 0;
-					for (; k < s[i]; k++)
-					{
-						var nx = (x, k) == (0, 0) ? 0 : (x | (1 << k));
-						var nv = v * 10 + k;
-						if (dp[i + 1][nx][1] == min) dp[i + 1][nx][1] = 0;
-						dp[i + 1][nx][1] += nv;
-					}
-
-					{
-						var nx = (x, k) == (0, 0) ? 0 : (x | (1 << k));
-						var nv = v * 10 + k;
-						if (dp[i + 1][nx][0] == min) dp[i + 1][nx][0] = 0;
-						dp[i + 1][nx][0] += nv;
-					}
-				}
-
-				if ((v = dp[i][x][1]) != min)
+				if ((v = dp[i][x]) != min)
 				{
 					for (int k = 0; k < 10; k++)
 					{
 						var nx = (x, k) == (0, 0) ? 0 : (x | (1 << k));
 						var nv = v * 10 + k;
-						if (dp[i + 1][nx][1] == min) dp[i + 1][nx][1] = 0;
-						dp[i + 1][nx][1] += nv;
+						if (dp[i + 1][nx] == min) dp[i + 1][nx] = 0;
+						dp[i + 1][nx] += nv;
 					}
 				}
 			}
 
+			for (int k = 0; k < s[i]; k++)
+			{
+				var nx = (sx, k) == (0, 0) ? 0 : (sx | (1 << k));
+				var nv = sv * 10 + k;
+				if (dp[i + 1][nx] == min) dp[i + 1][nx] = 0;
+				dp[i + 1][nx] += nv;
+			}
+
+			sx |= 1 << s[i];
+			sv = sv * 10 + s[i];
+			sv %= M;
+
 			for (int x = 0; x < pm; x++)
 			{
-				dp[i + 1][x][0] %= M;
-				dp[i + 1][x][1] %= M;
+				dp[i + 1][x] %= M;
 			}
 		}
 
 		var f = c.Select(x => 1 << x).Aggregate((x, y) => x | y);
-		return Enumerable.Range(0, pm).Where(x => (x & f) == f).Sum(x => dp[n][x].Where(v => v != min).Sum()) % M;
+		var r = Enumerable.Range(0, pm).Where(x => (x & f) == f).Where(x => dp[n][x] != min).Sum(x => dp[n][x]);
+		if ((sx & f) == f) r += sv;
+		return r % M;
 	}
 
 	const long min = -1;
 	const long M = 998244353;
-	static T[][][] NewArray3<T>(int n1, int n2, int n3, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => Array.ConvertAll(new bool[n3], ___ => v)));
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 }
