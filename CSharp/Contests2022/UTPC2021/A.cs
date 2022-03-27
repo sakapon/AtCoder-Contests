21be9713b313398a08652b61c02ff921f23a2cda
Keiho Sakapon <git@saka-pon.net>
2022-03-27 18:21:01 +0900

    AC

--- CSharp/Contests2022/UTPC2021/A.cs
+++ CSharp/Contests2022/UTPC2021/A.cs
@@ -1,21 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text.RegularExpressions;
 
 class A
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
+		Console.ReadLine();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var set = new HashSet<string>();
+		var l = new List<string>();
+		l.Add("UTPC");
+
+		for (int k = 0; k < 3; k++)
+		{
+			var ps = l.ToArray();
+			l.Clear();
+
+			foreach (var p in ps)
+			{
+				if (set.Contains(p)) continue;
+				set.Add(p);
+				if (Regex.IsMatch(s, p.Replace("*", @"\S"))) return k;
+
+				var cs = p.ToCharArray();
+				for (int i = 0; i < 4; i++)
+				{
+					if (cs[i] == '*') continue;
+					var c = cs[i];
+					cs[i] = '*';
+					l.Add(new string(cs));
+					cs[i] = c;
+				}
+
+				for (int i = 0; i < 4; i++)
+				{
+					for (int j = i + 1; j < 4; j++)
+					{
+						(cs[i], cs[j]) = (cs[j], cs[i]);
+						l.Add(new string(cs));
+						(cs[i], cs[j]) = (cs[j], cs[i]);
+					}
+				}
+			}
+		}
+		return 3;
 	}
 }
