d0ea77d4eecb42aa67bf49e77331941b3a7afd0c
Keiho Sakapon <git@saka-pon.net>
2022-05-21 23:54:39 +0900

    Other AC

---
+++ CSharp/Contests2022/ABC240/D2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class D2
+{
+	class Ball
+	{
+		public int v, c;
+	}
+
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var a = Read();
+
+		var r = new int[n];
+		var count = 0;
+
+		var q = new Stack<Ball>();
+		q.Push(new Ball());
+
+		for (int i = 0; i < n; i++)
+		{
+			var v = a[i];
+			var b = q.Peek();
+
+			if (v == b.v)
+			{
+				if (v == b.c + 1)
+				{
+					count -= v;
+					q.Pop();
+				}
+				else
+				{
+					b.c++;
+				}
+			}
+			else
+			{
+				q.Push(new Ball { v = v, c = 1 });
+			}
+
+			r[i] = ++count;
+		}
+
+		return string.Join("\n", r);
+	}
+}
