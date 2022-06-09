cb92cf4011adcef2acffc4d7d303e35a873fbf1f
Keiho Sakapon <git@saka-pon.net>
2022-06-09 11:09:50 +0900

    Add Deque class by ring buffer

---
+++ CSharp/CoderLib/CoderLib8/Collections/RingBuffer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CoderLib8.Collections
+{
+	public class RingBuffer<T>
+	{
+		int n;
+		T[] a;
+
+		public int Size => n;
+		public T this[int index]
+		{
+			get => a[index & (n - 1)];
+			set => a[index & (n - 1)] = value;
+		}
+
+		public RingBuffer(int size = 8)
+		{
+			a = new T[n = size];
+		}
+
+		public void Expand(int start)
+		{
+			start &= n - 1;
+			var b = new T[n <<= 1];
+			Array.Copy(a, start, b, start, a.Length - start);
+			Array.Copy(a, 0, b, a.Length, start);
+			a = b;
+		}
+	}
+}
