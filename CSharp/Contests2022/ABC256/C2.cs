dc8a35682b7469c314c4ab15d9005ff73a713aad
Keiho Sakapon <git@saka-pon.net>
2022-06-20 11:00:24 +0900

    Other AC

---
+++ CSharp/Contests2022/ABC256/C2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class C2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var a = Read();
+		var h = a[..3];
+		var w = a[3..];
+
+		var r = 0;
+		for (int v00 = 1; v00 <= 30; v00++)
+		{
+			for (int v01 = 1; v01 <= 30; v01++)
+			{
+				for (int v10 = 1; v10 <= 30; v10++)
+				{
+					for (int v11 = 1; v11 <= 30; v11++)
+					{
+						var v02 = h[0] - v00 - v01;
+						var v12 = h[1] - v10 - v11;
+						var v20 = w[0] - v00 - v10;
+						var v21 = w[1] - v01 - v11;
+
+						var v22h = h[2] - v20 - v21;
+						var v22w = w[2] - v02 - v12;
+						if (v02 > 0 && v12 > 0 && v20 > 0 && v21 > 0 && v22h > 0 && v22h == v22w) r++;
+					}
+				}
+			}
+		}
+		return r;
+	}
+}
---
+++ CSharp/Contests2022/ABC256/E2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class E2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var x = Read();
+		var c = Read();
+
+		var svs = new List<int>();
+		var uf = new UF(n + 1);
+
+		for (int v = 1; v <= n; v++)
+			if (!uf.Unite(v, x[v - 1]))
+				svs.Add(v);
+
+		var r = 0L;
+		foreach (var sv in svs)
+		{
+			var min = c[sv - 1];
+
+			for (int t = x[sv - 1]; t != sv; t = x[t - 1])
+				min = Math.Min(min, c[t - 1]);
+			r += min;
+		}
+
+		return r;
+	}
+}
+
+class UF
+{
+	int[] p, sizes;
+	public int GroupsCount;
+	public UF(int n)
+	{
+		p = Enumerable.Range(0, n).ToArray();
+		sizes = Array.ConvertAll(p, _ => 1);
+		GroupsCount = n;
+	}
+
+	public int GetRoot(int x) => p[x] == x ? x : p[x] = GetRoot(p[x]);
+	public int GetSize(int x) => sizes[GetRoot(x)];
+
+	public bool AreUnited(int x, int y) => GetRoot(x) == GetRoot(y);
+	public bool Unite(int x, int y)
+	{
+		if ((x = GetRoot(x)) == (y = GetRoot(y))) return false;
+
+		// 要素数が大きいほうのグループにマージします。
+		if (sizes[x] < sizes[y]) Merge(y, x);
+		else Merge(x, y);
+		return true;
+	}
+	protected virtual void Merge(int x, int y)
+	{
+		p[y] = x;
+		sizes[x] += sizes[y];
+		--GroupsCount;
+	}
+	public int[][] ToGroups() => Enumerable.Range(0, p.Length).GroupBy(GetRoot).Select(g => g.ToArray()).ToArray();
+}
