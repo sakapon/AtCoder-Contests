6d3577cb7a78d716c303020162b26e629c22da43
Keiho Sakapon <git@saka-pon.net>
2022-06-27 22:46:12 +0900

    Improve performance

--- CSharp/AOJCourses/ITP207/B.cs
+++ CSharp/AOJCourses/ITP207/B.cs
@@ -8,18 +8,21 @@ class B
 		var n = int.Parse(Console.ReadLine());
 
 		var set = new HashSet<string>();
+
+		Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
 		for (int i = 0; i < n; i++)
 		{
 			var q = Console.ReadLine().Split();
-			if (q[0] == "0")
+			if (q[0][0] == '0')
 			{
 				set.Add(q[1]);
 				Console.WriteLine(set.Count);
 			}
-			else if (q[0] == "1")
+			else if (q[0][0] == '1')
 				Console.WriteLine(set.Contains(q[1]) ? 1 : 0);
 			else
 				set.Remove(q[1]);
 		}
+		Console.Out.Flush();
 	}
 }
