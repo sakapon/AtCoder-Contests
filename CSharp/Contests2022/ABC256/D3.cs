6df4cdc49e12f8a84668add684913f862516cf12
Keiho Sakapon <git@saka-pon.net>
2022-06-27 10:16:34 +0900

    Other AC

---
+++ CSharp/Contests2022/ABC256/D3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class D3
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int l, int r) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
+
+		var segs = new List<(int l, int r)>();
+		var q = new Stack<int>();
+
+		foreach (var (v, close) in ps.SelectMany(p => new[] { (p.l, false), (p.r, true) }).OrderBy(p => p))
+		{
+			if (!close)
+			{
+				q.Push(v);
+			}
+			else
+			{
+				var l = q.Pop();
+				if (q.Count == 0) segs.Add((l, v));
+			}
+		}
+		return string.Join("\n", segs.Select(p => $"{p.l} {p.r}"));
+	}
+}
---
+++ CSharp/Contests2022/ABC257/C2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class C2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine();
+		var w = Read();
+
+		var ch = w.Where((x, i) => s[i] == '0').ToArray();
+		var ad = w.Where((x, i) => s[i] == '1').ToArray();
+		Array.Sort(ch);
+		Array.Sort(ad);
+
+		var bs_ch = new BSArray<int>(ch, true);
+		var bs_ad = new BSArray<int>(ad, true);
+
+		var r = ch.Length;
+		foreach (var x in w)
+		{
+			var f = bs_ch.GetFirstIndex(v => v >= x) + ad.Length - bs_ad.GetFirstIndex(v => v >= x);
+			r = Math.Max(r, f);
+		}
+		return r;
+	}
+}
+
+[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+public class BSArray<T>
+{
+	T[] a;
+	public T[] Items => a;
+	public int Count { get; }
+	public IComparer<T> Comparer { get; }
+	public T this[int i] => a[i];
+
+	public BSArray(IEnumerable<T> items, bool useRawItems = false, IComparer<T> comparer = null)
+	{
+		Comparer = comparer ?? Comparer<T>.Default;
+		if (items == null) throw new ArgumentNullException(nameof(items));
+		if (useRawItems) a = items as T[] ?? items.ToArray();
+		else a = items.OrderBy(x => x, Comparer).ToArray(); // stable sort
+		Count = a.Length;
+	}
+
+	public int GetFirstIndex(Func<T, bool> predicate) => First(0, Count, i => predicate(a[i]));
+	public int GetLastIndex(Func<T, bool> predicate) => Last(-1, Count - 1, i => predicate(a[i]));
+
+	public int GetCount(Func<T, bool> startPredicate, Func<T, bool> endPredicate)
+	{
+		var c = GetLastIndex(endPredicate) - GetFirstIndex(startPredicate) + 1;
+		return c >= 0 ? c : 0;
+	}
+
+	static int First(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
+	}
+	static int Last(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = r - (r - l - 1) / 2)) l = m; else r = m - 1;
+		return l;
+	}
+}
