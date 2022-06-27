ad6669b6efa0e30bda0f4788d047db937cc4ccd4
Keiho Sakapon <git@saka-pon.net>
2022-06-27 22:06:37 +0900

    Other AC

--- CSharp/CoderLib/CoderLib6/Collections/ChainHashSet.cs
+++ CSharp/CoderLib/CoderLib6/Collections/ChainHashSet.cs
@@ -5,6 +5,8 @@ namespace CoderLib6.Collections
 {
 	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B
 	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_C
+	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/6/ITP2_6_A
+	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/6/ITP2_6_B
 	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/7/ITP2_7_B
 	// Test: https://atcoder.jp/contests/typical90/tasks/typical90_aa
 	public class ChainHashSet<T> : IEnumerable<T>
