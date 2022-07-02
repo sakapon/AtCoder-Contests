aaf40d171ac8acf7270e3d4dbe7e72e86f405c7f
Keiho Sakapon <git@saka-pon.net>
2022-07-02 19:13:13 +0900

    Rename members

--- CSharp/CoderLib/CoderLib8/Collections/RAQ.cs
+++ CSharp/CoderLib/CoderLib8/Collections/RAQ.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 
+// Test: https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_E
 // RAQ に特化したクラスです。
 // 一般的なセグメント木のアルゴリズムとは異なります。
 namespace CoderLib8.Collections
 {
 	public class RAQ
 	{
-		int n2 = 1;
-		public int Count => n2;
-		long[] a2;
+		int n = 1;
+		public int Count => n;
+		long[] a;
 
-		public RAQ(int n, long[] a = null)
+		public RAQ(int count, long[] a0 = null)
 		{
-			while (n2 < n) n2 <<= 1;
-			a2 = new long[n2 << 1];
-			if (a != null) Array.Copy(a, 0, a2, n2, a.Length);
+			while (n < count) n <<= 1;
+			a = new long[n << 1];
+			if (a0 != null) Array.Copy(a0, 0, a, n, a0.Length);
 		}
 
 		public long this[int i]
@@ -22,18 +23,18 @@ namespace CoderLib8.Collections
 			get
 			{
 				var s = 0L;
-				for (i |= n2; i > 0; i >>= 1) s += a2[i];
+				for (i |= n; i > 0; i >>= 1) s += a[i];
 				return s;
 			}
-			set => a2[n2 | i] += value - this[i];
+			set => a[n | i] += value - this[i];
 		}
 
 		public void AddRange(int l, int r, long d)
 		{
-			for (l |= n2, r += n2; l < r; l >>= 1, r >>= 1)
+			for (l |= n, r += n; l < r; l >>= 1, r >>= 1)
 			{
-				if ((l & 1) != 0) a2[l++] += d;
-				if ((r & 1) != 0) a2[--r] += d;
+				if ((l & 1) != 0) a[l++] += d;
+				if ((r & 1) != 0) a[--r] += d;
 			}
 		}
 	}
--- CSharp/CoderLib/CoderLib8/Collections/RSQ.cs
+++ CSharp/CoderLib/CoderLib8/Collections/RSQ.cs
@@ -1,42 +1,44 @@
 ﻿using System;
 
+// Test: https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_B
+// Test: https://atcoder.jp/contests/practice2/tasks/practice2_b
 // RSQ に特化したクラスです。
 // 一般的なセグメント木のアルゴリズムとは異なります。
 namespace CoderLib8.Collections
 {
 	public class RSQ
 	{
-		int n2 = 1;
-		public int Count => n2;
-		long[] a2;
+		int n = 1;
+		public int Count => n;
+		long[] a;
 
-		public RSQ(int n, long[] a = null)
+		public RSQ(int count, long[] a0 = null)
 		{
-			while (n2 < n) n2 <<= 1;
-			a2 = new long[n2 << 1];
-			if (a != null)
+			while (n < count) n <<= 1;
+			a = new long[n << 1];
+			if (a0 != null)
 			{
-				Array.Copy(a, 0, a2, n2, a.Length);
-				for (int i = n2 - 1; i > 0; --i) a2[i] = a2[i << 1] + a2[(i << 1) | 1];
+				Array.Copy(a0, 0, a, n, a0.Length);
+				for (int i = n - 1; i > 0; --i) a[i] = a[i << 1] + a[(i << 1) | 1];
 			}
 		}
 
 		public long this[int i]
 		{
-			get => a2[n2 | i];
-			set => Add(i, value - a2[n2 | i]);
+			get => a[n | i];
+			set => Add(i, value - a[n | i]);
 		}
-		public void Add(int i, long d) { for (i |= n2; i > 0; i >>= 1) a2[i] += d; }
+		public void Add(int i, long d) { for (i |= n; i > 0; i >>= 1) a[i] += d; }
 
 		public long this[int l, int r]
 		{
 			get
 			{
 				var s = 0L;
-				for (l |= n2, r += n2; l < r; l >>= 1, r >>= 1)
+				for (l |= n, r += n; l < r; l >>= 1, r >>= 1)
 				{
-					if ((l & 1) != 0) s += a2[l++];
-					if ((r & 1) != 0) s += a2[--r];
+					if ((l & 1) != 0) s += a[l++];
+					if ((r & 1) != 0) s += a[--r];
 				}
 				return s;
 			}
