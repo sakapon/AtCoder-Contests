59756fe6671e94ac1574444f666a7891528cc8d8
Keiho Sakapon <git@saka-pon.net>
2022-07-03 23:46:01 +0900

    AC

--- CSharp/Contests2022/ABC258/B.cs
+++ CSharp/Contests2022/ABC258/B.cs
@@ -4,18 +4,38 @@ using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var a = Array.ConvertAll(new bool[n], _ => Console.ReadLine());
 
-		return string.Join(" ", a);
+		var rn = Enumerable.Range(0, n).ToArray();
+		var l = new List<string>();
+
+		l.AddRange(a);
+		l.AddRange(rn.Select(j => string.Join("", rn.Select(i => a[i][j]))));
+		l.AddRange(rn.Select(i => string.Join("", rn.Select(j => a[(i + j) % n][j]))));
+		l.AddRange(rn.Select(i => string.Join("", rn.Select(j => a[(i - j + n) % n][j]))));
+
+		var count = l.Count;
+		for (int i = 0; i < count; i++)
+		{
+			var s = l[i];
+			for (int j = 1; j < n; j++)
+			{
+				l.Add(s[j..] + s[..j]);
+			}
+		}
+
+		count = l.Count;
+		for (int i = 0; i < count; i++)
+		{
+			var s = l[i];
+			l.Add(string.Join("", s.Reverse()));
+		}
+
+		l.Sort();
+		return l[^1];
 	}
 }
--- CSharp/Contests2022/ABC258/D.cs
+++ CSharp/Contests2022/ABC258/D.cs
@@ -7,15 +7,39 @@ class D
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long a, long b) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, x) = Read2();
+		var ab = Array.ConvertAll(new bool[n], _ => Read2L());
 
-		return string.Join(" ", a);
+		var s = new StaticRSQ1(Array.ConvertAll(ab, p => p.a + p.b));
+		return Enumerable.Range(0, Math.Min(x, n)).Min(GetTime);
+
+		long GetTime(int i) => s.GetSum(0, i) + ab[i].a + ab[i].b * (x - i);
+	}
+}
+
+public class StaticRSQ1
+{
+	int n;
+	long[] s;
+	public long[] Raw => s;
+	public StaticRSQ1(long[] a)
+	{
+		n = a.Length;
+		s = new long[n + 1];
+		for (int i = 0; i < n; ++i) s[i + 1] = s[i] + a[i];
+	}
+
+	// [l, r)
+	// 範囲外のインデックスも可。
+	public long GetSum(int l, int r)
+	{
+		if (r < 0 || n < l) return 0;
+		if (l < 0) l = 0;
+		if (n < r) r = n;
+		return s[r] - s[l];
 	}
 }
