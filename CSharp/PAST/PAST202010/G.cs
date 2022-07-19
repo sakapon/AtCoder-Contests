ac2c4ff88a916a925f3ab9ed11015cabee7875c2
Keiho Sakapon <git@saka-pon.net>
2022-07-19 19:33:42 +0900

    Improve code

--- CSharp/PAST/PAST202010/G.cs
+++ CSharp/PAST/PAST202010/G.cs
@@ -4,11 +4,14 @@ using System.Linq;
 class G
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static void Main()
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
 	{
-		var h = Read();
-		int n = h[0], m = h[1];
-		var s = Array.ConvertAll(new int[n], _ => Console.ReadLine().ToCharArray());
+		var (n, m) = Read2();
+		var s = Array.ConvertAll(new bool[n], _ => Console.ReadLine().ToCharArray());
+
+		var sum = s.Sum(r => r.Count(c => c == '.')) + 1;
 
 		var r = 0;
 		for (int i = 0; i < n; i++)
@@ -18,39 +21,37 @@ class G
 				if (s[i][j] == '.') continue;
 
 				s[i][j] = '.';
-				r += AreUnited(n, m, s, i, j) ? 1 : 0;
+				r += AreUnited(i, j) ? 1 : 0;
 				s[i][j] = '#';
 			}
 		}
-		Console.WriteLine(r);
-	}
+		return r;
 
-	static bool AreUnited(int n, int m, char[][] s, int ti, int tj)
-	{
-		var uf = new UF(n * m);
-		int ToId(int x, int y) => x * m + y;
-
-		for (int i = 0; i < n; i++)
+		bool AreUnited(int ti, int tj)
 		{
-			for (int j = 1; j < m; j++)
-			{
-				if (s[i][j - 1] == '.' && s[i][j] == '.')
-					uf.Unite(ToId(i, j - 1), ToId(i, j));
-			}
-		}
+			var uf = new UF(n * m);
+			int ToId(int x, int y) => x * m + y;
 
-		for (int j = 0; j < m; j++)
-		{
-			for (int i = 1; i < n; i++)
+			for (int i = 0; i < n; i++)
 			{
-				if (s[i - 1][j] == '.' && s[i][j] == '.')
-					uf.Unite(ToId(i - 1, j), ToId(i, j));
+				for (int j = 1; j < m; j++)
+				{
+					if (s[i][j - 1] == '.' && s[i][j] == '.')
+						uf.Unite(ToId(i, j - 1), ToId(i, j));
+				}
 			}
-		}
 
-		var sum = s.Sum(r => r.Count(c => c == '.'));
-		var root = uf.GetRoot(ToId(ti, tj));
-		return Enumerable.Range(0, n * m).Count(v => uf.GetRoot(v) == root) == sum;
+			for (int j = 0; j < m; j++)
+			{
+				for (int i = 1; i < n; i++)
+				{
+					if (s[i - 1][j] == '.' && s[i][j] == '.')
+						uf.Unite(ToId(i - 1, j), ToId(i, j));
+				}
+			}
+
+			return uf.GetSize(ToId(ti, tj)) == sum;
+		}
 	}
 }
 
