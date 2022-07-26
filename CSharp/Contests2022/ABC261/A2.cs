4cb18887c2890d99861c7aef2f3fda83ee0e12f0
Keiho Sakapon <git@saka-pon.net>
2022-07-26 13:54:38 +0900

    Other AC

---
+++ CSharp/Contests2022/ABC261/A2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class A2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int, int, int) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (l1, r1, l2, r2) = Read4();
+
+		l1 = Math.Max(l1, l2);
+		r1 = Math.Min(r1, r2);
+		return Math.Max(r1 - l1, 0);
+	}
+}
---
+++ CSharp/Contests2022/ABC261/E2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+class E2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main()
+	{
+		var (n, c) = Read2();
+		var sb = new StringBuilder();
+
+		// 00: 0 に変更
+		// 10: 逆転
+		// 01: なし
+		// 11: 1 に変更
+		var op = new int[30];
+		Array.Fill(op, 2);
+		static bool Act(int op, bool b) => ((b ? 2 : 1) & op) != 0;
+
+		while (n-- > 0)
+		{
+			var (t, a) = Read2();
+
+			for (int i = 0; i < 30; i++)
+			{
+				var f = 1 << i;
+				var af = (a & f) != 0;
+
+				if (t == 1)
+				{
+					if (!af) op[i] = 0;
+				}
+				else if (t == 2)
+				{
+					if (af) op[i] = 3;
+				}
+				else
+				{
+					if (af) op[i] ^= 3;
+				}
+
+				if (Act(op[i], (c & f) != 0))
+				{
+					c |= f;
+				}
+				else
+				{
+					c &= ~f;
+				}
+			}
+
+			sb.Append(c).AppendLine();
+		}
+		Console.Write(sb);
+	}
+}
