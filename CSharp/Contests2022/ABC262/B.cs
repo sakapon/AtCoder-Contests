bf1ff1f12c669be587ae4711e72742aee3907c81
Keiho Sakapon <git@saka-pon.net>
2022-08-05 23:42:44 +0900

    AC

--- CSharp/Contests2022/ABC262/B.cs
+++ CSharp/Contests2022/ABC262/B.cs
@@ -1,21 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var ps = Array.ConvertAll(new bool[m], _ => Read());
 
-		return string.Join(" ", a);
+		var u = new bool[n + 1, n + 1];
+		foreach (var p in ps)
+		{
+			u[p[0], p[1]] = true;
+		}
+
+		var r = 0;
+		for (int a = 1; a <= n; a++)
+		{
+			for (int b = a + 1; b <= n; b++)
+			{
+				if (!u[a, b]) continue;
+				for (int c = b + 1; c <= n; c++)
+				{
+					if (u[b, c] && u[a, c]) r++;
+				}
+			}
+		}
+		return r;
 	}
 }
