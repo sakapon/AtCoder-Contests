0fcb414fca76ecc1c52b155bcb00a91ad366694f
Keiho Sakapon <git@saka-pon.net>
2022-08-17 01:27:44 +0900

    AC

--- CSharp/Contests2022/ABC263/F.cs
+++ CSharp/Contests2022/ABC263/F.cs
@@ -1,21 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var c = Array.ConvertAll(new bool[1 << n], _ => Read());
 
-		return string.Join(" ", a);
+		// i がちょうど j 回勝つ場合の、その範囲の合計の最大値
+		var dp = new long[1 << n];
+		var t = new long[1 << n];
+
+		for (int j = 0; j < n; j++)
+		{
+			Array.Clear(t, 0, t.Length);
+			var len = 1 << j;
+
+			for (int l = 0; l < 1 << n; l += len << 1)
+			{
+				var m = l + len;
+				var r = m + len;
+				var lmax = Enumerable.Range(l, len).Max(i => dp[i]);
+				var rmax = Enumerable.Range(m, len).Max(i => dp[i]);
+
+				for (int i = l; i < m; i++)
+				{
+					t[i] = dp[i] - (j > 0 ? c[i][j - 1] : 0) + c[i][j] + rmax;
+				}
+				for (int i = m; i < r; i++)
+				{
+					t[i] = dp[i] - (j > 0 ? c[i][j - 1] : 0) + c[i][j] + lmax;
+				}
+			}
+
+			(dp, t) = (t, dp);
+		}
+
+		return dp.Max();
 	}
 }
