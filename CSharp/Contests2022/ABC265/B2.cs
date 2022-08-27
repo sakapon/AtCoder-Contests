99d8bc239483eacbea13b3cc95b30ddb4d370db7
Keiho Sakapon <git@saka-pon.net>
2022-08-27 21:56:32 +0900

    Other AC

---
+++ CSharp/Contests2022/ABC265/B2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class B2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
+	{
+		var (n, m, t) = Read3();
+		var a = Read();
+		var ps = Array.ConvertAll(new bool[m], _ => Read());
+
+		var b = new int[n];
+		foreach (var p in ps)
+		{
+			b[p[0]] = p[1];
+		}
+
+		long r = t;
+		for (int i = 1; i < n; i++)
+		{
+			r += b[i];
+			r -= a[i - 1];
+			if (r <= 0) return false;
+		}
+		return true;
+	}
+}
---
+++ CSharp/Contests2022/ABC265/E2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class E2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, m) = Read2();
+		var a = ReadL();
+		var set = Array.ConvertAll(new bool[m], _ => Read2L()).ToHashSet();
+
+		(long, long) f(int i, int j, int k) => (a[0] * i + a[2] * j + a[4] * k, a[1] * i + a[3] * j + a[5] * k);
+
+		// (a, b) を i 回、(c, d) を j 回
+		var dp = new long[n + 1, n + 1];
+		var dt = new long[n + 1, n + 1];
+		dp[0, 0] = 1;
+
+		for (int d = 0; d < n; d++)
+		{
+			for (int i = 0; i <= d; i++)
+			{
+				var d2 = d - i;
+				for (int j = 0; j <= d2; j++)
+				{
+					if (dp[i, j] == 0) continue;
+					var k = d2 - j;
+
+					if (!set.Contains(f(i + 1, j, k)))
+					{
+						dt[i + 1, j] += dp[i, j];
+						dt[i + 1, j] %= M;
+					}
+					if (!set.Contains(f(i, j + 1, k)))
+					{
+						dt[i, j + 1] += dp[i, j];
+						dt[i, j + 1] %= M;
+					}
+					if (!set.Contains(f(i, j, k + 1)))
+					{
+						dt[i, j] += dp[i, j];
+						dt[i, j] %= M;
+					}
+				}
+			}
+
+			(dp, dt) = (dt, dp);
+			Array.Clear(dt, 0, dt.Length);
+		}
+
+		var r = 0L;
+		for (int i = 0; i <= n; i++)
+		{
+			for (int j = 0; j <= n; j++)
+			{
+				r += dp[i, j];
+			}
+		}
+		return r % M;
+	}
+
+	const long M = 998244353;
+}
