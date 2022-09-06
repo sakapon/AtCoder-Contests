b1b0983af4780f6bdc2885b1ba743f9d13be9f29
Keiho Sakapon <git@saka-pon.net>
2022-09-06 21:25:05 +0900

    Update library

--- CSharp/Contests2020/ABC150/F.cs
+++ CSharp/Contests2020/ABC150/F.cs
@@ -20,9 +20,10 @@ class F
 		var b0s = r30.Select(i => new string(b.Select(v => (v & (1 << i)) != 0 ? '1' : '0').ToArray())).ToArray();
 		var b1s = r30.Select(i => new string(b.Select(v => (v & (1 << i)) == 0 ? '1' : '0').ToArray())).ToArray();
 
-		var a2h = Array.ConvertAll(a2s, s => new RH(s));
-		var b0h = Array.ConvertAll(b0s, s => RH.Hash(s));
-		var b1h = Array.ConvertAll(b1s, s => RH.Hash(s));
+		var rh = new RollingHashBuilder(2 * n);
+		var a2h = Array.ConvertAll(a2s, s => rh.Build(s));
+		var b0h = Array.ConvertAll(b0s, s => RollingHashBuilder.Hash(s));
+		var b1h = Array.ConvertAll(b1s, s => RollingHashBuilder.Hash(s));
 
 		for (int k = 0; k < n; k++)
 		{
@@ -34,42 +35,55 @@ class F
 	}
 }
 
-class RH
+public class RollingHashBuilder
 {
 	const long B = 10007;
 	const long M = 1000000007;
-	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
+	public static long MInt(long x) => (x %= M) < 0 ? x + M : x;
 
-	string s;
-	int n;
-	long b;
-	long[] pow, pre;
+	readonly long[] pow;
 
-	public RH(string _s, long _b = B)
+	public RollingHashBuilder(int n)
 	{
-		s = _s;
-		n = s.Length;
-		b = _b;
-
 		pow = new long[n + 1];
 		pow[0] = 1;
-		pre = new long[n + 1];
-
-		for (int i = 0; i < n; ++i)
-		{
-			pow[i + 1] = pow[i] * b % M;
-			pre[i + 1] = (pre[i] * b + s[i]) % M;
-		}
+		for (int i = 0; i < n; ++i) pow[i + 1] = pow[i] * B % M;
 	}
 
-	public long Hash(int start, int count) => MInt(pre[start + count] - pre[start] * pow[count]);
-	public long Hash2(int minIn, int maxEx) => MInt(pre[maxEx] - pre[minIn] * pow[maxEx - minIn]);
+	public RollingHash Build(string s)
+	{
+		var n = s.Length;
+		var pre = new long[n + 1];
+		for (int i = 0; i < n; ++i) pre[i + 1] = (pre[i] * B + s[i]) % M;
+		return new RollingHash(pow, pre);
+	}
+	public RollingHash Build(long[] s)
+	{
+		var n = s.Length;
+		var pre = new long[n + 1];
+		for (int i = 0; i < n; ++i) pre[i + 1] = (pre[i] * B + s[i]) % M;
+		return new RollingHash(pow, pre);
+	}
 
-	public static long Hash(string s, long b = B) => Hash(s, 0, s.Length, b);
-	public static long Hash(string s, int start, int count, long b = B)
+	public static long Hash(string s) => Hash(s, 0, s.Length);
+	public static long Hash(string s, int start, int count)
 	{
 		var h = 0L;
-		for (int i = 0; i < count; ++i) h = (h * b + s[start + i]) % M;
+		for (int i = 0; i < count; ++i) h = (h * B + s[start + i]) % M;
 		return h;
 	}
+	public static long Hash(long[] s) => Hash(s, 0, s.Length);
+	public static long Hash(long[] s, int start, int count)
+	{
+		var h = 0L;
+		for (int i = 0; i < count; ++i) h = (h * B + s[start + i]) % M;
+		return h;
+	}
+}
+
+public class RollingHash
+{
+	readonly long[] pow, pre;
+	internal RollingHash(long[] pow_, long[] pre_) { pow = pow_; pre = pre_; }
+	public long Hash(int start, int count) => RollingHashBuilder.MInt(pre[start + count] - pre[start] * pow[count]);
 }
