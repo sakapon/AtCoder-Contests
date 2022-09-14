814112107a511cd2dd6e579367d902c003c73d81
Keiho Sakapon <git@saka-pon.net>
2022-09-14 09:46:20 +0900

    AC

--- CSharp/Contests2022/ARC147/A.cs
+++ CSharp/Contests2022/ARC147/A.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class A
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var n = Read()[0];
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		Array.Sort(a);
+		var q = new LinkedList<int>(a);
+
+		int k = 0;
+		for (; q.Count > 1; k++)
+		{
+			var v = q.Last.Value % q.First.Value;
+			if (v > 0) q.AddFirst(v);
+			q.RemoveLast();
+		}
+		return k;
 	}
 }
