0e64648463602c7eedc19dd18f36d4d618bef131
Keiho Sakapon <git@saka-pon.net>
2022-09-16 09:35:38 +0900

    Shorten code

--- CSharp/Contests2022/ARC147/B.cs
+++ CSharp/Contests2022/ARC147/B.cs
@@ -12,42 +12,33 @@ class B
 
 		var r = new List<string>();
 
+		void Swap(char c, int i)
+		{
+			var ni = i + c - 'A' + 1;
+			(a[i], a[ni]) = (a[ni], a[i]);
+			r.Add($"{c} {i + 1}");
+		}
+
 		// 偶数を前に移動
 		for (int i = 0; i < n; i += 2)
 			for (int j = i - 2; j >= 0; j -= 2)
-				if ((a[j] & 1) == 1 && (a[j + 2] & 1) == 0)
-				{
-					(a[j], a[j + 2]) = (a[j + 2], a[j]);
-					r.Add($"B {j + 1}");
-				}
+				if ((a[j] & 1) == 1 && (a[j + 2] & 1) == 0) Swap('B', j);
 				else break;
 
 		// 奇数を前に移動
 		for (int i = 1; i < n; i += 2)
 			for (int j = i - 2; j >= 0; j -= 2)
-				if ((a[j] & 1) == 0 && (a[j + 2] & 1) == 1)
-				{
-					(a[j], a[j + 2]) = (a[j + 2], a[j]);
-					r.Add($"B {j + 1}");
-				}
+				if ((a[j] & 1) == 0 && (a[j + 2] & 1) == 1) Swap('B', j);
 				else break;
 
 		// 偶数と奇数を交換
 		for (int i = 0; i < n; i += 2)
-			if ((a[i] & 1) == 0)
-			{
-				(a[i], a[i + 1]) = (a[i + 1], a[i]);
-				r.Add($"A {i + 1}");
-			}
+			if ((a[i] & 1) == 0) Swap('A', i);
 			else break;
 
 		for (int i = 0; i < n; i++)
 			for (int j = i - 2; j >= 0; j -= 2)
-				if (a[j] > a[j + 2])
-				{
-					(a[j], a[j + 2]) = (a[j + 2], a[j]);
-					r.Add($"B {j + 1}");
-				}
+				if (a[j] > a[j + 2]) Swap('B', j);
 				else break;
 
 		r.Insert(0, r.Count.ToString());
