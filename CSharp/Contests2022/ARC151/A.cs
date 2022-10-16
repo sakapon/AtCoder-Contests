add2fbb8809517936dee2294c31306c4666e92df
Keiho Sakapon <git@saka-pon.net>
2022-10-16 23:36:15 +0900

    AC

--- CSharp/Contests2022/ARC151/A.cs
+++ CSharp/Contests2022/ARC151/A.cs
@@ -4,18 +4,61 @@ using System.Linq;
 
 class A
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var s = Console.ReadLine().ToCharArray();
+		var t = Console.ReadLine().ToCharArray();
 
-		return string.Join(" ", a);
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == t[i])
+			{
+				s[i] = t[i] = '0';
+			}
+		}
+
+		var s1 = s.Count(c => c == '1');
+		var t1 = t.Count(c => c == '1');
+		var d = s1 + t1;
+		if (d % 2 != 0) return -1;
+
+		var m1s = Math.Min(s1, t1);
+		var m1t = m1s;
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == '1' && m1s > 0)
+			{
+				s[i] = '0';
+				m1s--;
+				s1--;
+			}
+			if (t[i] == '1' && m1t > 0)
+			{
+				t[i] = '0';
+				m1t--;
+				t1--;
+			}
+		}
+
+		if (s1 == 0)
+		{
+			s = t;
+			s1 = t1;
+		}
+
+		m1s = s1 / 2;
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == '1' && m1s > 0)
+			{
+				s[i] = '0';
+				m1s--;
+				s1--;
+			}
+		}
+
+		return new string(s);
 	}
 }
