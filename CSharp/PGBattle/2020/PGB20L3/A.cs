42536b9155993cf27a3f069c5baed4ef2d5e697c
Keiho Sakapon <git@saka-pon.net>
2022-10-18 22:51:45 +0900

    AC

--- CSharp/PGBattle/2020/PGB20L3/A.cs
+++ CSharp/PGBattle/2020/PGB20L3/A.cs
@@ -11,11 +11,20 @@ class A
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var es = Array.ConvertAll(new bool[n - 1], _ => Read());
 
-		return string.Join(" ", a);
+		var map = ToMap(n + 1, es, false);
+		return map[2..].Count(l => l.Count == 1);
+	}
+
+	static List<int>[] ToMap(int n, int[][] es, bool directed)
+	{
+		var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e[1]);
+			if (!directed) map[e[1]].Add(e[0]);
+		}
+		return map;
 	}
 }
--- CSharp/PGBattle/2020/PGB20L3/B.cs
+++ CSharp/PGBattle/2020/PGB20L3/B.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, s) = Read2();
+		var sb = new StringBuilder();
 
-		return string.Join(" ", a);
+		var a = new int[n];
+		Dfs(0, 1, s);
+		Console.Write(sb);
+
+		// index i 以降で、m 以上の値で、合計 s。終了ならば true。
+		void Dfs(int i, int m, int s)
+		{
+			if (i == n - 1)
+			{
+				a[^1] = s;
+				sb.AppendLine(string.Join(" ", a));
+				return;
+			}
+
+			for (int x = m; x <= s - x; x++)
+			{
+				a[i] = x;
+				Dfs(i + 1, x, s - x);
+			}
+		}
 	}
 }
