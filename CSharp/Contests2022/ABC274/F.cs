eff40b24102cd9609c8212b939dd08b0edabb40d
Keiho Sakapon <git@saka-pon.net>
2022-10-23 21:02:47 +0900

    AC

--- CSharp/Contests2022/ABC274/F.cs
+++ CSharp/Contests2022/ABC274/F.cs
@@ -6,16 +6,40 @@ class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int w, int x, int v) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, a) = Read2();
+		var ps = Array.ConvertAll(new bool[n], _ => Read3());
 
-		return string.Join(" ", a);
+		var r = 0;
+
+		// 魚 i を網の左端に固定します。
+		for (int i = 0; i < n; i++)
+		{
+			var l = new List<(double t, int w)>();
+			foreach (var (w, x0, v0) in ps)
+			{
+				var x = x0 - ps[i].x;
+				double v = v0 - ps[i].v;
+
+				if (0 <= x && x <= a) l.Add((0, w));
+
+				if (v == 0) continue;
+				var t0 = -x / v;
+				var ta = (a - x) / v;
+				if (t0 > 0 || t0 == 0 && v < 0) l.Add((t0, v > 0 ? w : -w));
+				if (ta > 0 || ta == 0 && v > 0) l.Add((ta, v < 0 ? w : -w));
+			}
+
+			var tw = 0;
+			foreach (var (_, w) in l.OrderBy(p => p.t).ThenBy(p => -p.w))
+			{
+				tw += w;
+				r = Math.Max(r, tw);
+			}
+		}
+		return r;
 	}
 }
