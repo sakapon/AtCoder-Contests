dd3be1611333cc2b1590890ab4c21cffa21090fa
Keiho Sakapon <git@saka-pon.net>
2022-10-31 16:03:20 +0900

    AC

---
+++ CSharp/Euler/Page001/P001.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class P001
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = 1000;
+		return Enumerable.Range(0, n).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+	}
+}
---
+++ CSharp/Euler/Page001/P002.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class P002
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = 4000000;
+
+		var r = 2;
+		var (t1, t2) = (1, 2);
+		int t;
+
+		while ((t = t1 + t2) <= n)
+		{
+			if (t % 2 == 0) r += t;
+			(t1, t2) = (t2, t);
+		}
+		return r;
+	}
+}
