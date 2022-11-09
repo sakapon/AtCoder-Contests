fd11b0ea65872cec589b2fee578d8d2098add287
Keiho Sakapon <git@saka-pon.net>
2022-11-09 21:58:39 +0900

    Add methods

--- CSharp/CoderLib/CoderLib8/Numerics/Combinatorics.cs
+++ CSharp/CoderLib/CoderLib8/Numerics/Combinatorics.cs
@@ -7,6 +7,44 @@ namespace CoderLib8.Numerics
 	// 列挙時に配列の内容が変更されます (同一の配列を参照)。
 	public static class Combinatorics
 	{
+		// 値は何でもかまいません。重複可能。
+		public static bool NextPermutation(int[] p)
+		{
+			var n = p.Length;
+
+			// p[i] < p[i + 1] を満たす最大の i
+			var i = n - 2;
+			while (i >= 0 && p[i] >= p[i + 1]) i--;
+			if (i == -1) return false;
+
+			// p[i] < p[j] を満たす最大の j
+			var j = i + 1;
+			while (j + 1 < n && p[i] < p[j + 1]) j++;
+
+			(p[i], p[j]) = (p[j], p[i]);
+			Array.Reverse(p, i + 1, n - i - 1);
+			return true;
+		}
+
+		// 値は何でもかまいません。重複可能。
+		public static bool PrevPermutation(int[] p)
+		{
+			var n = p.Length;
+
+			// p[i] > p[i + 1] を満たす最大の i
+			var i = n - 2;
+			while (i >= 0 && p[i] <= p[i + 1]) i--;
+			if (i == -1) return false;
+
+			// p[i] > p[j] を満たす最大の j
+			var j = i + 1;
+			while (j + 1 < n && p[i] > p[j + 1]) j++;
+
+			(p[i], p[j]) = (p[j], p[i]);
+			Array.Reverse(p, i + 1, n - i - 1);
+			return true;
+		}
+
 		public static void Permutation<T>(T[] values, int r, Action<T[]> action)
 		{
 			var n = values.Length;
