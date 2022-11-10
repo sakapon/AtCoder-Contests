4120430f59ea0c531afef98706453abbca1b08b2
Keiho Sakapon <git@saka-pon.net>
2022-11-10 14:56:04 +0900

    Add Dijkstra class with SortedSet

---
+++ CSharp/YJLC/YGR001/Lib/Dijkstra402.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+// SortedSet を利用します。
+namespace YGR001.Lib.Dijkstra402
+{
+	[System.Diagnostics.DebuggerDisplay(@"VertexesCount = {VertexesCount}")]
+	public class Dijkstra
+	{
+		public Vertex[] Vertexes { get; }
+		public int VertexesCount => Vertexes.Length;
+		public Vertex this[int v] => Vertexes[v];
+
+		public Dijkstra(int vertexesCount)
+		{
+			Vertexes = new Vertex[vertexesCount];
+			for (int v = 0; v < vertexesCount; ++v) Vertexes[v] = new Vertex(v);
+		}
+		public Dijkstra(int vertexesCount, IEnumerable<(int from, int to, int cost)> edges, bool twoWay) : this(vertexesCount)
+		{
+			foreach (var (from, to, cost) in edges) AddEdge(from, to, twoWay, cost);
+		}
+		public Dijkstra(int vertexesCount, IEnumerable<(int from, int to, long cost)> edges, bool twoWay) : this(vertexesCount)
+		{
+			foreach (var (from, to, cost) in edges) AddEdge(from, to, twoWay, cost);
+		}
+
+		public void AddEdge(int from, int to, bool twoWay, long cost)
+		{
+			Vertexes[from].Edges.Add((to, cost));
+			if (twoWay) Vertexes[to].Edges.Add((from, cost));
+		}
+
+		public void Execute(int sv, int ev = -1)
+		{
+			Vertexes[sv].Cost = 0;
+			var q = new SortedSet<(long, int)> { (0, sv) };
+
+			while (q.Count > 0)
+			{
+				var (c, vid) = q.Min;
+				q.Remove((c, vid));
+				if (vid == ev) break;
+				var v = Vertexes[vid];
+
+				foreach (var (to, cost) in v.Edges)
+				{
+					var nv = Vertexes[to];
+					var nc = c + cost;
+					if (nv.Cost <= nc) continue;
+					if (nv.Cost != long.MaxValue) q.Remove((nv.Cost, to));
+					nv.Cost = nc;
+					nv.Previous = v;
+					q.Add((nc, to));
+				}
+			}
+		}
+	}
+
+	[System.Diagnostics.DebuggerDisplay(@"\{{Id}: {Edges.Count} edges, Cost = {Cost}\}")]
+	public class Vertex
+	{
+		public int Id { get; }
+		public List<(int to, long cost)> Edges { get; } = new List<(int, long)>();
+		public long Cost { get; set; } = long.MaxValue;
+		public Vertex Previous { get; set; }
+		public Vertex(int id) { Id = id; }
+	}
+}
