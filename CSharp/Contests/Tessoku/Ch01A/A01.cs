429deaf8d0b2f4f7706db3ad9931a6a7796925e3
Keiho Sakapon <git@saka-pon.net>
2022-11-11 20:49:16 +0900

    AC

--- CSharp/Contests/Tessoku/Ch01A/A01.cs
+++ CSharp/Contests/Tessoku/Ch01A/A01.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class A
+class A01
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
@@ -11,11 +11,6 @@ class A
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
-
-		return string.Join(" ", a);
+		return n * n;
 	}
 }
