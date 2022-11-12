412ae8b12dc4a5e35e234e32a0c270f6929a019f
Keiho Sakapon <git@saka-pon.net>
2022-11-12 15:28:39 +0900

    Add UnweightedGraph class

---
+++ CSharp/Contests/Tessoku/Ch09A/A61V.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoderLib8.Graphs.Int.UnweightedGraph211;
+
+class A61V
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main()
+	{
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
+		var sb = new StringBuilder();
+
+		var graph = new UnweightedGraph(n + 1, es, true);
+		for (int v = 1; v <= n; v++)
+		{
+			sb.AppendLine($"{v}: {{{string.Join(", ", graph[v].Edges)}}}");
+		}
+		Console.Write(sb);
+	}
+}
---
+++ CSharp/Contests/Tessoku/Ch09A/A63V.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib8.Graphs.Int.UnweightedGraph211;
+
+class A63V
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+		var graph = new UnweightedGraph(n + 1, es, true);
+		graph.ShortestByBFS(1);
+		return string.Join("\n", graph.Vertexes[1..].Select(v => v.IsConnected ? v.Cost : -1));
+	}
+}
