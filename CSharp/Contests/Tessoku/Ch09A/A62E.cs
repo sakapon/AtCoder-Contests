043f54a582d6eec9a656e93bdbc7a9d4d0a708f1
Keiho Sakapon <git@saka-pon.net>
2022-11-14 22:48:26 +0900

    AC

---
+++ CSharp/Contests/Tessoku/Ch09A/A62E.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib8.Graphs.SPPs.Int.EdgeGraph311;
+
+class A62E
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+		var graph = new EdgeGraph(n + 1, es, true);
+		graph.ConnectivityByDFS(1);
+		return $"The graph is {(graph.Vertexes[1..].All(v => v.IsConnected) ? "" : "not ")}connected.";
+	}
+}
