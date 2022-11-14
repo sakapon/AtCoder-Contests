a5b600e77b7bfeca3245ab5e58dc2d1bb9f58d74
sakapon <git@saka-pon.net>
2022-11-14 14:54:27 +0900

    Add classes for grid

--- CSharp/CoderLib/CoderLib8/Graphs/Arrays/UnweightedGridHelper.cs
+++ CSharp/CoderLib/CoderLib8/Graphs/Arrays/UnweightedGridHelper.cs
@@ -10,8 +10,84 @@ namespace CoderLib8.Graphs.Arrays
 		int h, w;
 		public GridManager(int h, int w) { this.h = h; this.w = w; }
 
-		public int ToGridId(int i, int j) => w * i + j;
-		public (int i, int j) FromGridId(int id) => (id / w, id % w);
+		public int ToVertexId(int i, int j) => w * i + j;
+		public (int i, int j) FromVertexId(int v) => (v / w, v % w);
+
+		public int[] GetNexts(int v)
+		{
+			var (i, j) = (v / w, v % w);
+			var l = new List<int>();
+			if (j > 0) l.Add(v - 1);
+			if (j + 1 < w) l.Add(v + 1);
+			if (i > 0) l.Add(v - w);
+			if (i + 1 < h) l.Add(v + w);
+			return l.ToArray();
+		}
+	}
+
+	public class IntGrid
+	{
+		int h, w;
+		int[][] s;
+		public IntGrid(int h, int w, int[][] s)
+		{
+			this.h = h;
+			this.w = w;
+			this.s = s;
+		}
+
+		public int ToVertexId(int i, int j) => w * i + j;
+		public (int i, int j) FromVertexId(int v) => (v / w, v % w);
+
+		public int[][] GetWeightedNexts(int v)
+		{
+			var (i, j) = (v / w, v % w);
+			var l = new List<int[]>();
+			if (j > 0) l.Add(new[] { v, v - 1, s[i][j - 1] });
+			if (j + 1 < w) l.Add(new[] { v, v + 1, s[i][j + 1] });
+			if (i > 0) l.Add(new[] { v, v - w, s[i - 1][j] });
+			if (i + 1 < h) l.Add(new[] { v, v + w, s[i + 1][j] });
+			return l.ToArray();
+		}
+	}
+
+	public class StringGrid
+	{
+		int h, w;
+		string[] s;
+		char wall;
+		public StringGrid(int h, int w, string[] s, char wall = '#')
+		{
+			this.h = h;
+			this.w = w;
+			this.s = s;
+			this.wall = wall;
+		}
+
+		public int ToVertexId(int i, int j) => w * i + j;
+		public (int i, int j) FromVertexId(int v) => (v / w, v % w);
+
+		public int[] GetNexts(int v)
+		{
+			var (i, j) = (v / w, v % w);
+			var l = new List<int>();
+			if (j > 0 && s[i][j - 1] != wall) l.Add(v - 1);
+			if (j + 1 < w && s[i][j + 1] != wall) l.Add(v + 1);
+			if (i > 0 && s[i - 1][j] != wall) l.Add(v - w);
+			if (i + 1 < h && s[i + 1][j] != wall) l.Add(v + w);
+			return l.ToArray();
+		}
+
+		public int[][] GetWeightedNexts(int v)
+		{
+			var (i, j) = (v / w, v % w);
+			var l = new List<int[]>();
+			if (j > 0 && s[i][j - 1] != wall) l.Add(new[] { v, v - 1, s[i][j - 1] - '0' });
+			if (j + 1 < w && s[i][j + 1] != wall) l.Add(new[] { v, v + 1, s[i][j + 1] - '0' });
+			if (i > 0 && s[i - 1][j] != wall) l.Add(new[] { v, v - w, s[i - 1][j] - '0' });
+			if (i + 1 < h && s[i + 1][j] != wall) l.Add(new[] { v, v + w, s[i + 1][j] - '0' });
+			return l.ToArray();
+		}
 	}
 
 	public static class UnweightedGridHelper
