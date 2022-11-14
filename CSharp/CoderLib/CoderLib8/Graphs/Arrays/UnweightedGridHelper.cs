2efbd0ada6432cd37f6d18750b6c9dc3cdc346d9
Keiho Sakapon <git@saka-pon.net>
2022-11-14 10:24:45 +0900

    Add classes for grid

--- CSharp/CoderLib/CoderLib8/Graphs/Arrays/UnweightedGridHelper.cs
+++ CSharp/CoderLib/CoderLib8/Graphs/Arrays/UnweightedGridHelper.cs
@@ -5,6 +5,15 @@ using System.Collections.Generic;
 // Test: https://atcoder.jp/contests/atc002/tasks/abc007_3
 namespace CoderLib8.Graphs.Arrays
 {
+	public class GridManager
+	{
+		int h, w;
+		public GridManager(int h, int w) { this.h = h; this.w = w; }
+
+		public int ToGridId(int i, int j) => w * i + j;
+		public (int i, int j) FromGridId(int id) => (id / w, id % w);
+	}
+
 	public static class UnweightedGridHelper
 	{
 		// undirected
@@ -47,6 +56,53 @@ namespace CoderLib8.Graphs.Arrays
 					var v = w * i + j;
 					map[v].Add(v - w);
 					map[v - w].Add(v);
+				}
+			return map;
+		}
+	}
+
+	public static class WeightedGridHelper
+	{
+		// undirected
+		public static List<int[]>[] GetAdjacencyList(int h, int w, int[][] s)
+		{
+			var map = Array.ConvertAll(new bool[h * w], _ => new List<int[]>());
+			for (int i = 0; i < h; ++i)
+				for (int j = 1; j < w; ++j)
+				{
+					var v = w * i + j;
+					map[v].Add(new[] { v, v - 1, s[i][j - 1] });
+					map[v - 1].Add(new[] { v - 1, v, s[i][j] });
+				}
+			for (int j = 0; j < w; ++j)
+				for (int i = 1; i < h; ++i)
+				{
+					var v = w * i + j;
+					map[v].Add(new[] { v, v - w, s[i - 1][j] });
+					map[v - w].Add(new[] { v - w, v, s[i][j] });
+				}
+			return map;
+		}
+
+		// undirected
+		public static List<int[]>[] GetAdjacencyList(int h, int w, string[] s, char wall = '#')
+		{
+			var map = Array.ConvertAll(new bool[h * w], _ => new List<int[]>());
+			for (int i = 0; i < h; ++i)
+				for (int j = 1; j < w; ++j)
+				{
+					if (s[i][j] == wall || s[i][j - 1] == wall) continue;
+					var v = w * i + j;
+					map[v].Add(new[] { v, v - 1, s[i][j - 1] - '0' });
+					map[v - 1].Add(new[] { v - 1, v, s[i][j] - '0' });
+				}
+			for (int j = 0; j < w; ++j)
+				for (int i = 1; i < h; ++i)
+				{
+					if (s[i][j] == wall || s[i - 1][j] == wall) continue;
+					var v = w * i + j;
+					map[v].Add(new[] { v, v - w, s[i - 1][j] - '0' });
+					map[v - w].Add(new[] { v - w, v, s[i][j] - '0' });
 				}
 			return map;
 		}
