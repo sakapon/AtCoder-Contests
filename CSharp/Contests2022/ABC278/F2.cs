b0c9487dcc8a7e7c149ea02b10d85370c27bf354
Keiho Sakapon <git@saka-pon.net>
2022-11-23 18:34:26 +0900

    AC

--- CSharp/Contests2022/ABC278/F2.cs
+++ CSharp/Contests2022/ABC278/F2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class F
+class F2
 {
 	static void Main() => Console.WriteLine(Solve() ? "First" : "Second");
 	static bool Solve()
@@ -23,27 +23,29 @@ class F
 			}
 		}
 
-		// x: 使った単語の集合、i: 最後に使った単語
+		// x: 使った単語の集合、i: 最後に使った単語 (これから使う場合)
 		var dp = NewArray2<bool>(1 << n, n);
+		Array.Fill(dp[^1], true);
 		for (int x = (1 << n) - 2; x > 0; x--)
 		{
 			for (int i = 0; i < n; i++)
 			{
 				if ((x & (1 << i)) == 0) continue;
 
+				dp[x][i] = true;
 				foreach (var j in map[i])
 				{
 					var nx = x | (1 << j);
-					if (nx != x && !dp[nx][j])
+					if (nx != x && dp[nx][j])
 					{
-						dp[x][i] = true;
+						dp[x][i] = false;
 						break;
 					}
 				}
 			}
 		}
 
-		return Enumerable.Range(0, n).Any(i => !dp[1 << i][i]);
+		return Enumerable.Range(0, n).Any(i => dp[1 << i][i]);
 	}
 
 	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
