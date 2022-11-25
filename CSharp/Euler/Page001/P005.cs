081a3261b0894003f2c9749c715c9a80de05ac96
Keiho Sakapon <git@saka-pon.net>
2022-11-25 09:57:17 +0900

    AC

---
+++ CSharp/Euler/Page001/P005.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class P005
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		return 16 * 9 * 5 * 7 * 11 * 13 * 17 * 19;
+	}
+}
---
+++ CSharp/Euler/Page001/P007.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EulerLib8.Numerics;
+
+class P007
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var ps = Primes.GetPrimes(200000);
+		return ps[10000];
+	}
+}
