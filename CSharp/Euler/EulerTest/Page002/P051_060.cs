02d83cdfd9abf9fbbae9b65d1a7867c55063ecae
Keiho Sakapon <git@saka-pon.net>
2022-11-26 18:01:10 +0900

    Add templates

---
+++ CSharp/Euler/EulerTest/Page002/P051_060.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerTest.Page002
+{
+	[TestClass]
+	public class P051_060
+	{
+		#region Test Methods
+		[TestMethod] public void Test051() => TestHelper.Execute();
+		[TestMethod] public void Test052() => TestHelper.Execute();
+		[TestMethod] public void Test053() => TestHelper.Execute();
+		[TestMethod] public void Test054() => TestHelper.Execute();
+		[TestMethod] public void Test055() => TestHelper.Execute();
+		[TestMethod] public void Test056() => TestHelper.Execute();
+		[TestMethod] public void Test057() => TestHelper.Execute();
+		[TestMethod] public void Test058() => TestHelper.Execute();
+		[TestMethod] public void Test059() => TestHelper.Execute();
+		[TestMethod] public void Test060() => TestHelper.Execute();
+		#endregion
+
+		public static object P051()
+		{
+			return 0;
+		}
+
+		public static object P052()
+		{
+			return 0;
+		}
+
+		public static object P053()
+		{
+			return 0;
+		}
+
+		public static object P054()
+		{
+			return 0;
+		}
+
+		public static object P055()
+		{
+			return 0;
+		}
+
+		public static object P056()
+		{
+			return 0;
+		}
+
+		public static object P057()
+		{
+			return 0;
+		}
+
+		public static object P058()
+		{
+			return 0;
+		}
+
+		public static object P059()
+		{
+			return 0;
+		}
+
+		public static object P060()
+		{
+			return 0;
+		}
+	}
+}
