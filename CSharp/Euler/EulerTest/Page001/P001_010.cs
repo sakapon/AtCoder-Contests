f3f5c1d9b7ecc07a27825dbc0d7fa13e92cc85f2
Keiho Sakapon <git@saka-pon.net>
2022-11-26 16:15:00 +0900

    Add test

---
+++ CSharp/Euler/EulerTest/Page001/P001_010.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerTest.Page001
+{
+	[TestClass]
+	public class P001_010
+	{
+		#region Test Methods
+		[TestMethod]
+		public void Test001()
+		{
+			Console.WriteLine(P001());
+		}
+		#endregion
+
+		static object P001()
+		{
+			const int n = 1000;
+			return Enumerable.Range(0, n).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+		}
+	}
+}
