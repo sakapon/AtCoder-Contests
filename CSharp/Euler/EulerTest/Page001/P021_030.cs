ef34a890522b6d006559f0eb2c1a6454eae8be03
Keiho Sakapon <git@saka-pon.net>
2022-11-29 21:06:18 +0900

    Initialize

---
+++ CSharp/Euler/EulerTest/Page001/P021_030.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerTest.Page001
+{
+	[TestClass]
+	public class P021_030
+	{
+		#region Test Methods
+		[TestMethod] public void T021() => TestHelper.Execute();
+		[TestMethod] public void T022() => TestHelper.Execute();
+		[TestMethod] public void T023() => TestHelper.Execute();
+		[TestMethod] public void T024() => TestHelper.Execute();
+		[TestMethod] public void T025() => TestHelper.Execute();
+		[TestMethod] public void T026() => TestHelper.Execute();
+		[TestMethod] public void T027() => TestHelper.Execute();
+		[TestMethod] public void T028() => TestHelper.Execute();
+		[TestMethod] public void T029() => TestHelper.Execute();
+		[TestMethod] public void T030() => TestHelper.Execute();
+		#endregion
+
+		public static object P021()
+		{
+			return 0;
+		}
+
+		public static object P022()
+		{
+			return 0;
+		}
+
+		public static object P023()
+		{
+			return 0;
+		}
+
+		public static object P024()
+		{
+			return 0;
+		}
+
+		public static object P025()
+		{
+			return 0;
+		}
+
+		public static object P026()
+		{
+			return 0;
+		}
+
+		public static object P027()
+		{
+			return 0;
+		}
+
+		public static object P028()
+		{
+			return 0;
+		}
+
+		public static object P029()
+		{
+			return 0;
+		}
+
+		public static object P030()
+		{
+			return 0;
+		}
+	}
+}
---
+++ CSharp/Euler/EulerTest/Page001/P031_040.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerTest.Page001
+{
+	[TestClass]
+	public class P031_040
+	{
+		#region Test Methods
+		[TestMethod] public void T031() => TestHelper.Execute();
+		[TestMethod] public void T032() => TestHelper.Execute();
+		[TestMethod] public void T033() => TestHelper.Execute();
+		[TestMethod] public void T034() => TestHelper.Execute();
+		[TestMethod] public void T035() => TestHelper.Execute();
+		[TestMethod] public void T036() => TestHelper.Execute();
+		[TestMethod] public void T037() => TestHelper.Execute();
+		[TestMethod] public void T038() => TestHelper.Execute();
+		[TestMethod] public void T039() => TestHelper.Execute();
+		[TestMethod] public void T040() => TestHelper.Execute();
+		#endregion
+
+		public static object P031()
+		{
+			return 0;
+		}
+
+		public static object P032()
+		{
+			return 0;
+		}
+
+		public static object P033()
+		{
+			return 0;
+		}
+
+		public static object P034()
+		{
+			return 0;
+		}
+
+		public static object P035()
+		{
+			return 0;
+		}
+
+		public static object P036()
+		{
+			return 0;
+		}
+
+		public static object P037()
+		{
+			return 0;
+		}
+
+		public static object P038()
+		{
+			return 0;
+		}
+
+		public static object P039()
+		{
+			return 0;
+		}
+
+		public static object P040()
+		{
+			return 0;
+		}
+	}
+}
