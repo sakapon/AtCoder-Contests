81c8bc389398a368ada21baee217d12720b68860
Keiho Sakapon <git@saka-pon.net>
2022-11-29 20:57:19 +0900

    Initialize

---
+++ CSharp/Euler/EulerTest/Page004/P151_160.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerTest.Page004
+{
+	[TestClass]
+	public class P151_160
+	{
+		#region Test Methods
+		[TestMethod] public void T151() => TestHelper.Execute();
+		[TestMethod] public void T152() => TestHelper.Execute();
+		[TestMethod] public void T153() => TestHelper.Execute();
+		[TestMethod] public void T154() => TestHelper.Execute();
+		[TestMethod] public void T155() => TestHelper.Execute();
+		[TestMethod] public void T156() => TestHelper.Execute();
+		[TestMethod] public void T157() => TestHelper.Execute();
+		[TestMethod] public void T158() => TestHelper.Execute();
+		[TestMethod] public void T159() => TestHelper.Execute();
+		[TestMethod] public void T160() => TestHelper.Execute();
+		#endregion
+
+		public static object P151()
+		{
+			return 0;
+		}
+
+		public static object P152()
+		{
+			return 0;
+		}
+
+		public static object P153()
+		{
+			return 0;
+		}
+
+		public static object P154()
+		{
+			return 0;
+		}
+
+		public static object P155()
+		{
+			return 0;
+		}
+
+		public static object P156()
+		{
+			return 0;
+		}
+
+		public static object P157()
+		{
+			return 0;
+		}
+
+		public static object P158()
+		{
+			return 0;
+		}
+
+		public static object P159()
+		{
+			return 0;
+		}
+
+		public static object P160()
+		{
+			return 0;
+		}
+	}
+}
