5259b25a630c7d255bb9fbde81ac099714b09742
sakapon <git@saka-pon.net>
2022-12-02 11:11:26 +0900

    Move solutions

--- CSharp/Euler/EulerTest/Page002/P091_100.cs
+++ CSharp/Euler/EulerTest/Page002/P091_100.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using EulerLib8.Linq;
+using static EulerLib8.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EulerTest.Page002
@@ -63,12 +65,38 @@ namespace EulerTest.Page002
 
 		public static object P099()
 		{
-			return 0;
+			var t = GetText(Url099).Split('\n')
+				.Select(l => Array.ConvertAll(l.Split(','), int.Parse))
+				.Select((a, i) => (i, v: a[1] * Math.Log(a[0])))
+				.FirstMax(p => p.v);
+			return t.i + 1;
 		}
 
 		public static object P100()
 		{
-			return 0;
+			// n: total
+			// m: blue
+			// n * (n-1) = 2 * m * (m-1)
+			// x := 2 * n - 1
+			// y := 2 * m - 1
+
+			// Pell's equation
+			// x^2 - 2 y^2 = -1
+			// 初期値: (x0, y0) = (1, 1)
+			// 解 (x, y) は 1 + √2 の奇数乗から求められます。
+
+			const long n_min = 1000000000000;
+			var (x, y) = (1L, 1L);
+			var (n, m) = (1L, 1L);
+
+			while (n < n_min)
+			{
+				(x, y) = (3 * x + 4 * y, 2 * x + 3 * y);
+				(n, m) = ((x + 1) / 2, (y + 1) / 2);
+			}
+			return m;
 		}
+
+		const string Url099 = "https://projecteuler.net/project/resources/p099_base_exp.txt";
 	}
 }
