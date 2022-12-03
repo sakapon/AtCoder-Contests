4bb2281e7e4cb2974152b3b24e04d4c81c46e2d6
Keiho Sakapon <git@saka-pon.net>
2022-12-03 16:05:32 +0900

    Move solutions

--- CSharp/Euler/EulerTest/Page002/P081_090.cs
+++ CSharp/Euler/EulerTest/Page002/P081_090.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CoderLib8.Graphs.SPPs.Int.WeightedGraph211;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static EulerLib8.Common;
 
@@ -24,17 +25,84 @@ namespace EulerTest.Page002
 
 		public static object P081()
 		{
-			return 0;
+			var s = GetText(Url081).Split('\n', StringSplitOptions.RemoveEmptyEntries)
+				.Select(l => Array.ConvertAll(l.Split(','), int.Parse))
+				.ToArray();
+			var h = s.Length;
+			var w = s[0].Length;
+			var n = h * w;
+
+			var graph = GetWeightedAdjacencyList(h, w, s);
+			graph.Dijkstra(0, n - 1);
+			return s[0][0] + graph[n - 1].Cost;
+		}
+
+		public static WeightedGraph GetWeightedAdjacencyList(int h, int w, int[][] s)
+		{
+			var graph = new WeightedGraph(h * w);
+			for (int i = 0; i < h; ++i)
+				for (int j = 1; j < w; ++j)
+				{
+					var v = w * i + j;
+					graph.AddEdge(v - 1, v, false, s[i][j]);
+				}
+			for (int j = 0; j < w; ++j)
+				for (int i = 1; i < h; ++i)
+				{
+					var v = w * i + j;
+					graph.AddEdge(v - w, v, false, s[i][j]);
+				}
+			return graph;
 		}
 
 		public static object P082()
 		{
-			return 0;
+			var s = GetText(Url082).Split('\n', StringSplitOptions.RemoveEmptyEntries)
+				.Select(l => Array.ConvertAll(l.Split(','), int.Parse))
+				.ToArray();
+			var h = s.Length;
+			var w = s[0].Length;
+			var n = h * w;
+
+			var graph = new WeightedGraph(n + 2);
+
+			for (int i = 0; i < h; ++i)
+				for (int j = 1; j < w; ++j)
+				{
+					var v = w * i + j;
+					graph.AddEdge(v - 1, v, false, s[i][j]);
+				}
+			for (int j = 0; j < w; ++j)
+				for (int i = 1; i < h; ++i)
+				{
+					var v = w * i + j;
+					graph.AddEdge(v, v - w, false, s[i - 1][j]);
+					graph.AddEdge(v - w, v, false, s[i][j]);
+				}
+
+			for (int i = 0; i < h; ++i)
+			{
+				graph.AddEdge(n, i * w, false, s[i][0]);
+				graph.AddEdge(i * w + w - 1, n + 1, false, 0);
+			}
+
+			graph.Dijkstra(n, n + 1);
+			return graph[n + 1].Cost;
 		}
 
 		public static object P083()
 		{
-			return 0;
+			var s = GetText(Url083).Split('\n', StringSplitOptions.RemoveEmptyEntries)
+				.Select(l => Array.ConvertAll(l.Split(','), int.Parse))
+				.ToArray();
+			var h = s.Length;
+			var w = s[0].Length;
+			var n = h * w;
+
+			var grid = new WeightedGridHelper(h, w);
+			var graph = grid.GetWeightedAdjacencyList(s);
+			graph.Dijkstra(0, n - 1);
+			return s[0][0] + graph[n - 1].Cost;
 		}
 
 		public static object P084()
@@ -86,5 +154,9 @@ namespace EulerTest.Page002
 		{
 			return 0;
 		}
+
+		const string Url081 = "https://projecteuler.net/project/resources/p081_matrix.txt";
+		const string Url082 = "https://projecteuler.net/project/resources/p082_matrix.txt";
+		const string Url083 = "https://projecteuler.net/project/resources/p083_matrix.txt";
 	}
 }
