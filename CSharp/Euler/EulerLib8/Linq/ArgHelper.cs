5e39c8b6290e642220dd081de807ef7ff0caf21e
Keiho Sakapon <git@saka-pon.net>
2022-12-04 11:20:24 +0900

    Add methods

--- CSharp/Euler/EulerLib8/Linq/ArgHelper.cs
+++ CSharp/Euler/EulerLib8/Linq/ArgHelper.cs
@@ -5,6 +5,26 @@ namespace EulerLib8.Linq
 {
 	public static class ArgHelper
 	{
+		public static T FirstMax<T>(T o1, T o2) where T : IComparable<T> => o1.CompareTo(o2) < 0 ? o2 : o1;
+		public static T FirstMin<T>(T o1, T o2) where T : IComparable<T> => o1.CompareTo(o2) > 0 ? o2 : o1;
+		public static T LastMax<T>(T o1, T o2) where T : IComparable<T> => o1.CompareTo(o2) <= 0 ? o2 : o1;
+		public static T LastMin<T>(T o1, T o2) where T : IComparable<T> => o1.CompareTo(o2) >= 0 ? o2 : o1;
+
+		public static T FirstMax<T, TKey>(T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> => toKey(o1).CompareTo(toKey(o2)) < 0 ? o2 : o1;
+		public static T FirstMin<T, TKey>(T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> => toKey(o1).CompareTo(toKey(o2)) > 0 ? o2 : o1;
+		public static T LastMax<T, TKey>(T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> => toKey(o1).CompareTo(toKey(o2)) <= 0 ? o2 : o1;
+		public static T LastMin<T, TKey>(T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> => toKey(o1).CompareTo(toKey(o2)) >= 0 ? o2 : o1;
+
+		public static void ChFirstMax<T>(ref T o1, T o2) where T : IComparable<T> { if (o1.CompareTo(o2) < 0) o1 = o2; }
+		public static void ChFirstMin<T>(ref T o1, T o2) where T : IComparable<T> { if (o1.CompareTo(o2) > 0) o1 = o2; }
+		public static void ChLastMax<T>(ref T o1, T o2) where T : IComparable<T> { if (o1.CompareTo(o2) <= 0) o1 = o2; }
+		public static void ChLastMin<T>(ref T o1, T o2) where T : IComparable<T> { if (o1.CompareTo(o2) >= 0) o1 = o2; }
+
+		public static void ChFirstMax<T, TKey>(ref T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> { if (toKey(o1).CompareTo(toKey(o2)) < 0) o1 = o2; }
+		public static void ChFirstMin<T, TKey>(ref T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> { if (toKey(o1).CompareTo(toKey(o2)) > 0) o1 = o2; }
+		public static void ChLastMax<T, TKey>(ref T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> { if (toKey(o1).CompareTo(toKey(o2)) <= 0) o1 = o2; }
+		public static void ChLastMin<T, TKey>(ref T o1, T o2, Func<T, TKey> toKey) where TKey : IComparable<TKey> { if (toKey(o1).CompareTo(toKey(o2)) >= 0) o1 = o2; }
+
 		public static TSource FirstMax<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> toKey) where TKey : IComparable<TKey>
 		{
 			var e = source.GetEnumerator();
