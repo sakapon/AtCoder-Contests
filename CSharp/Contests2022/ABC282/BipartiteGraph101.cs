7e0b0230ac23ebea69d3951222b6285d0463e240
Keiho Sakapon <git@saka-pon.net>
2022-12-19 00:20:56 +0900

    AC

---
+++ CSharp/Contests2022/ABC282/BipartiteGraph101.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 連結成分ごとに探索するため、非連結でも可能です。
+// Root は連結成分の id を表します。
+namespace CoderLib8.Graphs.Int.BipartiteGraph101
+{
+	[System.Diagnostics.DebuggerDisplay(@"\{{Id}: {Edges?.Count ?? -1} edges, Color = {Color}\}")]
+	public class Vertex
+	{
+		public int Id { get; }
+		public List<int> Edges { get; set; }
+		public bool Color { get; set; }
+		public Vertex Parent { get; set; }
+		public Vertex Root { get; set; }
+		public Vertex(int id) { Id = id; }
+	}
+
+	[System.Diagnostics.DebuggerDisplay(@"VertexesCount = {VertexesCount}")]
+	public class BipartiteGraph
+	{
+		public List<int>[] Map { get; }
+		public int VertexesCount => Map.Length;
+
+		public BipartiteGraph(int vertexesCount)
+		{
+			Map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
+		}
+		public BipartiteGraph(int vertexesCount, IEnumerable<(int from, int to)> edges, bool twoWay) : this(vertexesCount)
+		{
+			foreach (var (from, to) in edges) AddEdge(from, to, twoWay);
+		}
+
+		public void AddEdge(int from, int to, bool twoWay)
+		{
+			Map[from].Add(to);
+			if (twoWay) Map[to].Add(from);
+		}
+
+		public Vertex[] BFSForest()
+		{
+			var vs = new Vertex[VertexesCount];
+			for (int v = 0; v < vs.Length; ++v) vs[v] = new Vertex(v);
+
+			var u = new bool[vs.Length];
+			var q = new Queue<int>();
+
+			for (int sv = 0; sv < vs.Length; ++sv)
+			{
+				if (u[sv]) continue;
+				vs[sv].Root = vs[sv];
+				u[sv] = true;
+				q.Enqueue(sv);
+
+				while (q.Count > 0)
+				{
+					var v = q.Dequeue();
+					var vo = vs[v];
+					vo.Edges = Map[v];
+
+					foreach (var nv in vo.Edges)
+					{
+						var nvo = vs[nv];
+						if (u[nv])
+						{
+							if (nvo.Color == vo.Color) return null;
+						}
+						else
+						{
+							nvo.Color = !vo.Color;
+							nvo.Parent = vo;
+							nvo.Root = vs[sv];
+							u[nv] = true;
+							q.Enqueue(nv);
+						}
+					}
+				}
+			}
+			return vs;
+		}
+	}
+}
---
+++ CSharp/Contests2022/ABC282/D2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib8.Graphs.Int.BipartiteGraph101;
+
+class D2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+		var r = 0L;
+		var graph = new BipartiteGraph(n + 1, es, true);
+		var vs = graph.BFSForest();
+		if (vs == null) return 0;
+
+		foreach (var g in vs[1..].GroupBy(v => v.Root.Id))
+		{
+			var gvs = g.ToArray();
+
+			var counts = new long[2];
+			counts[0] = gvs.Count(v => !v.Color);
+			counts[1] = gvs.Length - counts[0];
+
+			foreach (var v in gvs)
+			{
+				r += n - counts[v.Color ? 1 : 0] - v.Edges.Count;
+			}
+		}
+		return r / 2;
+	}
+}
