e93277dbea1873825e2aa2ffa196d591499546e3
Keiho Sakapon <git@saka-pon.net>
2022-12-30 01:14:48 +0900

    Update code

--- CSharp/CoderLib/CoderLib8/Graphs/SPPs/UnweightedGraph401_2.cs
+++ CSharp/CoderLib/CoderLib8/Graphs/SPPs/UnweightedGraph401_2.cs
@@ -12,26 +12,29 @@ namespace CoderLib8.Graphs.SPPs.Int.UnweightedGraph401
 			for (int v = 0; v < vs.Length; ++v) vs[v] = new Vertex(v);
 
 			vs[sv].Cost = 0;
-			var q = new Stack<(int, Stack<int>)>();
-			q.Push((sv, new Stack<int>(graph.GetEdges(sv))));
+			var q = new Stack<int>();
+			var es = new Queue<int>[graph.VertexesCount];
+			q.Push(sv);
+			es[sv] = new Queue<int>(graph.GetEdges(sv));
 
 			while (q.Count > 0)
 			{
-				var (v, es) = q.Peek();
+				var v = q.Peek();
 				if (v == ev) return vs;
-				if (es.Count == 0)
+				if (es[v].Count == 0)
 				{
 					q.Pop();
 					continue;
 				}
 
-				var nv = es.Pop();
+				var nv = es[v].Dequeue();
 				var vo = vs[v];
 				var nvo = vs[nv];
 				if (nvo.Cost != long.MaxValue) continue;
 				nvo.Cost = vo.Cost + 1;
 				nvo.Parent = vo;
-				q.Push((nv, new Stack<int>(graph.GetEdges(nv))));
+				q.Push(nv);
+				es[nv] = new Queue<int>(graph.GetEdges(nv));
 			}
 			return vs;
 		}
