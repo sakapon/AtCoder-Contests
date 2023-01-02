513350250b1fc23123340c232aae33468052e9b9
Keiho Sakapon <git@saka-pon.net>
2023-01-02 17:31:19 +0900

    Improve code

--- CSharp/PAST/PAST202206/N.cs
+++ CSharp/PAST/PAST202206/N.cs
@@ -17,14 +17,50 @@ class N
 		s[^1][^1] = 1L << 50;
 
 		var n = h * w;
-		var grid = new IntWeightedGrid_N(s);
-		var r = grid.Dijkstra(n, n + 1);
+		var graph = new ListWeightedGraph(n + 2);
+
+		for (int i = 0; i < h; ++i)
+			for (int j = 1; j < w; ++j)
+			{
+				var v = w * i + j;
+				graph.AddEdge(v, v - 1, false, s[i][j - 1]);
+				graph.AddEdge(v - 1, v, false, s[i][j]);
+			}
+		for (int j = 0; j < w; ++j)
+			for (int i = 1; i < h; ++i)
+			{
+				var v = w * i + j;
+				graph.AddEdge(v, v - w, false, s[i - 1][j]);
+				graph.AddEdge(v - w, v, false, s[i][j]);
+			}
+		for (int i = 1; i < h; ++i)
+			for (int j = 1; j < w; ++j)
+			{
+				var v = w * i + j;
+				graph.AddEdge(v, v - w - 1, false, s[i - 1][j - 1]);
+				graph.AddEdge(v - w - 1, v, false, s[i][j]);
+				graph.AddEdge(v - w, v - 1, false, s[i][j - 1]);
+				graph.AddEdge(v - 1, v - w, false, s[i - 1][j]);
+			}
+
+		for (int i = 0; i < h; i++)
+		{
+			graph.AddEdge(n, w * i, false, s[i][0]);
+			graph.AddEdge(w * i + (w - 1), n + 1, false, 0);
+		}
+		for (int j = 1; j < w; j++)
+		{
+			graph.AddEdge(n, w * (h - 1) + j, false, s[h - 1][j]);
+			graph.AddEdge(j, n + 1, false, 0);
+		}
+
+		var r = graph.Dijkstra(n, n + 1);
 		var path = r[n + 1].GetPathVertexes();
 
 		var g = NewArray2(h, w, '.');
 		foreach (var v in path[1..^1])
 		{
-			var (i, j) = grid.FromVertexId(v);
+			var (i, j) = FromVertexId(v);
 			g[i][j] = '#';
 		}
 
@@ -33,78 +69,10 @@ class N
 		{
 			Console.WriteLine(new string(cs));
 		}
+
+		int ToVertexId(int i, int j) => w * i + j;
+		(int i, int j) FromVertexId(int v) => (v / w, v % w);
 	}
 
 	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
-}
-
-public class IntWeightedGrid_N : WeightedGrid
-{
-	readonly long[][] s;
-	public long[][] Cells => s;
-	public long[] this[int i] => s[i];
-	public IntWeightedGrid_N(long[][] s) : base(s.Length, s[0].Length) { this.s = s; }
-
-	public override List<(int to, long cost)> GetEdges(int v)
-	{
-		if (v == VertexesCount)
-		{
-			var l = new List<(int, long)>();
-			for (int i = 1; i < h; i++)
-			{
-				var (ni, nj) = (i, 0);
-				l.Add((w * ni + nj, s[ni][nj]));
-			}
-			for (int j = 1; j < w - 1; j++)
-			{
-				var (ni, nj) = (h - 1, j);
-				l.Add((w * ni + nj, s[ni][nj]));
-			}
-			return l;
-		}
-		else
-		{
-			var (i, j) = (v / w, v % w);
-			var l = new List<(int, long)>();
-			foreach (var (di, dj) in NextsDelta8)
-			{
-				var (ni, nj) = (i + di, j + dj);
-				if (0 <= ni && ni < h && 0 <= nj && nj < w) l.Add((w * ni + nj, s[ni][nj]));
-			}
-			if (i == 0 || j == w - 1)
-			{
-				l.Add((VertexesCount + 1, 0));
-			}
-			return l;
-		}
-	}
-
-	public Vertex[] Dijkstra(int sv, int ev = -1)
-	{
-		var vs = new Vertex[VertexesCount + 2];
-		for (int v = 0; v < vs.Length; ++v) vs[v] = new Vertex(v);
-
-		vs[sv].Cost = 0;
-		var q = new SortedSet<(long, int)> { (0, sv) };
-
-		while (q.Count > 0)
-		{
-			var (c, v) = q.Min;
-			q.Remove((c, v));
-			if (v == ev) return vs;
-			var vo = vs[v];
-
-			foreach (var (nv, cost) in GetEdges(v))
-			{
-				var nvo = vs[nv];
-				var nc = c + cost;
-				if (nvo.Cost <= nc) continue;
-				if (nvo.Cost != long.MaxValue) q.Remove((nvo.Cost, nv));
-				q.Add((nc, nv));
-				nvo.Cost = nc;
-				nvo.Parent = vo;
-			}
-		}
-		return vs;
-	}
 }
