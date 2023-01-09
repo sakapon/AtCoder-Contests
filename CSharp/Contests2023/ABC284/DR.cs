8e8d453185f850156c43631cfbd303e664ed48ef
Keiho Sakapon <git@saka-pon.net>
2023-01-09 21:58:49 +0900

    AC

--- CSharp/Contests2023/ABC284/DR.cs
+++ CSharp/Contests2023/ABC284/DR.cs
@@ -10,32 +10,22 @@ class DR
 	{
 		var n = long.Parse(Console.ReadLine());
 
-		// WA
 		var (x, y) = Pollard(n);
+		if (IsPrime((long)x)) (x, y) = (y, x);
 		var (z, w) = Pollard(x);
-		BigInteger[] r;
 
-		if (z == 0)
-		{
-			(z, w) = Pollard(y);
-			r = new[] { x, z, w };
-		}
-		else
-		{
-			r = new[] { y, z, w };
-		}
-
-		r = r.OrderBy(x => x).ToArray();
-		if (r[1] == r[2]) Array.Reverse(r);
-		return $"{r[0]} {r[2]}";
+		if (y == z) return $"{y} {w}";
+		if (y == w) return $"{y} {z}";
+		return $"{z} {y}";
 	}
 
 	// 素因数分解可能である前提の実装
 	public static (BigInteger, BigInteger) Pollard(BigInteger n)
 	{
+		if ((n & 1) == 0) return (2, n >> 1);
 		BigInteger f(BigInteger x) => (x * x + 1) % n;
 
-		for (BigInteger i = 1; i < 20; i++)
+		for (BigInteger i = 1; ; i++)
 		{
 			var x = i;
 			var y = f(x);
@@ -49,8 +39,33 @@ class DR
 			if (d == 0 || d == n) continue;
 			return (d, n / d);
 		}
-		return (0, 0);
 	}
 
 	static BigInteger Gcd(BigInteger a, BigInteger b) { if (b == 0) return a; for (BigInteger r; (r = a % b) > 0; a = b, b = r) ; return b; }
+
+	// Miller-Rabin primality test
+	static long[] MRBases = new[] { 2L, 325, 9375, 28178, 450775, 9780504, 1795265022 };
+	public static bool IsPrime(long n)
+	{
+		if (n <= 1) return false;
+		if (n == 2) return true;
+		if ((n & 1) == 0) return false;
+
+		var d = n - 1;
+		while ((d & 1) == 0) d >>= 1;
+		foreach (var a in MRBases)
+		{
+			if (a % n == 0) return true;
+			if (BigInteger.ModPow(a, d, n) == 1) continue;
+			var comp = true;
+			var p = n - 1;
+			while ((p & 1) == 0)
+			{
+				p >>= 1;
+				if (BigInteger.ModPow(a, p, n) == n - 1) { comp = false; break; }
+			}
+			if (comp) return false;
+		}
+		return true;
+	}
 }
