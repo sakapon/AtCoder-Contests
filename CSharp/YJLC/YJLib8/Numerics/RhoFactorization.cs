45b55853236748c2096d15e65c7b8f6dd273e89c
Keiho Sakapon <git@saka-pon.net>
2023-01-10 10:43:30 +0900

    AC

---
+++ CSharp/YJLC/YJLib8/Numerics/RhoFactorization.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace YJLib8.Numerics
+{
+	public static class RhoFactorization
+	{
+		// Miller-Rabin primality test
+		static readonly long[] MRBases = new[] { 2L, 325, 9375, 28178, 450775, 9780504, 1795265022 };
+		public static bool IsPrime(long n)
+		{
+			if (n <= 1) return false;
+			if (n == 2) return true;
+			if ((n & 1) == 0) return false;
+
+			var s = 0;
+			var d = n - 1;
+			while ((d & 1) == 0) { ++s; d >>= 1; }
+
+			foreach (var a in MRBases)
+			{
+				if (a % n == 0) return true;
+				var x = BigInteger.ModPow(a, d, n);
+				if (x == 1) continue;
+				var r = 0;
+				for (; r < s; ++r, x = x * x % n) if (x == n - 1) break;
+				if (r == s) return false;
+			}
+			return true;
+		}
+
+		static long Gcd(long a, long b) { if (b == 0) return a; for (long r; (r = a % b) > 0; a = b, b = r) ; return b; }
+
+		// n: 奇数かつ合成数
+		static long PollardRho(long n)
+		{
+			long f(BigInteger x) => (long)((x * x + 1) % n);
+
+			for (long i = 1; ; ++i)
+			{
+				long x = i, y = f(x), d = 1L;
+				while (d == 1)
+				{
+					d = Gcd(Math.Abs(x - y), n);
+					x = f(x);
+					y = f(f(y));
+				}
+				if (d == n) continue;
+				return d;
+			}
+		}
+
+		// Pollard's rho algorithm
+		public static long[] Factorize(long n)
+		{
+			var ps = new List<long>();
+			while ((n & 1) == 0) { ps.Add(2); n >>= 1; }
+
+			var q = new Stack<long>();
+			if (n > 1) q.Push(n);
+
+			while (q.Count > 0)
+			{
+				var x = q.Pop();
+				if (IsPrime(x))
+				{
+					ps.Add(x);
+				}
+				else
+				{
+					var d = PollardRho(x);
+					q.Push(d);
+					q.Push(x / d);
+				}
+			}
+
+			var r = ps.ToArray();
+			Array.Sort(r);
+			return r;
+		}
+	}
+}
---
+++ CSharp/YJLC/YMA001/P_Factorize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using YJLib8.Numerics;
+
+class P_Factorize
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var qc = int.Parse(Console.ReadLine());
+		var a = Array.ConvertAll(new bool[qc], _ => long.Parse(Console.ReadLine()));
+
+		return string.Join("\n", a.Select(RhoFactorization.Factorize).Select(r => string.Join(" ", r.Prepend(r.Length))));
+	}
+}
