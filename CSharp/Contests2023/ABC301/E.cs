881a22afdbccb41ee951cebdbb9a2dd276e1c168
Keiho Sakapon <git@saka-pon.net>
2023-06-08 20:03:31 +0900

    Improve code

--- CSharp/Contests2023/ABC301/E.cs
+++ CSharp/Contests2023/ABC301/E.cs
@@ -33,11 +33,11 @@ class E
 		gv = m + 1;
 
 		var r = -1;
+		var dp = TSP(m, map);
 
-		var dp = TSP(m + 2, (1 << m + 2) - 1, map, sv);
-		for (int x = 3 << m; x < 1 << m + 2; x++)
+		for (int x = 0; x < 1 << m; x++)
 		{
-			if (dp[x][gv] <= t) r = Math.Max(r, BitOperations.PopCount((uint)x) - 2);
+			if (dp[x][gv] <= t) r = Math.Max(r, BitOperations.PopCount((uint)x));
 		}
 		return r;
 
@@ -74,42 +74,40 @@ class E
 		}
 	}
 
-	// 集合 s をすべて訪れる
-	static long[][] TSP(int n, int s, long[][] map, int sv)
+	// n: 途中で訪れる頂点の数。始点および終点は含まれません。
+	// 始点の頂点番号: n
+	// 終点の頂点番号: n + 1
+	static long[][] TSP(int n, long[][] map)
 	{
-		var dp = NewArray2(1 << n, n, long.MaxValue);
-		dp[1 << sv][sv] = 0;
+		// [途中で訪れた頂点の集合][最後に訪れた頂点。始点および終点を含む]
+		var dp = Array.ConvertAll(new bool[1 << n], _ => Array.ConvertAll(new bool[n + 2], __ => long.MaxValue));
+		dp[0][n] = 0;
 
-		AllSubsets(n, s, x =>
+		for (int x = 0; x < 1 << n; x++)
 		{
-			for (int j = 0; j < n; j++)
+			for (int j = 0; j <= n; j++)
 			{
 				if (dp[x][j] == long.MaxValue) continue;
 
 				for (int nj = 0; nj < n; nj++)
 				{
-					if ((s & (1 << nj)) == 0) continue;
 					if (map[j][nj] == long.MaxValue) continue;
 
 					var nc = dp[x][j] + map[j][nj];
 					if (dp[x | (1 << nj)][nj] <= nc) continue;
 					dp[x | (1 << nj)][nj] = nc;
 				}
+
+				// 終点
+				{
+					if (map[j][n + 1] == long.MaxValue) continue;
+
+					var nc = dp[x][j] + map[j][n + 1];
+					if (dp[x][n + 1] <= nc) continue;
+					dp[x][n + 1] = nc;
+				}
 			}
-			return false;
-		});
-
-		return dp;
-	}
-
-	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
-
-	public static void AllSubsets(int n, int s, Func<int, bool> action)
-	{
-		for (int x = 0; ; x = (x - s) & s)
-		{
-			if (action(x)) break;
-			if (x == s) break;
 		}
+		return dp;
 	}
 }
