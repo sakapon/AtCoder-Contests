b56c580120fc9fa2768efb0b29a9c21655087bef
Keiho Sakapon <git@saka-pon.net>
2023-08-01 18:21:39 +0900

    AC

--- CSharp/Contests2023/ABC304/F.cs
+++ CSharp/Contests2023/ABC304/F.cs
@@ -4,18 +4,58 @@ using System.Linq;
 
 class F
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var s = Console.ReadLine().Select(c => c == '#').ToArray();
 
-		return string.Join(" ", a);
+		var ds = Divisors(n);
+
+		// 約数 d で初めて現れるシフト表の数
+		var cs = new long[n + 1];
+
+		foreach (var d in ds)
+		{
+			if (d == n) continue;
+
+			var and = new bool[d];
+			Array.Fill(and, true);
+			for (int i = 0; i < n; i++)
+			{
+				and[i % d] &= s[i];
+			}
+
+			cs[d] = MPow(2, and.Count(b => b));
+
+			foreach (var d2 in ds)
+			{
+				if (d <= d2) break;
+				if (d % d2 == 0) cs[d] -= cs[d2];
+			}
+
+			cs[d] = MInt(cs[d]);
+		}
+
+		return cs.Sum() % M;
+	}
+
+	const long M = 998244353;
+	static long MInt(long x) => (x %= M) < 0 ? x + M : x;
+	static long MPow(long b, long i)
+	{
+		long r = 1;
+		for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+		return r;
+	}
+
+	static long[] Divisors(long n)
+	{
+		var r = new List<long>();
+		for (long x = 1; x * x <= n; ++x) if (n % x == 0) r.Add(x);
+		var i = r.Count - 1;
+		if (r[i] * r[i] == n) --i;
+		for (; i >= 0; --i) r.Add(n / r[i]);
+		return r.ToArray();
 	}
 }
