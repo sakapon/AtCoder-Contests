9d6f4e64e416bd87d876f5f8b74d18595102e480
Keiho Sakapon <git@saka-pon.net>
2023-08-02 16:44:57 +0900

    AC

--- CSharp/Contests2023/ABC305/F.cs
+++ CSharp/Contests2023/ABC305/F.cs
@@ -1,21 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
 
-		return string.Join(" ", a);
+		var sv = 1;
+		var ev = n;
+
+		var f = false;
+		int[] Nexts()
+		{
+			f = true;
+			return Read()[1..];
+		}
+		void Move(int v)
+		{
+			if (!f) Console.ReadLine();
+			f = false;
+			Console.WriteLine(v);
+		}
+
+		var u = new bool[n + 1];
+		u[sv] = true;
+		DFS(sv);
+		Console.ReadLine();
+
+		bool DFS(int v)
+		{
+			if (v == ev) return true;
+
+			foreach (var nv in Nexts())
+			{
+				if (u[nv]) continue;
+				u[nv] = true;
+				Move(nv);
+				if (DFS(nv)) return true;
+				Move(v);
+			}
+			return false;
+		}
 	}
 }
