6458b0757b96eab00bc8ac7dde51118de8d075b4
Keiho Sakapon <git@saka-pon.net>
2023-08-04 17:08:15 +0900

    AC

--- CSharp/Contests2023/ABC309/F.cs
+++ CSharp/Contests2023/ABC309/F.cs
@@ -1,21 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using WBTrees;
 
 class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
 		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		Array.ForEach(ps, Array.Sort);
+		var map = new WBMap<int, int>
+		{
+			[0] = 1 << 30,
+			[1 << 30] = 0,
+		};
+
+		foreach (var p in ps.OrderBy(p => -p[0]).ThenBy(p => p[1]))
+		{
+			var ge = map.GetFirst(t => t.Key > p[1]);
+			if (p[2] < ge.Item.Value) return true;
+
+			var eq = map.Get(p[1]);
+			if (eq.TryGetValue(out var v2))
+			{
+				if (v2 < p[2])
+				{
+					map[p[1]] = p[2];
+				}
+			}
+			else
+			{
+				if (ge.Item.Value < p[2])
+				{
+					eq = map.Add(p[1], p[2]);
+				}
+				else
+				{
+					eq = ge;
+				}
+			}
+
+			var le = eq;
+			while ((le = eq.GetPrevious()).Item.Value <= eq.Item.Value)
+			{
+				map.Remove(le.Item.Key);
+			}
+		}
+
+		return false;
 	}
 }
