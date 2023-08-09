3f98903d5d5fe0dd05095ce5ef1694f47c06e922
Keiho Sakapon <git@saka-pon.net>
2023-08-09 16:27:44 +0900

    AC

--- CSharp/Contests2023/ABC305/D.cs
+++ CSharp/Contests2023/ABC305/D.cs
@@ -6,16 +6,40 @@ class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var qc = int.Parse(Console.ReadLine());
+		(int l, int r)[] qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		return string.Join(" ", a);
+		var d = new Dictionary<int, int>();
+		// 起床: true
+		var u = new Dictionary<int, bool>();
+		d[0] = 0;
+		u[0] = true;
+
+		for (int i = 1; i < n; i++)
+		{
+			d[a[i]] = d[a[i - 1]];
+			if (u[a[i]] = i % 2 == 0) d[a[i]] += a[i] - a[i - 1];
+		}
+
+		var pt = 0;
+		foreach (var t in a.Concat(qs.Select(q => q.l)).Concat(qs.Select(q => q.r)).OrderBy(x => x))
+		{
+			if (u.ContainsKey(t))
+			{
+				pt = t;
+			}
+			else
+			{
+				d[t] = d[pt];
+				if (!u[pt]) d[t] += t - pt;
+			}
+		}
+
+		return string.Join("\n", qs.Select(q => d[q.r] - d[q.l]));
 	}
 }
