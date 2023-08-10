14fa0ac71f8280992729b16d176f0a328a070fb1
Keiho Sakapon <git@saka-pon.net>
2023-08-10 22:12:38 +0900

    Add Enumerable2 class

---
+++ CSharp/CoderLib/CoderLib8/Linq/Enumerable2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderLib8.Linq
+{
+	public static class Enumerable2
+	{
+		public static int Max(this IEnumerable<int> source, int iv = int.MinValue)
+		{
+			foreach (var v in source) if (iv < v) iv = v;
+			return iv;
+		}
+		public static int Min(this IEnumerable<int> source, int iv = int.MaxValue)
+		{
+			foreach (var v in source) if (iv > v) iv = v;
+			return iv;
+		}
+		public static long Max(this IEnumerable<long> source, long iv = long.MinValue)
+		{
+			foreach (var v in source) if (iv < v) iv = v;
+			return iv;
+		}
+		public static long Min(this IEnumerable<long> source, long iv = long.MaxValue)
+		{
+			foreach (var v in source) if (iv > v) iv = v;
+			return iv;
+		}
+		public static T Max<T>(this IEnumerable<T> source, T iv = default) where T : IComparable<T>
+		{
+			foreach (var v in source) if (iv.CompareTo(v) < 0) iv = v;
+			return iv;
+		}
+		public static T Min<T>(this IEnumerable<T> source, T iv = default) where T : IComparable<T>
+		{
+			foreach (var v in source) if (iv.CompareTo(v) > 0) iv = v;
+			return iv;
+		}
+	}
+}
