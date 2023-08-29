918e0ebf87fd281a56dde1d73c410b7e3510bd81
Keiho Sakapon <git@saka-pon.net>
2023-08-29 23:03:03 +0900

    Add functions

--- CSharp/CoderLib/CoderLib8/Combinatorics/PartitionHelper.cs
+++ CSharp/CoderLib/CoderLib8/Combinatorics/PartitionHelper.cs
@@ -53,5 +53,48 @@ namespace CoderLib8.Combinatorics
 				return false;
 			}
 		}
+
+		// 箱を区別しない、0 個以上
+		// 戻り値: 各グループにおける要素数。逆辞書順。
+		public static void Partition0(int n, int k, Func<int[], bool> action)
+		{
+			var p = new int[k];
+			DFS(0, n, n);
+
+			bool DFS(int i, int rem, int max)
+			{
+				if (rem == 0) return action(p);
+
+				for (int v = rem <= max ? rem : max; rem <= v * (k - i); --v)
+				{
+					p[i] = v;
+					if (DFS(i + 1, rem - v, v)) return true;
+				}
+				p[i] = 0;
+				return false;
+			}
+		}
+
+		// 箱を区別しない、1 個以上
+		// 戻り値: 各グループにおける要素数。逆辞書順。
+		public static void Partition1(int n, int k, Func<int[], bool> action)
+		{
+			var p = new int[k];
+			Array.Fill(p, 1);
+			DFS(0, n - k, n - k);
+
+			bool DFS(int i, int rem, int max)
+			{
+				if (rem == 0) return action(p);
+
+				for (int v = rem <= max ? rem : max; rem <= v * (k - i); --v)
+				{
+					p[i] = v + 1;
+					if (DFS(i + 1, rem - v, v)) return true;
+				}
+				p[i] = 1;
+				return false;
+			}
+		}
 	}
 }
