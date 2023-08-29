88b78d5b0b0e2f108991887adc80e38b423f2db5
Keiho Sakapon <git@saka-pon.net>
2023-08-29 17:21:00 +0900

    Update functions

--- CSharp/CoderLib/CoderLib8/Combinatorics/PartitionHelper.cs
+++ CSharp/CoderLib/CoderLib8/Combinatorics/PartitionHelper.cs
@@ -7,48 +7,50 @@ namespace CoderLib8.Combinatorics
 	public static class PartitionHelper
 	{
 		// 箱を区別する、0 個以上
-		// 戻り値: 辞書順。
-		public static void PartitionK0(int n, int k, Action<int[]> action)
+		// 戻り値: 各グループにおける要素数。辞書順。
+		public static void PartitionK0(int n, int k, Func<int[], bool> action)
 		{
 			var p = new int[k];
 			DFS(0, n);
 
-			void DFS(int i, int rem)
+			bool DFS(int i, int rem)
 			{
 				if (i == k - 1)
 				{
 					p[i] = rem;
-					action(p); return;
+					return action(p);
 				}
 
 				for (int v = 0; v <= rem; ++v)
 				{
 					p[i] = v;
-					DFS(i + 1, rem - v);
+					if (DFS(i + 1, rem - v)) return true;
 				}
+				return false;
 			}
 		}
 
 		// 箱を区別する、1 個以上
-		// 戻り値: 辞書順。
-		public static void PartitionK1(int n, int k, Action<int[]> action)
+		// 戻り値: 各グループにおける要素数。辞書順。
+		public static void PartitionK1(int n, int k, Func<int[], bool> action)
 		{
 			var p = new int[k];
 			DFS(0, n - k);
 
-			void DFS(int i, int rem)
+			bool DFS(int i, int rem)
 			{
 				if (i == k - 1)
 				{
 					p[i] = rem + 1;
-					action(p); return;
+					return action(p);
 				}
 
 				for (int v = 0; v <= rem; ++v)
 				{
 					p[i] = v + 1;
-					DFS(i + 1, rem - v);
+					if (DFS(i + 1, rem - v)) return true;
 				}
+				return false;
 			}
 		}
 	}
