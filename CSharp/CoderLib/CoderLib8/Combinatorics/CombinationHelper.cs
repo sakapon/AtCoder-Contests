6bcab2f7f85d02fe7f984e51a393a5201e0fdd3e
Keiho Sakapon <git@saka-pon.net>
2023-08-29 16:16:36 +0900

    Update functions

--- CSharp/CoderLib/CoderLib8/Combinatorics/CombinationHelper.cs
+++ CSharp/CoderLib/CoderLib8/Combinatorics/CombinationHelper.cs
@@ -6,40 +6,42 @@ namespace CoderLib8.Combinatorics
 	{
 		// [0, n) から r 個を選ぶ方法を列挙します。
 		// 戻り値: 選ばれた r 個の要素。辞書順。
-		public static void Combination(int n, int r, Action<int[]> action)
+		public static void Combination(int n, int r, Func<int[], bool> action)
 		{
 			var p = new int[r];
 			DFS(0, 0);
 
-			void DFS(int v, int si)
+			bool DFS(int v, int si)
 			{
-				if (v == r) { action(p); return; }
+				if (v == r) return action(p);
 
 				for (int i = si; r - v <= n - i; ++i)
 				{
 					p[v] = i;
-					DFS(v + 1, i + 1);
+					if (DFS(v + 1, i + 1)) return true;
 				}
+				return false;
 			}
 		}
 
 		// [0, n) から r 個を選ぶ方法を列挙します。
 		// 戻り値: n 個の各要素に対し、選択されたかどうか。
-		public static void CombinationByBool(int n, int r, Action<bool[]> action)
+		public static void CombinationByBool(int n, int r, Func<bool[], bool> action)
 		{
 			var u = new bool[n];
 			DFS(0, 0);
 
-			void DFS(int v, int si)
+			bool DFS(int v, int si)
 			{
-				if (v == r) { action(u); return; }
+				if (v == r) return action(u);
 
 				for (int i = si; r - v <= n - i; ++i)
 				{
 					u[i] = true;
-					DFS(v + 1, i + 1);
+					if (DFS(v + 1, i + 1)) return true;
 					u[i] = false;
 				}
+				return false;
 			}
 		}
 
