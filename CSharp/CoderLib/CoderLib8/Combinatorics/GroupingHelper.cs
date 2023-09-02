0f19977322324d68fdc1d93071e3dc6b5b7505bd
Keiho Sakapon <git@saka-pon.net>
2023-09-02 22:41:44 +0900

    Update function

--- CSharp/CoderLib/CoderLib8/Combinatorics/GroupingHelper.cs
+++ CSharp/CoderLib/CoderLib8/Combinatorics/GroupingHelper.cs
@@ -6,6 +6,9 @@ namespace CoderLib8.Combinatorics
 	// n 個の球を k 個の箱に入れる方法を列挙します。
 	// 球を区別します。
 	// 箱を区別しません。
+
+	//if (v == n - 1 ? action(b) : DFS(v + 1, i < i0 ? i0 : i0 + 1)) return true;
+	// とすることで高速化できます。
 	public static class GroupingHelper
 	{
 		#region 0 個以上
@@ -13,18 +16,19 @@ namespace CoderLib8.Combinatorics
 		public static void Assign0(int n, int k, Func<List<int>[], bool> action)
 		{
 			var b = Array.ConvertAll(new bool[k], _ => new List<int>());
-			DFS(0);
+			DFS(0, 0);
 
-			bool DFS(int v)
+			// i0: 最初の空の箱の番号
+			bool DFS(int v, int i0)
 			{
 				if (v == n) return action(b);
 
 				for (int i = 0; i < k; ++i)
 				{
 					b[i].Add(v);
-					if (DFS(v + 1)) return true;
+					if (DFS(v + 1, i < i0 ? i0 : i0 + 1)) return true;
 					b[i].RemoveAt(b[i].Count - 1);
-					if (b[i].Count == 0) break;
+					if (i == i0) break;
 				}
 				return false;
 			}
