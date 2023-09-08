2528ee52c89514e6778b6b557d9a72bf90e28f6c
Keiho Sakapon <git@saka-pon.net>
2023-09-08 12:25:14 +0900

    AC

--- CSharp/Contests2023/ABC315/D.cs
+++ CSharp/Contests2023/ABC315/D.cs
@@ -6,16 +6,85 @@ class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w) = Read2();
+		var s = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
 
-		return string.Join(" ", a);
+		var yoko = NewArray2<int>(h, 26);
+		var tate = NewArray2<int>(w, 26);
+
+		for (int i = 0; i < h; i++)
+		{
+			for (int j = 0; j < w; j++)
+			{
+				yoko[i][s[i][j] - 'a']++;
+				tate[j][s[i][j] - 'a']++;
+			}
+		}
+
+		var rh = h;
+		var rw = w;
+		var end_yoko = new int[26];
+		var end_tate = new int[26];
+
+		while (true)
+		{
+			var all_yoko = Find_yoko().ToArray();
+			var all_tate = Find_tate().ToArray();
+			if (all_yoko.Length == 0 && all_tate.Length == 0) break;
+
+			foreach (var (i, c) in all_yoko)
+			{
+				yoko[i][c] -= rw;
+				end_tate[c]++;
+			}
+			foreach (var (j, d) in all_tate)
+			{
+				tate[j][d] -= rh;
+				end_yoko[d]++;
+			}
+
+			rh -= all_yoko.Length;
+			rw -= all_tate.Length;
+		}
+		return rh * rw;
+
+		IEnumerable<(int, int)> Find_yoko()
+		{
+			if (rw <= 1) yield break;
+
+			for (int i = 0; i < h; i++)
+			{
+				for (int c = 0; c < 26; c++)
+				{
+					if (yoko[i][c] - end_yoko[c] == rw)
+					{
+						yield return (i, c);
+						break;
+					}
+				}
+			}
+		}
+
+		IEnumerable<(int, int)> Find_tate()
+		{
+			if (rh <= 1) yield break;
+
+			for (int j = 0; j < w; j++)
+			{
+				for (int c = 0; c < 26; c++)
+				{
+					if (tate[j][c] - end_tate[c] == rh)
+					{
+						yield return (j, c);
+						break;
+					}
+				}
+			}
+		}
 	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
 }
--- CSharp/Contests2023/ABC315/E2.cs
+++ CSharp/Contests2023/ABC315/E2.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class E
+class E2
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static void Main() => Console.WriteLine(Solve());
@@ -10,37 +10,21 @@ class E
 		var n = int.Parse(Console.ReadLine());
 		var map = Array.ConvertAll(new bool[n], _ => Read());
 
-		var indeg = new int[n + 1];
 		var u = new bool[n + 1];
-		var q = new Queue<int>();
 		var r = new List<int>();
+		DFS(1);
+		r.RemoveAt(r.Count - 1);
+		return string.Join(" ", r);
 
-		q.Enqueue(1);
-		while (q.Count > 0)
+		void DFS(int v)
 		{
-			var v = q.Dequeue();
 			foreach (var nv in map[v - 1][1..])
 			{
-				indeg[nv]++;
 				if (u[nv]) continue;
 				u[nv] = true;
-				q.Enqueue(nv);
+				DFS(nv);
 			}
+			r.Add(v);
 		}
-
-		q.Enqueue(1);
-		while (q.Count > 0)
-		{
-			var v = q.Dequeue();
-			foreach (var nv in map[v - 1][1..])
-			{
-				if (--indeg[nv] > 0) continue;
-				q.Enqueue(nv);
-				r.Add(nv);
-			}
-		}
-
-		r.Reverse();
-		return string.Join(" ", r);
 	}
 }
