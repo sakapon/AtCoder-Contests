e534cd2fcc64d19c11f160ee6bd186b5bdea6a5a
Keiho Sakapon <git@saka-pon.net>
2023-09-30 23:40:28 +0900

    AC

--- CSharp/Contests2023/ABC322/B.cs
+++ CSharp/Contests2023/ABC322/B.cs
@@ -4,18 +4,26 @@ using System.Linq;
 
 class B
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
+		Console.ReadLine();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var t = Console.ReadLine();
 
-		return string.Join(" ", a);
+		if (t.StartsWith(s))
+		{
+			if (t.EndsWith(s))
+				return 0;
+			else
+				return 1;
+		}
+		else
+		{
+			if (t.EndsWith(s))
+				return 2;
+			else
+				return 3;
+		}
 	}
 }
--- CSharp/Contests2023/ABC322/E.cs
+++ CSharp/Contests2023/ABC322/E.cs
@@ -1,21 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
+		var (n, k, p) = Read3();
 		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var dp = new long[(int)Math.Pow(p + 1, k)];
+		Array.Fill(dp, 1L << 60);
+		dp[0] = 0;
+
+		foreach (var ca in ps)
+		{
+			var c = ca[0];
+
+			for (int x = dp.Length - 1; x >= 0; x--)
+			{
+				if (dp[x] == 1L << 60) continue;
+
+				var ak = new int[k];
+				var nx = x;
+				for (int i = 0; i < k; i++)
+				{
+					ak[i] = nx % (p + 1);
+					ak[i] += ca[i + 1];
+					if (ak[i] > p) ak[i] = p;
+					nx /= p + 1;
+				}
+
+				nx = 0;
+				for (int i = k - 1; i >= 0; i--)
+				{
+					nx *= p + 1;
+					nx += ak[i];
+				}
+
+				Chmin(ref dp[nx], dp[x] + c);
+			}
+		}
+
+		var r = dp[^1];
+		if (r == 1L << 60) return -1;
+		return r;
 	}
+
+	public static long Chmin(ref long x, long v) => x > v ? x = v : x;
 }
