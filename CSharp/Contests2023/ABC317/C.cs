0d1426c7f9cbd0f8a488af297e414274ee23f526
Keiho Sakapon <git@saka-pon.net>
2023-10-01 15:58:20 +0900

    AC

--- CSharp/Contests2023/ABC317/C.cs
+++ CSharp/Contests2023/ABC317/C.cs
@@ -1,21 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read3());
 
-		return string.Join(" ", a);
+		var map = NewArray2(n, n, -1);
+		foreach (var (a, b, c) in es)
+		{
+			map[a - 1][b - 1] = c;
+			map[b - 1][a - 1] = c;
+		}
+
+		var r = 0;
+		var u = new bool[n];
+
+		for (int sv = 0; sv < n; sv++)
+		{
+			DFS(sv, 0);
+		}
+		return r;
+
+		void DFS(int v, int d)
+		{
+			Chmax(ref r, d);
+			u[v] = true;
+
+			for (int nv = 0; nv < n; nv++)
+			{
+				if (u[nv]) continue;
+				if (map[v][nv] == -1) continue;
+
+				DFS(nv, d + map[v][nv]);
+			}
+			u[v] = false;
+		}
 	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+	public static int Chmax(ref int x, int v) => x < v ? x = v : x;
 }
--- CSharp/Contests2023/ABC317/D.cs
+++ CSharp/Contests2023/ABC317/D.cs
@@ -1,21 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => Read3());
 
-		return string.Join(" ", a);
+		var sum = ps.Sum(p => p.Item3);
+		var dp = new long[sum + 1];
+		Array.Fill(dp, 1L << 60);
+		dp[0] = 0;
+
+		foreach (var (x, y, z) in ps)
+		{
+			var d = x > y ? 0 : (y - x + 1) / 2;
+
+			for (int j = sum; j >= 0; j--)
+			{
+				var nj = j + z;
+				if (nj > sum) continue;
+				Chmin(ref dp[nj], dp[j] + d);
+			}
+		}
+
+		return dp[((sum + 1) / 2)..].Min();
 	}
+
+	public static long Chmin(ref long x, long v) => x > v ? x = v : x;
 }
