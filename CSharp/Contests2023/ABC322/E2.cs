02a8817d3e3371aafb5fd0efdea010db8fa82d3d
Keiho Sakapon <git@saka-pon.net>
2023-10-07 10:41:46 +0900

    TLE

---
+++ CSharp/Contests2023/ABC322/E2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib8.Values;
+
+class E2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, k, p) = Read3();
+		var ps = Array.ConvertAll(new bool[n], _ => ReadL());
+
+		var dp = new Dictionary<IntVector, long>();
+		dp[new IntVector(k)] = 0;
+
+		foreach (var ca in ps)
+		{
+			var c = ca[0];
+			IntVector a = ca[1..];
+
+			var dt = new Dictionary<IntVector, long>(dp);
+
+			foreach (var (da, dc) in dp)
+			{
+				var na = da + a;
+				var nc = dc + c;
+
+				na = Array.ConvertAll(na.Raw, x => Math.Min(x, p));
+
+				if (dt.TryGetValue(na, out var nc0))
+					dt[na] = Math.Min(nc0, nc);
+				else
+					dt[na] = nc;
+			}
+
+			dp = dt;
+		}
+
+		var pa = Enumerable.Repeat((long)p, k).ToArray();
+		return dp.TryGetValue(pa, out var pc) ? pc : -1;
+	}
+}
---
+++ CSharp/Contests2023/ABC322/IntVector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoderLib8.Values
+{
+	// 任意次元ベクトル
+	public class IntVector : IEquatable<IntVector>
+	{
+		readonly long[] v;
+		public long[] Raw => v;
+		public long this[int i] => v[i];
+		public int Dimension => v.Length;
+
+		public IntVector(int n) { v = new long[n]; }
+		public IntVector(long[] v) { this.v = v; }
+		public override string ToString() => string.Join(" ", v);
+		public static IntVector Parse(string s) => Array.ConvertAll(s.Split(), long.Parse);
+
+		public static implicit operator IntVector(long[] v) => new IntVector(v);
+		public static explicit operator long[](IntVector v) => v.v;
+
+		#region Equality Operators
+		public bool Equals(IntVector other) => !(other is null) && v.SequenceEqual(other.v);
+		public static bool Equals(IntVector v1, IntVector v2) => v1?.Equals(v2) ?? (v2 is null);
+		public static bool operator ==(IntVector v1, IntVector v2) => Equals(v1, v2);
+		public static bool operator !=(IntVector v1, IntVector v2) => !Equals(v1, v2);
+		public override bool Equals(object obj) => Equals(obj as IntVector);
+		public override int GetHashCode() => v.Length == 0 ? 0 : v[0].GetHashCode();
+		#endregion
+
+		public static IntVector operator -(IntVector v) => Array.ConvertAll(v.v, x => -x);
+		public static IntVector operator +(IntVector v1, IntVector v2) => v1.v.Zip(v2.v, (x, y) => x + y).ToArray();
+		public static IntVector operator -(IntVector v1, IntVector v2) => v1.v.Zip(v2.v, (x, y) => x - y).ToArray();
+
+		public long NormL1 => v.Sum(x => Math.Abs(x));
+		public double Norm => Math.Sqrt(v.Sum(x => x * x));
+
+		public static long Dot(IntVector v1, IntVector v2) => v1.v.Zip(v2.v, (x, y) => x * y).Sum();
+		public static bool IsOrthogonal(IntVector v1, IntVector v2) => Dot(v1, v2) == 0;
+	}
+}
