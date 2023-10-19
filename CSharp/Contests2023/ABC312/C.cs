9dedebf15f9326eabbfc14c183a3b7d9a4b4bed8
Keiho Sakapon <git@saka-pon.net>
2023-10-20 00:08:30 +0900

    AC

--- CSharp/Contests2023/ABC312/C.cs
+++ CSharp/Contests2023/ABC312/C.cs
@@ -1,21 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, m) = Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var b = Read();
 
-		return string.Join(" ", a);
+		return First(0, 1 << 30, x => a.Count(v => v <= x) >= b.Count(v => v >= x));
+	}
+
+	static int First(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
 	}
 }
