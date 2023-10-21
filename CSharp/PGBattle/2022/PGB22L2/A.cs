60f2ef5ca15a88a152bac159c034c81bb1f46f56
Keiho Sakapon <git@saka-pon.net>
2023-10-21 11:06:17 +0900

    AC

--- CSharp/PGBattle/2022/PGB22L2/A.cs
+++ CSharp/PGBattle/2022/PGB22L2/A.cs
@@ -6,16 +6,22 @@ class A
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, qc) = Read2();
+		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		return string.Join(" ", a);
+		// true: 裏
+		var b = new bool[n];
+
+		foreach (var (l, r) in qs)
+		{
+			for (int i = l - 1; i < r; i++)
+			{
+				b[i] ^= true;
+			}
+		}
+		return b.Count(v => v);
 	}
 }
