338eb619321ba391e08291a34425bc86dac3ecaf
Keiho Sakapon <git@saka-pon.net>
2023-10-26 00:07:17 +0900

    AC

--- CSharp/Contests2023/ABC323/F.cs
+++ CSharp/Contests2023/ABC323/F.cs
@@ -1,21 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CoderLib8.Values;
 
 class F
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var a = ReadL();
+		var pa = new IntV(a[0], a[1]);
+		var pb = new IntV(a[2], a[3]);
+		var pc = new IntV(a[4], a[5]);
 
-		return string.Join(" ", a);
+		var sx = Math.Sign(pc.X - pb.X);
+		var pb_x = pb - new IntV(sx, 0);
+
+		var sy = Math.Sign(pc.Y - pb.Y);
+		var pb_y = pb - new IntV(0, sy);
+
+		var r = 0L;
+
+		if (sx == 0)
+		{
+			r += (pc - pb).NormL1;
+			r += (pa - pb_y).NormL1;
+			if (pb.X == pa.X && sy == Math.Sign(pa.Y - pb.Y)) r += 2;
+		}
+		else if (sy == 0)
+		{
+			r += (pc - pb).NormL1;
+			r += (pa - pb_x).NormL1;
+			if (pb.Y == pa.Y && sx == Math.Sign(pa.X - pb.X)) r += 2;
+		}
+		else
+		{
+			r += (pc - pb).NormL1;
+			r += 2;
+			r += Math.Min((pa - pb_x).NormL1, (pa - pb_y).NormL1);
+		}
+		return r;
 	}
 }
