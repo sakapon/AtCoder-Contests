e00ce9098c8eb83a25d5574c2bf876f0197d1994
Keiho Sakapon <git@saka-pon.net>
2023-11-02 22:59:51 +0900

    AC

--- CSharp/Contests2023/ABC324/F.cs
+++ CSharp/Contests2023/ABC324/F.cs
@@ -1,21 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read());
 
-		return string.Join(" ", a);
+		var map = Array.ConvertAll(new bool[n + 1], _ => new List<int[]>());
+		var indeg = new int[n + 1];
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e);
+			++indeg[e[1]];
+		}
+
+		var dp = new double[n + 1];
+		var q = new Queue<int>();
+
+		// b-cx の和が 0 以上となるパスが存在するような最後の x を求めます。
+		return Last(0, 1 << 14, x =>
+		{
+			Array.Fill(dp, -1 << 30);
+			dp[1] = 0;
+
+			for (int v = 1; v <= n; ++v)
+				if (indeg[v] == 0)
+					q.Enqueue(v);
+
+			var indeg2 = (int[])indeg.Clone();
+			while (q.Count > 0)
+			{
+				var v = q.Dequeue();
+				foreach (var e in map[v])
+				{
+					var nv = e[1];
+					Chmax(ref dp[nv], dp[v] + e[2] - e[3] * x);
+
+					if (--indeg2[nv] > 0) continue;
+					q.Enqueue(nv);
+				}
+			}
+			return dp[n] >= 0;
+		});
+	}
+
+	public static double Chmax(ref double x, double v) => x < v ? x = v : x;
+
+	static double Last(double l, double r, Func<double, bool> f, int digits = 9)
+	{
+		double m;
+		while (Math.Round(r - l, digits) > 0) if (f(m = r - (r - l) / 2)) l = m; else r = m;
+		return l;
 	}
 }
