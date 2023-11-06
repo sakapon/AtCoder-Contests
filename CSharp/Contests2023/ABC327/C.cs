e53b80f40f7f65982e69272138e21d4604b471f2
Keiho Sakapon <git@saka-pon.net>
2023-11-06 11:07:34 +0900

    Improve code

--- CSharp/Contests2023/ABC327/C.cs
+++ CSharp/Contests2023/ABC327/C.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class C
@@ -9,34 +8,12 @@ class C
 	static bool Solve()
 	{
 		var n = 9;
-		var n2 = 3;
 		var a = Array.ConvertAll(new bool[n], _ => Read());
-
 		var rn = Enumerable.Range(0, n).ToArray();
 
-		if (!rn.All(i => a[i].Distinct().Count() == n)) return false;
-		if (!rn.All(j => rn.Select(i => a[i][j]).Distinct().Count() == n)) return false;
-
-		for (int i = 0; i < n2; i++)
-		{
-			for (int j = 0; j < n2; j++)
-			{
-				var oi = 3 * i;
-				var oj = 3 * j;
-
-				var l = new List<int>();
-
-				for (int x = 0; x < n2; x++)
-				{
-					for (int y = 0; y < n2; y++)
-					{
-						l.Add(a[oi + x][oj + y]);
-					}
-				}
-
-				if (l.Distinct().Count() != 9) return false;
-			}
-		}
-		return true;
+		return
+			rn.All(i => a[i].Distinct().Count() == n) &&
+			rn.All(j => rn.Select(i => a[i][j]).Distinct().Count() == n) &&
+			rn.Select(v => (i: v / 3 * 3, j: v % 3 * 3)).All(o => rn.Select(v => a[o.i + v / 3][o.j + v % 3]).Distinct().Count() == n);
 	}
 }
