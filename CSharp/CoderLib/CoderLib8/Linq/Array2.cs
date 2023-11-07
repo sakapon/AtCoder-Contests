ad9320cc1da81d36bfaef815065d9b6f198968f0
Keiho Sakapon <git@saka-pon.net>
2023-11-07 22:30:29 +0900

    Add functions

--- CSharp/CoderLib/CoderLib8/Linq/Array2.cs
+++ CSharp/CoderLib/CoderLib8/Linq/Array2.cs
@@ -23,6 +23,12 @@ namespace CoderLib8.Linq
 		public static int[] RangeLR(int l, int r) => Range(l, r - l);
 		#endregion
 
+		#region Array
+		public static T[] ToArray<T>(this T[] a) => (T[])a.Clone();
+		public static void Clear<T>(this T[] a) => Array.Clear(a, 0, a.Length);
+		public static void Fill<T>(this T[] a, T value) => Array.Fill(a, value);
+		#endregion
+
 		#region Accumulation
 		public static TResult Aggregate<TSource, TResult>(this TSource[] a, TResult iv, Func<TResult, TSource, TResult> func)
 		{
@@ -85,6 +91,35 @@ namespace CoderLib8.Linq
 				if (iv >= v) (mi, iv) = (i, v);
 			}
 			return (mi, iv);
+		}
+
+		public static TSource[][] Chunk<TSource>(this TSource[] a, int size)
+		{
+			var q = Math.DivRem(a.Length, size, out var rem);
+			var r = new TSource[rem == 0 ? q : q + 1][];
+			for (var i = 0; i < q; ++i)
+				r[i] = a[(size * i)..(size * (i + 1))];
+			if (rem != 0) r[q] = a[(size * q)..];
+			return r;
+		}
+
+		public static (TKey key, TSource[] chunk)[] Chunk<TSource, TKey>(this TSource[] a, Func<TSource, TKey> toKey)
+		{
+			var l = new List<(TKey, TSource[])>();
+			TKey key = default;
+			var si = 0;
+			for (var i = 0; i < a.Length; ++i)
+			{
+				var k = toKey(a[i]);
+				if (!Equals(k, key))
+				{
+					if (i != 0) l.Add((key, a[si..i]));
+					key = k;
+					si = i;
+				}
+			}
+			if (a.Length != 0) l.Add((key, a[si..]));
+			return l.ToArray();
 		}
 	}
 }
