072c38d8d231eb9250df86786a8d931f24682fa8
Keiho Sakapon <git@saka-pon.net>
2023-11-09 11:23:06 +0900

    Move functions

--- CSharp/CoderLib/CoderLib8/Linq/Array2.cs
+++ CSharp/CoderLib/CoderLib8/Linq/Array2.cs
@@ -46,56 +46,6 @@ namespace CoderLib8.Linq
 		public static long Min(this long[] a) => a.Aggregate(long.MaxValue, Math.Min);
 		#endregion
 
-		public static (int index, Num value) FirstMax(this Num[] a, Num iv = Num.MinValue)
-		{
-			var mi = -1;
-			for (var i = a.Length - 1; i >= 0; --i)
-				if (iv <= a[i]) (mi, iv) = (i, a[i]);
-			return (mi, iv);
-		}
-		public static (int index, Num value) FirstMin(this Num[] a, Num iv = Num.MaxValue)
-		{
-			var mi = -1;
-			for (var i = a.Length - 1; i >= 0; --i)
-				if (iv >= a[i]) (mi, iv) = (i, a[i]);
-			return (mi, iv);
-		}
-		public static (int index, Num value) LastMax(this Num[] a, Num iv = Num.MinValue)
-		{
-			var mi = -1;
-			for (var i = 0; i < a.Length; ++i)
-				if (iv <= a[i]) (mi, iv) = (i, a[i]);
-			return (mi, iv);
-		}
-		public static (int index, Num value) LastMin(this Num[] a, Num iv = Num.MaxValue)
-		{
-			var mi = -1;
-			for (var i = 0; i < a.Length; ++i)
-				if (iv >= a[i]) (mi, iv) = (i, a[i]);
-			return (mi, iv);
-		}
-
-		public static (int index, Num value) FirstMax<TSource>(this TSource[] a, Func<TSource, Num> selector, Num iv = Num.MinValue)
-		{
-			var mi = -1;
-			for (var i = a.Length - 1; i >= 0; --i)
-			{
-				var v = selector(a[i]);
-				if (iv <= v) (mi, iv) = (i, v);
-			}
-			return (mi, iv);
-		}
-		public static (int index, Num value) FirstMin<TSource>(this TSource[] a, Func<TSource, Num> selector, Num iv = Num.MaxValue)
-		{
-			var mi = -1;
-			for (var i = a.Length - 1; i >= 0; --i)
-			{
-				var v = selector(a[i]);
-				if (iv >= v) (mi, iv) = (i, v);
-			}
-			return (mi, iv);
-		}
-
 		public static TSource[][] Chunk<TSource>(this TSource[] a, int size)
 		{
 			var q = Math.DivRem(a.Length, size, out var rem);
