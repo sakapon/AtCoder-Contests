dbe8cfa8388cd5ac526e4ed3c8e5f7c887ce8f9b
Keiho Sakapon <git@saka-pon.net>
2023-11-16 14:51:45 +0900

    AC

---
+++ CSharp/Contests2023/ABC321/C2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class C2
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var k = int.Parse(Console.ReadLine());
+
+		var l = new List<string> { "" };
+		for (int i = 0; i < 10; i++)
+		{
+			var si = i.ToString();
+			var c = l.Count;
+			for (int j = 0; j < c; j++)
+			{
+				l.Add(si + l[j]);
+			}
+		}
+		return l.Skip(1).OrderBy(long.Parse).ElementAt(k);
+	}
+}
