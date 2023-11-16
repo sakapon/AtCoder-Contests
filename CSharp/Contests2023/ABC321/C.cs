c7b8e39b0e2c9affac7d6a6b91d0193992ce0f28
Keiho Sakapon <git@saka-pon.net>
2023-11-16 14:33:32 +0900

    AC

--- CSharp/Contests2023/ABC321/C.cs
+++ CSharp/Contests2023/ABC321/C.cs
@@ -4,18 +4,22 @@ using System.Linq;
 
 class C
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var k = int.Parse(Console.ReadLine());
 
-		return string.Join(" ", a);
+		var r = Enumerable.Range(1, 1023)
+			.Select(x => ToElements(x).Select(i => (char)('0' + i)).ToArray())
+			.Select(cs => long.Parse(new string(cs)))
+			.OrderBy(x => x)
+			.ToArray();
+		return r[k];
+	}
+
+	static IEnumerable<int> ToElements(int x)
+	{
+		for (int i = 10; --i >= 0;)
+			if ((x & (1 << i)) != 0) yield return i;
 	}
 }
--- CSharp/Contests2023/ABC321/D.cs
+++ CSharp/Contests2023/ABC321/D.cs
@@ -1,21 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CoderLib8.Linq;
+using Num = System.Int64;
 
 class D
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m, p) = ReadL().ToTuple3<int, int, long>();
+		var a = ReadL();
+		var b = ReadL();
 
-		return string.Join(" ", a);
+		Array.Sort(a);
+		var rsq = new StaticRSQ1(a);
+
+		return b.Sum(bx =>
+		{
+			var ai = a.FirstIndexByBS(ax => ax >= p - bx);
+			return rsq.GetSum(0, ai) + ai * bx + (n - ai) * p;
+		});
+	}
+}
+
+public class StaticRSQ1
+{
+	int n;
+	long[] s;
+	public long[] Raw => s;
+	public StaticRSQ1(long[] a)
+	{
+		n = a.Length;
+		s = new long[n + 1];
+		for (int i = 0; i < n; ++i) s[i + 1] = s[i] + a[i];
+	}
+
+	// [l, r)
+	// 範囲外のインデックスも可。
+	public long GetSum(int l, int r)
+	{
+		if (r < 0 || n < l) return 0;
+		if (l < 0) l = 0;
+		if (n < r) r = n;
+		return s[r] - s[l];
 	}
 }
