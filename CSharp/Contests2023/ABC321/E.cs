cef85d3fe17f24324d12132c4dc77e5ba2c32cd6
Keiho Sakapon <git@saka-pon.net>
2023-11-17 22:10:00 +0900

    AC

--- CSharp/Contests2023/ABC321/E.cs
+++ CSharp/Contests2023/ABC321/E.cs
@@ -1,21 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using Range = System.ValueTuple<long, long>;
 
 class E
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static (long, long, long) Read3L() { var a = ReadL(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new bool[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, x, kl) = Read3L();
 
-		return string.Join(" ", a);
+		if (kl > 1 << 8) return 0;
+		var k = (int)kl;
+		if (k == 0) return 1;
+
+		var rn = new Range(1, n + 1);
+		var s = 0L;
+
+		// 頂点 n は dn 段目
+		var dn = 0;
+		for (int i = 0; i < 60; i++)
+			if ((n & (1L << i)) != 0) dn = i;
+
+		// 頂点 x は dx 段目
+		var dx = 0;
+		for (int i = 0; i < 60; i++)
+			if ((x & (1L << i)) != 0) dx = i;
+
+		for (int i = 0; i < dn; i++)
+		{
+			if (dx - k <= i && i <= dx + k)
+			{
+				if (i == dx - k)
+				{
+					s += 1;
+				}
+				else if (i == dx + k)
+				{
+					s += 1L << k;
+				}
+				else if ((dx + i - k) % 2 == 0)
+				{
+					var p = (dx + i - k) / 2;
+					if (p >= 0)
+					{
+						s += 1L << (i - p - 1);
+					}
+				}
+			}
+		}
+
+		if (dx - k <= dn && dn <= dx + k)
+		{
+			if (dn == dx + k)
+			{
+				s += SubtreeCount(x, k);
+			}
+			else if ((dx + dn - k) % 2 == 0)
+			{
+				var p = (dx + dn - k) / 2;
+				if (p >= 0)
+				{
+					var pv1 = x >> (dx - p - 1);
+					if ((pv1 & 1) == 0) pv1++;
+					else pv1--;
+					s += SubtreeCount(pv1, dn - p - 1);
+				}
+			}
+		}
+
+		return s;
+
+		// v からの距離が d の子孫の個数
+		long SubtreeCount(long v, int d)
+		{
+			var l = v << d;
+			var r = (v + 1) << d;
+			(l, r) = Intersect(rn, new Range(l, r));
+			return r - l;
+		}
+	}
+
+	static Range Intersect(Range r1, Range r2)
+	{
+		var l = Math.Max(r1.Item1, r2.Item1);
+		var r = Math.Min(r1.Item2, r2.Item2);
+		if (l >= r) return new Range(0, 0);
+		return new Range(l, r);
 	}
 }
--- CSharp/Contests2023/ABC321/F.cs
+++ CSharp/Contests2023/ABC321/F.cs
@@ -1,21 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (qc, k) = Read2();
+		var qs = Array.ConvertAll(new bool[qc], _ => Console.ReadLine().Split());
 
-		return string.Join(" ", a);
+		var r = new List<long>();
+		var dp = new long[k + 1];
+		dp[0] = 1;
+
+		foreach (var q in qs)
+		{
+			var x = int.Parse(q[1]);
+
+			if (q[0] == "+")
+			{
+				for (int i = k - 1; i >= 0; i--)
+				{
+					if (dp[i] == 0) continue;
+					var ni = i + x;
+					if (ni > k) continue;
+					dp[ni] += dp[i];
+					dp[ni] %= M;
+				}
+			}
+			else
+			{
+				for (int i = 0; i < k; i++)
+				{
+					if (dp[i] == 0) continue;
+					var ni = i + x;
+					if (ni > k) continue;
+					dp[ni] += M - dp[i];
+					dp[ni] %= M;
+				}
+			}
+
+			r.Add(dp[k]);
+		}
+
+		return string.Join("\n", r);
 	}
+
+	const long M = 998244353;
 }
