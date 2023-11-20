e4cf7ebe7dc30b143b90b99754c44a087cb89a72
Keiho Sakapon <git@saka-pon.net>
2023-11-20 11:20:49 +0900

    Update code

--- CSharp/Contests2023/ABC321/E.cs
+++ CSharp/Contests2023/ABC321/E.cs
@@ -11,9 +11,9 @@ class E
 	{
 		var (n, x, kl) = Read3L();
 
+		if (kl == 0) return 1;
 		if (kl > 1 << 8) return 0;
 		var k = (int)kl;
-		if (k == 0) return 1;
 
 		var rn = new Range(1, n + 1);
 		var s = 0L;
@@ -28,48 +28,31 @@ class E
 		for (int i = 0; i < 60; i++)
 			if ((x & (1L << i)) != 0) dx = i;
 
-		for (int i = 0; i < dn; i++)
-		{
-			if (dx - k <= i && i <= dx + k)
-			{
-				if (i == dx - k)
-				{
-					s += 1;
-				}
-				else if (i == dx + k)
-				{
-					s += 1L << k;
-				}
-				else if ((dx + i - k) % 2 == 0)
-				{
-					var p = (dx + i - k) / 2;
-					if (p >= 0)
-					{
-						s += 1L << (i - p - 1);
-					}
-				}
-			}
-		}
+		var d_min = Math.Max(dx - k, 0);
+		var d_max = Math.Min(dx + k, dn);
 
-		if (dx - k <= dn && dn <= dx + k)
+		for (int d = d_min; d <= d_max; d++)
 		{
-			if (dn == dx + k)
+			if ((dx + k - d) % 2 == 1) continue;
+
+			if (d == dx - k)
+			{
+				s += 1;
+			}
+			else if (d == dx + k)
 			{
 				s += SubtreeCount(x, k);
 			}
-			else if ((dx + dn - k) % 2 == 0)
+			else
 			{
-				var p = (dx + dn - k) / 2;
-				if (p >= 0)
-				{
-					var pv1 = x >> (dx - p - 1);
-					if ((pv1 & 1) == 0) pv1++;
-					else pv1--;
-					s += SubtreeCount(pv1, dn - p - 1);
-				}
+				var dp = (dx + d - k) / 2;
+				if (dp < 0) continue;
+				var pv1 = x >> (dx - dp - 1);
+				if ((pv1 & 1) == 0) pv1++;
+				else pv1--;
+				s += SubtreeCount(pv1, d - dp - 1);
 			}
 		}
-
 		return s;
 
 		// v からの距離が d の子孫の個数
