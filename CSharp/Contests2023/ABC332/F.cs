cd195bc1fc51fcb06310e3b1667988370d518484
Keiho Sakapon <git@saka-pon.net>
2023-12-11 07:52:31 +0900

    Initialize

--- CSharp/Contests2023/ABC332/F.cs
+++ CSharp/Contests2023/ABC332/F.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class F
+﻿class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
