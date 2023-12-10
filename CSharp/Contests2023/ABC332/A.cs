1a3ae15c70857aa17eab5a938577b3bdf6ef0a4d
Keiho Sakapon <git@saka-pon.net>
2023-12-10 23:15:00 +0900

    AC

--- CSharp/Contests2023/ABC332/A.cs
+++ CSharp/Contests2023/ABC332/A.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class A
+﻿class A
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int p, int q) Read2() { var a = Read(); return (a[0], a[1]); }
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, s, k) = Read3();
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
 
-		return string.Join(" ", a);
+		var r = ps.Sum(p => p.p * p.q);
+		if (r < s) r += k;
+		return r;
 	}
 }
