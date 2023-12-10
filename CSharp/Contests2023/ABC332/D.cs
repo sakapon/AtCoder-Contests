5f9df8b791fe57bc56dd911d77d2fcfbfb5db86a
Keiho Sakapon <git@saka-pon.net>
2023-12-10 23:48:39 +0900

    WA

--- CSharp/Contests2023/ABC332/D.cs
+++ CSharp/Contests2023/ABC332/D.cs
@@ -1,21 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class D
+﻿class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w) = Read2();
+		var a = Array.ConvertAll(new bool[h], _ => Read());
+		var b = Array.ConvertAll(new bool[h], _ => Read());
 
-		return string.Join(" ", a);
+		var r0 = SolveYoko();
+		if (r0 == -1) return -1;
+
+		a = Transpose(a);
+		b = Transpose(b);
+
+		var r1 = SolveYoko();
+		if (r1 == -1) return -1;
+
+		return r0 + r1;
+
+		int SolveYoko()
+		{
+			var aset = a.Select(c => string.Join(" ", c.OrderBy(x => x))).ToList();
+			var bset = b.Select(c => string.Join(" ", c.OrderBy(x => x))).ToArray();
+
+			var r = 0;
+			foreach (var s in bset)
+			{
+				var i = aset.IndexOf(s);
+				if (i == -1) return -1;
+				aset.RemoveAt(i);
+				r += i;
+			}
+			return r;
+		}
 	}
+
+	public static int[] GetColumn(int[][] m, int c) => Array.ConvertAll(m, r => r[c]);
+	public static int[][] Transpose(int[][] m) => m[0].Select((x, c) => GetColumn(m, c)).ToArray();
 }
