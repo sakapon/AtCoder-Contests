2c9414966d62c017dc9d0978d063ec01b00b55d4
Keiho Sakapon <git@saka-pon.net>
2023-12-16 20:14:18 +0900

    Update templates

--- CSharp/Contests2023/ABC000/A.cs
+++ CSharp/Contests2023/ABC000/A.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class A
+﻿class A
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
