caf5cb1cac682d570e73306fec0d92a7853d12a0
Keiho Sakapon <git@saka-pon.net>
2024-01-11 14:35:34 +0900

    Add methods

--- CSharp/CoderLib/CoderLib8/Collections/SeqArray.cs
+++ CSharp/CoderLib/CoderLib8/Collections/SeqArray.cs
@@ -19,12 +19,56 @@ namespace CoderLib8.Collections
 		public ArraySegment<T> this[int i] => new ArraySegment<T>(a, n2 * i, n2);
 		public T[] ToArray(int i) => a[(n2 * i)..(n2 * (i + 1))];
 
+		public T[] GetColumn(int j)
+		{
+			var r = new T[n1];
+			for (int i = 0; i < n1; ++i) r[i] = a[n2 * i + j];
+			return r;
+		}
+		public IEnumerable<T[]> GetColumns() { for (int j = 0; j < n2; ++j) yield return GetColumn(j); }
+
 		public void Fill(int i, T v) => Array.Fill(a, v, n2 * i, n2);
 		public void Fill(T v) => Array.Fill(a, v);
 		public void Clear() => Array.Clear(a, 0, a.Length);
 
 		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
 		public IEnumerator<ArraySegment<T>> GetEnumerator() { for (int i = 0; i < n1; ++i) yield return this[i]; }
+
+		public bool IsInRange(int i, int j) => 0 <= i && i < n1 && 0 <= j && j < n2;
+
+		public SeqArray2<T> Rotate180()
+		{
+			var b = (T[])a.Clone();
+			Array.Reverse(b);
+			return new SeqArray2<T>(n1, n2, b);
+		}
+
+		public SeqArray2<T> RotateLeft()
+		{
+			var r = new SeqArray2<T>(n2, n1);
+			for (int i = 0; i < n2; ++i)
+				for (int j = 0; j < n1; ++j)
+					r[i, j] = this[j, n2 - 1 - i];
+			return r;
+		}
+
+		public SeqArray2<T> RotateRight()
+		{
+			var r = new SeqArray2<T>(n2, n1);
+			for (int i = 0; i < n2; ++i)
+				for (int j = 0; j < n1; ++j)
+					r[i, j] = this[n1 - 1 - j, i];
+			return r;
+		}
+
+		public SeqArray2<T> Transpose()
+		{
+			var r = new SeqArray2<T>(n2, n1);
+			for (int i = 0; i < n2; ++i)
+				for (int j = 0; j < n1; ++j)
+					r[i, j] = this[j, i];
+			return r;
+		}
 	}
 
 	// 1 次元配列に 3 次元インデックスでアクセスします。
