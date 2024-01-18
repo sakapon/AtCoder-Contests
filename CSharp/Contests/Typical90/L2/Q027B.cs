9883e8d35dd2b3c59d0166eacd54c835b3e93863
Keiho Sakapon <git@saka-pon.net>
2024-01-18 23:03:02 +0900

    Other AC

---
+++ CSharp/Contests/Typical90/L2/Q027B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Q027B
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+
+		const int f = (1 << 18) - 1;
+		var map = Array.ConvertAll(new bool[f + 1], _ => new List<string>());
+		var r = new List<int>();
+
+		for (int i = 1; i <= n; i++)
+		{
+			var s = Console.ReadLine();
+			var l = map[Hash(s) & f];
+			if (l.Contains(s)) continue;
+			l.Add(s);
+			r.Add(i);
+		}
+		return string.Join("\n", r);
+	}
+
+	static int Hash(string s)
+	{
+		var h = 0;
+		foreach (var c in s) h = h * 987654323 + c;
+		return h;
+	}
+}
