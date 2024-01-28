3886ff9f995f9821cb3b03908d6e682e0e6e5848
Keiho Sakapon <git@saka-pon.net>
2024-01-28 11:00:51 +0900

    AC

--- CSharp/Contests2024/ABC338/E.cs
+++ CSharp/Contests2024/ABC338/E.cs
@@ -1,17 +1,38 @@
 ﻿class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static (int a, int b) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
 
-		return string.Join(" ", a);
+		var l = Enumerable.Range(1, 2 * n - 1).Prepend(2 * n).Prepend(-1).ToArray();
+		var r = Enumerable.Range(2, 2 * n - 1).Append(1).Prepend(-1).ToArray();
+
+		void Remove(int i)
+		{
+			var li = l[i];
+			var ri = r[i];
+			r[li] = ri;
+			l[ri] = li;
+		}
+
+		int Dist(int a, int b)
+		{
+			var d = Math.Abs(a - b);
+			return Math.Min(d, 2 * n - d);
+		}
+
+		var qs = ps.OrderBy(p => Dist(p.a, p.b));
+		foreach (var (a, b) in qs)
+		{
+			if (!(r[a] == b || l[a] == b)) return true;
+
+			Remove(a);
+			Remove(b);
+		}
+		return false;
 	}
 }
