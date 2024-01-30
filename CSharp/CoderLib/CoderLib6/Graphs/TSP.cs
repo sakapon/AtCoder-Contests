4a2111f54892f747b5aa0160f7f11dd29561e8a4
Keiho Sakapon <git@saka-pon.net>
2024-01-30 19:44:29 +0900

    Add library

---
+++ CSharp/CoderLib/CoderLib6/Graphs/TSP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Test: https://atcoder.jp/contests/abc180/tasks/abc180_e
+namespace CoderLib6.Graphs
+{
+	public static class TSP
+	{
+		public static T[] NewArray1<T>(int n, T v = default(T)) => Array.ConvertAll(new bool[n], _ => v);
+		public static T[][] NewArray2<T>(int n1, int n2, T v = default(T)) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+
+		public static long[][] Execute(int n, int sv, long[][] d)
+		{
+			// i: 訪問済の頂点集合
+			// j: 最後の頂点
+			var dp = NewArray2(1 << n, n, long.MaxValue);
+			dp[1U << sv][sv] = 0;
+
+			for (uint x = 0; x < 1U << n; ++x)
+			{
+				for (int j = 0; j < n; ++j)
+				{
+					if (dp[x][j] == long.MaxValue) continue;
+
+					for (int nj = 0; nj < n; ++nj)
+					{
+						var nx = x | (1U << nj);
+						if (nx == x) continue;
+						if (d[j][nj] == long.MaxValue) continue;
+
+						var nv = dp[x][j] + d[j][nj];
+						if (dp[nx][nj] > nv) dp[nx][nj] = nv;
+					}
+				}
+			}
+			return dp;
+		}
+	}
+}
--- CSharp/Contests2020/ABC180/E.cs
+++ CSharp/Contests2020/ABC180/E.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using CoderLib6.Graphs;
 
 class E
 {
@@ -22,37 +21,6 @@ class E
 		}
 
 		var dp = TSP.Execute(n, 0, d);
-		return Enumerable.Range(1, n - 1).Min(v => dp[(1 << n) - 1][v] + d[v][0]);
-	}
-}
-
-public static class TSP
-{
-	public static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
-
-	public static long[][] Execute(int n, int sv, long[][] d)
-	{
-		// i: 訪問済の頂点集合
-		// j: 最後の頂点
-		var dp = NewArray2(1 << n, n, long.MaxValue);
-		dp[1 << sv][sv] = 0;
-
-		for (uint x = 0; x < 1U << n; ++x)
-		{
-			for (int j = 0; j < n; ++j)
-			{
-				if (dp[x][j] == long.MaxValue) continue;
-
-				for (int nj = 0; nj < n; ++nj)
-				{
-					var nx = x | (1U << nj);
-					if (nx == x) continue;
-					if (d[j][nj] == long.MaxValue) continue;
-					var nv = dp[x][j] + d[j][nj];
-					if (dp[nx][nj] > nv) dp[nx][nj] = nv;
-				}
-			}
-		}
-		return dp;
+		return Enumerable.Range(1, n - 1).Min(v => dp[^1][v] + d[v][0]);
 	}
 }
