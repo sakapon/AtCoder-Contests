c06366fcafb7dbaffd7fed089630c62ea246d645
Keiho Sakapon <git@saka-pon.net>
2024-01-30 23:05:03 +0900

    AC

---
+++ CSharp/AOJCourses/DPL002/A2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CoderLib6.Graphs;
+
+class A2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var h = Read();
+		int n = h[0];
+		var es = Array.ConvertAll(new int[h[1]], _ => Read());
+
+		var d = TSP.NewArray2(n, n, long.MaxValue);
+		foreach (var e in es)
+			d[e[0]][e[1]] = e[2];
+
+		var sv = 0;
+		var dp = TSP.Execute(n, sv, d);
+		var r = dp[(1 << n) - 1][sv];
+		if (r == long.MaxValue) return -1;
+		return r;
+	}
+}
---
+++ CSharp/AOJCourses/DPL002/TSP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Test: https://atcoder.jp/contests/abc180/tasks/abc180_e
+namespace CoderLib6.Graphs
+{
+	public static class TSP
+	{
+		public static T[] NewArray1<T>(int n, T v = default(T)) => Array.ConvertAll(new bool[n], _ => v);
+		public static T[][] NewArray2<T>(int n1, int n2, T v = default(T)) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+
+		public static long[][] Execute(int n, int sv, long[][] d)
+		{
+			// i: 訪問済の頂点集合
+			// j: 最後の頂点
+			var dp = NewArray2(1 << n, n, long.MaxValue);
+			dp[0][sv] = 0;
+
+			for (uint x = 0; x < 1U << n; ++x)
+			{
+				for (int v = 0; v < n; ++v)
+				{
+					if (dp[x][v] == long.MaxValue) continue;
+
+					for (int nv = 0; nv < n; ++nv)
+					{
+						var nx = x | (1U << nv);
+						if (nx == x) continue;
+						if (d[v][nv] == long.MaxValue) continue;
+
+						var nc = dp[x][v] + d[v][nv];
+						if (dp[nx][nv] <= nc) continue;
+						dp[nx][nv] = nc;
+					}
+				}
+			}
+			return dp;
+		}
+	}
+}
