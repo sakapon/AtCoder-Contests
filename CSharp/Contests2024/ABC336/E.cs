76e16514f9dfa29742b32d9067b9cf82a998c022
Keiho Sakapon <git@saka-pon.net>
2024-02-03 01:07:46 +0900

    AC

--- CSharp/Contests2024/ABC336/E.cs
+++ CSharp/Contests2024/ABC336/E.cs
@@ -1,17 +1,66 @@
-﻿class E
+﻿using CoderLib8.Values;
+
+class E
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var s = Console.ReadLine().Select(c => c - '0').ToArray();
+		var n = s.Length;
 
-		return string.Join(" ", a);
+		const int smax = 130;
+
+		// i: 桁数
+		// j: 桁和
+		// k: mod m
+		// 値: 個数
+
+		// 同じ
+		var dp0 = new SeqArray2<long>(smax + 1, smax);
+		var dt0 = new SeqArray2<long>(smax + 1, smax);
+		// 未満
+		var dp1 = new SeqArray2<long>(smax + 1, smax);
+		var dt1 = new SeqArray2<long>(smax + 1, smax);
+
+		return Enumerable.Range(1, smax).Sum(ForSum);
+
+		// m: 桁和
+		long ForSum(int m)
+		{
+			dp0.Clear();
+			dp1.Clear();
+			dp0[0, 0] = 1;
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < smax; j++)
+				{
+					for (int k = 0; k < m; k++)
+					{
+						if (dp0[j, k] == 0 && dp1[j, k] == 0) continue;
+
+						for (int d = 0; d < 10; d++)
+						{
+							var nj = j + d;
+							if (nj >= smax) continue;
+							var nk = (10 * k + d) % m;
+
+							dt1[nj, nk] += dp1[j, k];
+
+							if (d == s[i])
+								dt0[nj, nk] += dp0[j, k];
+							else if (d < s[i])
+								dt1[nj, nk] += dp0[j, k];
+						}
+					}
+				}
+
+				(dp0, dt0) = (dt0, dp0);
+				(dp1, dt1) = (dt1, dp1);
+				dt0.Clear();
+				dt1.Clear();
+			}
+			return dp0[m, 0] + dp1[m, 0];
+		}
 	}
 }
