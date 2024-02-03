846c55f05518dd8e115694551c361aac64840cbb
Keiho Sakapon <git@saka-pon.net>
2024-02-03 20:56:18 +0900

    AC

--- CSharp/Contests2023/ABC328/F.cs
+++ CSharp/Contests2023/ABC328/F.cs
@@ -6,16 +6,49 @@ class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var ps = Array.ConvertAll(new bool[m], _ => Read3());
 
-		return string.Join(" ", a);
+		var rn = Enumerable.Range(0, n + 1).ToArray();
+		var gs = rn.Select(i => new List<int> { i }).ToArray();
+		var z = new long[n + 1];
+		var r = new List<int>();
+
+		for (int i = 0; i < m; i++)
+		{
+			var (a, b, d) = ps[i];
+
+			if (gs[a] == gs[b])
+			{
+				if (z[a] - z[b] == d)
+					r.Add(i + 1);
+			}
+			else
+			{
+				r.Add(i + 1);
+
+				if (gs[a].Count < gs[b].Count)
+				{
+					(a, b) = (b, a);
+					d = -d;
+				}
+				var l = gs[a];
+				var t = gs[b];
+				var v0 = z[a] - z[b] - d;
+
+				foreach (var j in t)
+				{
+					l.Add(j);
+					gs[j] = l;
+					z[j] += v0;
+				}
+			}
+		}
+
+		return string.Join(" ", r);
 	}
 }
