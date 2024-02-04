6a6dc20b7d09be4f9f1796828e2162964c15a34a
Keiho Sakapon <git@saka-pon.net>
2024-02-05 00:47:00 +0900

    AC

--- CSharp/Contests2021/ABC199/E.cs
+++ CSharp/Contests2021/ABC199/E.cs
@@ -1,17 +1,40 @@
-﻿class E
+﻿using System.Numerics;
+
+class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var ps = Array.ConvertAll(new bool[m], _ => Read3());
 
-		return string.Join(" ", a);
+		// 数列 a の先頭が集合 s
+		var dp = new long[1 << n];
+		dp[0] = 1;
+
+		for (uint s = 0; s < 1U << n; s++)
+		{
+			var c = BitOperations.PopCount(s) + 1;
+
+			for (int i = 0; i < n; i++)
+			{
+				var ns = s | (1U << i);
+				if (ns == s) continue;
+
+				var ok = true;
+				foreach (var (x, y, z) in ps)
+				{
+					if (c > x) continue;
+					if (BitOperations.PopCount(ns & ((1U << y) - 1)) > z) { ok = false; break; }
+				}
+
+				if (!ok) continue;
+				dp[ns] += dp[s];
+			}
+		}
+		return dp[^1];
 	}
 }
