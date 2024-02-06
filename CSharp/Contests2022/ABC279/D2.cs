e52ea4369f734fd67ea451e02ba75fb1a06559a9
Keiho Sakapon <git@saka-pon.net>
2024-02-06 11:23:56 +0900

    AC

---
+++ CSharp/Contests2022/ABC279/D2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class D2
+{
+	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (a, b) = Read2L();
+
+		var c = First(0, 1L << 60, x => GetTime(x + 1) - GetTime(x) >= 0);
+		return GetTime(c);
+
+		double GetTime(long count)
+		{
+			return b * (double)count + a / Math.Sqrt(1 + count);
+		}
+	}
+
+	static long First(long l, long r, Func<long, bool> f)
+	{
+		long m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
+	}
+}
