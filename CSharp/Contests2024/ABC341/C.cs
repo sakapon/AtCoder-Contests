1ad01012118c8b0ad58322923e0b10448b980a8b
Keiho Sakapon <git@saka-pon.net>
2024-02-24 16:20:07 +0900

    AC

--- CSharp/Contests2024/ABC341/C.cs
+++ CSharp/Contests2024/ABC341/C.cs
@@ -1,17 +1,29 @@
 ﻿class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w, n) = Read3();
+		var t = Console.ReadLine();
+		var s = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
 
-		return string.Join(" ", a);
+		var delta = new int[1 << 7];
+		delta['L'] = -1;
+		delta['R'] = 1;
+		delta['U'] = -w;
+		delta['D'] = w;
+
+		var ss = s.SelectMany(c => c).ToArray();
+		return Enumerable.Range(0, h * w).Count(Check);
+
+		bool Check(int v)
+		{
+			if (ss[v] == '#') return false;
+			foreach (var c in t)
+				if (ss[v += delta[c]] == '#') return false;
+			return true;
+		}
 	}
 }
