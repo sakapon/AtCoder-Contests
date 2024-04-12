2e43aed8e3ecfcc577248c58bd652cd90246e08d
Keiho Sakapon <git@saka-pon.net>
2024-04-13 05:26:49 +0900

    AC

--- CSharp/Contests2024/ABC348/D.cs
+++ CSharp/Contests2024/ABC348/D.cs
@@ -1,17 +1,43 @@
-﻿class D
+﻿using Bang.Graphs.Int.SPPs.Unweighted.v1_0_2;
+
+class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static (int r, int c, int e) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+	static bool Solve()
 	{
+		var (h, w) = Read2();
+		var s = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => Read3());
 
-		return string.Join(" ", a);
+		var rn = Enumerable.Range(0, n).ToArray();
+
+		var grid = new CharUnweightedGrid(s);
+		var sv = grid.FindVertexId('S');
+		var ev = grid.FindVertexId('T');
+
+		var ps2 = ps.Select(p => (v: grid.ToVertexId(p.r - 1, p.c - 1), p.e)).ToArray();
+		var medIndexes = rn.ToDictionary(i => ps2[i].v);
+
+		if (!medIndexes.ContainsKey(sv)) return false;
+		var si = medIndexes[sv];
+		var ei = n;
+
+		var map = rn.Select(GetMedDest).Append(new List<int>()).ToArray();
+		var graph = new ListUnweightedGraph(map);
+		return graph.ConnectivityByDFS(si, ei)[ei];
+
+		List<int> GetMedDest(int mi)
+		{
+			var d = grid.ShortestByBFS(ps2[mi].v);
+			var e = ps[mi].e;
+
+			var l = rn.Where(mj => d[ps2[mj].v] <= e).ToList();
+			if (d[ev] <= e) l.Add(ei);
+			return l;
+		}
 	}
 }
--- CSharp/Contests2024/ABC348/E.cs
+++ CSharp/Contests2024/ABC348/E.cs
@@ -1,17 +1,59 @@
-﻿class E
+﻿static class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var es = Array.ConvertAll(new bool[n - 1], _ => Read());
+		var c = ReadL().Prepend(0).ToArray();
 
-		return string.Join(" ", a);
+		var map = ToMap(n + 1, es, true);
+		var r = new long[n + 1];
+
+		var rv = 1;
+		DFS1(rv, -1);
+		foreach (var v in map[rv])
+		{
+			DFS2(v, rv, c[rv] - c[v], r[rv] - r[v] - c[v]);
+		}
+		return r[1..].Min();
+
+		void DFS1(int v, int pv)
+		{
+			foreach (var nv in map[v])
+			{
+				if (nv == pv) continue;
+				DFS1(nv, v);
+				c[v] += c[nv];
+				r[v] += r[nv] + c[nv];
+			}
+		}
+
+		void DFS2(int v, int pv, long s1, long s2)
+		{
+			var t = r[v];
+			r[v] += s1 + s2;
+
+			foreach (var nv in map[v])
+			{
+				if (nv == pv) continue;
+				DFS2(nv, v, s1 + c[v] - c[nv], s1 + s2 + t - r[nv] - c[nv]);
+			}
+		}
+	}
+
+	public static T[][] ToArrays<T>(this List<T>[] map) => Array.ConvertAll(map, l => l.ToArray());
+	public static int[][] ToMap(int n, int[][] es, bool twoWay) => ToListMap(n, es, twoWay).ToArrays();
+	public static List<int>[] ToListMap(int n, int[][] es, bool twoWay)
+	{
+		var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		foreach (var e in es)
+		{
+			map[e[0]].Add(e[1]);
+			if (twoWay) map[e[1]].Add(e[0]);
+		}
+		return map;
 	}
 }
