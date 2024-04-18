52e2619013299b7c4d76b595f02c623483e2c2b5
Keiho Sakapon <git@saka-pon.net>
2024-04-18 10:52:40 +0900

    AC (using DP)

---
+++ CSharp/Contests2019/ABC135/D2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class D2
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var s = Console.ReadLine();
+
+		var dp = new long[D];
+		var dt = new long[D];
+		dp[0] = 1;
+
+		foreach (var c in s)
+		{
+			if (c == '?')
+			{
+				for (int j = 0; j < D; j++)
+				{
+					for (int x = 0; x < 10; x++)
+					{
+						var nj = (j * 10 + x) % D;
+						dt[nj] += dp[j];
+					}
+				}
+			}
+			else
+			{
+				var v = c - '0';
+
+				for (int j = 0; j < D; j++)
+				{
+					var nj = (j * 10 + v) % D;
+					dt[nj] += dp[j];
+				}
+			}
+
+			for (int j = 0; j < D; j++)
+			{
+				dt[j] %= M;
+			}
+
+			(dp, dt) = (dt, dp);
+			Array.Clear(dt, 0, dt.Length);
+		}
+
+		return dp[5];
+	}
+
+	const int D = 13;
+	const long M = 1000000007;
+}
