c8f3a890553febb123c076d06de03f037c997cd5
Keiho Sakapon <git@saka-pon.net>
2024-04-21 23:54:16 +0900

    AC

--- CSharp/Contests2024/ABC350/F.cs
+++ CSharp/Contests2024/ABC350/F.cs
@@ -1,17 +1,82 @@
-﻿class F
+﻿using System.Text;
+
+class F
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var a = s.Split('(', ')');
+		a = Enumerable.Range(0, a.Length).Select(i => i % 2 == 0 ? a[i] : Transpose(a[i])).ToArray();
+		var q = new Queue<string>(a);
+
+		var parens = s.Where(c => !char.IsLetter(c)).ToArray();
+
+		var n = parens.Length + parens.Count(c => c == '(') + 2;
+		var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		var depths = new int[n];
+		Array.Fill(depths, -1);
+		var texts = new string[n];
+
+		var t = 0;
+		var ps = new Stack<int>();
+		ps.Push(t);
+		depths[t] = 0;
+
+		foreach (var c in parens)
+		{
+			var p = ps.Peek();
+			map[p].Add(++t);
+			texts[t] = q.Dequeue();
+
+			if (c == '(')
+			{
+				map[p].Add(++t);
+				ps.Push(t);
+				depths[t] = depths[p] + 1;
+			}
+			else
+			{
+				ps.Pop();
+			}
+		}
+		map[0].Add(++t);
+		texts[t] = q.Dequeue();
+
+		for (int v = 0; v < n; v++)
+		{
+			if (depths[v] % 2 == 1)
+			{
+				map[v].Reverse();
+			}
+		}
+
+		var sb = new StringBuilder();
+		DFS(0);
+		return sb;
+
+		void DFS(int v)
+		{
+			if (texts[v] == null)
+			{
+				foreach (var nv in map[v])
+				{
+					DFS(nv);
+				}
+			}
+			else
+			{
+				sb.Append(texts[v]);
+			}
+		}
+	}
+
+	static string Transpose(string s)
+	{
+		var a = s.ToArray();
+		Array.Reverse(a);
+		a = Array.ConvertAll(a, c => char.IsLower(c) ? char.ToUpper(c) : char.ToLower(c));
+		return new string(a);
 	}
 }
