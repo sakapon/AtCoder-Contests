29f196c3b4f43ef4cb785a84930735bef1b6297f
Keiho Sakapon <git@saka-pon.net>
2024-04-24 16:52:43 +0900

    Update code

--- CSharp/AOJCourses/ALDS110/A3.cs
+++ CSharp/AOJCourses/ALDS110/A3.cs
@@ -6,22 +6,13 @@ class A3
 	{
 		var n = int.Parse(Console.ReadLine());
 
-		dp = new long[n + 1];
 		Console.WriteLine(Rec(n));
 	}
 
 	// TLE
-	static long[] dp;
 	static long Rec(int n)
 	{
 		if (n <= 1) return 1;
-		return dp[n] = Rec(n - 1) + Rec(n - 2);
-	}
-
-	// TLE
-	static long Rec0(int n)
-	{
-		if (n <= 1) return 1;
-		return Rec0(n - 1) + Rec0(n - 2);
+		return Rec(n - 1) + Rec(n - 2);
 	}
 }
