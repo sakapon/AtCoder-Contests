1782ea7a4a660d614a5938623f7b6cce2d80f767
Keiho Sakapon <git@saka-pon.net>
2024-04-24 23:59:37 +0900

    AC

---
+++ CSharp/Contests/DP/D2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class D2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, W) = Read2();
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
+
+		var dp = new long[W + 1];
+		Array.Fill(dp, -1);
+		dp[0] = 0;
+
+		foreach (var (w, v) in ps)
+		{
+			for (int i = W - w; i >= 0; i--)
+			{
+				if (dp[i] == -1) continue;
+				Chmax(ref dp[i + w], dp[i] + v);
+			}
+		}
+		return dp.Max();
+	}
+
+	public static long Chmax(ref long x, long v) => x < v ? x = v : x;
+}
