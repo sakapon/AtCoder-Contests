796bb7a54dadbb26a45fa24142e5b1b9016cb315
sakapon <git@saka-pon.net>
2024-05-02 16:34:04 +0900

    AC

---
+++ CSharp/Contests2019/ABC126/D2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class D2
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var es = Array.ConvertAll(new bool[n - 1], _ => Read3());
+
+		var map = Array.ConvertAll(new bool[n + 1], _ => new List<(int to, bool even)>());
+		foreach (var (u, v, w) in es)
+		{
+			map[u].Add((v, w % 2 == 0));
+			map[v].Add((u, w % 2 == 0));
+		}
+
+		var dp = new int[n + 1];
+		DFS(1, -1);
+		return string.Join("\n", dp[1..]);
+
+		void DFS(int v, int pv)
+		{
+			foreach (var (nv, b) in map[v])
+			{
+				if (nv == pv) continue;
+				dp[nv] = b ? dp[v] : 1 - dp[v];
+				DFS(nv, v);
+			}
+		}
+	}
+}
---
+++ CSharp/Contests2023/ABC309/EDP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class EDP
+{
+	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var (n, m) = Read2();
+		var p = Read();
+		var xys = Array.ConvertAll(new bool[m], _ => Read2());
+
+		var map = Array.ConvertAll(new bool[n + 1], _ => new List<int>());
+		for (int v = 2; v <= n; v++)
+		{
+			map[p[v - 2]].Add(v);
+		}
+
+		var dp = new int[n + 1];
+		Array.Fill(dp, -1);
+		foreach (var (x, y) in xys)
+		{
+			Chmax(ref dp[x], y);
+		}
+
+		DFS(1);
+		return dp.Count(x => x >= 0);
+
+		void DFS(int v)
+		{
+			foreach (var nv in map[v])
+			{
+				Chmax(ref dp[nv], dp[v] - 1);
+				DFS(nv);
+			}
+		}
+	}
+
+	static int Chmax(ref int x, int v) => x < v ? x = v : x;
+}
