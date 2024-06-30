e1a034d1bb600dcf28cffed6c727938f26663d8e
Keiho Sakapon <git@saka-pon.net>
2024-06-30 23:20:15 +0900

    AC

---
+++ CSharp/Contests2024/ABC360/ArrayItemSet201.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+// static item set
+// 配列による実装です。
+// 実用可能です。
+namespace CoderLib8.Collections.Statics.Typed
+{
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class ArrayItemSet<T> : IEnumerable<T>
+	{
+		#region Initialization
+		readonly int n;
+		readonly T[] a;
+		readonly T minItem, maxItem;
+		readonly IComparer<T> comparer;
+
+		// ソート済みであることを前提とします。
+		public ArrayItemSet(T[] items, T minItem = default, T maxItem = default, IComparer<T> comparer = null)
+		{
+			a = items ?? throw new ArgumentNullException(nameof(items));
+			n = a.Length;
+			this.minItem = minItem;
+			this.maxItem = maxItem;
+			this.comparer = comparer ?? Comparer<T>.Default;
+		}
+
+		public int Count => n;
+		public T[] Items => a;
+		public T MinItem => minItem;
+		public T MaxItem => maxItem;
+		public IComparer<T> Comparer => comparer;
+		#endregion
+
+		#region By Index
+		// 定義域 Z
+		public T GetAt(int index) => index < 0 ? minItem : index >= n ? maxItem : a[index];
+		#endregion
+
+		#region By Item Range
+
+		// 値域 (-1, n]
+		public int GetFirstIndexGeq(T item)
+		{
+			int m, l = 0, r = n;
+			while (l < r) if (comparer.Compare(a[m = l + r - 1 >> 1], item) >= 0) r = m; else l = m + 1;
+			return r;
+		}
+		// 値域 [-1, n)
+		public int GetLastIndexLeq(T item)
+		{
+			int m, l = -1, r = n - 1;
+			while (l < r) if (comparer.Compare(a[m = l + r + 1 >> 1], item) <= 0) l = m; else r = m - 1;
+			return l;
+		}
+
+		// secondary methods
+		public T GetFirstGeq(T item) => GetAt(GetFirstIndexGeq(item));
+		public T GetLastLeq(T item) => GetAt(GetLastIndexLeq(item));
+		public int GetCountGeq(T item) => n - GetFirstIndexGeq(item);
+		public int GetCountLeq(T item) => GetLastIndexLeq(item) + 1;
+		public int GetCount(T startItem, T endItem) => GetFirstIndexGeq(endItem) - GetFirstIndexGeq(startItem);
+		#endregion
+
+		#region By Item
+		public int GetFirstIndex(T item)
+		{
+			var i = GetFirstIndexGeq(item);
+			return i < n && comparer.Compare(a[i], item) == 0 ? i : -1;
+		}
+		public int GetLastIndex(T item)
+		{
+			var i = GetLastIndexLeq(item);
+			return i >= 0 && comparer.Compare(a[i], item) == 0 ? i : -1;
+		}
+
+		// secondary methods
+		public bool Contains(T item) => GetFirstIndex(item) != -1;
+		public int GetCount(T item) => GetLastIndexLeq(item) - GetFirstIndexGeq(item) + 1;
+		#endregion
+
+		#region Get Items
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+		public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)a).GetEnumerator();
+
+		public IEnumerable<T> GetItems(T startItem, T endItem)
+		{
+			for (var i = GetFirstIndexGeq(startItem); i < n && comparer.Compare(a[i], endItem) < 0; ++i) yield return a[i];
+		}
+
+		public IEnumerable<T> GetItemsByIndex(int startIndex, int endIndex)
+		{
+			for (var i = startIndex; i < endIndex; ++i) yield return a[i];
+		}
+		#endregion
+	}
+}
--- CSharp/Contests2024/ABC360/D.cs
+++ CSharp/Contests2024/ABC360/D.cs
@@ -1,17 +1,24 @@
-﻿class D
+﻿using CoderLib8.Collections.Statics.Typed;
+
+class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
+		var (n, t) = Read2L();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var x = ReadL();
 
-		return string.Join(" ", a);
+		var rn = Enumerable.Range(0, (int)n).ToArray();
+		var x0 = rn.Where(i => s[i] == '0').Select(i => x[i]).ToArray();
+		var x1 = rn.Where(i => s[i] == '1').Select(i => x[i]).ToArray();
+
+		Array.Sort(x0);
+		var set = new ArrayItemSet<long>(x0);
+		return x1.Sum(v => (long)set.GetCount(v + 1, v + 2 * t + 1));
 	}
 }
