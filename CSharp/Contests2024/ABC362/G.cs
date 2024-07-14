45252829204ee018a3d71da7868f1d75d7e758d0
Keiho Sakapon <git@saka-pon.net>
2024-07-14 11:16:12 +0900

    AC

--- CSharp/Contests2024/ABC362/G.cs
+++ CSharp/Contests2024/ABC362/G.cs
@@ -1,17 +1,89 @@
 ﻿class G
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var n = s.Length;
+		var qc = int.Parse(Console.ReadLine());
+		var qs = Array.ConvertAll(new bool[qc], _ => Console.ReadLine());
 
-		return string.Join(" ", a);
+		var sa = ManberMyers(s);
+		var cs = s.ToCharArray();
+		var c = StringComparison.Ordinal;
+
+		var r = qs.Select(t =>
+		{
+			var t2 = t + '~';
+			var l = First(0, n + 1, i => new ReadOnlySpan<char>(cs, sa[i], n - sa[i]).CompareTo(t, c) >= 0);
+			var r = First(0, n + 1, i => new ReadOnlySpan<char>(cs, sa[i], n - sa[i]).CompareTo(t2, c) >= 0);
+			return r - l;
+		});
+		return string.Join("\n", r);
+	}
+
+	static int First(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
+	}
+
+	static int[] ManberMyers(string s)
+	{
+		var n = s.Length;
+
+		// order -> index
+		var sa = new int[n + 1];
+		// index -> order
+		var rank = new int[n + 1];
+		for (int i = 0; i < n; ++i)
+		{
+			sa[i] = n - i;
+			rank[i] = s[i];
+		}
+		if (s.All(c => c == s[0])) return sa;
+
+		// Empty のランクを 0 とします。
+		// rank_k(i) と rank_k(i+k) から rank_2k(i) を作ります。
+		var k = 1;
+		Converter<int, long> toKey = i => rank[i] * 100000000L + rank[Math.Min(i + k, n)];
+		var keys = Array.ConvertAll(sa, toKey);
+
+		// k == 1
+		{
+			Array.Sort(keys, sa);
+
+			for (int i = 1; i <= n; ++i)
+				rank[sa[i]] = keys[i] == keys[i - 1] ? rank[sa[i - 1]] : i;
+		}
+
+		var next = true;
+		while (next && (k <<= 1) < n)
+		{
+			next = false;
+			for (int j = n; j >= 0; --j)
+			{
+				var start = rank[sa[j]];
+				if (start == j) continue;
+				var count = j - start + 1;
+
+				// ソートが完了していない部分のみソートします。
+				for (int i = start; i <= j; ++i)
+					keys[i] = toKey(sa[i]);
+				Array.Sort(keys, sa, start, count);
+
+				// rank の一部が早く更新されるため、while の回数が少なくなる可能性があります。
+				for (int i = start + 1; i <= j; ++i)
+				{
+					var eq = keys[i] == keys[i - 1];
+					rank[sa[i]] = eq ? rank[sa[i - 1]] : i;
+					if (eq) next = true;
+				}
+
+				j = start;
+			}
+		}
+		return sa;
 	}
 }
