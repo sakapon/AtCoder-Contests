014ae2f310dd9239ec2a7ab56589abfd8029d207
Keiho Sakapon <git@saka-pon.net>
2024-07-21 23:58:21 +0900

    AC

--- CSharp/Contests2024/ABC363/C.cs
+++ CSharp/Contests2024/ABC363/C.cs
@@ -2,16 +2,56 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, k) = Read2();
+		var s = Console.ReadLine().ToCharArray();
 
-		return string.Join(" ", a);
+		if (s.Distinct().Count() == n) return Enumerable.Range(1, n).Aggregate((x, y) => x * y);
+
+		var r = 0;
+		Array.Sort(s);
+
+		do
+		{
+			var ok = true;
+			for (int i = 0; i + k <= n; i++)
+			{
+				if (IsPalindrome(s, i, k))
+				{
+					ok = false;
+					break;
+				}
+			}
+			if (ok) r++;
+		}
+		while (NextPermutation(s));
+
+		return r;
+	}
+
+	public static bool NextPermutation(char[] p)
+	{
+		var n = p.Length;
+
+		// p[i] < p[i + 1] を満たす最大の i
+		var i = n - 2;
+		while (i >= 0 && p[i] >= p[i + 1]) --i;
+		if (i < 0) return false;
+
+		// p[i] < p[j] を満たす最大の j
+		var j = i + 1;
+		while (j + 1 < n && p[i] < p[j + 1]) ++j;
+
+		(p[i], p[j]) = (p[j], p[i]);
+		Array.Reverse(p, i + 1, n - i - 1);
+		return true;
+	}
+
+	static bool IsPalindrome(char[] s, int start, int count)
+	{
+		for (int i = 0; i < count; ++i) if (s[start + i] != s[start + count - 1 - i]) return false;
+		return true;
 	}
 }
--- CSharp/Contests2024/ABC363/F.cs
+++ CSharp/Contests2024/ABC363/F.cs
@@ -1,17 +1,48 @@
 ﻿class F
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var n = long.Parse(Console.ReadLine());
 
-		return string.Join(" ", a);
+		const string Invalid = "-1";
+		var d = new Dictionary<long, string>();
+		return Try(n);
+
+		string Try(long n)
+		{
+			if (d.ContainsKey(n)) return d[n];
+
+			var ns = n.ToString();
+			if (!ns.Contains('0') && IsPalindrome(ns)) return d[n] = ns;
+
+			for (int x = 2; x < 1000000; x++)
+			{
+				if (n % x != 0) continue;
+				var xs = x.ToString();
+				if (xs.Contains('0')) continue;
+
+				var cs = xs.ToCharArray();
+				Array.Reverse(cs);
+				var xs_ = new string(cs);
+				var x_ = int.Parse(xs_);
+
+				var n2 = n / x;
+				if (n2 % x_ != 0) continue;
+				n2 /= x_;
+
+				var r = Try(n2);
+				if (r == Invalid) continue;
+				return d[n] = $"{xs}*{r}*{xs_}";
+			}
+
+			return d[n] = Invalid;
+		}
+	}
+
+	static bool IsPalindrome(string s)
+	{
+		for (int i = 0; i < s.Length; ++i) if (s[i] != s[s.Length - 1 - i]) return false;
+		return true;
 	}
 }
