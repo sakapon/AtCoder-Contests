d2a0ad0fa26344a2cc302879fadda143b0dc952b
Keiho Sakapon <git@saka-pon.net>
2024-07-27 23:32:42 +0900

    AC

--- CSharp/Contests2024/ABC364/D.cs
+++ CSharp/Contests2024/ABC364/D.cs
@@ -1,17 +1,30 @@
-﻿class D
+﻿using CoderLib8.Collections.Statics.Typed;
+
+class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, qc) = Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		return string.Join(" ", a);
+		Array.Sort(a);
+		var set = new ArrayItemSet<int>(a);
+		return string.Join("\n", qs.Select(q => Query(q.Item1, q.Item2)));
+
+		int Query(int b, int k)
+		{
+			return First(0, 1 << 29, d => set.GetCount(b - d, b + d + 1) >= k);
+		}
+	}
+
+	static int First(int l, int r, Func<int, bool> f)
+	{
+		int m;
+		while (l < r) if (f(m = l + (r - l - 1) / 2)) r = m; else l = m + 1;
+		return r;
 	}
 }
--- CSharp/Contests2024/ABC364/E.cs
+++ CSharp/Contests2024/ABC364/E.cs
@@ -1,17 +1,37 @@
 ﻿class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int a, int b) Read2() { var a = Read(); return (a[0], a[1]); }
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, x, y) = Read3();
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
 
-		return string.Join(" ", a);
+		const int max = 1 << 30;
+
+		// i: 個数、j: 甘さ、値: しょっぱさ
+		var dp = NewArray2(n + 1, x + 1, max);
+		dp[0][0] = 0;
+
+		foreach (var (a, b) in ps)
+		{
+			for (int i = n - 1; i >= 0; i--)
+			{
+				for (int j = 0; j < x; j++)
+				{
+					var nj = j + a;
+					var nv = dp[i][j] + b;
+					if (nj > x) continue;
+					if (nv > y) continue;
+					Chmin(ref dp[i + 1][nj], nv);
+				}
+			}
+		}
+		return Math.Min(n, Enumerable.Range(0, n + 1).Last(i => dp[i].Any(v => v < max)) + 1);
 	}
+
+	static T[][] NewArray2<T>(int n1, int n2, T v = default) => Array.ConvertAll(new bool[n1], _ => Array.ConvertAll(new bool[n2], __ => v));
+	public static int Chmin(ref int x, int v) => x > v ? x = v : x;
 }
