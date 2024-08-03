d4140a486f6cb006affc595518b19f4b766c35ae
Keiho Sakapon <git@saka-pon.net>
2024-08-03 23:45:46 +0900

    AC

--- CSharp/Contests2024/ABC365/D.cs
+++ CSharp/Contests2024/ABC365/D.cs
@@ -1,17 +1,37 @@
 ﻿class D
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		// RPS : 012
+		const string RPS = "RPS";
 
-		return string.Join(" ", a);
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine().Select(c => RPS.IndexOf(c)).ToArray();
+
+		var dp = new int[3];
+		var dt = new int[3];
+
+		for (int i = 0; i < n; i++)
+		{
+			Array.Fill(dt, -1);
+
+			for (int j = 0; j < 2; j++)
+			{
+				var te = (s[i] + j) % 3;
+
+				for (int te0 = 0; te0 < 3; te0++)
+				{
+					if (te0 == te) continue;
+					if (dp[te0] == -1) continue;
+					Chmax(ref dt[te], dp[te0] + j);
+				}
+			}
+
+			(dp, dt) = (dt, dp);
+		}
+		return dp.Max();
 	}
+
+	public static int Chmax(ref int x, int v) => x < v ? x = v : x;
 }
--- CSharp/Contests2024/ABC365/E.cs
+++ CSharp/Contests2024/ABC365/E.cs
@@ -1,17 +1,33 @@
 ﻿class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var r = 0L;
+
+		for (int k = 0; k < 30; k++)
+		{
+			var f = 1 << k;
+
+			var b = a[0] & f;
+			var s = 0L;
+			for (int i = 1; i < n; i++)
+			{
+				b ^= a[i] & f;
+				s += b;
+			}
+
+			for (int i = 0; i < n - 1; i++)
+			{
+				r += s;
+				if ((a[i] & f) != 0) s = (long)(n - 1 - i) * f - s;
+				s -= a[i + 1] & f;
+			}
+		}
+		return r;
 	}
 }
