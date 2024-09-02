aee39a8004034318f52430d268aa4d7fdc40cea9
Keiho Sakapon <git@saka-pon.net>
2024-09-02 16:19:26 +0900

    AC

--- CSharp/Contests2024/ABC369/D2.cs
+++ CSharp/Contests2024/ABC369/D2.cs
@@ -1,4 +1,4 @@
-﻿class D
+﻿class D2
 {
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
@@ -7,15 +7,13 @@
 		var n = int.Parse(Console.ReadLine());
 		var a = ReadL();
 
-		var dp0 = new long[n + 1];
-		var dp1 = new long[n + 1];
-		dp1[0] = -1L << 60;
+		var dp0 = 0L;
+		var dp1 = -1L << 60;
 
 		for (int i = 0; i < n; i++)
 		{
-			dp0[i + 1] = Math.Max(dp0[i], dp1[i] + 2 * a[i]);
-			dp1[i + 1] = Math.Max(dp0[i] + a[i], dp1[i]);
+			(dp0, dp1) = (Math.Max(dp0, dp1 + 2 * a[i]), Math.Max(dp0 + a[i], dp1));
 		}
-		return Math.Max(dp0[n], dp1[n]);
+		return Math.Max(dp0, dp1);
 	}
 }
