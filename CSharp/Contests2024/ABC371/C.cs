5fd6a420c31233d3866bc9839e08275527322996
Keiho Sakapon <git@saka-pon.net>
2024-09-14 23:46:37 +0900

    AC

--- CSharp/Contests2024/ABC371/C.cs
+++ CSharp/Contests2024/ABC371/C.cs
@@ -1,17 +1,51 @@
 ﻿class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int u, int v) Read2() { var a = Read(); return (a[0] - 1, a[1] - 1); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var mg = int.Parse(Console.ReadLine());
+		var esg = Array.ConvertAll(new bool[mg], _ => Read2());
+		var mh = int.Parse(Console.ReadLine());
+		var esh = Array.ConvertAll(new bool[mh], _ => Read2());
+		var a = Enumerable.Range(0, n - 1).Select(i => new int[i + 1].Concat(Read()).ToArray()).ToArray();
 
-		return string.Join(" ", a);
+		var r = 1 << 30;
+		var p = new int[n];
+		for (int i = 0; i < n; ++i) p[i] = i;
+
+		do
+		{
+			var set = esg
+				.Select(e => (u: p[e.u], v: p[e.v]))
+				.Select(e => e.u < e.v ? e : (u: e.v, v: e.u))
+				.ToHashSet();
+			set.SymmetricExceptWith(esh);
+			var s = set.Sum(e => a[e.u][e.v]);
+			r = Math.Min(r, s);
+		}
+		while (NextPermutation(p));
+
+		return r;
+	}
+
+	public static bool NextPermutation(int[] p)
+	{
+		var n = p.Length;
+
+		// p[i] < p[i + 1] を満たす最大の i
+		var i = n - 2;
+		while (i >= 0 && p[i] >= p[i + 1]) --i;
+		if (i < 0) return false;
+
+		// p[i] < p[j] を満たす最大の j
+		var j = i + 1;
+		while (j + 1 < n && p[i] < p[j + 1]) ++j;
+
+		(p[i], p[j]) = (p[j], p[i]);
+		Array.Reverse(p, i + 1, n - i - 1);
+		return true;
 	}
 }
--- CSharp/Contests2024/ABC371/D.cs
+++ CSharp/Contests2024/ABC371/D.cs
@@ -1,17 +1,40 @@
-﻿class D
+﻿using AlgorithmLib10.SegTrees.SegTrees214;
+
+class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var x = Read();
+		var p = Read();
+		var qc = int.Parse(Console.ReadLine());
+		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		return string.Join(" ", a);
+		var rsq_ = new Int32RSQTree();
+		var rsq = new Int32RSQTree();
+
+		for (int i = 0; i < n; i++)
+		{
+			if (x[i] >= 0)
+			{
+				rsq.Add(x[i], p[i]);
+			}
+			else
+			{
+				rsq_.Add(-x[i], p[i]);
+			}
+		}
+
+		var res = qs.Select(q =>
+		{
+			var (l, r) = q;
+			return
+				(l < 0 ? rsq_[Math.Max(1, -r), -l + 1] : 0) +
+				(r >= 0 ? rsq[Math.Max(0, l), r + 1] : 0);
+		});
+		return string.Join("\n", res);
 	}
 }
