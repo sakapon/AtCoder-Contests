90798cbe1e727e4b89ec63be1c0ba862e15a3234
Keiho Sakapon <git@saka-pon.net>
2025-04-06 16:24:01 +0900

    Add sol

--- CSharp/Contests2025/AHC045/A121.cs
+++ CSharp/Contests2025/AHC045/A121.cs
@@ -3,6 +3,10 @@
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 
+	const int ThreadsCount = 1 << 4;
+	const double ShiftRate = 0.08;
+	const int Timeout = 1800;
+
 	static DateTime startTime;
 	public static double CurrentTime => (DateTime.Now - startTime).TotalMilliseconds;
 
@@ -13,7 +17,10 @@
 	static int[] rn0;
 	static (double x, double y)[] ps;
 	static int[,] dMatrix;
-	static PriorityQueue<(int, int), int> pq = new PriorityQueue<(int, int), int>();
+
+	static (double, double) ps_sum;
+	static (double, double) ps_avg;
+	static int[] orderedGroupIds;
 
 	class Group
 	{
@@ -45,36 +52,13 @@
 			}
 		}
 
-		var groups = new Group[m];
-		var u = new bool[n];
-		var pq = new PriorityQueue<(int, int), int>();
+		ps_sum = (ps.Sum(p => p.x), ps.Sum(p => p.y));
+		ps_avg = (ps_sum.Item1 / n, ps_sum.Item2 / n);
+		orderedGroupIds = Enumerable.Range(0, m).OrderBy(j => gcs[j]).ToArray();
 
-		var waitingIds = rn0.ToList();
-		var (px_sum, py_sum) = (ps.Sum(p => p.x), ps.Sum(p => p.y));
-		var (px_avg, py_avg) = (px_sum / n, py_sum / n);
-
-		double GetDistance2FromCenter(int id)
-		{
-			var (xi, yi) = ps[id];
-			xi -= px_avg; yi -= py_avg;
-			return xi * xi + yi * yi;
-		}
-
-		foreach (var gi in Enumerable.Range(0, m).OrderBy(j => gcs[j]))
-		{
-			var id0 = waitingIds.MaxBy(GetDistance2FromCenter);
-			var (d_sum, g) = CreateGroup(waitingIds, u, id0, gcs[gi]);
-			groups[gi] = g;
-
-			foreach (var id in g.CityIds)
-			{
-				px_sum -= ps[id].x; py_sum -= ps[id].y;
-			}
-			(px_avg, py_avg) = (px_sum / waitingIds.Count, py_sum / waitingIds.Count);
-		}
-
-		if (waitingIds.Count > 0) throw new InvalidOperationException();
-		if (!Array.TrueForAll(u, b => b)) throw new InvalidOperationException();
+		var results = new (int score, Group[] sol)[ThreadsCount];
+		Parallel.For(0, ThreadsCount, i => results[i] = Solve());
+		var (score, groups) = results.MinBy(p => p.score);
 
 #if !DEBUG
 		for (int gi = 0; gi < m; gi++)
@@ -91,6 +75,7 @@
 
 #if DEBUG
 		Console.WriteLine($"{(int)CurrentTime} ms");
+		Console.WriteLine(score);
 
 		var outFileName = $"{DateTime.Now:yyyyMMdd-HHmmss}.txt";
 		using var outWriter = File.CreateText(outFileName);
@@ -105,45 +90,103 @@
 		}
 	}
 
-	static (int d_sum, Group) CreateGroup(List<int> waitingIds, bool[] u, int id0, int count)
+	static (int, Group[]) Solve()
 	{
-		var d_sum = 0;
-		var ids = new List<int>();
-		var edges = new List<(int, int)>();
+		var minScore = int.MaxValue;
+		var minSol = default(Group[]);
 
-		while (count-- > 0)
+		while (CurrentTime < Timeout)
 		{
-			var id = GetCityId();
-			waitingIds.Remove(id);
-			u[id] = true;
-			ids.Add(id);
+			var (score, sol) = SolveOne();
 
-			foreach (var id2 in waitingIds)
-				pq.Enqueue((id, id2), dMatrix[id, id2]);
-
-			int GetCityId()
+			if (minScore > score)
 			{
-				if (pq.Count == 0) return id0;
-
-				while (true)
-				{
-					pq.TryDequeue(out var p, out var d);
-					var (v1, v2) = p;
-					if (u[v2]) continue;
-					d_sum += d;
-					edges.Add((v1, v2));
-					return v2;
-				}
+				minScore = score;
+				minSol = sol;
 			}
 		}
 
-		pq.Clear();
-		var g = new Group
+		return (minScore, minSol);
+	}
+
+	static (int d_sum, Group[]) SolveOne()
+	{
+		var d_sum = 0;
+		var groups = new Group[m];
+		var waitingIds = rn0.ToList();
+		var u = new bool[n];
+		var pq = new PriorityQueue<(int, int), int>();
+
+		var (px_sum, py_sum) = ps_sum;
+		var (px_avg, py_avg) = ps_avg;
+
+		double GetDistance2FromCenter(int id)
 		{
-			CityIds = ids.ToArray(),
-			Edges = edges.ToArray()
-		};
-		return (d_sum, g);
+			var (xi, yi) = ps[id];
+			xi -= px_avg; yi -= py_avg;
+			return xi * xi + yi * yi;
+		}
+
+		var gis = (int[])orderedGroupIds.Clone();
+		for (int j = 1; j < m; j++)
+			if (random.NextDouble() < ShiftRate)
+				(gis[j - 1], gis[j]) = (gis[j], gis[j - 1]);
+
+		foreach (var gi in gis)
+		{
+			var id0 = waitingIds.MaxBy(GetDistance2FromCenter);
+			var (d, g) = CreateGroup(id0, gcs[gi]);
+			d_sum += d;
+			groups[gi] = g;
+
+			foreach (var id in g.CityIds)
+			{
+				px_sum -= ps[id].x; py_sum -= ps[id].y;
+			}
+			(px_avg, py_avg) = (px_sum / waitingIds.Count, py_sum / waitingIds.Count);
+		}
+		return (d_sum, groups);
+
+		(int d_sum, Group) CreateGroup(int id0, int count)
+		{
+			var d_sum = 0;
+			var ids = new List<int>();
+			var edges = new List<(int, int)>();
+
+			while (count-- > 0)
+			{
+				var id = GetCityId();
+				waitingIds.Remove(id);
+				u[id] = true;
+				ids.Add(id);
+
+				foreach (var id2 in waitingIds)
+					pq.Enqueue((id, id2), dMatrix[id, id2]);
+
+				int GetCityId()
+				{
+					if (pq.Count == 0) return id0;
+
+					while (true)
+					{
+						pq.TryDequeue(out var p, out var d);
+						var (v1, v2) = p;
+						if (u[v2]) continue;
+						d_sum += d;
+						edges.Add((v1, v2));
+						return v2;
+					}
+				}
+			}
+
+			pq.Clear();
+			var g = new Group
+			{
+				CityIds = ids.ToArray(),
+				Edges = edges.ToArray()
+			};
+			return (d_sum, g);
+		}
 	}
 
 	static (int, int)[] Query(int[] gis)
