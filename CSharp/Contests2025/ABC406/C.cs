bd12d07dfa46beccf484bf0f68ff4fba728e3a7a
Keiho Sakapon <git@saka-pon.net>
2025-05-26 00:25:02 +0900

    AC

--- CSharp/Contests2025/ABC406/C.cs
+++ CSharp/Contests2025/ABC406/C.cs
@@ -1,17 +1,43 @@
 ﻿class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var p = Read();
 
-		return string.Join(" ", a);
+		var r = 0L;
+		var cs = Enumerable.Range(0, n - 1)
+			.GroupCountsBySeq(i => p[i] < p[i + 1])
+			.Where(p => p.Key)
+			.Select(p => p.Value)
+			.ToArray();
+
+		for (int i = 1; i < cs.Length; i++)
+			r += (long)cs[i - 1] * cs[i];
+		return r;
+	}
+}
+
+static class GE
+{
+	public static IEnumerable<KeyValuePair<TK, int>> GroupCountsBySeq<TS, TK>(this IEnumerable<TS> source, Func<TS, TK> toKey)
+	{
+		var c = EqualityComparer<TK>.Default;
+		TK k = default(TK), kt;
+		var count = 0;
+
+		foreach (var o in source)
+		{
+			if (!c.Equals(k, kt = toKey(o)))
+			{
+				if (count > 0) yield return new KeyValuePair<TK, int>(k, count);
+				k = kt;
+				count = 0;
+			}
+			++count;
+		}
+		if (count > 0) yield return new KeyValuePair<TK, int>(k, count);
 	}
 }
--- CSharp/Contests2025/ABC406/D.cs
+++ CSharp/Contests2025/ABC406/D.cs
@@ -1,17 +1,46 @@
 ﻿class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int x, int y) Read2() { var a = Read(); return (a[0], a[1]); }
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w, n) = Read3();
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
+		var qc = int.Parse(Console.ReadLine());
+		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		return string.Join(" ", a);
+		var setx = Array.ConvertAll(new bool[h + 1], _ => new HashSet<int>());
+		var sety = Array.ConvertAll(new bool[w + 1], _ => new HashSet<int>());
+
+		for (int id = 0; id < n; id++)
+		{
+			var (x, y) = ps[id];
+			setx[x].Add(id);
+			sety[y].Add(id);
+		}
+
+		var r = new List<int>();
+		foreach (var (t, v) in qs)
+		{
+			if (t == 1)
+			{
+				var set = setx[v];
+				r.Add(set.Count);
+				foreach (var id in set)
+					sety[ps[id].y].Remove(id);
+				set.Clear();
+			}
+			else
+			{
+				var set = sety[v];
+				r.Add(set.Count);
+				foreach (var id in set)
+					setx[ps[id].x].Remove(id);
+				set.Clear();
+			}
+		}
+		return string.Join("\n", r);
 	}
 }
