eb6dc0e7723a25bdf00c5e1e14b86ba603025d9f
Keiho Sakapon <git@saka-pon.net>
2025-06-15 00:27:18 +0900

    AC

--- CSharp/Contests2025/ABC410/B.cs
+++ CSharp/Contests2025/ABC410/B.cs
@@ -2,16 +2,28 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, q) = Read2();
+		var x = Read();
 
-		return string.Join(" ", a);
+		var r = new List<int>();
+		var b = new int[n + 1];
+		b[0] = 1 << 30;
+		var rn = Enumerable.Range(1, n).ToArray();
+
+		foreach (var v in x)
+		{
+			var u = v;
+			if (v == 0)
+			{
+				var min = b.Min();
+				u = rn.First(i => b[i] == min);
+			}
+			b[u]++;
+			r.Add(u);
+		}
+		return string.Join(" ", r);
 	}
 }
--- CSharp/Contests2025/ABC410/C.cs
+++ CSharp/Contests2025/ABC410/C.cs
@@ -2,16 +2,32 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, qc) = Read2();
+		var qs = Array.ConvertAll(new bool[qc], _ => Read());
 
-		return string.Join(" ", a);
+		var r = new List<int>();
+		var a = Enumerable.Range(1, n).ToArray();
+		var offset = 0;
+
+		foreach (var q in qs)
+		{
+			if (q[0] == 1)
+			{
+				a[(q[1] - 1 + offset) % n] = q[2];
+			}
+			else if (q[0] == 2)
+			{
+				r.Add(a[(q[1] - 1 + offset) % n]);
+			}
+			else
+			{
+				offset += q[1];
+				offset %= n;
+			}
+		}
+		return string.Join("\n", r);
 	}
 }
