f5918b1f5257e0029ac0c2c5b30d20d94666c876
Keiho Sakapon <saka_pon@live.jp>
2025-08-03 00:03:06 +0900

    AC

--- CSharp/Contests2025/ABC417/C.cs
+++ CSharp/Contests2025/ABC417/C.cs
@@ -1,17 +1,21 @@
 ﻿class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var r = 0L;
+		var c = new int[400000];
+
+		for (int j = 0; j < n; j++)
+		{
+			var v = a[j] - j;
+			if (v < 0) r += c[-v];
+			c[a[j] + j]++;
+		}
+		return r;
 	}
 }
--- CSharp/Contests2025/ABC417/E.cs
+++ CSharp/Contests2025/ABC417/E.cs
@@ -1,17 +1,22 @@
-﻿class E
+﻿using Bang.Graphs.Int.SPPs.Unweighted.v1_0_2;
+
+class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static (int, int, int, int) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m, x, y) = Read4();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
 
-		return string.Join(" ", a);
+		var g = new ListUnweightedGraph(n + 1, es, true);
+		foreach (var l in g.AdjacencyList)
+			l.Sort();
+
+		var r = g.DFSTree(x, y);
+		var p = r[y].GetPathVertexes();
+		return string.Join(" ", p);
 	}
 }
