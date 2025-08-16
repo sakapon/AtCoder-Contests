288c2155541734d6f2eb83387197884a7c911e8f
Keiho Sakapon <git@saka-pon.net>
2025-08-16 22:16:45 +0900

    AC

--- CSharp/Contests2025/ABC419/B.cs
+++ CSharp/Contests2025/ABC419/B.cs
@@ -1,17 +1,24 @@
 ﻿class B
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
-	static object Solve()
+	static void Main()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var qc = int.Parse(Console.ReadLine());
+		var qs = Array.ConvertAll(new bool[qc], _ => Read());
 
-		return string.Join(" ", a);
+		var l = new List<int>();
+		foreach (var q in qs)
+		{
+			if (q[0] == 1)
+			{
+				l.Add(q[1]);
+				l.Sort();
+			}
+			else
+			{
+				Console.WriteLine(l[0]);
+				l.RemoveAt(0);
+			}
+		}
 	}
 }
--- CSharp/Contests2025/ABC419/D.cs
+++ CSharp/Contests2025/ABC419/D.cs
@@ -2,16 +2,29 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
+		var (n, m) = Read2();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var t = Console.ReadLine();
+		var ps = Array.ConvertAll(new bool[m], _ => Read2());
 
-		return string.Join(" ", a);
+		var b = new bool[n + 1];
+		foreach (var (l, r) in ps)
+		{
+			b[l - 1] ^= true;
+			b[r] ^= true;
+		}
+
+		var cs = new char[n];
+		var useT = false;
+
+		for (int i = 0; i < n; i++)
+		{
+			useT ^= b[i];
+			cs[i] = useT ? t[i] : s[i];
+		}
+		return new string(cs);
 	}
 }
