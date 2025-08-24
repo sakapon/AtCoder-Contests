cdb293ca55c45d5051956b5fbd079b5aa31a923c
Keiho Sakapon <git@saka-pon.net>
2025-08-25 00:44:52 +0900

    AC

--- CSharp/Contests2025/ABC420/B.cs
+++ CSharp/Contests2025/ABC420/B.cs
@@ -2,16 +2,28 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var s = Array.ConvertAll(new bool[n], _ => Console.ReadLine());
 
-		return string.Join(" ", a);
+		var p = new int[n + 1];
+		var rn = Enumerable.Range(0, n).ToArray();
+
+		for (int j = 0; j < m; j++)
+		{
+			var x = rn.Count(i => s[i][j] == '0');
+			var y = n - x;
+
+			var c = x < y ? '0' : '1';
+			for (int i = 0; i < n; i++)
+			{
+				if (s[i][j] == c) p[i + 1]++;
+			}
+		}
+
+		var max = p.Max();
+		return string.Join(" ", Enumerable.Range(1, n).Where(v => p[v] == max));
 	}
 }
--- CSharp/Contests2025/ABC420/C.cs
+++ CSharp/Contests2025/ABC420/C.cs
@@ -2,16 +2,34 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, qc) = Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var b = Read();
+		var qs = Array.ConvertAll(new bool[qc], _ => Console.ReadLine().Split());
 
-		return string.Join(" ", a);
+		var rn = Enumerable.Range(0, n).ToArray();
+		var s = rn.Sum(i => (long)Math.Min(a[i], b[i]));
+
+		var r = new List<long>();
+
+		foreach (var q in qs)
+		{
+			var x = int.Parse(q[1]) - 1;
+			var v = int.Parse(q[2]);
+
+			s -= Math.Min(a[x], b[x]);
+			if (q[0][0] == 'A')
+				a[x] = v;
+			else
+				b[x] = v;
+			s += Math.Min(a[x], b[x]);
+
+			r.Add(s);
+		}
+
+		return string.Join("\n", r);
 	}
 }
