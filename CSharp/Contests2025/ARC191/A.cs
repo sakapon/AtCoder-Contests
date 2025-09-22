06bea17e058983c23137bca84a7ae31c55d4fe3e
sakapon <git@saka-pon.net>
2025-09-22 10:03:00 +0900

    WA

--- CSharp/Contests2025/ARC191/A.cs
+++ CSharp/Contests2025/ARC191/A.cs
@@ -2,16 +2,31 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var s = Console.ReadLine().ToCharArray();
+		var t = Console.ReadLine();
 
-		return string.Join(" ", a);
+		var q = new Queue<int>(Enumerable.Range(0, m).OrderBy(i => -t[i]));
+		var mj = -1;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (q.Count == 0) break;
+			var j = q.Peek();
+			if (s[i] >= t[j]) continue;
+			s[i] = t[j];
+			q.Dequeue();
+			if (mj < j) mj = j;
+		}
+
+		if (q.Count > 0)
+		{
+			var qj = q.Max();
+			if (mj < qj) s[n - 1] = t[qj];
+		}
+		return new string(s);
 	}
 }
