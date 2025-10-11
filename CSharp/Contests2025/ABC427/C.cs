63bb5203162af1e954ea53aa6742f85973f793b5
Keiho Sakapon <git@saka-pon.net>
2025-10-11 22:32:49 +0900

    AC

--- CSharp/Contests2025/ABC427/C.cs
+++ CSharp/Contests2025/ABC427/C.cs
@@ -2,16 +2,40 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
 
-		return string.Join(" ", a);
+		var map = new bool[n, n];
+		foreach (var (u, v) in es)
+		{
+			map[u - 1, v - 1] = true;
+			map[v - 1, u - 1] = true;
+		}
+
+		var r = 1000;
+		for (uint x = 0; x < 1 << n; x++)
+		{
+			var g0 = new List<int>();
+			var g1 = new List<int>();
+			for (int i = 0; i < n; i++)
+			{
+				if ((x & (1 << i)) == 0) g0.Add(i);
+				else g1.Add(i);
+			}
+
+			var c = 0;
+			foreach (var i in g0)
+				foreach (var j in g0)
+					if (map[i, j]) c++;
+			foreach (var i in g1)
+				foreach (var j in g1)
+					if (map[i, j]) c++;
+			c /= 2;
+			r = Math.Min(r, c);
+		}
+		return r;
 	}
 }
--- CSharp/Contests2025/ABC427/D.cs
+++ CSharp/Contests2025/ABC427/D.cs
@@ -2,16 +2,32 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
+		var (n, m, k) = Read3();
 		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
 
-		return string.Join(" ", a);
+		var map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		foreach (var (u, v) in es)
+			map[u - 1].Add(v - 1);
+
+		var wins = s.Select(c => c == 'B').ToArray();
+		var wins_t = new bool[n];
+
+		while (k-- > 0)
+		{
+			for (int u = 0; u < n; u++)
+				wins_t[u] = map[u].Any(v => wins[v]);
+			(wins, wins_t) = (wins_t, wins);
+
+			for (int u = 0; u < n; u++)
+				wins_t[u] = map[u].All(v => wins[v]);
+			(wins, wins_t) = (wins_t, wins);
+		}
+
+		return wins[0] ? "Bob" : "Alice";
 	}
 }
