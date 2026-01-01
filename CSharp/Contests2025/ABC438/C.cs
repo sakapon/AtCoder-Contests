2368f631ddbca58791409e4a4a9ed4ab35f2b135
Keiho Sakapon <git@saka-pon.net>
2026-01-01 16:09:50 +0900

    AC

--- CSharp/Contests2025/ABC438/C.cs
+++ CSharp/Contests2025/ABC438/C.cs
@@ -1,17 +1,30 @@
 ﻿class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var q = new Stack<(int value, int count)>();
+		q.Push((0, 0));
+
+		foreach (var v in a)
+		{
+			var (value, count) = q.Peek();
+
+			if (v == value)
+			{
+				q.Pop();
+				if (++count < 4) q.Push((value, count));
+			}
+			else
+			{
+				q.Push((v, 1));
+			}
+		}
+
+		return q.Sum(p => p.count);
 	}
 }
