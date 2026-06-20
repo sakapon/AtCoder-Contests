a6baa9e3fe326a6c0be5cf27e4c3b336b74cfdbb
Keiho Sakapon <git@saka-pon.net>
2026-06-20 23:29:16 +0900

    AC

--- CSharp/Contests2026/ABC463/C.cs
+++ CSharp/Contests2026/ABC463/C.cs
@@ -2,16 +2,26 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var ps = Array.ConvertAll(new bool[n], _ => Read2());
+		var qc = int.Parse(Console.ReadLine());
+		var ts = Read();
 
-		return string.Join(" ", a);
+		var r = new int[qc];
+
+		var qq = new Queue<(int t, int qi)>(ts.Select((t, qi) => (t, qi)).OrderBy(p => p.t));
+
+		foreach (var (h, l) in ps.OrderBy(p => -p.Item1))
+		{
+			while (qq.Count > 0 && qq.Peek().t < l)
+			{
+				var (_, qi) = qq.Dequeue();
+				r[qi] = h;
+			}
+		}
+		return string.Join("\n", r);
 	}
 }
--- CSharp/Contests2026/ABC463/E.cs
+++ CSharp/Contests2026/ABC463/E.cs
@@ -1,17 +1,26 @@
-﻿class E
+﻿using Bang.Graphs.Int.SPPs.Weighted.v1_0_2;
+
+class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m, y) = Read3();
+		var es = Array.ConvertAll(new bool[m], _ => Read3());
+		var x = Read();
 
-		return string.Join(" ", a);
+		var g = new ListWeightedGraph(n + 3, es, true);
+		g.AddEdge(n + 1, n + 2, false, y);
+
+		for (int v = 1; v <= n; v++)
+		{
+			g.AddEdge(v, n + 1, false, x[v - 1]);
+			g.AddEdge(n + 2, v, false, x[v - 1]);
+		}
+
+		var r = g.Dijkstra(1);
+		return string.Join(" ", r.Skip(2).Take(n - 1));
 	}
 }
