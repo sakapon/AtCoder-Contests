8362d01c26d78660ea047eeee66cac70c3d8faa3
Keiho Sakapon <git@saka-pon.net>
2026-06-27 23:36:04 +0900

    AC

--- CSharp/Contests2026/ABC464/B.cs
+++ CSharp/Contests2026/ABC464/B.cs
@@ -2,16 +2,24 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (h, w) = Read2();
+		var c = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
 
-		return string.Join(" ", a);
+		var rh = Enumerable.Range(0, h).ToArray();
+		var rw = Enumerable.Range(0, w).ToArray();
+
+		var iMin = rh.Where(i => c[i].Contains('#')).Min();
+		var iMax = rh.Where(i => c[i].Contains('#')).Max();
+		iMax++;
+
+		var jMin = rw.Where(j => rh.Any(i => c[i][j] == '#')).Min();
+		var jMax = rw.Where(j => rh.Any(i => c[i][j] == '#')).Max();
+		jMax++;
+
+		var r = c[iMin..iMax].Select(s => s[jMin..jMax]).ToArray();
+		return string.Join("\n", r);
 	}
 }
--- CSharp/Contests2026/ABC464/C.cs
+++ CSharp/Contests2026/ABC464/C.cs
@@ -2,16 +2,31 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+	static (int a, int d, int b) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var ps = Array.ConvertAll(new bool[n], _ => Read3());
 
-		return string.Join(" ", a);
+		var counts = new int[n + 1];
+		foreach (var (a, d, b) in ps)
+			counts[a]++;
+		var colors = counts.Count(x => x > 0);
+
+		var r = new List<int>();
+		var q = new Queue<(int a, int d, int b)>(ps.OrderBy(p => p.d));
+
+		for (int j = 1; j <= m; j++)
+		{
+			while (q.Count > 0 && q.Peek().d == j)
+			{
+				var (a, d, b) = q.Dequeue();
+				if (--counts[a] == 0) colors--;
+				if (counts[b]++ == 0) colors++;
+			}
+			r.Add(colors);
+		}
+		return string.Join("\n", r);
 	}
 }
