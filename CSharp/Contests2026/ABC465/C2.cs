07e2c158132ceccb50118dc4affa96fb0fd29fb7
Keiho Sakapon <git@saka-pon.net>
2026-07-04 23:23:13 +0900

    AC

--- CSharp/Contests2026/ABC465/C2.cs
+++ CSharp/Contests2026/ABC465/C2.cs
@@ -1,4 +1,4 @@
-﻿class C
+﻿class C2
 {
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
@@ -6,19 +6,19 @@
 		var n = int.Parse(Console.ReadLine());
 		var s = Console.ReadLine();
 
-		var neighbors = Array.ConvertAll(new bool[n + 2], _ => new List<int>());
+		var nodes = Enumerable.Range(0, n + 2).Select(v => new Node(v)).ToArray();
 		var sv = 1;
 
 		void AddEdge(int u, int v)
 		{
-			neighbors[u].Add(v);
-			neighbors[v].Add(u);
+			nodes[u].Neighbors.Add(nodes[v]);
+			nodes[v].Neighbors.Add(nodes[u]);
 		}
 
 		void RemoveEdge(int u, int v)
 		{
-			neighbors[u].Remove(v);
-			neighbors[v].Remove(u);
+			nodes[u].Neighbors.Remove(nodes[v]);
+			nodes[v].Neighbors.Remove(nodes[u]);
 		}
 
 		for (int v = 1; v <= n; v++)
@@ -36,14 +36,20 @@
 		}
 
 		var r = new List<int>();
-		for (int v = sv, pv = -1; v != -1;)
+		for (Node node = nodes[sv], pNode = null; node != null;)
 		{
-			r.Add(v);
-			var next = neighbors[v].FirstOrDefault(n => n != pv, -1);
-			pv = v;
-			v = next;
+			r.Add(node.Id);
+			var next = node.Neighbors.FirstOrDefault(n => n != pNode);
+			pNode = node;
+			node = next;
 		}
 
 		return string.Join(" ", r[..n]);
 	}
+}
+
+class Node(int id)
+{
+	public int Id = id;
+	public List<Node> Neighbors = [];
 }
---
+++ CSharp/Contests2026/ABC465/C3.cs
@@ -0,0 +1,21 @@
+﻿class C3
+{
+	static void Main() => Console.WriteLine(Solve());
+	static object Solve()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine();
+
+		var a = new int[n];
+		int l = 0, r = n - 1;
+		var isLeft = false;
+
+		for (int k = n; k > 0; k--)
+		{
+			if (s[k - 1] == 'o') isLeft = !isLeft;
+			if (isLeft) a[l++] = k;
+			else a[r--] = k;
+		}
+		return string.Join(" ", a);
+	}
+}
