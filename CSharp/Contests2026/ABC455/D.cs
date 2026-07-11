b40cf682dc44beca0a58567c8c949f64e1b37c9b
Keiho Sakapon <git@saka-pon.net>
2026-07-11 19:56:34 +0900

    AC

--- CSharp/Contests2026/ABC455/D.cs
+++ CSharp/Contests2026/ABC455/D.cs
@@ -1,17 +1,28 @@
-﻿class D
+﻿using CoderLib8.Collections;
+
+class D
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, qc) = Read2();
+		var qs = Array.ConvertAll(new bool[qc], _ => Read2());
 
-		return string.Join(" ", a);
+		var g = new LinkedListGraph(n + 1);
+		foreach (var (c, p) in qs)
+		{
+			g.RemoveEdgeBefore(c);
+			g.AddEdge(p, c);
+		}
+
+		var a = new int[n + 1];
+		for (int v = 1; v <= n; v++)
+		{
+			if (g.Nodes[v].Left != null) continue;
+			a[v] = g.GetPath(v).Count();
+		}
+		return string.Join(" ", a[1..]);
 	}
 }
---
+++ CSharp/Contests2026/ABC455/LinkedListGraph.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 直線状のグラフを表します。
+// 自己ループ、サイクルが発生する可能性があります。
+namespace CoderLib8.Collections
+{
+	public class LinkedListGraph
+	{
+		public class Node
+		{
+			public int Id { get; }
+			public Node Left { get; set; }
+			public Node Right { get; set; }
+
+			public Node(int id) => Id = id;
+		}
+
+		readonly int n;
+		public int NodesCount => n;
+		public Node[] Nodes { get; }
+
+		public LinkedListGraph(int n)
+		{
+			this.n = n;
+			Nodes = new Node[n];
+			for (int v = 0; v < n; ++v)
+				Nodes[v] = new Node(v);
+		}
+
+		public bool AddEdge(int l, int r)
+		{
+			if (Nodes[l].Right != null) return false;
+			if (Nodes[r].Left != null) return false;
+			Nodes[l].Right = Nodes[r];
+			Nodes[r].Left = Nodes[l];
+			return true;
+		}
+		public bool AddEdgeIgnore(int u, int v)
+		{
+			return AddEdge(u, v) || AddEdge(v, u);
+		}
+
+		public bool RemoveEdgeBefore(int v)
+		{
+			if (Nodes[v].Left == null) return false;
+			Nodes[v].Left.Right = null;
+			Nodes[v].Left = null;
+			return true;
+		}
+		public bool RemoveEdgeAfter(int v)
+		{
+			if (Nodes[v].Right == null) return false;
+			Nodes[v].Right.Left = null;
+			Nodes[v].Right = null;
+			return true;
+		}
+
+		public bool RemoveEdge(int l, int r)
+		{
+			if (Nodes[l].Right != Nodes[r]) return false;
+			Nodes[l].Right = null;
+			Nodes[r].Left = null;
+			return true;
+		}
+		public bool RemoveEdgeIgnore(int u, int v)
+		{
+			return RemoveEdge(u, v) || RemoveEdge(v, u);
+		}
+
+		public IEnumerable<int> GetPath(int v)
+		{
+			var node0 = Nodes[v];
+			var node = node0;
+			while (node != null)
+			{
+				yield return node.Id;
+				node = node.Right;
+				if (node == node0) yield break;
+			}
+		}
+		public IEnumerable<int> GetPathReverse(int v)
+		{
+			var node0 = Nodes[v];
+			var node = node0;
+			while (node != null)
+			{
+				yield return node.Id;
+				node = node.Left;
+				if (node == node0) yield break;
+			}
+		}
+	}
+}
