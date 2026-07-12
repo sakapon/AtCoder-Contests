8cf6ac245f2f08feb75303a799afb21a767dad4a
Keiho Sakapon <git@saka-pon.net>
2026-07-12 23:55:41 +0900

    AC

--- CSharp/Contests2026/ARC224/A.cs
+++ CSharp/Contests2026/ARC224/A.cs
@@ -1,17 +1,11 @@
 ﻿class A
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
-
-		return string.Join(" ", a);
+		var k = long.Parse(Console.ReadLine());
+		return Enumerable.Range(1, 100)
+			.Select(i => (i * k).ToString())
+			.First(s => s.Contains("00"));
 	}
 }
--- CSharp/Contests2026/ARC224/C.cs
+++ CSharp/Contests2026/ARC224/C.cs
@@ -1,17 +1,18 @@
-﻿class C
+﻿using Bang.Graphs.Int.SPPs.Unweighted.v1_0_2;
+
+class C
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
 
-		return string.Join(" ", a);
+		var g = new ListUnweightedGraph(n + 1, es, true);
+		var r = g.DFSTree(1);
+
+		return string.Join(" ", r.Vertexes[1..].Select(v => v.Cost));
 	}
 }
