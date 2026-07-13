f80171ad0e84f2e784044c32fb715f70ea90ff03
Keiho Sakapon <git@saka-pon.net>
2026-07-13 22:12:22 +0900

    AC

--- CSharp/Contests2026/ABC465/C2.cs
+++ CSharp/Contests2026/ABC465/C2.cs
@@ -1,4 +1,6 @@
-﻿class C2
+﻿using Bang.Graphs.Int.SPPs.Unweighted.v1_0_2;
+
+class C2
 {
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
@@ -6,50 +8,24 @@
 		var n = int.Parse(Console.ReadLine());
 		var s = Console.ReadLine();
 
-		var nodes = Enumerable.Range(0, n + 2).Select(v => new Node(v)).ToArray();
+		var g = new ListUnweightedGraph(n + 2);
 		var sv = 1;
-
-		void AddEdge(int u, int v)
-		{
-			nodes[u].Neighbors.Add(nodes[v]);
-			nodes[v].Neighbors.Add(nodes[u]);
-		}
-
-		void RemoveEdge(int u, int v)
-		{
-			nodes[u].Neighbors.Remove(nodes[v]);
-			nodes[v].Neighbors.Remove(nodes[u]);
-		}
 
 		for (int v = 1; v <= n; v++)
 		{
-			AddEdge(v, v + 1);
+			if (s[v - 1] == 'o')
+			{
+				g.AddEdge(sv, v + 1, true);
+				sv = v;
+			}
+			else
+			{
+				g.AddEdge(v, v + 1, true);
+			}
 		}
 
-		for (int k = 1; k <= n; k++)
-		{
-			if (s[k - 1] == 'x') continue;
-
-			RemoveEdge(k, k + 1);
-			AddEdge(sv, k + 1);
-			sv = k;
-		}
-
-		var r = new List<int>();
-		for (Node node = nodes[sv], pNode = null; node != null;)
-		{
-			r.Add(node.Id);
-			var next = node.Neighbors.FirstOrDefault(n => n != pNode);
-			pNode = node;
-			node = next;
-		}
-
+		var tree = g.DFSTree(sv);
+		var r = tree[n + 1].GetPathVertexes();
 		return string.Join(" ", r[..n]);
 	}
-}
-
-class Node(int id)
-{
-	public int Id = id;
-	public List<Node> Neighbors = [];
 }
