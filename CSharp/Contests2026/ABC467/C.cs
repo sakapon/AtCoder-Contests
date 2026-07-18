5909cc99d0a12d7f51fd5f8dfecf296b47374120
Keiho Sakapon <git@saka-pon.net>
2026-07-19 00:06:15 +0900

    AC

--- CSharp/Contests2026/ABC467/C.cs
+++ CSharp/Contests2026/ABC467/C.cs
@@ -2,16 +2,18 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
+		var (n, m) = Read2();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var b = Read();
 
-		return string.Join(" ", a);
+		var a2 = new int[n];
+		for (int i = 1; i < n; i++)
+			a2[i] = a2[i - 1] == b[i - 1] ? 0 : 1;
+
+		var c = a.Zip(a2).Count(p => p.First == p.Second);
+		return Math.Min(c, n - c);
 	}
 }
--- CSharp/Contests2026/ABC467/D.cs
+++ CSharp/Contests2026/ABC467/D.cs
@@ -1,17 +1,40 @@
-﻿class D
+﻿using System.Numerics;
+
+class D
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-	static void Main() => Console.WriteLine(Solve());
+	static void Main() => Console.WriteLine(string.Join("\n", new int[int.Parse(Console.ReadLine())].Select(_ => Solve())));
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var a = ReadL();
 
-		return string.Join(" ", a);
+		var (a1, b1, c1) = GetMiddleLineEquation(a[0], a[1], a[2], a[3]);
+		var (a2, b2, c2) = GetMiddleLineEquation(a[4], a[5], a[6], a[7]);
+
+		if (a1 * b2 != a2 * b1) return "Yes";
+		if (a1 != 0 && a1 * c2 == a2 * c1) return "Yes";
+		if (b1 != 0 && b1 * c2 == b2 * c1) return "Yes";
+		return "No";
+	}
+
+	// 指定された2点からの距離が等しい点の集合である、直線の方程式を求めます。
+	// ax + by + c = 0 の形式で返します。
+	//public static (double a, double b, double c) GetMiddleLineEquation(double x1, double y1, double x2, double y2)
+	//{
+	//	var dx = x2 - x1;
+	//	var dy = y2 - y1;
+	//	return (dx * 2, dy * 2, -dx * (x1 + x2) - dy * (y1 + y2));
+	//}
+	//public static (long a, long b, long c) GetMiddleLineEquation(long x1, long y1, long x2, long y2)
+	//{
+	//	var dx = x2 - x1;
+	//	var dy = y2 - y1;
+	//	return (dx * 2, dy * 2, -dx * (x1 + x2) - dy * (y1 + y2));
+	//}
+	public static (BigInteger a, BigInteger b, BigInteger c) GetMiddleLineEquation(BigInteger x1, BigInteger y1, BigInteger x2, BigInteger y2)
+	{
+		var dx = x2 - x1;
+		var dy = y2 - y1;
+		return (dx * 2, dy * 2, -dx * (x1 + x2) - dy * (y1 + y2));
 	}
 }
