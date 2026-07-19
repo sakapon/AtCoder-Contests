c41036deb78c886b617d40609d0606d1ae79e993
Keiho Sakapon <git@saka-pon.net>
2026-07-19 23:18:04 +0900

    AC

--- CSharp/Contests2026/ARC225/E.cs
+++ CSharp/Contests2026/ARC225/E.cs
@@ -1,17 +1,45 @@
 ﻿class E
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var p = Read();
 
-		return string.Join(" ", a);
+		return p.Select((x, i) => x - 1 - i).Sum(x => (long)Math.Abs(x)) - InversionNumber(p);
+	}
+
+	public static long InversionNumber<T>(T[] a, IComparer<T> c = null)
+	{
+		var n = a.Length;
+		var t = new T[n];
+		c = c ?? Comparer<T>.Default;
+		var r = 0L;
+
+		for (int k = 1; k < n; k <<= 1)
+		{
+			var ti = 0;
+			for (int L = 0; L < n; L += k << 1)
+			{
+				int R1 = L | k, R2 = R1 + k;
+				if (R2 > n) R2 = n;
+				int i1 = L, i2 = R1;
+				while (ti < R2)
+				{
+					if (i2 >= R2 || i1 < R1 && i2 < R2 && c.Compare(a[i1], a[i2]) <= 0)
+					{
+						t[ti++] = a[i1++];
+					}
+					else
+					{
+						r += R1 - i1;
+						t[ti++] = a[i2++];
+					}
+				}
+			}
+			Array.Copy(t, a, n);
+		}
+		return r;
 	}
 }
