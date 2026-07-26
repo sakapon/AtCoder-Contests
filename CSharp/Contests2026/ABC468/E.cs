129fba7e789586375d36f8e83106f3771a9e6eb1
Keiho Sakapon <git@saka-pon.net>
2026-07-26 15:51:59 +0900

    AC

--- CSharp/Contests2026/ABC468/E.cs
+++ CSharp/Contests2026/ABC468/E.cs
@@ -1,17 +1,35 @@
 ﻿class E
 {
-	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var a = ReadL();
 
-		return string.Join(" ", a);
+		var s = new List<long>();
+		var t = a.Sum();
+
+		var n2 = (n + 1) / 2;
+		for (int i = 0; i < n2; i++)
+		{
+			var ps = s.Count == 0 ? 0L : s[^1];
+			s.Add((ps + t) % M);
+
+			t -= a[i];
+			t -= a[n - 1 - i];
+		}
+
+		s.AddRange(s.Take(n / 2).Reverse().ToArray());
+		return s.Select((x, i) => x * MInv(i + 1) % M).Sum() % M;
 	}
+
+	const long M = 998244353;
+	static long MPow(long b, long i)
+	{
+		long r = 1;
+		for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+		return r;
+	}
+	static long MInv(long x) => MPow(x, M - 2);
 }
