ac8111165d441ac7b9c25ee777c52ab4732b302c
Keiho Sakapon <git@saka-pon.net>
2026-08-02 01:28:51 +0900

    AC

--- CSharp/Contests2026/ABC469/F.cs
+++ CSharp/Contests2026/ABC469/F.cs
@@ -1,17 +1,40 @@
-﻿class F
+﻿using Oomph.Data.UF09Lib.UFs.v301;
+
+class F
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
 		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
 		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
 
-		return string.Join(" ", a);
+		var max = 1000000;
+		var map = new int[max + 1];
+		for (int v = 1; v <= n; v++)
+			map[a[v - 1]] = v;
+
+		var r = 0L;
+		var uf = new UnionFind(n + 1);
+
+		for (int x = max; x > 0; x--)
+		{
+			var v0 = 0;
+
+			for (int p = x; p <= max; p += x)
+			{
+				var v = map[p];
+				if (v == 0) continue;
+
+				if (v0 == 0)
+				{
+					v0 = v;
+					continue;
+				}
+
+				if (uf.Union(v0, v)) r += x;
+			}
+		}
+		return r;
 	}
 }
