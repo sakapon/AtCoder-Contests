df945426bbcd60eb00f2e089eed32e9f9f4ce09c
Keiho Sakapon <git@saka-pon.net>
2026-08-03 01:36:25 +0900

    AC

--- CSharp/Contests2026/ABC469/D.cs
+++ CSharp/Contests2026/ABC469/D.cs
@@ -2,16 +2,45 @@
 {
 	static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 	static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-	static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 	static void Main() => Console.WriteLine(Solve());
 	static object Solve()
 	{
-		var n = int.Parse(Console.ReadLine());
-		var (n2, m) = Read2();
-		var s = Console.ReadLine();
-		var a = Read();
-		var ps = Array.ConvertAll(new bool[n], _ => Read());
+		var (n, m) = Read2();
+		var es = Array.ConvertAll(new bool[m], _ => Read2());
 
-		return string.Join(" ", a);
+		var counts = new int[n];
+		var counts2 = new Dictionary<(int, int), int>();
+
+		foreach (var e in es)
+		{
+			var (u, v) = e;
+			u--;
+			v--;
+
+			counts[u]++;
+			counts[v]++;
+			counts2[(u, v)] = counts2.GetValueOrDefault((u, v)) + 1;
+			counts2[(v, u)] = counts2.GetValueOrDefault((v, u)) + 1;
+		}
+
+		var ordered = Enumerable.Range(0, n)
+			.Select(v => (v, c: counts[v]))
+			.OrderBy(g => -g.c)
+			.ToArray();
+
+		var r = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			var (v, c) = ordered[i];
+			if (c < (m + 1) / 2) break;
+
+			for (int j = i + 1; j < n; j++)
+			{
+				if (counts[v] + counts[ordered[j].v] - counts2.GetValueOrDefault((v, ordered[j].v)) == m)
+					r++;
+			}
+		}
+		return r;
 	}
 }
